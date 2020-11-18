3118add7f28a4466856e09af64c2d1fff196dfd4
Michael Guarino <mguarino46@gmail.com>
2020-11-17 20:25:12 -0500

    create initial role seed

---
+++ priv/repo/seeds/03_roles.exs
@@ -0,0 +1,17 @@
+import Botanist
+
+alias Watchman.{Repo, Schema}
+
+seed do
+  group = Repo.get_by!(Schema.Group, name: "general")
+
+  %Schema.Role{}
+  |> Schema.Role.changeset(%{
+    name: "read-only",
+    description: "read only access to all resources",
+    repositories: ["*"],
+    permissions: %{read: true},
+    role_bindings: [%{group_id: group.id}]
+  })
+  |> Repo.insert!()
+end
