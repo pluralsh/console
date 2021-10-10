5fb993f5edd3fd264a05639e75f444f4c902e979
michaelguarino <mguarino46@gmail.com>
2021-10-10 14:22:16 -0400

    sidebar revisions

--- assets/src/components/Sidebar.js
+++ assets/src/components/Sidebar.js
@@ -9,9 +9,23 @@ import { InstallationContext } from './Installations'
 import './sidebar.css'
 import { SubmenuContext, Submenu } from './navigation/Submenu'
 import { TOOLBAR_HEIGHT } from './Console'
+import styled from 'styled-components'
+import { normalizeColor } from 'grommet/utils'
+
+const hoverable = styled.div`
+  &:hover span {
+    color: ${props => normalizeColor('white', props.theme)};
+  }
+
+  &:hover svg {
+    stroke: ${props => normalizeColor('white', props.theme)} !important;
+    fill: ${props => normalizeColor('white', props.theme)} !important;
+  }
+`
 
 export const SIDEBAR_ICON_HEIGHT = '42px'
 const APP_ICON = `${process.env.PUBLIC_URL}/console-full.png`
+const ICON_HEIGHT = '15px'
 
 export function SidebarIcon({icon, text, name: sidebarName, selected, path}) {
   const {name} = useContext(SubmenuContext)
@@ -20,14 +34,14 @@ export function SidebarIcon({icon, text, name: sidebarName, selected, path}) {
   const textColor = selected && !inSubmenu ? 'white' : 'light-5'
 
   return (
-    <Box flex={false} fill='horizontal' 
+    <Box as={hoverable} flex={false} fill='horizontal' 
          background={(selected && !inSubmenu) ? 'sidebarHover' : null}>
       <Box focusIndicator={false} fill='horizontal' align='center' direction='row' 
         height={SIDEBAR_ICON_HEIGHT} hoverIndicator='sidebarHover' 
         onClick={!inSubmenu && selected ? null : () => history.push(path)} 
         pad={{horizontal: 'small'}}>
         <Box direction='row' align='center' gap='15px' fill='horizontal'>
-          {icon}
+          {React.createElement(icon, {size: ICON_HEIGHT, color: textColor})}
           <Text size='small' color={textColor}>{text}</Text>
         </Box>
         {sidebarName && !selected && <Next size='12px' />}
@@ -49,27 +63,26 @@ function Me() {
          onClick={() => history.push('/me/edit')}>
       <Avatar user={me} size='45px' />
       <Box>
-        <Text size='small' truncate>{me.name}</Text>
+        <Text size='small' color='light-5' truncate>{me.name}</Text>
         <Text size='small'>{me.email}</Text>
       </Box>
     </Box>
   )
 }
 
-const ICON_HEIGHT = '15px'
 
 const OPTIONS = [
-  {text: 'Builds', icon: <Deploy size={ICON_HEIGHT} />, path: '/'},
-  {text: 'Runbooks', icon: <Book size={ICON_HEIGHT} />, path: '/runbooks/{repo}'},
-  {text: 'Components', icon: <Cubes size={ICON_HEIGHT} />, path: '/components/{repo}'},
-  {text: 'Nodes', icon: <Nodes size={ICON_HEIGHT} />, path: '/nodes'},
-  {text: 'Configuration', icon: <Configure size={ICON_HEIGHT} />, path: '/config/{repo}' },
-  {text: 'Incidents', icon: <Alert size={ICON_HEIGHT} />, path: '/incidents'},
-  {text: 'Dashboards', icon: <BarChart size={ICON_HEIGHT} />, path: '/dashboards/{repo}'},
-  {text: 'Logs', icon: <TextAlignFull size={ICON_HEIGHT} />, path: '/logs/{repo}'},
-  {text: "Users", icon: <Group size={ICON_HEIGHT} />, path: '/directory'},
-  {text: "Audits", name: 'audits', icon: <List size={ICON_HEIGHT} />, path: '/audits'},
-  {text: 'Webhooks', icon: <Network size={ICON_HEIGHT} />, path: '/webhooks'},
+  {text: 'Builds', icon: Deploy, path: '/'},
+  {text: 'Runbooks', icon: Book, path: '/runbooks/{repo}'},
+  {text: 'Components', icon: Cubes, path: '/components/{repo}'},
+  {text: 'Nodes', icon: Nodes, path: '/nodes'},
+  {text: 'Configuration', icon: Configure, path: '/config/{repo}' },
+  {text: 'Incidents', icon: Alert, path: '/incidents'},
+  {text: 'Dashboards', icon: BarChart, path: '/dashboards/{repo}'},
+  {text: 'Logs', icon: TextAlignFull, path: '/logs/{repo}'},
+  {text: "Users", icon: Group, path: '/directory'},
+  {text: "Audits", name: 'audits', icon: List, path: '/audits'},
+  {text: 'Webhooks', icon: Network, path: '/webhooks'},
 ]
 
 const IMAGE_HEIGHT='47px'
