c6f4aae00dd45132f29f69568c6eae0f90d8b910
michaelguarino <mguarino46@gmail.com>
2021-11-18 00:38:29 -0500

    clean up recommender ui

--- assets/src/components/kubernetes/ScalingRecommender.js
+++ assets/src/components/kubernetes/ScalingRecommender.js
@@ -5,31 +5,54 @@ import { SCALING_RECOMMENDATION } from './queries'
 import { LoopingLogo } from '../utils/AnimatedLogo'
 import { MetadataRow } from './Metadata'
 import { Box, Layer, Text } from 'grommet'
+import filesize from 'filesize'
 
 const POLL_INTERVAL = 10000
+
+const mem = (memory) => filesize(parseInt(memory))
+
+function RecommendationNub({text, value}) {
+  return (
+    <Box direction='row' align='center' gap='xsmall'>
+      <Text size='small' weight={500}>{text}</Text>
+      <Text size='small'>{value}</Text>
+    </Box>
+  )
+}
+
+function Recommendation({rec: {cpu, memory}}) {
+  return (
+    <Box gap='2px' border={{side: 'left'}} pad={{horizontal: 'small'}}>
+      <RecommendationNub text='cpu' value={cpu} />
+      <RecommendationNub text='memory' value={mem(memory)} />
+    </Box>
+  )
+}
 
 function ScalingRecommendations({recommendations}) {
   return (
     <Box flex={false} pad='small'>
-      <Tabs defaultTab='info'>
+      <Tabs defaultTab={recommendations[0].containerName}>
         <TabHeader>
-          {recommendations.map(({name}) => (
-            <TabHeaderItem key={name} name={name}>
-              <Text size='small' weight={500}>{name}</Text>
+          {recommendations.map(({containerName}) => (
+            <TabHeaderItem key={containerName} name={containerName}>
+              <Text size='small' weight={500}>{containerName}</Text>
             </TabHeaderItem>
           ))}
         </TabHeader>
         {recommendations.map((recommendation) => (
           <TabContent key={recommendation.containerName} name={recommendation.containerName}>
-            <MetadataRow name='lower bound'>
-              <Text size='small'>cpu: {recommendation.lowerBound?.cpu}, mem: {recommendation.lowerBound?.memory}</Text>
-            </MetadataRow>
-            <MetadataRow name='upper bound'>
-              <Text size='small'>cpu: {recommendation.upperBound?.cpu}, mem: {recommendation.upperBound?.memory}</Text>
-            </MetadataRow>
-            <MetadataRow name='target'>
-              <Text size='small'>cpu: {recommendation.uncappedTarget?.cpu}, mem: {recommendation.uncappedTarget?.memory}</Text>
-            </MetadataRow>
+            <Box pad='small'>
+              <MetadataRow name='lower bound'>
+                <Recommendation rec={recommendation.lowerBound} />
+              </MetadataRow>
+              <MetadataRow name='upper bound'>
+                <Recommendation rec={recommendation.upperBound} />
+              </MetadataRow>
+              <MetadataRow name='target'>
+                <Recommendation rec={recommendation.uncappedTarget} />
+              </MetadataRow>
+            </Box>
           </TabContent>
         ))}
       </Tabs>
@@ -43,7 +66,10 @@ export function ScalingRecommender({kind, name, namespace}) {
     pollInterval: POLL_INTERVAL
   })
 
-  const recommendations = data?.status?.recommendation?.containerRecommendations
+  console.log(data)
+
+  const recommendations = data?.scalingRecommendation?.status?.recommendation?.containerRecommendations
+  console.log(recommendations)
 
   return (
     <Box fill='horizontal' style={{minHeight: '250px', overflow: 'auto'}}>
@@ -59,7 +85,7 @@ export function ScalingRecommenderModal({kind, name, namespace}) {
 
   return (
     <>
-    <Button label='Scaling Recommendation' onClick={() => setOpen(true)} />
+    <Button label='Scaling' onClick={() => setOpen(true)} />
     {open && (
       <Layer onClickOutside={close} onEsc={close}>
         <Box width='50vw'>
