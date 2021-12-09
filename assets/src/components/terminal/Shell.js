5ddf0d7a727b265bb19a829bcdd088473785525f
michaelguarino <mguarino46@gmail.com>
2021-12-08 23:12:47 -0500

    working terminal interaction

--- assets/src/components/terminal/Shell.js
+++ assets/src/components/terminal/Shell.js
@@ -1,28 +1,35 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { Box } from 'grommet'
 import { socket } from '../../helpers/client'
 import { XTerm } from 'xterm-for-react'
 import { Dracula } from 'xterm-theme'
+import { FitAddon } from 'xterm-addon-fit'
+import './shell.css'
 
 export function Shell({room, header}) {
   const xterm = useRef(null)
   const [channel, setChannel] = useState(null)
+  const fitAddon = useMemo(() => new FitAddon(), [])
+
   useEffect(() => {
-    if (!xterm && !xterm.current && !xterm.current.terminal) return
-    xterm.current.terminal.writeln(header + "\n\n")
+    if (!xterm.current?.terminal) return
+    fitAddon.fit()
+    xterm.current.terminal.write(header + "\r\n")
     console.log(socket)
     const chan = socket.channel(room)
-    chan.on("stdo", ({message}) => xterm.current.terminal.writeln(message))
+    chan.on("stdo", ({message}) => xterm.current?.terminal?.write(message))
     console.log(chan)
     chan.join()
     setChannel(chan)
     return chan.leave
-  }, [room, xterm])
+  }, [room, xterm, fitAddon])
 
   return (
     <Box fill background='backgroundColor'>
       <XTerm 
+        className='terminal'
         ref={xterm}
+        addons={[fitAddon]}
         options={{theme: Dracula}}
         onData={(text) => (
           channel.push("command", {cmd: text})
