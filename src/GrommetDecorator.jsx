c671724edb500dd0d4f980e25463bedb03157f70
David Hérault <dherault@gmail.com>
2022-04-11 23:57:17 +0300

    Add link wrapper to Sidebar

--- src/GrommetDecorator.jsx
+++ src/GrommetDecorator.jsx
@@ -1,21 +1,24 @@
 import { Box, Grommet } from 'grommet'
+import { BrowserRouter } from 'react-router-dom'
 
 import theme from './theme'
 
 function GrommetDecorator(Story, { title }) {
   if (title === 'Sidebar') {
     return (
-      <Grommet
-        full
-        theme={theme}
-      >
-        <Box
-          style={{ height: '100vh' }}
-          direction="row"
+      <BrowserRouter>
+        <Grommet
+          full
+          theme={theme}
         >
-          <Story />
-        </Box>
-      </Grommet>
+          <Box
+            style={{ height: '100vh' }}
+            direction="row"
+          >
+            <Story />
+          </Box>
+        </Grommet>
+      </BrowserRouter>
     )
   }
 
