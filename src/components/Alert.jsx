51e2ca3dc2e1db4b428b545a2217e0341e868a42
David Hérault <dherault@gmail.com>
2022-04-12 15:47:00 +0300

    Add Alert

---
+++ src/components/Alert.jsx
@@ -0,0 +1,95 @@
+import PropTypes from 'prop-types'
+import { Box, Text } from 'grommet'
+import { normalizeColor } from 'grommet/utils'
+import styled from 'styled-components'
+
+import StatusIpIcon from './icons/StatusIpIcon'
+import StatusOkIcon from './icons/StatusOkIcon'
+import ErrorIcon from './icons/ErrorIcon'
+import CloseIcon from './icons/CloseIcon'
+
+const severityToBackgroundColor = {
+  success: 'status-ok-dark',
+  warning: 'status-warning-dark',
+  error: 'status-critical-dark',
+  info: 'background-light',
+}
+
+const severityToIcon = {
+  success: StatusIpIcon,
+  warning: StatusOkIcon,
+  error: ErrorIcon,
+  info: StatusOkIcon,
+}
+
+const severityToColor = {
+  success: 'status-ok',
+  warning: 'status-warning',
+  error: 'status-critical',
+  info: 'brand',
+}
+
+const Container = styled(Box)`
+  position: relative;
+  padding: 16px 24px;
+  border-radius: 4px;
+  background-color: ${({ theme, severity }) => normalizeColor(severityToBackgroundColor[severity] || 'background-light', theme)};
+`
+
+const CloseContainer = styled(Box)`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  cursor: pointer;
+`
+
+function Alert({ severity, children, title, onClose, ...props }) {
+  const Icon = severityToIcon[severity] || StatusOkIcon
+  const color = severityToColor[severity] || 'brand'
+
+  return (
+    <Container
+      severity={severity}
+      direction="row"
+      align="center"
+    >
+      {typeof onClose === 'function' && (
+        <CloseContainer onClick={onClose}>
+          <CloseIcon
+            color="white"
+            size={12}
+          />
+        </CloseContainer>
+      )}
+      <Icon
+        size={24}
+        color={color}
+      />
+      <Box margin={{ left: '24px' }}>
+        <Text
+          weight="bold"
+          margin={{ bottom: '16px' }}
+        >
+          {title}
+        </Text>
+        <Text>
+          {children}
+        </Text>
+      </Box>
+    </Container>
+  )
+}
+
+Alert.propTypes = {
+  severity: PropTypes.oneOf(['success', 'warning', 'error', 'info']),
+  title: PropTypes.string,
+  onClose: PropTypes.func,
+}
+
+Alert.defaultProps = {
+  severity: 'info',
+  title: '',
+  onClose: null,
+}
+
+export default Alert
--- src/components/icons/StatusIpIcon.jsx
+++ src/components/icons/StatusIpIcon.jsx
@@ -10,8 +10,7 @@ export default createIcon(({ size, color, ...props }) => (
   >
     <path
       d="M8 0C3.6 0 0 3.6 0 8C0 12.4 3.6 16 8 16C12.4 16 16 12.4 16 8C16 3.6 12.4 0 8 0ZM6.9 12L3.7 8.8L4.6 7.9L6.8 10.1L11.3 4.4L12.3 5.2L6.9 12Z"
-      fill="#07E5A7"
+      fill={color}
     />
   </svg>
 ))
-
