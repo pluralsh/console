b0af1212c868a043dce6f8414b8e0cfe437a746f
David Hérault <dherault@gmail.com>
2022-04-26 20:36:58 +0300

    Refactor Token

--- src/components/Token.tsx
+++ src/components/Token.tsx
@@ -1,11 +1,10 @@
 import { PropsWithChildren } from 'react'
-import { Box, Text } from 'grommet'
-import styled from 'styled-components'
+import { Div, P, Span } from 'honorable'
 import PropTypes from 'prop-types'
 
 import CloseIcon from './icons/CloseIcon'
 
-type TokenProps = PropsWithChildren<{
+type TokenProps = typeof Div & PropsWithChildren<{
   onClose?: () => void
 }>
 
@@ -13,38 +12,34 @@ const propTypes = {
   onClose: PropTypes.func,
 }
 
-const Container = styled(Box)`
-  display: inline-flex;
-  min-height: 28px;
-`
-const IconContainer = styled.span`
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  margin-left: 12px;
-  cursor: pointer;
-`
-
 function Token({ children, onClose = () => {}, ...props }: TokenProps) {
 
   return (
-    <Container
-      background="background-light"
-      direction="row"
-      align="center"
-      round="4px"
-      pad={{ horizontal: '8px' }}
+    <Div
+      px={0.5}
+      xflex="x4"
+      display="inline-flex"
+      minHeight={28}
+      borderRadius={4}
+      backgroundColor="background-light"
       {...props}
     >
-      <Text
-        size="small"
-      >
+      <P size="small">
         {children}
-      </Text>
-      <IconContainer onClick={onClose}>
+      </P>
+      <Span
+        p={0.5}
+        ml={0.5}
+        mr={-0.333}
+        xflex="x5"
+        borderRadius={1000}
+        cursor="pointer"
+        hoverIndicator="background-contrast"
+        onClick={onClose}
+      >
         <CloseIcon size={8} />
-      </IconContainer>
-    </Container>
+      </Span>
+    </Div>
   )
 }
 
