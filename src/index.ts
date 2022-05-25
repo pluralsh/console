0a68f02da1fac2160fb800fbeec0def1b752cbb1
David Hérault <dherault@gmail.com>
2022-05-25 19:27:48 +0300

    Export Button

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { Avatar, Switch } from 'honorable'
+export { Avatar, Button, Switch } from 'honorable'
 
 // Icons
 export { default as ArrowTopRightIcon } from './components/icons/ArrowTopRightIcon'
