eae234c935ec647385f998598a0166392c18178d
Chris Klink <chris.klink@gmail.com>
2022-06-01 14:44:38 -0700

    Added transitions to Carousel

--- src/components/TextCarousel.stories.tsx
+++ src/components/TextCarousel.stories.tsx
@@ -1,6 +1,6 @@
 import { A } from 'honorable'
 
-import Carousel from './TextCarousel'
+import Carousel, { TextCarousel } from './TextCarousel'
 
 export default {
   title: 'Text Carousel',
@@ -10,14 +10,14 @@ export default {
 
 function Template(args: any) {
   return (
-    <Carousel
+    <TextCarousel
       {...args}
     >
-      <>You can use service accounts to have an entire team manage a set of installations for one of your plural clusters, learn more <A href="">here</A>.</>,
-      <>You can use <b>plural shell sync</b> to transfer your state from the Plural Cloud Shell to the command line.</>,
-      <>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</>,
-      <>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</>,
-    </Carousel>
+      <>You can use service accounts to have an entire team manage a set of installations for one of your plural clusters, learn more <A href="#">here</A>.</>
+      <>You can use <b>plural shell sync</b> to transfer your state from the Plural Cloud Shell to the command line.</>
+      <>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <b>tempor incididunt</b> ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</>
+      <>Sed ut perspiciatis unde <b>omnis</b> iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</>
+    </TextCarousel>
   )
 }
 
--- src/components/TextCarousel.tsx
+++ src/components/TextCarousel.tsx
@@ -1,7 +1,6 @@
 import { A, Button, ButtonProps, Div, DivProps, Flex, P, PProps } from 'honorable'
 import { Children, useState } from 'react'
-
-import { keyframes } from '@emotion/react'
+import { CSSTransition } from 'react-transition-group'
 
 export type Props = {
   mode?: 'indeterminate' | 'determinate',
@@ -18,7 +17,8 @@ function Dot({ active = false, ...props }: { active?: boolean } & DivProps) {
       {...props}
     >
       <Div
-        backgroundColor={active ? 'link-inline' : 'fill-two'}
+        backgroundColor={active ? 'action-link-inline' : 'fill-two'}
+        // _hover={{ backgroundColor: active ? 'action-link-inline' : 'action-input-hover' }}
         width="8px"
         height="8px"
         borderRadius="4px"
@@ -27,53 +27,139 @@ function Dot({ active = false, ...props }: { active?: boolean } & DivProps) {
   )
 }
 
-function FlexItem(props: DivProps) {
+function TextCarouselItem(props:PProps) {
   return (
     <Div
+      pt="16px"
+      pb="8"
+      px="16px"
+    >
+      <P
+        body2
+        color="text-light"
+        fontStyle="italic"
+        {...props}
+      />
+    </Div>
+  )
+}
+
+export function TextCarousel({ children, ...props }:DivProps) {
+  return (
+    <Carousel {...props}>
+      {Children.map(children, (child: any) => (
+        <TextCarouselItem>
+          {child}
+        </TextCarouselItem>
+      )
+      )}
+    </Carousel>
+  )
+}
+
+function FlexItem(props: DivProps) {
+  return (
+    <Flex
       width="100%"
-    //   backgroundColor="blue"
-    //   outline="1px solid green"
       flexShrink="0"
       textAlign="center"
+      alignItems="stretch"
       {...props}
     />
   )
 }
 
-function TextCarouselItem(props:PProps) {
-  return (
-    <P
-      color="text-light"
-      
-      {...props}
-    />
-  )
+const transitionProps = {
+  opacity: '0',
+  visibility: 'hidden',
+  '&.appear, &.appear-active, &.appear-done': {
+    opacity: '1',
+    transform: 'translateY(0)',
+    visibility: 'visible',
+  },
+  '&.enter': {
+    visibility: 'visible',
+    transform: 'scale(1.2, 1.2)',
+    filter: 'blur(10px)',
+  },
+  '&.enter-active': {
+    transition: 'all 1.2s ease',
+    transitionDelay: '0.3s',
+    filter: 'blur(0)',
+  },
+  '&.enter-active,&.enter-done': {
+    visibility: 'visible',
+    transform: 'scale(1, 1)',
+    opacity: '1',
+  },
+  '&.exit': {
+    transform: 'translateY(0)',
+    opacity: 1,
+    visibility: 'visible',
+  },
+  '&.exit-active': {
+    transition: 'all 1.2s ease',
+  },
+  '&.exit-active, &.exit-done': {
+    transform: ' scale(0.75, 0.75)',
+    opacity: '0',
+    filter: 'blur(5px)',
+  },
+  '&.exit-done': {
+    visibility: 'hidden',
+  },
 }
 
 export default function Carousel({ children, ...props }: DivProps) {
-  [activeIndex, setActiveIndex] = useState(0)
+  const [activeIndex, setActiveIndex] = useState(0)
 
   return (
-    <Div>
+    <Div
+      {...props}
+      backgroundColor="fill-one"
+      border="1px solid border"
+      borderRadius="normal"
+    >
       <Flex
-        mt="16px"
         overflow="hidden"
-        alignItems="center"
+        alignItems="stretch"
       >
         {Children.map(children, (child: any, i: any) => (
-          <FlexItem transform={`translateX(${-i * 100}%)`}>
-            {child}
+          <FlexItem
+            transform={`translateX(${-i * 100}%)`}
+            pointerEvents={activeIndex === i ? 'auto' : 'none'}
+          >
+            <CSSTransition
+              in={activeIndex === i}
+              appear
+              timeout={2000}
+            >
+              <Flex 
+                className="stretched"
+                alignItems="center"
+                {...transitionProps}
+              >
+                {child}
+              </Flex>
+            </CSSTransition>
           </FlexItem>
         )
         )}
       </Flex>
-
       <Flex
         mt="8px"
+        mb="16px"
+        justifyContent="center"
       >
-        <Dot /><Dot /><Dot /><Dot />
+        {Children.map(children, (child: any, i: any) => (
+          <Dot
+            active={activeIndex === i}
+            onClick={() => {
+              setActiveIndex(i)
+            }}
+          />
+        ))}
       </Flex>
     </Div>
   )
-
 }
