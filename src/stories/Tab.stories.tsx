7a7619785ebbed775dec9129b829d29fc125cef7
David Hérault <dherault@gmail.com>
2022-06-05 18:07:36 +0300

    Add vertical tabs

--- src/stories/Tab.stories.tsx
+++ src/stories/Tab.stories.tsx
@@ -72,3 +72,74 @@ export const Default = Template.bind({})
 
 Default.args = {
 }
+
+function Template2(args: any) {
+  const [active, setActive] = useState(0)
+
+  return (
+    <>
+      <Flex
+        direction="column"
+        width={256 - 64}
+      >
+        <Tab
+          active={active === 0}
+          onClick={() => setActive(0)}
+          {...args}
+        >
+          Active tab
+        </Tab>
+        <Tab
+          active={active === 1}
+          onClick={() => setActive(1)}
+          {...args}
+        >
+          Inactive tab
+        </Tab>
+        <Tab
+          active={active === 2}
+          onClick={() => setActive(2)}
+          {...args}
+        >
+          Inactive tab
+        </Tab>
+      </Flex>
+      <Flex
+        mt={2}
+        direction="column"
+        width={256 - 64}
+      >
+        <Tab
+          startIcon={<ErrorIcon />}
+          active={active === 0}
+          onClick={() => setActive(0)}
+          {...args}
+        >
+          Active tab
+        </Tab>
+        <Tab
+          startIcon={<ErrorIcon />}
+          active={active === 1}
+          onClick={() => setActive(1)}
+          {...args}
+        >
+          Inactive tab
+        </Tab>
+        <Tab
+          startIcon={<ErrorIcon />}
+          active={active === 2}
+          onClick={() => setActive(2)}
+          {...args}
+        >
+          Inactive tab
+        </Tab>
+      </Flex>
+    </>
+  )
+}
+
+export const Vertical = Template2.bind({})
+
+Vertical.args = {
+  vertical: true,
+}
