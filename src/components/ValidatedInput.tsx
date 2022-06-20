8d2bfa4cbdfd993f5afd30a31ad744414d2297a2
michaeljguarino <mjg@plural.sh>
2022-06-20 19:49:55 -0400

    colorize caption and allow overloading

--- src/components/ValidatedInput.tsx
+++ src/components/ValidatedInput.tsx
@@ -1,36 +1,47 @@
-import { PropsWithChildren, ReactNode, Ref, forwardRef, useCallback, useState } from 'react'
+import React, { PropsWithChildren, ReactNode, Ref, forwardRef, useCallback, useState } from 'react'
 import PropTypes from 'prop-types'
-import { Input, InputProps } from 'honorable'
+import { Input, InputProps, Span } from 'honorable'
 
 import FormField from './FormField'
 
 export type ValidationResponse = {error: boolean, message: string} | null
+export type CaptionProps = {caption: string, color: string}
 
 export type ValidatedInputProps = InputProps & PropsWithChildren<{
   label?: ReactNode
   hint?: ReactNode
+  caption?: (props: CaptionProps) => React.ReactElement
   validation?: (val: string) => ValidationResponse
 }>
 
 const propTypes = {
   label: PropTypes.node,
   hint: PropTypes.node,
+  caption: PropTypes.func,
   validation: PropTypes.func,
 }
 
-function ValidatedInputRef({ label, hint, validation, onChange, width, ...input } : ValidatedInputProps, ref: Ref<any>) {
+function defaultCaption({ caption, color }: CaptionProps) : React.ReactElement {
+  return (
+    <Span color={color}>{caption}</Span>
+  )
+}
+
+function ValidatedInputRef({ label, hint, validation, onChange, width, caption, ...input } : ValidatedInputProps, ref: Ref<any>) {
   const [error, setError] = useState(null)
   const wrappedOnChange = useCallback((e: any) => {
     if (e.target?.value && validation) setError(validation(e.target.value))
     if (onChange) onChange(e)
   }, [onChange, validation])
+
+  const captionComp = caption || defaultCaption
     
   return (
     <FormField
       ref={ref}
       label={label}
       hint={hint}
-      caption={error?.message}
+      caption={error ? React.createElement(captionComp, { caption: error.message, color: error.error ? 'text-error' : 'text-success' }) : null}
       width={width}
     >
       <Input
