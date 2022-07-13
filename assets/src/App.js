4a3030278d5027bd6f4807ed4a32f0b29657f2a0
Marcin Maciaszczyk <marcin@plural.sh>
2022-07-13 12:43:11 +0200

    Setup unit tests

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import {Switch, Route, BrowserRouter} from 'react-router-dom'
 import { Grommet } from 'grommet'
 import { DEFAULT_THEME } from './theme';
 import Console from './components/Console';
@@ -9,21 +9,27 @@ import { OAuthCallback } from './components/OauthCallback';
 import "react-toggle/style.css"
 import 'react-pulse-dot/dist/index.css'
 import { IntercomProvider } from 'react-use-intercom'
+import {ApolloProvider} from "react-apollo";
+import {client} from "./helpers/client";
 
 const INTERCOM_APP_ID = 'p127zb9y'
 
 export default function App() {
   return (
-    <IntercomProvider appId={INTERCOM_APP_ID}>
-    <Grommet theme={DEFAULT_THEME}>
-      <Switch>
-        <Route path='/login' component={Login} />
-        <Route path='/access' component={GrantAccess} />
-        <Route path='/oauth/callback' component={OAuthCallback} />
-        <Route path='/invite/:inviteId' component={Invite} />
-        <Route path='/' component={Console} />
-      </Switch>
-    </Grommet>
-    </IntercomProvider>
+    <ApolloProvider client={client}>
+      <IntercomProvider appId={INTERCOM_APP_ID}>
+      <Grommet theme={DEFAULT_THEME}>
+        <BrowserRouter>
+          <Switch>
+            <Route path='/login' component={Login} />
+            <Route path='/access' component={GrantAccess} />
+            <Route path='/oauth/callback' component={OAuthCallback} />
+            <Route path='/invite/:inviteId' component={Invite} />
+            <Route path='/' component={Console} />
+          </Switch>
+        </BrowserRouter>
+      </Grommet>
+      </IntercomProvider>
+    </ApolloProvider>
   );
 }
