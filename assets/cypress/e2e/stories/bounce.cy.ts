af91191c15e85d02f56823916ae08888c710d80a
Sebastian Florek <sebastian@plural.sh>
2022-07-19 12:11:07 +0200

    Update test

--- assets/cypress/e2e/stories/bounce.cy.ts
+++ assets/cypress/e2e/stories/bounce.cy.ts
@@ -2,8 +2,8 @@ import {LoginPage} from '@pages/login';
 
 describe('bounce the airbyte app', () => {
   it('log in to the console', () => {
+    expect(false).to.be.true;
     LoginPage.visit();
     LoginPage.login();
-    expect(false)
   })
 });
