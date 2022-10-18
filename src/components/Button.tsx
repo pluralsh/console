a35b5822fc58fdc0b6f94fb3338c8ec5997013be
Marcin Maciaszczyk <marcin@plural.sh>
2022-10-18 13:44:08 +0200

    Refactor button pulse animation (#226)
    * Refactor button pulse animation
    
    * Bump version

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,38 +1,24 @@
-import { ExtendTheme, Button as HonorableButton, mergeTheme } from 'honorable'
+import { Button as HonorableButton } from 'honorable'
 import type { ButtonProps as HonorableButtonProps } from 'honorable'
-import styled from 'styled-components'
+import { keyframes } from '@emotion/react'
 
 export type ButtonProps = HonorableButtonProps & {pulse?: boolean}
 
-const ButtonWrapper = styled.div`
-@keyframes pulse {
+const pulseKeyframes = keyframes`
   0% { box-shadow: 0 0 7px 2px #fff1; }
   70% { box-shadow: 0 0 7px 4px #fff2; }
   100% { box-shadow: 0 0 7px 2px #fff1; }
 }`
 
 export default function Button({ pulse = false, ...props }: ButtonProps) {
-  let themeExtension: any = {}
-
-  if (pulse) {
-    themeExtension = mergeTheme(themeExtension, {
-      Button: {
-        Root: [{
-          boxShadow: '0 0 7px 2px #fff1',
-          animation: 'pulse 4s infinite',
-          ':hover': {
-            animation: 'none',
-          },
-        }],
-      },
-    })
-  }
-
   return (
-    <ButtonWrapper>
-      <ExtendTheme theme={themeExtension}>
-        <HonorableButton {...props} />
-      </ExtendTheme>
-    </ButtonWrapper>
+    <HonorableButton
+      animationIterationCount="infinite"
+      animationDuration="4s"
+      animationName={pulse ? pulseKeyframes : undefined}
+      boxShadow={pulse ? '0 0 7px 2px #fff1' : undefined}
+      _hover={{ animationPlayState: 'paused' }}
+      {...props}
+    />
   )
 }
