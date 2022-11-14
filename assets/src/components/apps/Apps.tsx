5adbf6ff4496c647b7d2b83c2c6e341567e66879
Marcin Maciaszczyk <marcin9yk@icloud.com>
2022-11-14 11:33:25 +0100

    Add filtering

--- assets/src/components/apps/Apps.tsx
+++ assets/src/components/apps/Apps.tsx
@@ -1,15 +1,32 @@
+import { Readiness, appState } from 'components/Application'
 import { InstallationContext } from 'components/Installations'
 import { Div, Flex } from 'honorable'
-import { Input, MagnifyingGlassIcon, PageTitle } from 'pluralsh-design-system'
-import { useContext, useState } from 'react'
+import {
+  Input,
+  MagnifyingGlassIcon,
+  PageTitle,
+  SubTab,
+  TabList,
+} from 'pluralsh-design-system'
+import { useContext, useRef, useState } from 'react'
 
 import App from './AppCard'
+
+const FILTERS = [
+  { key: '', label: 'All' },
+  { key: Readiness.Ready, label: 'Ready' },
+  { key: Readiness.Complete, label: 'Complete' },
+  { key: Readiness.InProgress, label: 'Pending' },
+  { key: Readiness.Failed, label: 'Failed' },
+]
 
 export default function Apps() {
   const {
     applications, setCurrentApplication, currentApplication,
   }: any = useContext(InstallationContext)
   const [query, setQuery] = useState('')
+  const [filter, setFilter] = useState('')
+  const tabStateRef = useRef<any>(null)
 
   return (
     <Div fill>
@@ -17,8 +34,26 @@ export default function Apps() {
         heading="Apps"
         margin="large"
       >
+        <Flex grow={1} />
+        <TabList
+          stateRef={tabStateRef}
+          stateProps={{
+            orientation: 'horizontal',
+            selectedKey: filter,
+            onSelectionChange: setFilter,
+          }}
+        >
+          {FILTERS.map(({ label, key }) => (
+            <SubTab
+              key={key}
+              textValue={label}
+            >
+              {label}
+            </SubTab>
+          ))}
+        </TabList>
         <Input
-          placeholder="Search an application"
+          placeholder="Filter applications"
           startIcon={(<MagnifyingGlassIcon size={14} />)}
           value={query}
           onChange={event => setQuery(event.target.value)}
@@ -30,11 +65,12 @@ export default function Apps() {
         wrap="wrap"
       >
         {applications
-          .filter(application => !query || application.name.startsWith(query))
-          .map(application => (
+          .filter(app => !query || app.name.startsWith(query)) // TODO: Use better search method.
+          .filter(app => !filter || appState(app).readiness === filter) // TODO: Improve performance.
+          .map(app => (
             <App
-              key={application.name}
-              application={application}
+              key={app.name}
+              application={app}
               current={currentApplication}
               setCurrentApplication={setCurrentApplication}
             />
