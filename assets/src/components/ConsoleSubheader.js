424cb79521e826c5a56730da5dccc186a4441128
Marcin Maciaszczyk <marcin9yk@icloud.com>
2022-11-17 13:13:05 +0100

    Add TODOs

--- assets/src/components/ConsoleSubheader.js
+++ assets/src/components/ConsoleSubheader.js
@@ -31,6 +31,7 @@ export default function ConsoleSubheader() {
           <Button
             floating
             small
+            // TODO: Add disabled state.
             paddingHorizontal="xsmall"
             onClick={() => navigate(-1)}
           >
@@ -39,6 +40,7 @@ export default function ConsoleSubheader() {
           <Button
             floating
             small
+            // TODO: Add disabled state.
             paddingHorizontal="xsmall"
             onClick={() => navigate(1)}
           >
