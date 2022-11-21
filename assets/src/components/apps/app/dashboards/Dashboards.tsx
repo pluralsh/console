c2ab423c4f6d9f44fe2d3ed31f4da9388499404d
Marcin Maciaszczyk <marcin9yk@icloud.com>
2022-11-21 12:34:32 +0100

    Add dashboards empty state

--- assets/src/components/apps/app/dashboards/Dashboards.tsx
+++ assets/src/components/apps/app/dashboards/Dashboards.tsx
@@ -1,11 +1,11 @@
 import { BreadcrumbsContext } from 'components/Breadcrumbs'
 import { InstallationContext } from 'components/Installations'
-import { PageTitle } from '@pluralsh/design-system'
+import { DashboardIcon, EmptyState, PageTitle } from '@pluralsh/design-system'
 import { useContext, useEffect } from 'react'
 import { useQuery } from 'react-apollo'
 import { DASHBOARDS_Q } from 'components/graphql/dashboards'
 
-import { Div } from 'honorable'
+import { A, Div, Flex } from 'honorable'
 
 import DashboardCard from './DashboardCard'
 
@@ -28,7 +28,7 @@ export default function Dashboards() {
   const { dashboards } = data
 
   return (
-    <Div>
+    <Flex direction="column">
       <PageTitle heading="Dashboards" />
       {dashboards.map(({ spec: { name, description } }) => (
         <DashboardCard
@@ -36,6 +36,33 @@ export default function Dashboards() {
           description={description}
         />
       ))}
-    </Div>
+      {dashboards?.length < 1 && (
+        <Flex justify="center">
+          <EmptyState
+            marginTop={96}
+            width={500}
+            icon={<DashboardIcon size={64} />}
+            message="No dashboards available"
+            description={(
+              <>
+                <Div>If you're interested in adding your dashboards to this</Div>
+                <Div>
+                  application,&nbsp;
+                  <A
+                    inline
+                    href="https://www.plural.sh/community"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    visit our docs
+                  </A>
+                &nbsp;for more details.
+                </Div>
+              </> as any // Workaround as JSX elements are not allowed here.
+            )}
+          />
+        </Flex>
+      )}
+    </Flex>
   )
 }
