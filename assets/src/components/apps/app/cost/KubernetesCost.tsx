31638ba6233dd34cb1456f7f75f8eae5e2beecd4
Marcin Maciaszczyk <marcin9yk@icloud.com>
2022-11-28 13:22:33 +0100

    Style Kubernetes cost

---
+++ assets/src/components/apps/app/cost/KubernetesCost.tsx
@@ -0,0 +1,80 @@
+import { useMemo } from 'react'
+import { sum } from 'lodash'
+import { Flex, Span } from 'honorable'
+import { ProgressBar } from '@pluralsh/design-system'
+
+const MINUTES_MONTH = 60 * 24 * 30
+
+const round = v => Math.round(v * 100) / 100
+
+const scale = (amount, scalar) => round(amount * scalar)
+
+export default function KubernetesCost({ cost }) {
+  const scalar = cost ? Math.max(MINUTES_MONTH / cost.minutes, 1) : 1
+
+  const data = useMemo(() => {
+    const miscCost = cost.totalCost - (cost.cpuCost + cost.ramCost + cost.pvCost)
+
+    return [
+      { dim: 'cpu', cost: scale(cost.cpuCost, scalar) },
+      { dim: 'memory', cost: scale(cost.ramCost, scalar) },
+      { dim: 'storage', cost: scale(cost.pvCost, scalar) },
+      { dim: 'misc.', cost: scale(miscCost, scalar) },
+    ]
+  }, [cost, scalar])
+
+  const total = round(sum(data.map(({ cost }) => cost)))
+
+  return (
+    <Flex
+      basis={600}
+      direction="column"
+      grow={1}
+      shrink={1}
+    >
+      <Flex marginVertical={9}>
+        <Span
+          body1
+          flexBasis={520}
+          fontWeight={600}
+        >
+          Kubernetes cost
+        </Span>
+        <Span
+          flexBasis={80}
+          color="text-light"
+        >${total}
+        </Span>
+      </Flex>
+      {data.map(entry => (
+        <Flex
+          align="center"
+          justify="space-between"
+          gap="medium"
+          marginVertical={9}
+        >
+          <Flex
+            color="text-xlight"
+            overline
+            basis={200}
+          >
+            {entry.dim}
+          </Flex>
+          <Flex basis={320}>
+            <ProgressBar
+              mode="determinate"
+              progress={entry.cost / total}
+              // TODO: Change color to blue 400 instead of blue 200. Allow it in design system.
+            />
+          </Flex>
+          <Flex
+            basis={80}
+            color="text-light"
+          >
+            ${Number(entry.cost).toLocaleString('en-US', { minimumFractionDigits: 2 })}
+          </Flex>
+        </Flex>
+      ))}
+    </Flex>
+  )
+}
