1324d3d55a215ecb4fd0e73a133f23ea1d3b7ec1
Marcin Maciaszczyk <marcin9yk@icloud.com>
2022-11-29 11:21:16 +0100

    Add zero state

--- assets/src/components/apps/app/runbooks/Runbooks.tsx
+++ assets/src/components/apps/app/runbooks/Runbooks.tsx
@@ -1,12 +1,12 @@
 import { BreadcrumbsContext } from 'components/Breadcrumbs'
-import { PageTitle, RunBookIcon } from '@pluralsh/design-system'
+import { EmptyState, PageTitle, RunBookIcon } from '@pluralsh/design-system'
 import { useContext, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { POLL_INTERVAL } from 'components/runbooks/constants'
 import { RUNBOOKS_Q } from 'components/runbooks/queries'
 import { useQuery } from 'react-apollo'
 import { ListItem } from 'components/apps/misc'
-import { Flex } from 'honorable'
+import { A, Div, Flex } from 'honorable'
 
 export const getBorderColor = status => (status?.alerts?.length > 0 ? 'border-warning' : '')
 
@@ -43,6 +43,32 @@ export default function Runbooks() {
           onClick={() => navigate(`/apps/${appName}/runbooks/${id}`)}
         />
       ))}
+      {runbooks?.length < 1 && (
+        <Flex justify="center">
+          <EmptyState
+            marginTop={96}
+            width={600}
+            icon={<RunBookIcon size={64} />}
+            message="No runbooks available"
+            description={(
+              <>
+                <Div>If you're interested in adding runbooks to this application,&nbsp;</Div>
+                <Div>
+                  <A
+                    inline
+                    href="https://www.plural.sh/community"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    visit our docs
+                  </A>
+                &nbsp;for more details.
+                </Div>
+              </> as any // Workaround as JSX elements are not allowed here.
+            )}
+          />
+        </Flex>
+      )}
     </Flex>
   )
 }
