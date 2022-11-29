a69e92e6e9501c3b6d8759ac27086068baee19b2
Sebastian Florek <sebastian@plural.sh>
2022-11-29 13:52:50 +0100

    fix(build): fix yarn start script (#367)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
-import react from 'vite-preset-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
   },
