169e8f6425bdd670650f3728bb271bf6b039f48e
Marcin Maciaszczyk <marcin9yk@icloud.com>
2022-12-21 14:21:06 +0100

    Replace code editor

--- assets/src/components/apps/app/config/ConfigurationEditor.tsx
+++ assets/src/components/apps/app/config/ConfigurationEditor.tsx
@@ -1,49 +1,31 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { UPDATE_CONFIGURATION } from 'components/graphql/plural'
-import { Button } from 'forge-core'
 import { useMutation } from 'react-apollo'
-import { Box } from 'grommet'
-import AceEditor from 'react-ace'
 
-import 'ace-builds/src-noconflict/mode-yaml'
-import 'ace-builds/src-noconflict/theme-terminal'
+import { CodeEditor } from '@pluralsh/design-system'
 
 import { ConfigType } from './misc'
 
-export function ConfigurationEditor({ application: { name, configuration: { helm, terraform } }, view }) {
+export function ConfigurationEditor({ application: { name: repository, configuration: { helm, terraform } }, view }) {
   const navigate = useNavigate()
-  const [config, setConfig] = useState<any>(helm)
+  const [content, setContent] = useState<any>(helm)
   const onCompleted = useCallback(() => navigate('/'), [navigate])
   const [mutation, { loading }] = useMutation(UPDATE_CONFIGURATION, {
-    variables: { repository: name, content: config, type: view },
-    onCompleted,
+    variables: { repository, content, type: view }, onCompleted,
   })
 
-  useEffect(() => setConfig(view === ConfigType.HELM ? helm : terraform), [view, helm, terraform])
-
   return (
-    <Box fill>
-      <Button
-        flat
-        label="Commit"
-        onClick={mutation}
-        background="brand"
-        loading={loading}
-      />
-      <AceEditor
-        mode={view === ConfigType.HELM ? 'yaml' : 'terraform'}
-        theme="terminal"
-        height="calc(100vh - 125px)"
-        width="100%"
-        name={name}
-        value={config}
-        showGutter
-        showPrintMargin
-        highlightActiveLine
-        editorProps={{ $blockScrolling: true }}
-        onChange={setConfig}
-      />
-    </Box>
+    <CodeEditor
+      value={view === ConfigType.HELM ? helm : terraform}
+      language={view === ConfigType.HELM ? 'yaml' : 'hcl'}
+      save
+      saving={loading}
+      onSave={value => {
+        setContent(value)
+        mutation()
+      }}
+      saveLabel="Commit"
+    />
   )
 }
