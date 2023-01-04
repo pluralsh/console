a3d01103cd97e7255dc54432f403c23346265072
Klink <85062+dogmar@users.noreply.github.com>
2023-01-04 10:33:45 -0800

    Make ComponentEvents more typesafe

--- assets/src/components/apps/app/components/component/ComponentEvents.tsx
+++ assets/src/components/apps/app/components/component/ComponentEvents.tsx
@@ -6,41 +6,45 @@ import { useOutletContext, useParams } from 'react-router-dom'
 import { Date } from 'components/utils/Date'
 
 import { createColumnHelper } from '@tanstack/react-table'
+import { Event as EventT } from 'generated/graphql'
 
-const COLUMN_HELPER = createColumnHelper<any>()
+const COLUMN_HELPER = createColumnHelper<EventT>()
 
 const columns = [
   COLUMN_HELPER.accessor(event => event.type, {
     id: 'type',
-    cell: (type: any) => type.getValue(),
+    cell: type => type.getValue(),
     header: 'Type',
   }),
   COLUMN_HELPER.accessor(event => event.reason, {
     id: 'reason',
-    cell: (reason: any) => reason.getValue(),
+    cell: reason => reason.getValue(),
     header: 'Reason',
   }),
   COLUMN_HELPER.accessor(event => event.message, {
     id: 'message',
-    cell: (message: any) => message.getValue(),
+    cell: message => message.getValue(),
     header: 'Message',
   }),
   COLUMN_HELPER.accessor(event => event.count, {
     id: 'count',
-    cell: (count: any) => count.getValue(),
+    cell: count => count.getValue(),
     header: 'Count',
   }),
   COLUMN_HELPER.accessor(event => event.lastTimestamp, {
     id: 'lastTimestamp',
-    cell: (lastTimestamp: any) => <Date date={lastTimestamp.getValue()} />,
+    cell: lastTimestamp => <Date date={lastTimestamp.getValue()} />,
     header: 'Last seen',
   }),
 ]
 
 export default function ComponentEvents() {
   const { appName, componentKind, componentName } = useParams()
-  const { data } = useOutletContext<any>()
+  const outletContext = useOutletContext<object>() ?? {}
+  const data = 'data' in outletContext ? (outletContext.data ?? {}) : {}
   const { setBreadcrumbs }: any = useContext(BreadcrumbsContext)
+
+  console.log('outletContext', outletContext)
 
   useEffect(() => setBreadcrumbs([
     { text: 'Apps', url: '/' },
@@ -51,8 +55,8 @@ export default function ComponentEvents() {
 
   // To avoid mapping between component types and fields of data returned by API
   // we are picking first available value from API object for now.
-  const value: any = Object.values(data).find(value => value !== undefined)
-  const events: any = value?.events
+  const value = Object.values(data).find(value => value !== undefined)
+  const events: EventT[] = value?.events || []
 
   return (
     <>
