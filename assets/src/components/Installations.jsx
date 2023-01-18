5c4885cc434e26491031f1bb9ae50e272930d693
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-01-18 12:22:18 +0100

    Remove unused code

--- assets/src/components/Installations.jsx
+++ assets/src/components/Installations.jsx
@@ -20,20 +20,6 @@ export function ApplicationIcon({ application: { spec: { descriptor: { icons } }
   )
 }
 
-export const hasIcon = ({ spec: { descriptor: { icons } } }) => icons.length > 0
-
-export function useEnsureCurrent(repo) {
-  const { applications, currentApplication, setCurrentUnsafe } = useContext(InstallationContext)
-
-  useEffect(() => {
-    const desired = applications.find(({ name }) => name === repo)
-
-    if (desired && currentApplication.name !== desired.name) {
-      setCurrentUnsafe(desired)
-    }
-  }, [repo, applications, currentApplication])
-}
-
 function applyDelta(prev, { delta, payload }) {
   switch (delta) {
   case 'CREATE':
@@ -51,7 +37,7 @@ export function InstallationsProvider({ children }) {
   useEffect(() => subscribeToMore({
     document: APPLICATION_SUB,
     updateQuery: (prev, { subscriptionData: { data } }) => (data ? applyDelta(prev, data.applicationDelta) : prev),
-  }), [])
+  }), [subscribeToMore])
 
   if (loading || !data) return <LoopingLogo />
 
