34882f79d4198b0d239b59485b7c6949c313eb2a
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-01-24 10:49:59 +0100

    Restore OAuth changes from master

--- assets/src/components/OauthCallback.jsx
+++ assets/src/components/OauthCallback.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react'
 import gql from 'graphql-tag'
 import { Box } from 'grommet'
-import { GqlError } from 'forge-core'
-import { useMutation } from '@apollo/client'
-import { useLocation } from 'react-router-dom'
+import { Alert, AlertStatus, GqlError } from 'forge-core'
+import { useMutation } from 'react-apollo'
+import { useLocation } from 'react-router'
 import qs from 'query-string'
 
 import { setToken } from '../helpers/auth'
@@ -11,6 +11,7 @@ import { setToken } from '../helpers/auth'
 import { localized } from '../helpers/hostname'
 
 import { LoopingLogo } from './utils/AnimatedLogo'
+import { LoginPortal } from './Login'
 
 const CALLBACK = gql`
   mutation Callback($code: String!, $redirect: String) {
@@ -18,9 +19,28 @@ const CALLBACK = gql`
   }
 `
 
+function OAuthError({ error: { error, error_description: description } }) {
+  return (
+    <LoginPortal>
+      <Box gap="medium">
+        <Box
+          gap="xsmall"
+          align="center"
+        >
+          <Alert
+            status={AlertStatus.ERROR}
+            header={error}
+            description={description || 'You cannot log into this console instance, make sure your Plural user was added to its OIDC provider'}
+          />
+        </Box>
+      </Box>
+    </LoginPortal>
+  )
+}
+
 export function OAuthCallback() {
   const location = useLocation()
-  const { code } = qs.parse(location.search)
+  const { code, ...oauthError } = qs.parse(location.search)
   const [mutation, { error, loading }] = useMutation(CALLBACK, {
     variables: { code, redirect: localized('/oauth/callback') },
     onCompleted: result => {
@@ -31,8 +51,10 @@ export function OAuthCallback() {
   })
 
   useEffect(() => {
-    mutation()
+    if (code) mutation()
   }, [code])
+
+  if (!code) return <OAuthError error={oauthError} />
 
   return (
     <Box
