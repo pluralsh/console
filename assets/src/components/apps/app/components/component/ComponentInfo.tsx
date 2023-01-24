c880f1c6da2b8d216369c74a99873da1c1d8104b
Klink <85062+dogmar@users.noreply.github.com>
2023-01-24 10:22:29 -0800

    Added ability to mutate scaling recommendation

--- assets/src/components/apps/app/components/component/ComponentInfo.tsx
+++ assets/src/components/apps/app/components/component/ComponentInfo.tsx
@@ -127,8 +127,9 @@ export default function ComponentInfo() {
         <Flex gap="medium">
           <ScalingRecommenderModal
             kind={kind}
-            name={componentName}
+            componentName={componentName}
             namespace={appName}
+
           />
           <ViewLogsButton metadata={value?.metadata} />
         </Flex>
