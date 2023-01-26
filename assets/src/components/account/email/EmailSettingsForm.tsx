1a7167a20523c6018d9b8eeceb4bec2faa20c7ee
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-01-26 12:24:45 +0100

    Make save button sticy to bottom and card scrollable in email settings

--- assets/src/components/account/email/EmailSettingsForm.tsx
+++ assets/src/components/account/email/EmailSettingsForm.tsx
@@ -1,7 +1,7 @@
 import { useMutation } from '@apollo/client'
-import { Button, ValidatedInput } from '@pluralsh/design-system'
+import { Button, Card, ValidatedInput } from '@pluralsh/design-system'
 import { UPDATE_SMTP } from 'components/graphql/plural'
-import { Flex } from 'honorable'
+import { Div, Flex } from 'honorable'
 import { useState } from 'react'
 
 const clean = smtp => {
@@ -17,49 +17,61 @@ export default function EmailSettingsForm({ smtp }) {
   })
 
   return (
-    <Flex
+    <Card
       direction="column"
-      gap="small"
+      overflow="hidden"
     >
-      <ValidatedInput
-        label="Server"
-        placeholder="smtp.sendrid.net"
-        width="100%"
-        value={form.server || ''}
-        oonChange={({ target: { value } }) => setForm({ ...form, server: value })}
-      />
-      <ValidatedInput
-        label="Port"
-        placeholder="587"
-        width="100%"
-        value={form.port || ''}
-        onChange={({ target: { value } }) => setForm({ ...form, port: parseInt(value) })}
-      />
-      <ValidatedInput
-        label="Sender"
-        hint="From address for outgoing emails"
-        width="100%"
-        value={form.sender || ''}
-        onChange={({ target: { value } }) => setForm({ ...form, sender: value })}
-      />
-      <ValidatedInput
-        label="User"
-        hint="Username for SMTP authentication"
-        width="100%"
-        value={form.user || ''}
-        onChange={({ target: { value } }) => setForm({ ...form, user: value })}
-      />
-      <ValidatedInput
-        label="Password"
-        hint="Password for SMTP authentication"
-        type="password"
-        width="100%"
-        value={form.password || ''}
-        onChange={({ target: { value } }) => setForm({ ...form, password: value })}
-      />
+      <Div
+        direction="column"
+        gap="small"
+        overflowY="auto"
+        height="calc(100% - 88px)"
+        paddingHorizontal={128}
+        paddingVertical="xlarge"
+      >
+        <ValidatedInput
+          label="Server"
+          placeholder="smtp.sendrid.net"
+          width="100%"
+          value={form.server || ''}
+          oonChange={({ target: { value } }) => setForm({ ...form, server: value })}
+        />
+        <ValidatedInput
+          label="Port"
+          placeholder="587"
+          width="100%"
+          value={form.port || ''}
+          onChange={({ target: { value } }) => setForm({ ...form, port: parseInt(value) })}
+        />
+        <ValidatedInput
+          label="Sender"
+          hint="From address for outgoing emails"
+          width="100%"
+          value={form.sender || ''}
+          onChange={({ target: { value } }) => setForm({ ...form, sender: value })}
+        />
+        <ValidatedInput
+          label="User"
+          hint="Username for SMTP authentication"
+          width="100%"
+          value={form.user || ''}
+          onChange={({ target: { value } }) => setForm({ ...form, user: value })}
+        />
+        <ValidatedInput
+          label="Password"
+          hint="Password for SMTP authentication"
+          type="password"
+          width="100%"
+          value={form.password || ''}
+          onChange={({ target: { value } }) => setForm({ ...form, password: value })}
+        />
+      </Div>
       <Flex
-        justifyContent="flex-end"
-        marginTop="small"
+        align="end"
+        height={88}
+        justifyContent="end"
+        paddingHorizontal={134}
+        paddingBottom="xlarge"
       >
         <Button
           onClick={() => mutation()}
@@ -68,6 +80,6 @@ export default function EmailSettingsForm({ smtp }) {
           Update
         </Button>
       </Flex>
-    </Flex>
+    </Card>
   )
 }
