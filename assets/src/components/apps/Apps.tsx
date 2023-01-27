392f7422280ba966095925c03c5765970f0866d3
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-01-27 11:00:33 +0100

    Add number of apps to labels

--- assets/src/components/apps/Apps.tsx
+++ assets/src/components/apps/Apps.tsx
@@ -4,6 +4,7 @@ import { A, Div, Flex } from 'honorable'
 import {
   AppsIcon,
   Button,
+  Chip,
   DiscordIcon,
   EmptyState,
   Input,
@@ -15,6 +16,7 @@ import {
   TabList,
 } from '@pluralsh/design-system'
 import {
+  useCallback,
   useContext,
   useEffect,
   useMemo,
@@ -27,14 +29,18 @@ import { Readiness, readinessToLabel } from 'utils/status'
 
 import { ScrollablePage } from 'components/utils/layout/ScrollablePage'
 
+import { isEmpty } from 'lodash'
+
 import App from './AppCard'
 import { appState } from './misc'
 
+const ALL_FILTER = 'All'
+
 const FILTERS = [
-  { key: '', label: 'All' },
-  { key: Readiness.Ready, label: readinessToLabel[Readiness.Ready] },
-  { key: Readiness.InProgress, label: readinessToLabel[Readiness.InProgress] },
-  { key: Readiness.Failed, label: readinessToLabel[Readiness.Failed] },
+  { key: ALL_FILTER, label: 'All', color: 'neutral' },
+  { key: Readiness.Ready, label: readinessToLabel[Readiness.Ready], color: 'success' },
+  { key: Readiness.InProgress, label: readinessToLabel[Readiness.InProgress], color: 'warning' },
+  { key: Readiness.Failed, label: readinessToLabel[Readiness.Failed], color: 'error' },
 ]
 
 function QueryEmptyState({ query, setQuery }) {
@@ -123,29 +129,35 @@ const searchOptions = {
 }
 
 export default function Apps() {
-  const { applications } = useContext<any>(InstallationContext)
+  const { applications = [] } = useContext<any>(InstallationContext)
   const { setBreadcrumbs } = useContext<any>(BreadcrumbsContext)
   const [query, setQuery] = useState<string>('')
-  const [filter, setFilter] = useState<any>('')
+  const [filter, setFilter] = useState<any>(ALL_FILTER)
   const tabStateRef = useRef<any>(null)
 
-  useEffect(() => {
-    setBreadcrumbs([{ text: 'apps', url: '/' }])
-  }, [setBreadcrumbs])
+  useEffect(() => setBreadcrumbs([{ text: 'apps', url: '/' }]), [setBreadcrumbs])
+
+  const handleFilter = useCallback(f => applications
+    .filter(app => !f || appState(app).readiness === f), [applications])
+
+  const appsByState = useMemo(() => ({
+    [ALL_FILTER]: applications,
+    [Readiness.Ready]: handleFilter(Readiness.Ready),
+    [Readiness.InProgress]: handleFilter(Readiness.InProgress),
+    [Readiness.Failed]: handleFilter(Readiness.Failed),
+  }), [applications, handleFilter])
 
   const filteredApps = useMemo(() => {
-    const apps = applications || []
-
-    const filteredByState = apps.filter(app => !filter || appState(app).readiness === filter)
+    const filteredByState = appsByState[filter]
 
     const fuse = new Fuse(filteredByState, searchOptions)
     const filteredByQuery = query ? fuse.search(query).map(({ item }) => item) : filteredByState
 
     return filteredByQuery
-  }, [applications, filter, query])
+  }, [appsByState, filter, query])
   const noFilteredApps = filteredApps?.length < 1
 
-  if (!applications) return <LoopingLogo />
+  if (isEmpty(applications)) return <LoopingLogo />
 
   return (
     <Flex
@@ -165,14 +177,29 @@ export default function Apps() {
                 onSelectionChange: setFilter,
               }}
             >
-              {FILTERS.map(({ label, key }) => (
-                <SubTab
-                  key={key}
-                  textValue={label}
-                >
-                  {label}
-                </SubTab>
-              ))}
+              {FILTERS.map(({ label, key, color }) => {
+                const appCount = appsByState[key]?.length
+
+                return (
+                  <SubTab
+                    key={key}
+                    textValue={label}
+                  >
+                    <Flex align="center">
+                      {label}
+                      {!!appCount && (
+                        <Chip
+                          marginLeft="xsmall"
+                          size="small"
+                          severity={color}
+                        >
+                          {appCount}
+                        </Chip>
+                      )}
+                    </Flex>
+                  </SubTab>
+                )
+              })}
             </TabList>
             <Input
               placeholder="Filter applications"
