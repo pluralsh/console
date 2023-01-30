1b49681202a6f0a6af97248503a95f010a546374
Klink <85062+dogmar@users.noreply.github.com>
2023-01-30 00:41:03 -0800

    feat: Add dropdown Components filter (#226)

--- assets/src/components/apps/app/components/Components.tsx
+++ assets/src/components/apps/app/components/Components.tsx
@@ -1,11 +1,65 @@
-import { Card, PageTitle } from '@pluralsh/design-system'
-import { useContext, useEffect } from 'react'
+import {
+  Card,
+  ComponentsIcon,
+  EmptyState,
+  ListBoxFooter,
+  ListBoxItem,
+  PageTitle,
+  Select,
+  SelectButton,
+} from '@pluralsh/design-system'
+import {
+  ComponentPropsWithRef,
+  Key,
+  Ref,
+  forwardRef,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
 import { useParams } from 'react-router-dom'
 import { InstallationContext } from 'components/Installations'
 
 import { BreadcrumbsContext } from 'components/Breadcrumbs'
 
+import styled, { useTheme } from 'styled-components'
+import { Component as ComponentT } from 'generated/graphql'
+
 import Component from './Component'
+
+import { ComponentIcon } from './misc'
+
+const FooterSelectAllInner = styled(ListBoxFooter)(({ theme }) => ({
+  color: theme.colors['text-primary-accent'],
+}))
+
+function FooterSelectAll({ ...props }) {
+  const theme = useTheme()
+
+  return (
+    <FooterSelectAllInner
+      leftContent={(
+        <ComponentsIcon
+          size={16}
+          color={theme.colors['text-primary-accent'] as string}
+        />
+      )}
+      {...props}
+    >
+      Select all
+    </FooterSelectAllInner>
+  )
+}
+
+const FilterTrigger = styled(SelectButton)(({ theme }) => ({
+  width: 220,
+  '&, *': {
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    flexShrink: 1,
+  },
+}))
 
 export default function Components() {
   const { appName } = useParams()
@@ -17,23 +71,71 @@ export default function Components() {
     { text: 'apps', url: '/' },
     { text: appName, url: `/apps/${appName}` },
     { text: 'cost analysis', url: `/apps/${appName}/cost` },
-  ]), [appName, setBreadcrumbs])
+  ]),
+  [appName, setBreadcrumbs])
+
+  const componentKinds = Array.from((currentApp?.status?.components as ComponentT[])?.reduce((kinds, component, i) => {
+    kinds.add(component.kind)
+
+    return kinds
+  },
+  new Set<string>([]))).sort()
+  const [selectedKinds, setSelectedKinds] = useState<Set<Key>>(new Set(componentKinds))
+  const filteredComponents = currentApp?.status?.components.filter(comp => selectedKinds.has(comp.kind))
+  const sortedSelectedKinds = Array.from(selectedKinds).sort()
 
   return (
     <>
-      <PageTitle heading="Components" />
+      <PageTitle heading="Components">
+        <Select
+          label="All components"
+          triggerButton={(
+            <FilterTrigger>
+              {sortedSelectedKinds.length >= componentKinds.length
+                ? 'All components'
+                : sortedSelectedKinds.length === 0
+                  ? 'Select types'
+                  : sortedSelectedKinds.join(', ')}
+            </FilterTrigger>
+          )}
+          selectionMode="multiple"
+          selectedKeys={selectedKinds}
+          onSelectionChange={keys => {
+            setSelectedKinds(keys)
+          }}
+          placement="right"
+          dropdownFooterFixed={(
+            <FooterSelectAll
+              onClick={() => setSelectedKinds(new Set(componentKinds))}
+            />
+          )}
+          maxHeight={300}
+        >
+          {componentKinds.map(kind => (
+            <ListBoxItem
+              key={kind}
+              leftContent={<ComponentIcon kind={kind} />}
+              label={kind}
+            />
+          ))}
+        </Select>
+      </PageTitle>
       <Card
         direction="column"
         paddingRight="xxxsmall"
         overflowY="auto"
       >
-        {currentApp.status.components.map((component, i) => (
-          <Component
-            key={i}
-            component={component}
-            last={currentApp.status.components.length === i + 1}
-          />
-        ))}
+        {(filteredComponents || []).length === 0 ? (
+          <EmptyState message="No components match your selection" />
+        ) : (
+          filteredComponents?.map((component, i) => (
+            <Component
+              key={i}
+              component={component}
+              last={currentApp.status.components.length === i + 1}
+            />
+          ))
+        )}
       </Card>
     </>
   )
