307a0e795a13ca431bc579025f7e990bf962aed9
Sebastian Florek <sebastian@plural.sh>
2023-02-08 10:20:07 +0100

    Get a full list of repos

---
+++ assets/src/components/repos/installer/query.ts
@@ -0,0 +1,16 @@
+import { gql } from 'apollo-boost'
+
+import { PageInfo } from '../../graphql/base'
+import { RepositoryFragment } from '../../graphql/plural'
+
+// TODO: Add support for query search and virtual scrolling
+export const QUERY_REPOS = gql`
+  query Repositories {
+    repositories(query: "", first: 1000) {
+      pageInfo { ...PageInfo }
+      edges { node { ...RepositoryFragment } }
+    }
+  }
+  ${PageInfo}
+  ${RepositoryFragment}
+`
