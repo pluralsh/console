c52e083a84b2a798e749107d18ed9869a540cdb7
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-02-08 09:26:38 +0100

    Add status panel

--- assets/src/components/layout/AppNav.tsx
+++ assets/src/components/layout/AppNav.tsx
@@ -1,16 +1,22 @@
 import {
   AppsIcon,
+  Card,
   Chip,
+  CloseIcon,
   ErrorIcon,
   IconFrame,
+  Input,
   StatusIpIcon,
   SuccessIcon,
 } from '@pluralsh/design-system'
 import { appState } from 'components/apps/misc'
 import { InstallationContext } from 'components/Installations'
+import { Layer } from 'grommet'
+import { Div } from 'honorable'
 import sortBy from 'lodash/sortBy'
-import { useContext, useMemo } from 'react'
+import { useContext, useMemo, useState } from 'react'
 import { Readiness, ReadinessT } from 'utils/status'
+import styled from 'styled-components'
 
 function readinessOrder(readiness: ReadinessT) {
   switch (readiness) {
@@ -53,7 +59,60 @@ function StatusIcon({ readiness }: {readiness: ReadinessT}) {
   }
 }
 
+const StatusPanelHeaderWrap = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+})
+
+const StatusPanelHeader = styled.span({
+  fontSize: 18,
+  fontWeight: 500,
+  lineHeight: '24px',
+})
+
+export function StatusPanel({ statuses, onClose = _ => {} }) {
+  return (
+    <Layer
+      plain
+      onClickOutside={onClose}
+      position="top-right"
+      margin={{ top: '80px' }}
+    >
+      <Card
+        fillLevel={2}
+        width={420}
+        overflow="hidden"
+        margin="large"
+      >
+        <Div
+          padding="medium"
+          borderBottom="1px solid border-fill-two"
+        >
+          <StatusPanelHeaderWrap>
+            <StatusPanelHeader>Apps</StatusPanelHeader>
+            <CloseIcon
+              cursor="pointer"
+              onClick={e => onClose(e)}
+            />
+          </StatusPanelHeaderWrap>
+          <Input
+            marginTop="small"
+            placeholder="Filter applications"
+          />
+        </Div>
+        <Div
+          overflowY="auto"
+          maxHeight="100vh"
+        >
+          {statuses.map(({ app, readiness }) => <div>{app.name}</div>)}
+        </Div>
+      </Card>
+    </Layer>
+  )
+}
+
 export default function AppNav() {
+  const [open, setOpen] = useState<boolean>(true)
   const { applications = [] } = useContext<any>(InstallationContext)
 
   const statuses = useMemo(() => {
@@ -63,13 +122,23 @@ export default function AppNav() {
   }, [applications])
 
   return (
-    <Chip
-      icon={<AppsIcon />}
-      clickable
-      size="small"
-    >
-      Apps
-      <StatusIcon readiness={statuses.length > 0 && statuses[0].readiness} />
-    </Chip>
+    <>
+      <Chip
+        backgroundColor={open ? 'fill-one-selected' : 'fill-one'}
+        icon={<AppsIcon />}
+        clickable
+        onClick={() => setOpen(true)}
+        size="small"
+      >
+        Apps
+        <StatusIcon readiness={statuses.length > 0 && statuses[0].readiness} />
+      </Chip>
+      {open && (
+        <StatusPanel
+          statuses={statuses}
+          onClose={() => setOpen(false)}
+        />
+      )}
+    </>
   )
 }
