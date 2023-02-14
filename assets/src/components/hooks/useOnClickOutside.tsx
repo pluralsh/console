9658ffc0958cd702da7a4e14218e9fb95783722c
Klink <85062+dogmar@users.noreply.github.com>
2023-02-14 15:04:28 -0800

    remove logging

--- assets/src/components/hooks/useOnClickOutside.tsx
+++ assets/src/components/hooks/useOnClickOutside.tsx
@@ -4,9 +4,7 @@ import { MutableRefObject, useEffect } from 'react'
 
 export function useOnClickOutside(ref:MutableRefObject<any>, handler:(event:Event)=> any) {
   useEffect(() => {
-    console.log('useEffect called again', ref)
     const listener = (event: Event) => {
-      console.log('listener', ref.current)
       // Do nothing if clicking ref's element or descendent elements
       if (!ref.current || ref.current.contains(event.target)) {
         return
