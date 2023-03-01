90bb2b42c3c545703bafd4292fd9d49716890a58
Klink <85062+dogmar@users.noreply.github.com>
2023-03-01 14:58:25 -0800

    Fix Group editing

--- assets/src/components/account/groups/GroupEdit.tsx
+++ assets/src/components/account/groups/GroupEdit.tsx
@@ -1,195 +1,227 @@
-import { useApolloClient, useMutation } from '@apollo/client'
+import { useApolloClient } from '@apollo/client'
 import {
   Button,
   ComboBox,
   FormField,
   Modal,
-  PeopleIcon,
-  PersonPlusIcon,
-  Stepper,
   ValidatedInput,
 } from '@pluralsh/design-system'
-import { useEffect, useState } from 'react'
-import { Div, Flex } from 'honorable'
+import {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react'
+import { Flex, usePrevious } from 'honorable'
 
 import { fetchUsers } from 'components/utils/BindingInput'
 
-import { StepperSteps } from '@pluralsh/design-system/dist/components/Stepper'
+import { Actions } from 'components/utils/Actions'
+
+import { useUpdateState } from 'components/hooks/useUpdateState'
+
+import {
+  Group,
+  GroupMembersDocument,
+  useCreateGroupMemberMutation,
+  useUpdateGroupMutation,
+} from 'generated/graphql'
 
 import { GqlError } from '../../utils/Alert'
 
-import { CREATE_GROUP_MEMBERS, GROUP_MEMBERS, UPDATE_GROUP } from './queries'
-
 import GroupMembers from './GroupMembers'
 
-const stepBase = {
-  circleSize: 32,
-  iconSize: 16,
-  vertical: true,
-}
-
-const steps: StepperSteps = [
-  {
-    key: 'info',
-    stepTitle: <Div marginRight="small">Group info</Div>,
-    IconComponent: PeopleIcon,
-    ...stepBase,
-  },
-  {
-    key: 'bindings',
-    stepTitle: <Div marginRight="small">User bindings</Div>,
-    IconComponent: PersonPlusIcon,
-    ...stepBase,
-  },
-]
-
-export default function GroupEdit({ group, edit, setEdit }: any) {
+export function EditGroupMembers({
+  group,
+  open,
+  onClose,
+}: {
+  group: Group
+  open: boolean
+  onClose: (...args: any) => void
+}) {
   const client = useApolloClient()
-  const [value, setValue] = useState('')
-  const [name, setName] = useState(group.name)
-  const [description, setDescription] = useState(group.description)
-  const [mutation, { loading, error }] = useMutation(UPDATE_GROUP, {
-    variables: { id: group.id, attributes: { name, description } },
-    onCompleted: () => setEdit(false),
-  })
-  const [addMut] = useMutation(CREATE_GROUP_MEMBERS, {
-    variables: { groupId: group.id },
-    refetchQueries: [{ query: GROUP_MEMBERS, variables: { id: group.id } }],
-  })
-  const [suggestions, setSuggestions] = useState([])
-  const [step, setStep] = useState<0|1>(0)
+  const [userFilter, setUserFilter] = useState('')
+  const [suggestions, setSuggestions] = useState<{ label: any; value: any }[]>([])
 
-  // Run only on first render. Make sure there will be data in Combo Box to start with.
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => fetchUsers(client, value, setSuggestions), [])
+  const [errorMsg, setErrorMsg] = useState<ReactNode>()
+  const prevOpen = usePrevious(open)
+
+  const [addMut, { loading, error }] = useCreateGroupMemberMutation({
+    variables: { groupId: group.id } as any,
+    refetchQueries: [
+      { query: GroupMembersDocument, variables: { id: group.id } },
+    ],
+  })
 
   useEffect(() => {
-    setName(group.name)
-    setDescription(group.description)
-  }, [group])
+    fetchUsers(client, userFilter, setSuggestions)
+  }, [client, userFilter])
+
+  const reset = useCallback(() => {
+    console.log('reset')
+    setErrorMsg(undefined)
+  }, [])
+
+  useEffect(() => {
+    if (open && open !== prevOpen) {
+      reset()
+    }
+  })
+
+  useEffect(() => {
+    console.log('MESSAGe', error?.graphQLErrors[0].message)
+    if (
+      error?.graphQLErrors[0].message === 'group_id has already been taken'
+      || error?.graphQLErrors[0].message.startsWith('constraint error')
+    ) {
+      setErrorMsg(undefined)
+
+      return
+    }
+    setErrorMsg(error && (
+      <GqlError
+        header="Problem editing group members"
+        error={error}
+      />
+    ))
+  }, [error])
 
   return (
     <Modal
+      header={<>Edit members of ‘{group.name}’</>}
       portal
-      open={edit}
+      open={open}
       size="large"
-      onClose={() => setEdit(false)}
+      onClose={onClose}
+      actions={(
+        <Button
+          onClick={onClose}
+          loading={loading}
+        >
+          Done
+        </Button>
+      )}
     >
       <Flex
-        direction="column"
-        gap="medium"
+        flexDirection="column"
+        gap="large"
       >
-        <Flex>
-          <Stepper
-            compact
-            steps={steps}
-            stepIndex={step}
-          />
-        </Flex>
-
-        {/* Group info */}
-        {step === 0 && (
-          <Flex
-            flexDirection="column"
-            gap="large"
-          >
-            <ValidatedInput
-              label="Name"
-              value={name}
-              onChange={({ target: { value } }) => setName(value)}
-            />
-            <ValidatedInput
-              label="Description"
-              value={description}
-              onChange={({ target: { value } }) => setDescription(value)}
-            />
-          </Flex>
-        )}
-
-        {/* Bindings */}
-        {step === 1 && (
-          <Flex
-            flexDirection="column"
-            gap="large"
-          >
-            <FormField
-              label="Add users"
-              width="100%"
-              {...{
-                '& :last-child': {
-                  marginTop: 0,
-                },
-              }}
-            >
-              <ComboBox
-                inputValue={value}
-                  // @ts-expect-error
-                placeholder="Search a user"
-                onSelectionChange={key => {
-                  setValue('')
-                    // @ts-expect-error
-                  addMut({ variables: { userId: key } })
-                }}
-                onInputChange={value => {
-                  setValue(value)
-                  fetchUsers(client, value, setSuggestions)
-                }}
-              >
-                {suggestions.map(({ label }) => label)}
-              </ComboBox>
-            </FormField>
-            <GroupMembers
-              group={group}
-              edit
-            />
-          </Flex>
-        )}
-
-        {error && (
-          <GqlError
-            header="Something went wrong"
-            error={error}
-          />
-        )}
-
-        <Flex
-          gap="medium"
-          justify="end"
+        {errorMsg}
+        <FormField
+          label="Add users"
+          width="100%"
+          {...{
+            '& :last-child': {
+              marginTop: 0,
+            },
+          }}
         >
-          {step === 0 && (
-            <>
-              <Button
-                secondary
-                onClick={() => setEdit(false)}
-              >
-                Cancel
-              </Button>
-              <Button
-                onClick={() => setStep(1)}
-                loading={loading}
-              >
-                Next
-              </Button>
-            </>
-          )}
+          <ComboBox
+            // isOpen
+            inputProps={{
+              value: userFilter,
+              placeholder: 'Search a user inputProps',
+            }}
+            inputValue={userFilter}
+            // @ts-expect-error
+            placeholder="Search a user"
+            onSelectionChange={key => {
+              console.log('selection Changed', key)
+              setUserFilter('')
+              if (key && typeof key === 'string') {
+                // @ts-expect-error
+                addMut({ variables: { userId: key } })
+              }
+            }}
+            onInputChange={value => {
+              setUserFilter(value)
+            }}
+          >
+            {suggestions.map(({ label }) => label)}
+          </ComboBox>
+        </FormField>
+        <GroupMembers
+          group={group}
+          edit
+        />
+      </Flex>
+    </Modal>
+  )
+}
 
-          {step === 1 && (
-            <>
-              <Button
-                secondary
-                onClick={() => setStep(0)}
-              >
-                Back
-              </Button>
-              <Button
-                onClick={() => mutation()}
-                loading={loading}
-              >
-                Update
-              </Button>
-            </>
-          )}
-        </Flex>
+export function EditGroupAttributes({ group, open, onClose }: any) {
+  const prevOpen = usePrevious(open)
+  const [errorMsg, setErrorMsg] = useState<ReactNode>()
+
+  const {
+    state: formState,
+    reset: resetForm,
+    update,
+    hasUpdates,
+  } = useUpdateState({
+    name: group.name,
+    description: group.description,
+    users: [],
+  })
+  const { name, description } = formState
+
+  const [mutation, { loading, error }] = useUpdateGroupMutation({
+    variables: { id: group.id, attributes: { name, description } },
+    onCompleted: onClose,
+  })
+
+  const reset = useCallback(() => {
+    resetForm()
+    setErrorMsg(undefined)
+  }, [resetForm])
+
+  useEffect(() => {
+    if (open && open !== prevOpen) {
+      reset()
+    }
+  })
+  useEffect(() => {
+    console.log('useeffect error', error)
+    setErrorMsg(error && (
+      <GqlError
+        header="Problem editing group attributes"
+        error={error}
+      />
+    ))
+  }, [error])
+
+  return (
+    <Modal
+      header={<>Edit attributes of ‘{group.name}’</>}
+      portal
+      open={open}
+      size="large"
+      onClose={onClose}
+      actions={(
+        <Actions
+          cancel={onClose}
+          submit={hasUpdates ? () => mutation() : undefined}
+          loading={loading}
+          action="Update"
+        />
+      )}
+    >
+      <Flex
+        flexDirection="column"
+        gap="large"
+      >
+        {errorMsg}
+        <ValidatedInput
+          label="Name"
+          value={name}
+          onChange={({ target: { value } }) => update({ name: value })}
+        />
+        <ValidatedInput
+          label="Description"
+          value={description}
+          onChange={({ target: { value } }) => update({ description: value })}
+        />
       </Flex>
     </Modal>
   )
