3879644164283e4e4e629a7dbcda1f088911638b
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-03-06 10:43:34 +0100

    Add account settings view

---
+++ assets/src/components/account/settings/EmailSettings.tsx
@@ -0,0 +1,31 @@
+import { ArrowTopRightIcon, Button } from '@pluralsh/design-system'
+import { ScrollablePage } from 'components/utils/layout/ScrollablePage'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Description = styled.div(({ theme }) => ({
+  ...theme.partials.text.body2Bold,
+}))
+
+export default function AccountSettings() {
+  return (
+    <ScrollablePage
+      scrollable={false}
+      heading="Account settings"
+    >
+      <Description>Manage your account settings from app.plural.sh.</Description>
+      <Button
+        as={Link}
+        to="https://app.plural.sh/account/edit"
+        target="_blank"
+        rel="noopener noreferrer"
+        endIcon={<ArrowTopRightIcon />}
+        fontWeight={600}
+        width="max-content"
+        marginTop="small"
+      >
+        Edit in app.plural.sh
+      </Button>
+    </ScrollablePage>
+  )
+}
