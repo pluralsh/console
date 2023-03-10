7fd1f7caae1b5ac9e1ae852cf89a6ecd434cf651
michaeljguarino <mguarino46@gmail.com>
2023-03-10 10:10:59 -0500

    fix admin required error message

--- lib/console/graphql/middleware/admin_required.ex
+++ lib/console/graphql/middleware/admin_required.ex
@@ -4,6 +4,6 @@ defmodule Console.Middleware.AdminRequired do
 
   def call(%{context: %{current_user: %User{roles: %{admin: true}}}} = res, _config), do: res
   def call(resolution, _) do
-    Absinthe.Resolution.put_result(resolution, {:error, "unauthenticated"})
+    Absinthe.Resolution.put_result(resolution, {:error, "you must be an admin to perform this action"})
   end
 end
