1a6fd60c1d4b22e7bcf636e9b9ea5d2e1c5cfeff
David van der Spek <vanderspek.david@gmail.com>
2023-03-10 11:27:47 +0100

    add source super property
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- assets/src/index.tsx
+++ assets/src/index.tsx
@@ -15,6 +15,11 @@ posthog.init('phc_r0v4jbKz8Rr27mfqgO15AN5BMuuvnU8hCFedd6zpSDy',
     opt_out_capturing_by_default: true,
   })
 
+// add `source: console` to all events
+posthog.register({
+  source: 'console',
+})
+
 const container = document.getElementById('root') as Element
 const root = createRoot(container)
 
