2e3989f82c83191549b110cdbc42d79dc8c8e4e3
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-03-22 13:39:22 +0100

    Add icon to header

--- assets/src/components/billing/BillingFeatureBlockModal.tsx
+++ assets/src/components/billing/BillingFeatureBlockModal.tsx
@@ -1,16 +1,30 @@
-import { Button, Modal } from '@pluralsh/design-system'
+import { Button, Modal, WarningIcon } from '@pluralsh/design-system'
 import { Dispatch } from 'react'
+import styled from 'styled-components'
 
 type BillingFeatureBlockModalProps = {
   message?: string
   onClose: Dispatch<void>
 }
 
+const Header = styled.div({
+  display: 'inline-flex',
+  justifyContent: 'center',
+})
+
 export default function BillingFeatureBlockModal({ message = 'Upgrade to Plural Professional to use this feature.', onClose }: BillingFeatureBlockModalProps) {
   return (
     <Modal
       BackdropProps={{ zIndex: 20 }}
-      header="Upgrade needed"
+      header={(
+        <Header>
+          <WarningIcon
+            color="icon-warning"
+            marginRight="xsmall"
+          />
+          Upgrade needed
+        </Header>
+      )}
       open
       onClose={() => onClose()}
       size="large"
