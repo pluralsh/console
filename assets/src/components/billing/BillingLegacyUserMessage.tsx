7278e45ebed7398f044be4ed99eb6c40579b2b3f
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-03-24 09:38:58 +0100

    Update legacy user banner

--- assets/src/components/billing/BillingLegacyUserMessage.tsx
+++ assets/src/components/billing/BillingLegacyUserMessage.tsx
@@ -1,8 +1,11 @@
 import { useContext } from 'react'
 import moment from 'moment'
 import styled from 'styled-components'
+import { ErrorIcon, IconFrame } from '@pluralsh/design-system'
 
 import SubscriptionContext from '../contexts/SubscriptionContext'
+
+const Wrap = styled.div({ display: 'flex', alignItems: 'center' })
 
 const Message = styled.p(({ theme }) => ({
   ...theme.partials.text.overline,
@@ -18,18 +21,28 @@ export default function BillingLegacyUserMessage() {
 
   if (isPaidPlan || !(isGrandfathered || isGrandfathetingExpired)) return null
 
+  const message = isGrandfathetingExpired
+    ? 'Legacy user access expired. '
+    : `Legacy user access until ${moment(account?.grandfatheredUntil).format('MMM DD, YYYY')}. `
+
   return (
-    <Message>
-      Legacy user access
-      {isGrandfathetingExpired ? ' expired' : ` until ${moment(account?.grandfatheredUntil).format('MMM DD, YYYY')}`}
-      {' '}
-      <MessageLink
-        href="https://app.plural.sh/account/billing"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        upgrade now
-      </MessageLink>
-    </Message>
+    <Wrap>
+      {isGrandfathetingExpired && (
+        <IconFrame
+          icon={<ErrorIcon color="icon-error" />}
+          textValue={message}
+        />
+      )}
+      <Message>
+        {message}
+        <MessageLink
+          href="https://app.plural.sh/account/billing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Upgrade now
+        </MessageLink>
+      </Message>
+    </Wrap>
   )
 }
