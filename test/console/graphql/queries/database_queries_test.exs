790a3b3c683e9d339a9fe44bab1021e55204ffc8
michaeljguarino <mguarino46@gmail.com>
2023-08-07 11:42:24 -0400

    Add feature gates for db queries (#390)

--- test/console/graphql/queries/database_queries_test.exs
+++ test/console/graphql/queries/database_queries_test.exs
@@ -8,6 +8,7 @@ defmodule Console.GraphQl.DatabaseQueriesTest do
       pgs = Enum.map(~w(first second), &postgres/1)
       admin = admin_user()
       expect(Kube.Client, :list_postgresqls, fn -> {:ok, pgs} end)
+      expect(Console.Features, :available?, fn :databaseManagement -> true end)
 
       {:ok, %{data: %{"postgresDatabases" => found}}} = run_query("""
         query {
@@ -19,6 +20,19 @@ defmodule Console.GraphQl.DatabaseQueriesTest do
 
       assert Enum.map(found, & &1["metadata"]["name"]) == Enum.map(pgs, & &1.metadata.name)
     end
+
+    test "it cannot list postgres databases" do
+      admin = admin_user()
+      expect(Console.Features, :available?, fn :databaseManagement -> false end)
+
+      {:ok, %{errors: [_ | _]}} = run_query("""
+        query {
+          postgresDatabases {
+            metadata { name }
+          }
+        }
+      """, %{}, %{current_user: admin})
+    end
   end
 
   describe "postgresDatabase" do
@@ -26,6 +40,7 @@ defmodule Console.GraphQl.DatabaseQueriesTest do
       pg = postgres("name")
       admin = admin_user()
       expect(Kube.Client, :get_postgresql, fn _, _ -> {:ok, pg} end)
+      expect(Console.Features, :available?, fn :databaseManagement -> true end)
 
       {:ok, %{data: %{"postgresDatabase" => found}}} = run_query("""
         query {
@@ -54,6 +69,7 @@ defmodule Console.GraphQl.DatabaseQueriesTest do
       admin = admin_user()
       instances = insert_list(3, :postgres_instance, name: pg.metadata.name, namespace: pg.metadata.namespace)
       expect(Kube.Client, :get_postgresql, fn _, _ -> {:ok, pg} end)
+      expect(Console.Features, :available?, fn :databaseManagement -> true end)
 
       {:ok, %{data: %{"postgresDatabase" => found}}} = run_query("""
         query {
@@ -66,6 +82,19 @@ defmodule Console.GraphQl.DatabaseQueriesTest do
 
       assert MapSet.new(instances, & &1.uid)
              |> MapSet.equal?(MapSet.new(found["instances"], & &1["uid"]))
+    end
+
+    test "it cannot fetch a postgres database if feature is not enabled" do
+      admin = admin_user()
+      expect(Console.Features, :available?, fn :databaseManagement -> false end)
+
+      {:ok, %{errors: [_ | _]}} = run_query("""
+        query {
+          postgresDatabase(name: "name", namespace: "namespace") {
+            metadata { name }
+          }
+        }
+      """, %{}, %{current_user: admin})
     end
 
     test "nonadmins cannot fetch db" do
