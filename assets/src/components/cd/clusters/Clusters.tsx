f1afcf5dcf6358f203879d016a5925fed9806848
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-10-02 18:57:02 +0200

    feat: Cluster upgrades initial work (#425)

--- assets/src/components/cd/clusters/Clusters.tsx
+++ assets/src/components/cd/clusters/Clusters.tsx
@@ -1,15 +1,165 @@
 import { FullHeightTableWrap } from 'components/utils/layout/FullHeightTableWrap'
-import { EmptyState, Table } from '@pluralsh/design-system'
-import { useClustersQuery } from 'generated/graphql'
+import {
+  CaretRightIcon,
+  Chip,
+  ClusterIcon,
+  EmptyState,
+  IconFrame,
+  Table,
+} from '@pluralsh/design-system'
+import { Cluster, useClustersQuery } from 'generated/graphql'
 import { useMemo } from 'react'
 import { isEmpty } from 'lodash'
-
 import LoadingIndicator from 'components/utils/LoadingIndicator'
+import { createColumnHelper } from '@tanstack/react-table'
+import { A } from 'honorable'
+import { Link, useNavigate } from 'react-router-dom'
+import { useTheme } from 'styled-components'
 
+import { ColWithIcon } from '../repos/GitRepositories'
+import { Edge } from '../../../utils/graphql'
 import { useSetCDHeaderContent } from '../ContinuousDeployment'
+import { providerToURL } from '../../utils/ProviderIcon'
 
-import { columns } from './columns'
 import CreateCluster from './CreateCluster'
+import ClustersUpgrade from './ClustersUpgrade'
+
+const columnHelper = createColumnHelper<Edge<Cluster>>()
+
+export const columns = [
+  columnHelper.accessor(({ node }) => node?.name, {
+    id: 'cluster',
+    header: 'Cluster',
+    cell: ({ getValue }) => (
+      <ColWithIcon icon={<ClusterIcon width={16} />}>
+        <A
+          as={Link}
+          to="/cd/clusters/" // TODO: Update once details view is available.
+          whiteSpace="nowrap"
+        >
+          {getValue()}
+        </A>
+      </ColWithIcon>
+    ),
+  }),
+  columnHelper.accessor(({ node }) => node?.provider?.name || 'Unknown', {
+    id: 'cloud',
+    header: 'Cloud',
+    cell: ({ getValue }) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const theme = useTheme()
+
+      return (
+        <ColWithIcon icon={providerToURL(getValue(), theme.mode === 'dark')}>
+          {getValue()}
+        </ColWithIcon>
+      )
+    },
+  }),
+  columnHelper.accessor(({ node }) => node, {
+    id: 'version',
+    header: 'Version',
+    cell: ({
+      row: {
+        original: { node },
+      },
+    }) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const theme = useTheme()
+
+      return (
+        <div>
+          <div>Current: v{node?.currentVersion}</div>
+          <div
+            css={{
+              ...theme.partials.text.caption,
+              color: theme.colors['text-xlight'],
+            }}
+          >
+            Desired: v{node?.version}
+          </div>
+        </div>
+      )
+    },
+  }),
+  // TODO: Add once VPC ID is available.
+  // columnHelper.accessor(({ node }) => node?.version, {
+  //   id: 'vpc',
+  //   header: 'VPC ID',
+  //   cell: () => 'TODO',
+  // }),
+  // TODO: Add once owner is available.
+  // columnHelper.accessor(({ node }) => node?.version, {
+  //   id: 'owner',
+  //   header: 'Owner',
+  //   cell: () => (
+  //     <UserDetails
+  //       name="TODO"
+  //       avatar={null}
+  //       email="todo@todo.todo"
+  //     />
+  //   ),
+  // }),
+  // TODO: Add both once resource data is available.
+  // columnHelper.accessor(({ node }) => node?.nodePools, {
+  //   id: 'cpu',
+  //   header: 'CPU',
+  //   cell: () => <div>TODO</div>,
+  // }),
+  // columnHelper.accessor(({ node }) => node?.nodePools, {
+  //   id: 'memory',
+  //   header: 'Memory',
+  //   cell: () => <div>TODO</div>,
+  // }),
+  columnHelper.accessor(({ node }) => node, {
+    id: 'status',
+    header: 'Status',
+    cell: ({ getValue }) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const theme = useTheme()
+      const cluster = getValue()
+      const hasUpgrade = true // TODO: Update it.
+
+      return (
+        <div
+          css={{
+            alignItems: 'center',
+            display: 'flex',
+            gap: theme.spacing.xsmall,
+          }}
+        >
+          {hasUpgrade ? (
+            <ClustersUpgrade cluster={cluster} />
+          ) : (
+            <Chip severity="success">Healthy</Chip>
+          )}
+        </div>
+      )
+    },
+  }),
+  columnHelper.accessor(({ node }) => node?.version, {
+    id: 'actions',
+    header: '',
+    cell: () => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const navigate = useNavigate()
+
+      return (
+        <div css={{ alignItems: 'center', alignSelf: 'end', display: 'flex' }}>
+          <IconFrame
+            clickable
+            onClick={() => navigate('/cd/clusters/')} // TODO: Update once details view is available.
+            size="medium"
+            icon={<CaretRightIcon />}
+            textValue="Go to cluster details"
+            tooltip
+            type="tertiary"
+          />
+        </div>
+      )
+    },
+  }),
+]
 
 export default function Clusters() {
   const { data } = useClustersQuery()
---
+++ assets/src/components/cd/clusters/ClustersUpgrade.tsx
@@ -0,0 +1,251 @@
+import {
+  Accordion,
+  Button,
+  ClusterIcon,
+  ErrorIcon,
+  GitHubLogoIcon,
+  Modal,
+  Table,
+  WarningIcon,
+} from '@pluralsh/design-system'
+import { useCallback, useState } from 'react'
+import { useTheme } from 'styled-components'
+import { createColumnHelper } from '@tanstack/react-table'
+
+import isEmpty from 'lodash/isEmpty'
+
+import { ApiDeprecation, Cluster } from '../../../generated/graphql'
+import { ColWithIcon } from '../repos/GitRepositories'
+import CopyButton from '../../utils/CopyButton'
+import { ProviderIcons } from '../../utils/ProviderIcon'
+import { Confirm } from '../../utils/Confirm'
+
+function ClustersUpgradeNow({ cluster }: { cluster?: Cluster | null }) {
+  const [confirm, setConfirm] = useState(false)
+  const hasDeprecations = !isEmpty(cluster?.apiDeprecations)
+  const upgrade = useCallback(() => console.log('TODO'), [])
+  const onClick = useCallback(
+    () => (!hasDeprecations ? upgrade() : setConfirm(true)),
+    [hasDeprecations, upgrade]
+  )
+
+  return (
+    <div css={{ alignItems: 'center', alignSelf: 'end', display: 'flex' }}>
+      <Button
+        small
+        destructive={hasDeprecations}
+        floating={!hasDeprecations}
+        width="fit-content"
+        onClick={onClick}
+      >
+        Upgrade now
+      </Button>
+      <Confirm
+        open={confirm}
+        title="Confirm upgrade"
+        text="This could be a destructive action. Before updating your Kubernetes version check and fix all deprecated resources."
+        close={() => setConfirm(false)}
+        submit={upgrade}
+        loading={false}
+        destructive
+      />
+    </div>
+  )
+}
+
+const columnHelperDeprecations = createColumnHelper<ApiDeprecation>()
+
+const deprecationsColumns = [
+  columnHelperDeprecations.accessor(({ component }) => component?.name, {
+    id: 'deprecated',
+    header: 'Deprecated',
+    meta: { truncate: true },
+    cell: ({ getValue }) => <div>{getValue()}</div>,
+  }),
+  columnHelperDeprecations.accessor(({ component }) => component?.name, {
+    id: 'deprecatedCopy',
+    header: '',
+    cell: ({ getValue }) => <CopyButton text={getValue()} />,
+  }),
+  columnHelperDeprecations.accessor(({ replacement }) => replacement, {
+    id: 'fix',
+    header: 'Fix',
+    meta: { truncate: true },
+    cell: ({ getValue }) => <div>{getValue()}</div>,
+  }),
+  columnHelperDeprecations.accessor(({ replacement }) => replacement, {
+    id: 'fixCopy',
+    header: '',
+    cell: ({ getValue }) => <CopyButton text={getValue()} />,
+  }),
+  columnHelperDeprecations.accessor(() => undefined, {
+    id: 'repository',
+    header: 'Repository',
+    cell: () => (
+      <div css={{ alignItems: 'center', alignSelf: 'end', display: 'flex' }}>
+        <Button
+          small
+          floating
+          width="fit-content"
+          startIcon={<GitHubLogoIcon />}
+        >
+          Fix now
+        </Button>
+      </div>
+    ),
+  }),
+]
+
+const columnHelperUpgrade = createColumnHelper<Cluster>()
+
+const upgradeColumns = [
+  columnHelperUpgrade.accessor(({ name }) => name, {
+    id: 'cluster',
+    header: 'Cluster',
+    cell: ({ getValue }) => (
+      <ColWithIcon icon={<ClusterIcon width={16} />}>
+        <div css={{ whiteSpace: 'nowrap' }}>{getValue()}</div>
+      </ColWithIcon>
+    ),
+  }),
+  columnHelperUpgrade.accessor((cluster) => cluster?.currentVersion, {
+    id: 'version',
+    header: 'Version',
+    cell: ({ getValue }) => <div>v{getValue()}</div>,
+  }),
+  columnHelperUpgrade.accessor((cluster) => cluster?.currentVersion, {
+    id: 'targetVersion',
+    header: 'Target version',
+    cell: () => <ColWithIcon icon={ProviderIcons.GENERIC}>TODO</ColWithIcon>,
+  }),
+  columnHelperUpgrade.accessor((cluster) => cluster, {
+    id: 'actions',
+    header: '',
+    cell: ({ getValue }) => <ClustersUpgradeNow cluster={getValue()} />,
+  }),
+]
+
+export default function ClustersUpgrade({
+  cluster,
+}: {
+  cluster?: Cluster | null
+}) {
+  const theme = useTheme()
+  const [isOpen, setIsOpen] = useState(false)
+  const closeModal = useCallback(() => setIsOpen(false), [])
+  const onClose = useCallback(() => setIsOpen(false), [])
+
+  const apiDeprecations: ApiDeprecation[] = [
+    {
+      component: {
+        id: '',
+        kind: '',
+        synced: false,
+        name: 'networking.k8s/io/v1 Ingress <name>',
+      },
+      replacement: 'networking.k8s/io/v1 Ingress <name>',
+    },
+    {
+      component: {
+        id: '',
+        kind: '',
+        synced: false,
+        name: 'networking.k8s/io/v1 Ingress <name>',
+      },
+      replacement: 'networking.k8s/io/v1 Ingress <name>',
+    },
+  ] // TODO: Remove
+
+  const hasDeprecations = !isEmpty(apiDeprecations)
+
+  return (
+    <>
+      <Button
+        small
+        floating
+        width="fit-content"
+        startIcon={
+          hasDeprecations ? (
+            <ErrorIcon
+              color="icon-danger"
+              width={16}
+            />
+          ) : (
+            <WarningIcon
+              color="icon-warning"
+              width={16}
+            />
+          )
+        }
+        onClick={() => setIsOpen(true)}
+      >
+        {hasDeprecations ? 'Deprecations' : 'Upgrade'}
+      </Button>
+      <Modal
+        header="Upgrade Kubernetes"
+        open={isOpen}
+        onClose={onClose}
+        portal
+        size="large"
+        maxWidth={1024}
+        width={1024}
+        actions={
+          <Button
+            primary
+            onClick={closeModal}
+          >
+            Close
+          </Button>
+        }
+      >
+        <div
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: theme.spacing.large,
+          }}
+        >
+          {hasDeprecations ? (
+            <>
+              <div
+                css={{
+                  ...theme.partials.text.body1,
+                  color: theme.colors['text-light'],
+                }}
+              >
+                Before upgrading the Kubernetes version fix all deprecated
+                resources listed below:
+              </div>
+              <Table
+                data={apiDeprecations}
+                columns={deprecationsColumns}
+                css={{
+                  maxHeight: 310,
+                  height: '100%',
+                }}
+              />
+            </>
+          ) : (
+            <div
+              css={{
+                ...theme.partials.text.body1,
+                color: theme.colors['text-light'],
+              }}
+            >
+              No deprecated resources detected. Ready to update.
+            </div>
+          )}
+          <Table
+            data={[cluster]}
+            columns={upgradeColumns}
+            css={{
+              maxHeight: 'unset',
+              height: '100%',
+            }}
+          />
+          <Accordion label="Helpful resources">TODO</Accordion>
+        </div>
+      </Modal>
+    </>
+  )
+}
