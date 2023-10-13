f4b788fa152ee72bb3941f431fd6278e88de4472
Klink <85062+dogmar@users.noreply.github.com>
2023-10-13 09:52:44 -0700

    Fix ts errors

--- assets/src/components/cd/cluster/ClusterMetadataPanel.tsx
+++ assets/src/components/cd/cluster/ClusterMetadataPanel.tsx
@@ -14,7 +14,7 @@ import { useTheme } from 'styled-components'
 
 import isEmpty from 'lodash/isEmpty'
 
-import { Cluster } from '../../../generated/graphql'
+import { ClusterFragment } from '../../../generated/graphql'
 import CopyButton from '../../utils/CopyButton'
 import ProviderIcon from '../../utils/Provider'
 import ClusterUpgrade from '../clusters/ClusterUpgrade'
@@ -22,7 +22,7 @@ import ClusterUpgrade from '../clusters/ClusterUpgrade'
 export default function ClusterMetadataPanel({
   cluster,
 }: {
-  cluster?: Cluster | null
+  cluster?: ClusterFragment | null
 }): ReactElement | null {
   const theme = useTheme()
   const [open, setOpen] = useState(false)
