d3b1bddbaeb48016cc60c0f36264a8d20be43549
michaeljguarino <mguarino46@gmail.com>
2023-10-13 12:13:28 -0400

    improve robustness of error handler

--- lib/console/graphql/middleware/error_handler.ex
+++ lib/console/graphql/middleware/error_handler.ex
@@ -1,5 +1,6 @@
 defmodule Console.Middleware.ErrorHandler do
   import Console.GraphQl.Helpers
+  require Logger
   @behaviour Absinthe.Middleware
 
   @impl true
@@ -11,6 +12,11 @@ defmodule Console.Middleware.ErrorHandler do
   defp format({:error, %Ecto.Changeset{} = cs}), do: resolve_changeset(cs)
   defp format({:error, _, %{"message" => msg}}), do: {:error, msg}
   defp format({:error, {:http_error, _, %{"message" => msg}}}), do: {:error, msg}
-  defp format({:error, _, _}), do: {:error, "kubernetes error"}
+  defp format({:error, {:http_error, _, err}}) when is_binary(err), do: {:error, err}
+  defp format({:error, _} = err), do: err
+  defp format({:error, _, _} = err) do
+    Logger.error "found unknown error: #{inspect(err)}"
+    {:error, "kubernetes error"}
+  end
   defp format(error), do: error
 end
