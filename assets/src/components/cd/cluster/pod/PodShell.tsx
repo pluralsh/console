810204bb76b8628b788956b55005b8b492c80a2a
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-11-09 16:17:05 +0100

    keep url up to date

--- assets/src/components/cd/cluster/pod/PodShell.tsx
+++ assets/src/components/cd/cluster/pod/PodShell.tsx
@@ -1,9 +1,14 @@
-import { useOutletContext, useParams, useSearchParams } from 'react-router-dom'
+import {
+  useNavigate,
+  useOutletContext,
+  useParams,
+  useSearchParams,
+} from 'react-router-dom'
 
 import { ScrollablePage } from 'components/utils/layout/ScrollablePage'
 import { Pod } from 'generated/graphql'
 import { useTheme } from 'styled-components'
-import { Key, useMemo, useRef, useState } from 'react'
+import { Key, useEffect, useMemo, useRef, useState } from 'react'
 import { FormField, ListBoxItem, Select } from '@pluralsh/design-system'
 
 import { ShellContext, TerminalActions } from '../../../terminal/Terminal'
@@ -11,8 +16,9 @@ import { ShellWithContext } from '../../../cluster/containers/ContainerShell'
 
 export default function PodShell() {
   const { pod } = useOutletContext() as { pod: Pod }
-  const { clusterId } = useParams()
+  const { clusterId, namespace, name } = useParams()
   const [searchParams] = useSearchParams()
+  const container = searchParams.get('container')
   const ref = useRef<TerminalActions>({ handleResetSize: () => {} })
 
   if (!pod) {
@@ -20,6 +26,7 @@ export default function PodShell() {
   }
 
   const theme = useTheme()
+  const navigate = useNavigate()
   const containers: Array<string> = useMemo(
     () => [
       ...(pod.spec?.initContainers?.map((c) => c!.name!) ?? []),
@@ -28,8 +35,14 @@ export default function PodShell() {
     [pod]
   )
   const [selected, setSelected] = useState<Key>(
-    searchParams.get('container') ?? (containers.at(0) as Key)
+    container ?? (containers.at(0) as Key)
   )
+
+  useEffect(() => {
+    navigate(
+      `/cd/clusters/${clusterId}/pods/${namespace}/${name}/shell?container=${selected}`
+    )
+  }, [selected, clusterId, namespace, name, container, navigate])
 
   return (
     <ScrollablePage
