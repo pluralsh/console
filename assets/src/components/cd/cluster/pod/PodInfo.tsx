f3150ecce17a4bc1303d90ca8a1f610435d23a16
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-11-12 04:37:59 +0100

    feat: Add exec into pod for CD clusters (#489)

--- assets/src/components/cd/cluster/pod/PodInfo.tsx
+++ assets/src/components/cd/cluster/pod/PodInfo.tsx
@@ -3,10 +3,7 @@ import { Flex } from 'honorable'
 import { ScrollablePage } from 'components/utils/layout/ScrollablePage'
 import { Pod } from 'generated/graphql'
 import { statusesToRecord } from 'components/cluster/pods/PodInfo'
-
 import { Button, LogsIcon } from '@pluralsh/design-system'
-
-import { useMemo } from 'react'
 
 import PodConditions from '../../../cluster/pods/PodConditions'
 import Metadata from '../../../cluster/pods/PodMetadata'
@@ -38,31 +35,38 @@ function ViewLogsButton() {
   )
 }
 
-export const ColActions = ({
-  clusterId,
-  podName,
-  namespace,
-}: {
-  clusterId?: string
-  podName?: string
-  namespace?: string
-}) =>
-  columnHelper.display({
-    id: 'actions',
-    cell: ({
-      row: {
-        original: { name, readiness },
-      },
-    }: any) =>
+export const ColActions = columnHelper.display({
+  id: 'actions',
+  cell: ({
+    table,
+    row: {
+      original: { name, readiness },
+    },
+  }: any) => {
+    const { clusterId, namespace, podName } = table.options.meta as any
+
+    return (
       readiness &&
       readiness === Readiness.Ready && (
         <ShellLink
           to={`/cd/clusters/${clusterId}/pods/${namespace}/${podName}/shell?container=${name}`}
           textValue={`Launch ${name} shell`}
         />
-      ),
-    header: '',
-  })
+      )
+    )
+  },
+  header: '',
+})
+
+const columns = [
+  ColName,
+  ColImage,
+  ColMemoryReservation,
+  ColCpuReservation,
+  ColPorts,
+  ColStatus,
+  ColActions,
+]
 
 export default function PodInfo() {
   const { clusterId } = useParams()
@@ -74,22 +78,6 @@ export default function PodInfo() {
     pod.status?.initContainerStatuses
   )
   const conditions = pod?.status?.conditions || []
-  const columns = useMemo(
-    () => [
-      ColName,
-      ColImage,
-      ColMemoryReservation,
-      ColCpuReservation,
-      ColPorts,
-      ColStatus,
-      ColActions({
-        clusterId,
-        podName: pod.metadata.name,
-        namespace: pod.metadata.namespace || '',
-      }),
-    ],
-    [pod]
-  )
 
   return (
     <ScrollablePage
@@ -107,6 +95,7 @@ export default function PodInfo() {
             containerStatuses={containerStatuses}
             initContainers={initContainers}
             initContainerStatuses={initContainerStatuses}
+            clusterId={clusterId}
             namespace={pod.metadata.namespace || ''}
             podName={pod.metadata.name}
             columns={columns}
