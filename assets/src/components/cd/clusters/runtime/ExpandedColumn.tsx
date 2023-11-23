245e378711705e41fcc478cfe34b0ca04f48f630
michaeljguarino <mguarino46@gmail.com>
2023-11-22 22:38:26 -0500

    Add expander to display all versions for a discovered add-on (#515)

---
+++ assets/src/components/cd/clusters/runtime/ExpandedColumn.tsx
@@ -0,0 +1,44 @@
+import { Table } from '@pluralsh/design-system'
+
+import { useTheme } from 'styled-components'
+
+import { RuntimeService, expandedColumns } from './columns'
+
+export default function ExpandedColumn({
+  runtimeService,
+}: {
+  runtimeService: RuntimeService
+}) {
+  const theme = useTheme()
+
+  console.log(runtimeService)
+  const versions = runtimeService?.addon?.versions || []
+
+  return (
+    <div
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: theme.spacing.small,
+        marginBottom: theme.spacing.medium,
+      }}
+    >
+      <div
+        css={{
+          ...theme.partials.text.body1,
+          color: theme.colors['text-light'],
+        }}
+      >
+        Full version history of this add-on:
+      </div>
+      <Table
+        data={versions}
+        columns={expandedColumns}
+        css={{
+          maxHeight: 250,
+          height: '100%',
+        }}
+      />
+    </div>
+  )
+}
--- assets/src/components/cd/clusters/runtime/columns.tsx
+++ assets/src/components/cd/clusters/runtime/columns.tsx
@@ -3,24 +3,88 @@ import { RuntimeServicesQuery } from 'generated/graphql'
 
 import { ColWithIcon } from 'components/utils/table/ColWithIcon'
 import { TableText } from 'components/cluster/TableElements'
-import { ErrorIcon } from '@pluralsh/design-system'
+import {
+  CaretDownIcon,
+  CaretRightIcon,
+  ErrorIcon,
+} from '@pluralsh/design-system'
+
+import { useTheme } from 'styled-components'
 
 import { GitPointer } from '../deprecationsColumns'
 
 type RuntimeServiceCluster = NonNullable<RuntimeServicesQuery['cluster']>
-type RuntimeService = NonNullable<RuntimeServiceCluster['runtimeServices']>[0]
+export type RuntimeService = NonNullable<
+  RuntimeServiceCluster['runtimeServices']
+>[0]
+type AddOnVersion = NonNullable<
+  NonNullable<NonNullable<RuntimeService>['addon']>['versions']
+>[0]
+
 const columnHelperRuntime = createColumnHelper<RuntimeService>()
+const columnHelperExpanded = createColumnHelper<AddOnVersion>()
+
+function AddOnName({ addon, row, expanded }) {
+  const theme = useTheme()
+
+  return (
+    <ColWithIcon icon={addon.icon}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: theme.spacing.xxsmall,
+        }}
+      >
+        {row.original?.name}
+        {expanded ? (
+          <CaretDownIcon
+            size={12}
+            onClick={row.getToggleExpandedHandler()}
+          />
+        ) : (
+          <CaretRightIcon
+            size={12}
+            onClick={row.getToggleExpandedHandler()}
+          />
+        )}
+      </div>
+    </ColWithIcon>
+  )
+}
+
+export const expandedColumns = [
+  columnHelperExpanded.accessor((row) => row?.version, {
+    id: 'version',
+    header: 'Add-On Version',
+    cell: ({ getValue }) => <TableText>{getValue()}</TableText>,
+  }),
+  columnHelperExpanded.accessor((row) => row?.kube, {
+    id: 'kube',
+    header: 'Kubernetes Versions',
+    cell: ({ getValue }) => (
+      <TableText>{(getValue() || []).join(', ')}</TableText>
+    ),
+  }),
+]
 
 export const runtimeColumns = [
   columnHelperRuntime.accessor((row) => row?.addon, {
     id: 'name',
     header: 'Name',
-    cell: ({ getValue, row: { original } }) => {
+    cell: ({ getValue, row }) => {
       const addon = getValue()
+      const expanded = row.getIsExpanded()
 
       if (!addon) return null
 
-      return <ColWithIcon icon={addon.icon}>{original?.name}</ColWithIcon>
+      return (
+        <AddOnName
+          addon={addon}
+          expanded={expanded}
+          row={row}
+        />
+      )
     },
   }),
   columnHelperRuntime.accessor((row) => row?.version, {
