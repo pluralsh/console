5757f4d29c8029af62e04875ddf596fa1798b9c7
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-12-19 12:11:23 +0100

    rewrite tests for adopting existing clusters

--- controller/internal/controller/cluster_controller_ginkgo_test.go
+++ controller/internal/controller/cluster_controller_ginkgo_test.go
@@ -33,19 +33,26 @@ func sanitizeClusterStatus(status v1alpha1.ClusterStatus) v1alpha1.ClusterStatus
 var _ = Describe("Cluster Controller", Ordered, func() {
 	Context("When creating a resource", func() {
 		const (
-			awsProviderName   = "aws-test-provider"
-			providerConsoleID = "12345-67890"
-			awsClusterName    = "aws-test-cluster"
-			byokClusterName   = "byok-test-cluster"
-			clusterConsoleID  = "12345-67890"
+			awsProviderName              = "aws-provider"
+			awsProviderConsoleID         = "aws-provider-console-id"
+			awsClusterName               = "aws-cluster"
+			awsClusterConsoleID          = "aws-cluster-console-id"
+			byokClusterName              = "byok-test-cluster"
+			byokClusterConsoleID         = "byok-cluster-console-id"
+			awsReadonlyClusterName       = "aws-readonly-cluster"
+			awsReadonlyClusterConsoleID  = "aws-readonly-cluster-console-id"
+			byokReadonlyClusterName      = "byok-readonly-cluster"
+			byokReadonlyClusterConsoleID = "byok-readonly-cluster-console-id"
 		)
 
 		ctx := context.Background()
 		awsNamespacedName := types.NamespacedName{Name: awsClusterName, Namespace: "default"}
 		byokNamespacedName := types.NamespacedName{Name: awsClusterName, Namespace: "default"}
+		awsReadonlyNamespacedName := types.NamespacedName{Name: awsReadonlyClusterName, Namespace: "default"}
+		byokReadonlyNamespacedName := types.NamespacedName{Name: byokReadonlyClusterName, Namespace: "default"}
 
 		BeforeAll(func() {
-			By("creating AWS provider")
+			By("Creating AWS provider")
 			Expect(utils.MaybeCreate(k8sClient, &v1alpha1.Provider{
 				ObjectMeta: metav1.ObjectMeta{Name: awsProviderName},
 				Spec: v1alpha1.ProviderSpec{
@@ -53,10 +60,10 @@ var _ = Describe("Cluster Controller", Ordered, func() {
 					Name:  awsProviderName,
 				},
 			}, func(p *v1alpha1.Provider) {
-				p.Status.ID = lo.ToPtr(providerConsoleID)
+				p.Status.ID = lo.ToPtr(awsProviderConsoleID)
 			})).To(Succeed())
 
-			By("creating AWS cluster")
+			By("Creating AWS cluster")
 			Expect(utils.MaybeCreate(k8sClient, &v1alpha1.Cluster{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      awsClusterName,
@@ -70,7 +77,7 @@ var _ = Describe("Cluster Controller", Ordered, func() {
 				},
 			}, nil)).To(Succeed())
 
-			By("creating BYOK cluster")
+			By("Creating BYOK cluster")
 			Expect(utils.MaybeCreate(k8sClient, &v1alpha1.Cluster{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      byokClusterName,
@@ -81,27 +88,63 @@ var _ = Describe("Cluster Controller", Ordered, func() {
 					Cloud:  "byok",
 				},
 			}, nil)).To(Succeed())
+
+			By("Creating AWS cluster that will adopt existing Console resource")
+			Expect(utils.MaybeCreate(k8sClient, &v1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      awsReadonlyClusterName,
+					Namespace: "default",
+				},
+				Spec: v1alpha1.ClusterSpec{
+					Handle: lo.ToPtr(awsReadonlyClusterName),
+					Cloud:  "aws",
+				},
+			}, nil)).To(Succeed())
+
+			By("Creating BYOK cluster that will adopt existing Console resource")
+			Expect(utils.MaybeCreate(k8sClient, &v1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      byokReadonlyClusterName,
+					Namespace: "default",
+				},
+				Spec: v1alpha1.ClusterSpec{
+					Handle: lo.ToPtr(byokReadonlyClusterName),
+					Cloud:  "byok",
+				},
+			}, nil)).To(Succeed())
 		})
 
 		AfterAll(func() {
+			By("Cleanup AWS cluster")
 			awsCluster := &v1alpha1.Cluster{}
 			err := k8sClient.Get(ctx, awsNamespacedName, awsCluster)
 			Expect(err).NotTo(HaveOccurred())
-			By("Cleanup the specific resource instance Cluster")
 			Expect(k8sClient.Delete(ctx, awsCluster)).To(Succeed())
 
+			By("Cleanup BYOK cluster")
 			byokCluster := &v1alpha1.Cluster{}
 			err = k8sClient.Get(ctx, byokNamespacedName, byokCluster)
 			Expect(err).NotTo(HaveOccurred())
-			By("cleanup BYOK cluster")
 			Expect(k8sClient.Delete(ctx, byokCluster)).To(Succeed())
+
+			By("Cleanup AWS readonly cluster")
+			awsReadonlyCluster := &v1alpha1.Cluster{}
+			err = k8sClient.Get(ctx, awsReadonlyNamespacedName, awsReadonlyCluster)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(k8sClient.Delete(ctx, awsReadonlyCluster)).To(Succeed())
+
+			By("Cleanup BYOK readonly cluster")
+			byokReadonlyCluster := &v1alpha1.Cluster{}
+			err = k8sClient.Get(ctx, byokReadonlyNamespacedName, byokReadonlyCluster)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(k8sClient.Delete(ctx, byokReadonlyCluster)).To(Succeed())
 		})
 
 		It("should successfully reconcile AWS cluster", func() {
 			fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
 			fakeConsoleClient.On("GetClusterByHandle", mock.AnythingOfType("*string")).Return(nil, errors.NewNotFound(schema.GroupResource{}, awsClusterName))
 			fakeConsoleClient.On("IsClusterExisting", mock.AnythingOfType("*string")).Return(false)
-			fakeConsoleClient.On("CreateCluster", mock.Anything).Return(&gqlclient.ClusterFragment{ID: clusterConsoleID}, nil)
+			fakeConsoleClient.On("CreateCluster", mock.Anything).Return(&gqlclient.ClusterFragment{ID: awsClusterConsoleID}, nil)
 
 			controllerReconciler := &ClusterReconciler{
 				Client:        k8sClient,
@@ -116,8 +159,8 @@ var _ = Describe("Cluster Controller", Ordered, func() {
 			err = k8sClient.Get(ctx, awsNamespacedName, cluster)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(sanitizeClusterStatus(cluster.Status)).To(Equal(sanitizeClusterStatus(v1alpha1.ClusterStatus{
-				ID:  lo.ToPtr(clusterConsoleID),
-				SHA: lo.ToPtr("CI5QLJIIR62PCOX2PVNBUEUUO2XXJ7SYZNQE2ZNY7N3F4ADISJNA===="),
+				ID:  lo.ToPtr(awsClusterConsoleID),
+				SHA: lo.ToPtr("J7CMSICIXLWV7MCWNPBZUA6FEOI3HGTQMNVLYD6VZXX6Y66S6ETQ===="),
 				Conditions: []metav1.Condition{
 					{
 						Type:   v1alpha1.ReadonlyConditionType.String(),
@@ -137,7 +180,7 @@ var _ = Describe("Cluster Controller", Ordered, func() {
 			fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
 			fakeConsoleClient.On("GetClusterByHandle", mock.AnythingOfType("*string")).Return(nil, errors.NewNotFound(schema.GroupResource{}, awsClusterName))
 			fakeConsoleClient.On("IsClusterExisting", mock.AnythingOfType("*string")).Return(false)
-			fakeConsoleClient.On("CreateCluster", mock.Anything).Return(&gqlclient.ClusterFragment{ID: clusterConsoleID}, nil)
+			fakeConsoleClient.On("CreateCluster", mock.Anything).Return(&gqlclient.ClusterFragment{ID: byokClusterConsoleID}, nil)
 
 			controllerReconciler := &ClusterReconciler{
 				Client:        k8sClient,
@@ -152,8 +195,8 @@ var _ = Describe("Cluster Controller", Ordered, func() {
 			err = k8sClient.Get(ctx, awsNamespacedName, cluster)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(sanitizeClusterStatus(cluster.Status)).To(Equal(sanitizeClusterStatus(v1alpha1.ClusterStatus{
-				ID:  lo.ToPtr(clusterConsoleID),
-				SHA: lo.ToPtr("CI5QLJIIR62PCOX2PVNBUEUUO2XXJ7SYZNQE2ZNY7N3F4ADISJNA===="),
+				ID:  lo.ToPtr(byokClusterConsoleID),
+				SHA: lo.ToPtr("J7CMSICIXLWV7MCWNPBZUA6FEOI3HGTQMNVLYD6VZXX6Y66S6ETQ===="),
 				Conditions: []metav1.Condition{
 					{
 						Type:   v1alpha1.ReadonlyConditionType.String(),
@@ -166,6 +209,82 @@ var _ = Describe("Cluster Controller", Ordered, func() {
 						Reason: v1alpha1.ReadyConditionReason.String(),
 					},
 				},
+			})))
+		})
+
+		It("should successfully reconcile AWS readonly cluster", func() {
+			fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
+			fakeConsoleClient.On("GetClusterByHandle", mock.AnythingOfType("*string")).Return(&gqlclient.ClusterFragment{
+				ID:             awsReadonlyClusterConsoleID,
+				CurrentVersion: lo.ToPtr("1.24.11"),
+			}, nil)
+
+			controllerReconciler := &ClusterReconciler{
+				Client:        k8sClient,
+				Scheme:        k8sClient.Scheme(),
+				ConsoleClient: fakeConsoleClient,
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{NamespacedName: awsReadonlyNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+
+			cluster := &v1alpha1.Cluster{}
+			err = k8sClient.Get(ctx, awsReadonlyNamespacedName, cluster)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(sanitizeClusterStatus(cluster.Status)).To(Equal(sanitizeClusterStatus(v1alpha1.ClusterStatus{
+				ID: lo.ToPtr(awsReadonlyClusterConsoleID),
+				Conditions: []metav1.Condition{
+					{
+						Type:    v1alpha1.ReadonlyConditionType.String(),
+						Status:  metav1.ConditionTrue,
+						Reason:  v1alpha1.ReadonlyConditionReason.String(),
+						Message: v1alpha1.ReadonlyTrueConditionMessage.String(),
+					},
+					{
+						Type:   v1alpha1.ReadyConditionType.String(),
+						Status: metav1.ConditionTrue,
+						Reason: v1alpha1.ReadyConditionReason.String(),
+					},
+				},
+				CurrentVersion: lo.ToPtr("1.24.11"),
+			})))
+		})
+
+		It("should successfully reconcile BYOK readonly cluster", func() {
+			fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
+			fakeConsoleClient.On("GetClusterByHandle", mock.AnythingOfType("*string")).Return(&gqlclient.ClusterFragment{
+				ID:             byokReadonlyClusterConsoleID,
+				CurrentVersion: lo.ToPtr("1.24.11"),
+			}, nil)
+
+			controllerReconciler := &ClusterReconciler{
+				Client:        k8sClient,
+				Scheme:        k8sClient.Scheme(),
+				ConsoleClient: fakeConsoleClient,
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{NamespacedName: byokReadonlyNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+
+			cluster := &v1alpha1.Cluster{}
+			err = k8sClient.Get(ctx, byokReadonlyNamespacedName, cluster)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(sanitizeClusterStatus(cluster.Status)).To(Equal(sanitizeClusterStatus(v1alpha1.ClusterStatus{
+				ID: lo.ToPtr(byokReadonlyClusterConsoleID),
+				Conditions: []metav1.Condition{
+					{
+						Type:    v1alpha1.ReadonlyConditionType.String(),
+						Status:  metav1.ConditionTrue,
+						Reason:  v1alpha1.ReadonlyConditionReason.String(),
+						Message: v1alpha1.ReadonlyTrueConditionMessage.String(),
+					},
+					{
+						Type:   v1alpha1.ReadyConditionType.String(),
+						Status: metav1.ConditionTrue,
+						Reason: v1alpha1.ReadyConditionReason.String(),
+					},
+				},
+				CurrentVersion: lo.ToPtr("1.24.11"),
 			})))
 		})
 	})
--- controller/internal/controller/cluster_controller_test.go
+++ controller/internal/controller/cluster_controller_test.go
@@ -1,161 +1,160 @@
 package controller_test
 
-//func TestUpdateCluster(t *testing.T) {
-//	const (
-//		clusterName       = "test-cluster"
-//		clusterConsoleID  = "12345-67890"
-//		providerName      = "test-provider"
-//		providerNamespace = "test-provider"
-//		providerConsoleID = "12345-67890"
-//	)
+//	func TestUpdateCluster(t *testing.T) {
+//		const (
+//			clusterName       = "test-cluster"
+//			clusterConsoleID  = "12345-67890"
+//			providerName      = "test-provider"
+//			providerNamespace = "test-provider"
+//			providerConsoleID = "12345-67890"
+//		)
 //
-//	tests := []struct {
-//		name                     string
-//		cluster                  string
-//		returnIsClusterExisting  bool
-//		returnUpdateCluster      *gqlclient.ClusterFragment
-//		returnErrorUpdateCluster error
-//		existingObjects          []ctrlruntimeclient.Object
-//		expectedStatus           v1alpha1.ClusterStatus
-//	}{
-//		{
-//			name:    "scenario 1: update AWS cluster",
-//			cluster: clusterName,
-//			expectedStatus: v1alpha1.ClusterStatus{
-//				ID:  lo.ToPtr(clusterConsoleID),
-//				SHA: lo.ToPtr("DU5PTA62PGOS35CPPCNSRG6PGXUUIWTXVBK5BFXCCGCAAM2K6HYA===="),
-//				Conditions: []metav1.Condition{
-//					{
-//						Type:   v1alpha1.ReadonlyConditionType.String(),
-//						Status: metav1.ConditionFalse,
-//						Reason: v1alpha1.ReadonlyConditionReason.String(),
+//		tests := []struct {
+//			name                     string
+//			cluster                  string
+//			returnIsClusterExisting  bool
+//			returnUpdateCluster      *gqlclient.ClusterFragment
+//			returnErrorUpdateCluster error
+//			existingObjects          []ctrlruntimeclient.Object
+//			expectedStatus           v1alpha1.ClusterStatus
+//		}{
+//			{
+//				name:    "scenario 1: update AWS cluster",
+//				cluster: clusterName,
+//				expectedStatus: v1alpha1.ClusterStatus{
+//					ID:  lo.ToPtr(clusterConsoleID),
+//					SHA: lo.ToPtr("DU5PTA62PGOS35CPPCNSRG6PGXUUIWTXVBK5BFXCCGCAAM2K6HYA===="),
+//					Conditions: []metav1.Condition{
+//						{
+//							Type:   v1alpha1.ReadonlyConditionType.String(),
+//							Status: metav1.ConditionFalse,
+//							Reason: v1alpha1.ReadonlyConditionReason.String(),
+//						},
+//						{
+//							Type:   v1alpha1.ReadyConditionType.String(),
+//							Status: metav1.ConditionTrue,
+//							Reason: v1alpha1.ReadyConditionReason.String(),
+//						},
 //					},
-//					{
-//						Type:   v1alpha1.ReadyConditionType.String(),
-//						Status: metav1.ConditionTrue,
-//						Reason: v1alpha1.ReadyConditionReason.String(),
+//				},
+//				returnIsClusterExisting: true,
+//				returnUpdateCluster:     &gqlclient.ClusterFragment{ID: clusterConsoleID},
+//				existingObjects: []ctrlruntimeclient.Object{
+//					&v1alpha1.Cluster{
+//						ObjectMeta: metav1.ObjectMeta{Name: clusterName},
+//						Spec: v1alpha1.ClusterSpec{
+//							Handle:      lo.ToPtr(clusterName),
+//							Version:     lo.ToPtr("1.24"),
+//							Cloud:       "aws",
+//							ProviderRef: &corev1.ObjectReference{Name: providerName},
+//						},
+//						Status: v1alpha1.ClusterStatus{
+//							ID:  lo.ToPtr(clusterConsoleID),
+//							SHA: lo.ToPtr("XGLLQCLXY5LEQV2UAQDUSOZ2MN24L67HDIGWRK2MA5STBBRNMVDA===="),
+//							Conditions: []metav1.Condition{
+//								{
+//									Type:   v1alpha1.ReadonlyConditionType.String(),
+//									Status: metav1.ConditionFalse,
+//									Reason: v1alpha1.ReadonlyConditionReason.String(),
+//								},
+//								{
+//									Type:   v1alpha1.ReadyConditionType.String(),
+//									Status: metav1.ConditionTrue,
+//									Reason: v1alpha1.ReadyConditionReason.String(),
+//								},
+//							},
+//						},
+//					},
+//					&v1alpha1.Provider{
+//						ObjectMeta: metav1.ObjectMeta{Name: providerName},
+//						Spec: v1alpha1.ProviderSpec{
+//							Cloud:     "aws",
+//							Name:      providerName,
+//							Namespace: providerNamespace,
+//						},
+//						Status: v1alpha1.ProviderStatus{ID: lo.ToPtr(providerConsoleID)},
 //					},
 //				},
 //			},
-//			returnIsClusterExisting: true,
-//			returnUpdateCluster:     &gqlclient.ClusterFragment{ID: clusterConsoleID},
-//			existingObjects: []ctrlruntimeclient.Object{
-//				&v1alpha1.Cluster{
-//					ObjectMeta: metav1.ObjectMeta{Name: clusterName},
-//					Spec: v1alpha1.ClusterSpec{
-//						Handle:      lo.ToPtr(clusterName),
-//						Version:     lo.ToPtr("1.24"),
-//						Cloud:       "aws",
-//						ProviderRef: &corev1.ObjectReference{Name: providerName},
+//			{
+//				name:    "scenario 2: update BYOK cluster",
+//				cluster: clusterName,
+//				expectedStatus: v1alpha1.ClusterStatus{
+//					ID:  lo.ToPtr(clusterConsoleID),
+//					SHA: lo.ToPtr("XGLLQCLXY5LEQV2UAQDUSOZ2MN24L67HDIGWRK2MA5STBBRNMVDA===="),
+//					Conditions: []metav1.Condition{
+//						{
+//							Type:   v1alpha1.ReadonlyConditionType.String(),
+//							Status: metav1.ConditionFalse,
+//							Reason: v1alpha1.ReadonlyConditionReason.String(),
+//						},
+//						{
+//							Type:   v1alpha1.ReadyConditionType.String(),
+//							Status: metav1.ConditionTrue,
+//							Reason: v1alpha1.ReadyConditionReason.String(),
+//						},
 //					},
-//					Status: v1alpha1.ClusterStatus{
-//						ID:  lo.ToPtr(clusterConsoleID),
-//						SHA: lo.ToPtr("XGLLQCLXY5LEQV2UAQDUSOZ2MN24L67HDIGWRK2MA5STBBRNMVDA===="),
-//						Conditions: []metav1.Condition{
-//							{
-//								Type:   v1alpha1.ReadonlyConditionType.String(),
-//								Status: metav1.ConditionFalse,
-//								Reason: v1alpha1.ReadonlyConditionReason.String(),
-//							},
-//							{
-//								Type:   v1alpha1.ReadyConditionType.String(),
-//								Status: metav1.ConditionTrue,
-//								Reason: v1alpha1.ReadyConditionReason.String(),
+//				},
+//				returnIsClusterExisting: true,
+//				returnUpdateCluster:     &gqlclient.ClusterFragment{ID: clusterConsoleID},
+//				existingObjects: []ctrlruntimeclient.Object{
+//					&v1alpha1.Cluster{
+//						ObjectMeta: metav1.ObjectMeta{Name: clusterName},
+//						Spec: v1alpha1.ClusterSpec{
+//							Handle: lo.ToPtr(clusterName),
+//							Cloud:  "byok",
+//						},
+//						Status: v1alpha1.ClusterStatus{
+//							ID:  lo.ToPtr(clusterConsoleID),
+//							SHA: lo.ToPtr("DU5PTA62PGOS35CPPCNSRG6PGXUUIWTXVBK5BFXCCGCAAM2K6HYA===="),
+//							Conditions: []metav1.Condition{
+//								{
+//									Type:   v1alpha1.ReadonlyConditionType.String(),
+//									Status: metav1.ConditionFalse,
+//									Reason: v1alpha1.ReadonlyConditionReason.String(),
+//								},
+//								{
+//									Type:   v1alpha1.ReadyConditionType.String(),
+//									Status: metav1.ConditionTrue,
+//									Reason: v1alpha1.ReadyConditionReason.String(),
+//								},
 //							},
 //						},
 //					},
 //				},
-//				&v1alpha1.Provider{
-//					ObjectMeta: metav1.ObjectMeta{Name: providerName},
-//					Spec: v1alpha1.ProviderSpec{
-//						Cloud:     "aws",
-//						Name:      providerName,
-//						Namespace: providerNamespace,
-//					},
-//					Status: v1alpha1.ProviderStatus{ID: lo.ToPtr(providerConsoleID)},
-//				},
 //			},
-//		},
-//		{
-//			name:    "scenario 2: update BYOK cluster",
-//			cluster: clusterName,
-//			expectedStatus: v1alpha1.ClusterStatus{
-//				ID:  lo.ToPtr(clusterConsoleID),
-//				SHA: lo.ToPtr("XGLLQCLXY5LEQV2UAQDUSOZ2MN24L67HDIGWRK2MA5STBBRNMVDA===="),
-//				Conditions: []metav1.Condition{
-//					{
-//						Type:   v1alpha1.ReadonlyConditionType.String(),
-//						Status: metav1.ConditionFalse,
-//						Reason: v1alpha1.ReadonlyConditionReason.String(),
-//					},
-//					{
-//						Type:   v1alpha1.ReadyConditionType.String(),
-//						Status: metav1.ConditionTrue,
-//						Reason: v1alpha1.ReadyConditionReason.String(),
-//					},
-//				},
-//			},
-//			returnIsClusterExisting: true,
-//			returnUpdateCluster:     &gqlclient.ClusterFragment{ID: clusterConsoleID},
-//			existingObjects: []ctrlruntimeclient.Object{
-//				&v1alpha1.Cluster{
-//					ObjectMeta: metav1.ObjectMeta{Name: clusterName},
-//					Spec: v1alpha1.ClusterSpec{
-//						Handle: lo.ToPtr(clusterName),
-//						Cloud:  "byok",
-//					},
-//					Status: v1alpha1.ClusterStatus{
-//						ID:  lo.ToPtr(clusterConsoleID),
-//						SHA: lo.ToPtr("DU5PTA62PGOS35CPPCNSRG6PGXUUIWTXVBK5BFXCCGCAAM2K6HYA===="),
-//						Conditions: []metav1.Condition{
-//							{
-//								Type:   v1alpha1.ReadonlyConditionType.String(),
-//								Status: metav1.ConditionFalse,
-//								Reason: v1alpha1.ReadonlyConditionReason.String(),
-//							},
-//							{
-//								Type:   v1alpha1.ReadyConditionType.String(),
-//								Status: metav1.ConditionTrue,
-//								Reason: v1alpha1.ReadyConditionReason.String(),
-//							},
-//						},
-//					},
-//				},
-//			},
-//		},
+//		}
+//
+//		for _, test := range tests {
+//			t.Run(test.name, func(t *testing.T) {
+//				fakeClient := fake.NewClientBuilder().WithScheme(scheme.Scheme).WithObjects(test.existingObjects...).Build()
+//
+//				fakeConsoleClient := mocks.NewConsoleClient(t)
+//				fakeConsoleClient.On("IsClusterExisting", mock.AnythingOfType("*string")).Return(test.returnIsClusterExisting)
+//				fakeConsoleClient.On("UpdateCluster", mock.AnythingOfType("string"), mock.Anything).Return(test.returnUpdateCluster, test.returnErrorUpdateCluster)
+//
+//				ctx := context.Background()
+//
+//				target := &controller.ClusterReconciler{
+//					Client:        fakeClient,
+//					Scheme:        scheme.Scheme,
+//					ConsoleClient: fakeConsoleClient,
+//				}
+//
+//				_, err := target.Reconcile(ctx, reconcile.Request{NamespacedName: types.NamespacedName{Name: test.cluster}})
+//				assert.NoError(t, err)
+//
+//				existingCluster := &v1alpha1.Cluster{}
+//				err = fakeClient.Get(ctx, ctrlruntimeclient.ObjectKey{Name: test.cluster}, existingCluster)
+//
+//				existingStatusJson, _ := json.Marshal(sanitizeClusterStatus(existingCluster.Status))
+//				expectedStatusJson, _ := json.Marshal(sanitizeClusterStatus(test.expectedStatus))
+//
+//				assert.NoError(t, err)
+//				assert.EqualValues(t, string(expectedStatusJson), string(existingStatusJson))
+//			})
+//		}
 //	}
-//
-//	for _, test := range tests {
-//		t.Run(test.name, func(t *testing.T) {
-//			fakeClient := fake.NewClientBuilder().WithScheme(scheme.Scheme).WithObjects(test.existingObjects...).Build()
-//
-//			fakeConsoleClient := mocks.NewConsoleClient(t)
-//			fakeConsoleClient.On("IsClusterExisting", mock.AnythingOfType("*string")).Return(test.returnIsClusterExisting)
-//			fakeConsoleClient.On("UpdateCluster", mock.AnythingOfType("string"), mock.Anything).Return(test.returnUpdateCluster, test.returnErrorUpdateCluster)
-//
-//			ctx := context.Background()
-//
-//			target := &controller.ClusterReconciler{
-//				Client:        fakeClient,
-//				Scheme:        scheme.Scheme,
-//				ConsoleClient: fakeConsoleClient,
-//			}
-//
-//			_, err := target.Reconcile(ctx, reconcile.Request{NamespacedName: types.NamespacedName{Name: test.cluster}})
-//			assert.NoError(t, err)
-//
-//			existingCluster := &v1alpha1.Cluster{}
-//			err = fakeClient.Get(ctx, ctrlruntimeclient.ObjectKey{Name: test.cluster}, existingCluster)
-//
-//			existingStatusJson, _ := json.Marshal(sanitizeClusterStatus(existingCluster.Status))
-//			expectedStatusJson, _ := json.Marshal(sanitizeClusterStatus(test.expectedStatus))
-//
-//			assert.NoError(t, err)
-//			assert.EqualValues(t, string(expectedStatusJson), string(existingStatusJson))
-//		})
-//	}
-//}
-//
 //func TestAdoptExistingCluster(t *testing.T) {
 //	const (
 //		clusterName      = "test-cluster"
@@ -171,24 +170,8 @@ package controller_test
 //		expectedStatus                v1alpha1.ClusterStatus
 //	}{
 //		{
-//			name:    "scenario 1: adopt existing AWS cluster",
-//			cluster: clusterName,
-//			expectedStatus: v1alpha1.ClusterStatus{
-//				ID: lo.ToPtr(clusterConsoleID),
-//				Conditions: []metav1.Condition{
-//					{
-//						Type:    v1alpha1.ReadonlyConditionType.String(),
-//						Status:  metav1.ConditionTrue,
-//						Reason:  v1alpha1.ReadonlyConditionReason.String(),
-//						Message: v1alpha1.ReadonlyTrueConditionMessage.String(),
-//					},
-//					{
-//						Type:   v1alpha1.ReadyConditionType.String(),
-//						Status: metav1.ConditionTrue,
-//						Reason: v1alpha1.ReadyConditionReason.String(),
-//					},
-//				},
-//			},
+//			name:                          "scenario 1: adopt existing AWS cluster",
+//			cluster:                       clusterName,
 //			returnGetClusterByHandle:      &gqlclient.ClusterFragment{ID: clusterConsoleID},
 //			returnErrorGetClusterByHandle: nil,
 //			existingObjects: []ctrlruntimeclient.Object{
@@ -218,10 +201,7 @@ package controller_test
 //					},
 //				},
 //			},
-//			returnGetClusterByHandle: &gqlclient.ClusterFragment{
-//				ID:             clusterConsoleID,
-//				CurrentVersion: lo.ToPtr("1.24.11"),
-//			},
+//			returnGetClusterByHandle: ,
 //			returnErrorGetClusterByHandle: nil,
 //			existingObjects: []ctrlruntimeclient.Object{
 //				&v1alpha1.Cluster{
@@ -240,7 +220,6 @@ package controller_test
 //			fakeClient := fake.NewClientBuilder().WithScheme(scheme.Scheme).WithObjects(test.existingObjects...).Build()
 //
 //			fakeConsoleClient := mocks.NewConsoleClient(t)
-//			fakeConsoleClient.On("GetClusterByHandle", mock.AnythingOfType("*string")).Return(test.returnGetClusterByHandle, test.returnErrorGetClusterByHandle)
 //
 //			ctx := context.Background()
 //
