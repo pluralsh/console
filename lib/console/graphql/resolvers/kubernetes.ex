f8c9a8fd6b199ceea94f723e29143b06e8a10929
michaeljguarino <mguarino46@gmail.com>
2023-12-19 16:45:19 -0500

    logging

--- lib/console/graphql/resolvers/kubernetes.ex
+++ lib/console/graphql/resolvers/kubernetes.ex
@@ -102,7 +102,7 @@ defmodule Console.GraphQl.Resolvers.Kubernetes do
 
   def ingress_certificates(%{metadata: %{namespace: ns}, spec: %{tls: [_ | _] = tls}}) do
     names = MapSet.new(tls, & &1.secret_name)
-    with {:ok, %{items: certs}} <- Client.list_certificate(ns) |> IO.inspect() do
+    with {:ok, %{items: certs}} <- Client.list_certificate(ns) do
       {:ok, %{items: Enum.filter(certs, &MapSet.member?(names, &1.metadata.name)) |> IO.inspect()}}
     end
   end
