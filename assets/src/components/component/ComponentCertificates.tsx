59d695e605a6b6647df8a426652236fefc98ebec
Klink <85062+dogmar@users.noreply.github.com>
2023-12-19 15:53:39 -0800

    Put real data into Certificates table

--- assets/src/components/component/ComponentCertificates.tsx
+++ assets/src/components/component/ComponentCertificates.tsx
@@ -1,27 +1,13 @@
-import { EmptyState, Table } from '@pluralsh/design-system'
+import { Chip, EmptyState, Table } from '@pluralsh/design-system'
 import { createColumnHelper } from '@tanstack/react-table'
 import { FullHeightTableWrap } from 'components/utils/layout/FullHeightTableWrap'
 import { CertificateFragment } from 'generated/graphql'
 import { produce } from 'immer'
 import { ComponentProps, useMemo } from 'react'
 import { useOutletContext } from 'react-router-dom'
+import { useTheme } from 'styled-components'
 
 const columnHelper = createColumnHelper<CertificateFragment>()
-
-const fakeCert: CertificateFragment = {
-  metadata: { name: 'Dummy certificate' },
-  raw: 'raw text',
-  status: { renewalTime: '????', notBefore: '????', notAfter: '????' },
-  spec: { secretName: 'Secret name' },
-}
-
-const fakeCerts: CertificateFragment[] = new Array<CertificateFragment>(100)
-  .fill(fakeCert)
-  .map((c, i) =>
-    produce(c, (d) => {
-      d.metadata.name = `${d.metadata.name} ${i + 1}`
-    })
-  )
 
 const ColName = columnHelper.accessor((row) => row.metadata?.name, {
   id: 'name',
@@ -32,25 +18,74 @@ const ColName = columnHelper.accessor((row) => row.metadata?.name, {
   },
 })
 
-const ColStatus = columnHelper.accessor((row) => row.status?.renewalTime, {
+/** Map condtion type to Chip severity */
+export const toSeverity = {
+  ready: 'success',
+  unhealthy: 'danger',
+} as const satisfies Record<string, ComponentProps<typeof Chip>['severity']>
+
+const ColStatus = columnHelper.accessor((row) => row.status?.conditions, {
   id: 'status',
   header: 'Status',
   meta: { gridTemplate: 'minmax(max-content, 1fr)' },
-  cell: function Cell({ getValue }) {
-    return getValue()
+  cell: function Cell({ row }) {
+    const theme = useTheme()
+
+    console.log(row?.original)
+    const conditions = row.original?.status?.conditions ?? []
+
+    return (
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: theme.spacing.xsmall,
+        }}
+      >
+        {conditions.map(
+          (c) =>
+            c && (
+              <Chip
+                severity={toSeverity[c.type.toLowerCase()] || 'neutral'}
+                toolTip={c.message || undefined}
+              >
+                {c.type}
+              </Chip>
+            )
+        )}
+      </div>
+    )
   },
 })
 
-const ColSpec = columnHelper.accessor((row) => row.spec?.secretName, {
-  id: 'spec',
-  header: 'Secret',
-  meta: { gridTemplate: 'minmax(max-content, 4fr)' },
+const ColStatusMessage = columnHelper.accessor(
+  (row) => row.status?.conditions,
+  {
+    id: 'statusMsg',
+    header: 'Status message',
+    meta: { gridTemplate: 'minmax(max-content, 1fr)' },
+    cell: function Cell({ row }) {
+      const theme = useTheme()
 
-  cell: function Cell({ getValue }) {
-    return getValue()
-  },
-})
-const columns = [ColName, ColStatus, ColSpec]
+      console.log(row?.original)
+      const conditions = row.original?.status?.conditions ?? []
+
+      return (
+        <div
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: theme.spacing.xsmall,
+          }}
+        >
+          {conditions.map((c) => c && <p>{c?.message}</p>)}
+        </div>
+      )
+    },
+  }
+)
+
+const columns = [ColName, ColStatus, ColStatusMessage]
 
 function CertificatesTable({
   certificates,
@@ -75,15 +110,14 @@ export default function ComponentCertificates() {
 
   // To avoid mapping between component types and fields of data returned by API
   // we are picking first available value from API object for now.
-  const certificates =
-    useMemo(
-      () =>
-        data
-          ? (Object.values(data).find((value) => value !== undefined) as any)
-              ?.certificates ?? null
-          : null,
-      [data]
-    ) || fakeCerts
+  const certificates = useMemo(
+    () =>
+      data
+        ? (Object.values(data).find((value) => value !== undefined) as any)
+            ?.certificates ?? null
+        : null,
+    [data]
+  )
 
   if (!certificates) {
     return <EmptyState message="This component has no certificates" />
