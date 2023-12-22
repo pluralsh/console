08d03bc680d2e334ea450a853a54204958a39e44
michaeljguarino <mguarino46@gmail.com>
2023-12-21 23:43:58 -0500

    fix scopes lints

--- lib/console/graphql/middleware/scope.ex
+++ lib/console/graphql/middleware/scope.ex
@@ -2,9 +2,7 @@ defmodule Console.Middleware.Scope do
   @behaviour Absinthe.Middleware
   alias Console.Schema.User
 
-  @dummy "!!invalid!!"
-
-  def call(%{context: %{current_user: %User{scopes: [_ | _]} = user}} = res, opts) do
+  def call(%{context: %{current_user: %User{scopes: [_ | _]}}} = res, opts) do
     api = Keyword.get(opts, :api)
     put_in(res.context.current_user.api, api)
   end
