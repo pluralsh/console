225c89987311201c7248846766c8f84dae4d003c
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-01-05 13:23:01 +0100

    update pipelines model

--- controller/api/v1alpha1/pipeline_types.go
+++ controller/api/v1alpha1/pipeline_types.go
@@ -17,31 +17,194 @@ limitations under the License.
 package v1alpha1
 
 import (
+	console "github.com/pluralsh/console-client-go"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// PipelineSpec defines the desired state of Pipeline
+// PipelineSpec defines the desired state of Pipeline.
 type PipelineSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Stages of a pipeline.
+	Stages []PipelineStage `json:"stages,omitempty"`
 
-	// Foo is an example field of Pipeline. Edit pipeline_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Edges of a pipeline.
+	Edges []PipelineEdge `json:"edges,omitempty"`
+}
+
+// PipelineStage defines the Pipeline stage.
+type PipelineStage struct {
+	// Name of this stage.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Type:=string
+	Name string `json:"name"`
+
+	// Services including optional promotion criteria.
+	Services []PipelineStageService `json:"services,omitempty"`
+}
+
+// PipelineStageService is the configuration of a service within a pipeline stage,
+// including optional promotion criteria.
+type PipelineStageService struct {
+	// ClusterRef of this service.
+	ClusterRef *v1.ObjectReference `json:"clusterRef,omitempty"`
+
+	ServiceRef *v1.ObjectReference `json:"serviceRef,omitempty"`
+
+	Criteria *PipelineStageServicePromotionCriteria `json:"criteria,omitempty"`
+}
+
+// PipelineStageServicePromotionCriteria represents actions to perform if this stage service were promoted.
+type PipelineStageServicePromotionCriteria struct {
+	// ClusterRef of the Cluster for the source service.
+	ClusterRef *v1.ObjectReference `json:"clusterRef,omitempty"`
+
+	// ServiceRef pointing to source service to promote from.
+	ServiceRef *v1.ObjectReference `json:"serviceRef,omitempty"`
+
+	// Secrets to copy over in a promotion.
+	Secrets []string `json:"secrets,omitempty"`
+}
+
+// PipelineEdge is a specification of an edge between two pipeline stages.
+type PipelineEdge struct {
+	// FromID is stage ID the edge is from, can also be specified by name.
+	// +kubebuilder:validation:Type:=string
+	FromID *string `json:"fromId,omitempty"`
+
+	// ToID is stage ID the edge is to, can also be specified by name.
+	// +kubebuilder:validation:Type:=string
+	ToID *string `json:"toId,omitempty"`
+
+	// From is the name of the pipeline stage this edge emits from.
+	// +kubebuilder:validation:Type:=string
+	From *string `json:"from,omitempty"`
+
+	// To is the name of the pipeline stage this edge points to.
+	// +kubebuilder:validation:Type:=string
+	To *string `json:"to,omitempty"`
+
+	// Gates are any optional promotion gates you wish to configure.
+	// +kubebuilder:validation:Optional
+	Gates []PipelineGate `json:"gates,omitempty"`
+}
+
+// PipelineGate will configure a promotion gate for a pipeline.
+type PipelineGate struct {
+	// Name of this gate.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Type:=string
+	Name string `json:"name"`
+
+	// Type of gate this is.
+	Type console.GateType `json:"type"`
+
+	// ClusterRef of a Cluster this gate will execute on.
+	ClusterRef v1.ObjectReference `json:"clusterRef,omitempty"`
+
+	// Spec contains specification for more complex gate types.
+	Spec *GateSpec `json:"spec,omitempty"`
+}
+
+// GateSpec is a more refined spec for parameters needed for complex gates.
+type GateSpec struct {
+	Job *GateJob `json:"job,omitempty"`
+}
+
+// GateJob is a spec for a job gate.
+type GateJob struct {
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Type:=string
+	Namespace string `json:"namespace"`
+
+	// +kubebuilder:validation:Optional
+	Containers []*Container `json:"containers,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Annotations map[string]string `json:"annotations,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Type:=string
+	ServiceAccount *string `json:"serviceAccount,omitempty"`
+
+	// Raw can be used if you'd rather define the job spec via straight Kubernetes manifest file.
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Type:=string
+	Raw *string `json:"raw,omitempty"`
+}
+
+type Container struct {
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Type:=string
+	Image string `json:"image"`
+
+	// +kubebuilder:validation:Optional
+	Args []*string `json:"args,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Env []*Env `json:"env,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	EnvFrom []*EnvFrom `json:"envFrom,omitempty"`
+}
+
+type Env struct {
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Type:=string
+	Name string `json:"name"`
+
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Type:=string
+	Value string `json:"value"`
+}
+
+type EnvFrom struct {
+	// +kubebuilder:validation:Type:=string
+	Secret string `json:"secret"`
+
+	// +kubebuilder:validation:Type:=string
+	ConfigMap string `json:"configMap"`
 }
 
 // PipelineStatus defines the observed state of Pipeline
 type PipelineStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// ID from Console.
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Type:=string
+	ID *string `json:"id,omitempty"`
+
+	// SHA of last applied configuration.
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Type:=string
+	SHA *string `json:"sha,omitempty"`
+
+	// Represents the observations of a Pipeline current state.
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	// +listType=map
+	// +listMapKey=type
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
 }
 
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
+func (cs *PipelineStatus) HasID() bool {
+	return cs.ID != nil && len(*cs.ID) > 0
+}
+
+func (cs *PipelineStatus) HasSHA() bool {
+	return cs.SHA != nil && len(*cs.SHA) > 0
+}
+
+func (cs *PipelineStatus) IsSHAChanged(sha string) bool {
+	return cs.HasSHA() && *cs.SHA != sha
+}
 
 // Pipeline is the Schema for the pipelines API
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Namespaced
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="ID",type="string",JSONPath=".status.id",description="Console ID"
 type Pipeline struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -50,9 +213,8 @@ type Pipeline struct {
 	Status PipelineStatus `json:"status,omitempty"`
 }
 
-//+kubebuilder:object:root=true
-
 // PipelineList contains a list of Pipeline
+// +kubebuilder:object:root=true
 type PipelineList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
