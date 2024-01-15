6fae1cb90144cf65d2a5eba9e6379d6e01d477c4
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-01-15 13:03:00 +0100

    add diff viewer

--- assets/src/components/cd/services/service/ServiceDesiredState.tsx
+++ assets/src/components/cd/services/service/ServiceDesiredState.tsx
@@ -1,17 +1,18 @@
 import { useNavigate } from 'react-router-dom'
-
 import {
   CD_ABS_PATH,
   SERVICES_REL_PATH,
   getServiceDetailsPath,
 } from 'routes/cdRoutesConsts'
-
 import { ScrollablePage } from 'components/utils/layout/ScrollablePage'
+import DiffViewer from 'react-diff-viewer'
+import { useTheme } from 'styled-components'
 
 import { useServiceContext } from './ServiceDetails'
 
 export default function ServiceDesiredState() {
   const navigate = useNavigate()
+  const theme = useTheme()
   const { service } = useServiceContext()
 
   if (!service) {
@@ -35,8 +36,48 @@ export default function ServiceDesiredState() {
       heading="Desired state"
       scrollable={false}
     >
-      ...
-      {/* TODO: Content goes here. */}
+      {/* TODO */}
+      <DiffViewer
+        oldValue="apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-deployment
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  replicas: 2
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.14.2
+        ports:
+        - containerPort: 80"
+        newValue="apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-deployment
+spec:
+  selector:
+    matchLabels:
+      app: test
+  replicas: 5
+  template:
+    metadata:
+      labels:
+        app: test
+    spec:
+      containers:
+      - name: test
+        image: test:1.14.8
+        ports:
+        - containerPort: 80"
+        useDarkTheme={theme.mode === 'dark'}
+      />
     </ScrollablePage>
   )
 }
