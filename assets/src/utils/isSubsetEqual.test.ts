01c2598622803511de6ef9350d12297fe1949404
Klink <85062+dogmar@users.noreply.github.com>
2024-01-25 10:13:40 -0800

    Prevent re-renders when `useUpdateState` updates would not change the state shallowly.

---
+++ assets/src/utils/isSubsetEqual.test.ts
@@ -0,0 +1,32 @@
+import { isSubsetEqual } from './isSubsetEqual'
+
+const set = {
+  a: 'a string',
+  b: false,
+  c: 123.3,
+  d: {
+    a: true,
+    b: 2,
+    c: 'another string',
+    d: false,
+  },
+}
+
+describe('Email utils', () => {
+  it('should detect valid email addresses', () => {
+    expect(isSubsetEqual({}, {})).toBeTruthy()
+    expect(isSubsetEqual(set, {})).toBeTruthy()
+    expect(isSubsetEqual(set, set)).toBeTruthy()
+    expect(isSubsetEqual(set, { a: 'a string' })).toBeTruthy()
+    expect(isSubsetEqual(set, { a: 'a different string' })).toBeFalsy()
+    expect(isSubsetEqual(set, { b: false })).toBeTruthy()
+    expect(isSubsetEqual(set, { b: true })).toBeFalsy()
+    expect(isSubsetEqual({}, set)).toBeFalsy()
+    expect(isSubsetEqual(set, { d: { a: true } })).toBeFalsy()
+    expect(
+      isSubsetEqual(set, {
+        d: { a: true, b: 2, c: 'another string', d: false },
+      })
+    ).toBeTruthy()
+  })
+})
