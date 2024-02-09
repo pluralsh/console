213ecb697bf5c47249648396b25cd8cc9c4ca5d4
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-02-09 12:04:41 +0100

    update modal styling

--- assets/src/components/profile/AccessTokensCreateScope.tsx
+++ assets/src/components/profile/AccessTokensCreateScope.tsx
@@ -1,9 +1,16 @@
 import { useTheme } from 'styled-components'
 import { useCallback, useState } from 'react'
-import { Button, Card, Chip, FormField, Input } from '@pluralsh/design-system'
+import {
+  Button,
+  Card,
+  Chip,
+  CloseIcon,
+  FormField,
+  IconFrame,
+  Input,
+} from '@pluralsh/design-system'
 import { isEmpty } from 'lodash'
 
-import { DeleteIconButton } from '../utils/IconButtons'
 import { ChipList } from '../cd/services/CreateGlobalService'
 
 import { Scope } from './AccessTokensCreate'
@@ -11,13 +18,11 @@ import { Scope } from './AccessTokensCreate'
 export function AccessTokensCreateScope({
   scope,
   setScope,
-  index,
   remove,
   canRemove,
 }: {
   scope: Scope
   setScope: any
-  index: number
   remove: any
   canRemove: boolean
 }) {
@@ -67,105 +72,99 @@ export function AccessTokensCreateScope({
           padding: theme.spacing.medium,
           display: 'flex',
           flexDirection: 'column',
-          gap: theme.spacing.medium,
+          position: 'relative',
         },
       }}
       fillLevel={2}
     >
-      <div
-        css={{
-          ...theme.partials.text.overline,
-          alignItems: 'center',
-          color: theme.colors['text-xlight'],
-          display: 'flex',
-          flexGrow: 1,
-          justifyContent: 'space-between',
-        }}
+      <IconFrame
+        size="small"
+        clickable
+        color={canRemove ? 'icon-xlight' : 'icon-disabled'}
+        disabled={!canRemove}
+        tooltip
+        onClick={remove}
+        icon={<CloseIcon />}
+        textValue="Delete"
+        position="absolute"
+        right={16}
+        top={16}
+      />
+      <FormField
+        label="API"
+        hint="Add at least one API, i.e. updateServiceDeployment."
+        marginTop="small"
+        error={isEmpty(scope.apis)}
+        required
       >
-        <span>Scope #{index + 1}</span>
-        <DeleteIconButton
-          onClick={remove}
-          tooltip={canRemove}
-          disabled={!canRemove}
+        <div css={{ display: 'flex', flexGrow: 1, gap: theme.spacing.small }}>
+          <Input
+            flexGrow={1}
+            value={api}
+            error={isEmpty(scope.apis)}
+            onChange={(e) => setApi(e.currentTarget.value)}
+          />
+          <Button
+            secondary
+            disabled={isEmpty(api)}
+            onClick={addApi}
+          >
+            Add
+          </Button>
+        </div>
+      </FormField>
+      {!isEmpty(scope.apis) && (
+        <ChipList
+          maxVisible={Infinity}
+          chips={scope.apis.map((a, idx) => (
+            <Chip
+              size="small"
+              clickable
+              onClick={() => removeApi(idx)}
+              closeButton
+            >
+              {a}
+            </Chip>
+          ))}
         />
-      </div>
-      <div>
-        <FormField
-          label="API"
-          hint="Add at least one API, i.e. updateServiceDeployment."
-          error={isEmpty(scope.apis)}
-          required
-        >
-          <div css={{ display: 'flex', flexGrow: 1, gap: theme.spacing.small }}>
-            <Input
-              flexGrow={1}
-              value={api}
-              error={isEmpty(scope.apis)}
-              onChange={(e) => setApi(e.currentTarget.value)}
-            />
-            <Button
-              secondary
-              disabled={isEmpty(api)}
-              onClick={addApi}
-            >
-              Add
-            </Button>
-          </div>
-        </FormField>
-        {!isEmpty(scope.apis) && (
-          <ChipList
-            maxVisible={Infinity}
-            chips={scope.apis.map((a, idx) => (
-              <Chip
-                size="small"
-                clickable
-                onClick={() => removeApi(idx)}
-                closeButton
-              >
-                {a}
-              </Chip>
-            ))}
+      )}
+      <FormField
+        label="ID"
+        hint="Add resource identifiers. Leave it blank or use * to match all resources."
+        marginTop="small"
+      >
+        <div css={{ display: 'flex', flexGrow: 1, gap: theme.spacing.small }}>
+          <Input
+            flexGrow={1}
+            value={id}
+            onChange={(e) => {
+              setId(e.currentTarget.value)
+            }}
           />
-        )}
-      </div>
-      <div>
-        <FormField
-          label="ID"
-          hint="Add resource identifiers. Leave it blank or use * to match all resources."
-        >
-          <div css={{ display: 'flex', flexGrow: 1, gap: theme.spacing.small }}>
-            <Input
-              flexGrow={1}
-              value={id}
-              onChange={(e) => {
-                setId(e.currentTarget.value)
-              }}
-            />
-            <Button
-              secondary
-              disabled={isEmpty(id)}
-              onClick={addId}
+          <Button
+            secondary
+            disabled={isEmpty(id)}
+            onClick={addId}
+          >
+            Add
+          </Button>
+        </div>
+      </FormField>
+      {!isEmpty(scope.ids) && (
+        <ChipList
+          maxVisible={Infinity}
+          chips={scope.ids.map((i, idx) => (
+            <Chip
+              size="small"
+              clickable
+              onClick={() => removeId(idx)}
+              closeButton
             >
-              Add
-            </Button>
-          </div>
-        </FormField>
-        {!isEmpty(scope.ids) && (
-          <ChipList
-            maxVisible={Infinity}
-            chips={scope.ids.map((i, idx) => (
-              <Chip
-                size="small"
-                clickable
-                onClick={() => removeId(idx)}
-                closeButton
-              >
-                {i}
-              </Chip>
-            ))}
-          />
-        )}
-      </div>
+              {i}
+            </Chip>
+          ))}
+        />
+      )}
     </Card>
   )
 }
