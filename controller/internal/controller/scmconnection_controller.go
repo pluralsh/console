2a6bd0d39f89832774fa01dfee3cf6513a24c6f2
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-02-14 15:44:28 +0100

    fix gofmt

--- controller/internal/controller/scmconnection_controller.go
+++ controller/internal/controller/scmconnection_controller.go
@@ -4,9 +4,8 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/samber/lo"
-
 	console "github.com/pluralsh/console-client-go"
+	"github.com/samber/lo"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
