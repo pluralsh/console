ae41563fe30382388ea5369cfdceba6621d5916c
Klink <85062+dogmar@users.noreply.github.com>
2024-02-21 11:52:23 -0800

    prettier

--- assets/src/components/contexts/DeploymentSettingsContext.tsx
+++ assets/src/components/contexts/DeploymentSettingsContext.tsx
@@ -1,40 +1,40 @@
-import { createContext, useContext, useMemo } from 'react';
-import { POLL_INTERVAL } from 'components/cd/ContinuousDeployment';
-import { useCDEnabled } from 'components/cd/utils/useCDEnabled';
+import { createContext, useContext, useMemo } from 'react'
+import { POLL_INTERVAL } from 'components/cd/ContinuousDeployment'
+import { useCDEnabled } from 'components/cd/utils/useCDEnabled'
 import {
   DeploymentSettingsFragment,
-  useDeploymentSettingsQuery
-} from 'generated/graphql';
+  useDeploymentSettingsQuery,
+} from 'generated/graphql'
 
 const DeploymentSettingsContext = createContext<
   DeploymentSettingsFragment | undefined | null
->(null);
+>(null)
 
 export function useDeploymentSettings() {
-  const ctx = useContext(DeploymentSettingsContext);
+  const ctx = useContext(DeploymentSettingsContext)
 
-  return ctx || ({} as Partial<DeploymentSettingsFragment>);
+  return ctx || ({} as Partial<DeploymentSettingsFragment>)
 }
 
 export function DeploymentSettingsProvider({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   const { data } = useDeploymentSettingsQuery({
     pollInterval: POLL_INTERVAL,
-  });
+  })
 
-  useCDEnabled({ redirect: true });
+  useCDEnabled({ redirect: true })
 
   const providerValue = useMemo(
     () => data?.deploymentSettings,
     [data?.deploymentSettings]
-  );
+  )
 
   return (
     <DeploymentSettingsContext.Provider value={providerValue}>
       {children}
     </DeploymentSettingsContext.Provider>
-  );
+  )
 }
