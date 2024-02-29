fb6d743dcda9417b70deea6da5de2b4b1f557888
Klink <85062+dogmar@users.noreply.github.com>
2024-02-29 09:55:10 -0800

    remove test configs

--- assets/src/components/login/reducePersonaConfigs.tsx
+++ assets/src/components/login/reducePersonaConfigs.tsx
@@ -5,66 +5,12 @@ import {
 import { isObject, mergeWith } from 'lodash'
 import { isNonNullable } from 'utils/isNonNullable'
 
-const IS_TESTING = false
-const TEST_CONFIGS: PersonaConfigurationFragment[] = [
-  {
-    all: false,
-    deployments: {
-      addOns: true,
-      clusters: false,
-      deployments: false,
-      pipelines: true,
-      providers: false,
-      services: false,
-    },
-    sidebar: {
-      audits: false,
-      kubernetes: true,
-      pullRequests: false,
-      settings: false,
-    },
-  },
-  // {
-  //   all: true,
-  //   deployments: {
-  //     addOns: false,
-  //     clusters: true,
-  //     deployments: false,
-  //     pipelines: false,
-  //     providers: false,
-  //     services: false,
-  //   },
-  //   sidebar: {
-  //     audits: false,
-  //     kubernetes: true,
-  //     pullRequests: false,
-  //     settings: true,
-  //   },
-  // },
-  // {
-  //   deployments: {
-  //     addOns: false,
-  //     clusters: false,
-  //     deployments: true,
-  //     pipelines: false,
-  //     providers: false,
-  //     services: true,
-  //   },
-  // },
-  {
-    sidebar: {
-      audits: true,
-      settings: true,
-    },
-  },
-]
-
 export const reducePersonaConfigs = (
   personas: Nullable<Nullable<PersonaFragment>[]>
 ): PersonaConfigurationFragment => {
-  const configs: Nullable<PersonaConfigurationFragment[]> = IS_TESTING
-    ? TEST_CONFIGS
-    : personas?.map((persona) => persona?.configuration).filter(isNonNullable)
+  const configs: Nullable<PersonaConfigurationFragment[]> = personas
+    ?.map((persona) => persona?.configuration)
+    .filter(isNonNullable)
 
   if (!configs?.length) return { all: true }
 
