4247f396d36a3e604637925c4de3e1111f374c62
Klink <85062+dogmar@users.noreply.github.com>
2024-03-04 13:20:18 -0800

    feat: Show badge with open PR count in Pipeline Stage nodes (#747)

--- assets/src/components/help/CountBadge.tsx
+++ assets/src/components/help/CountBadge.tsx
@@ -2,11 +2,17 @@ import styled from 'styled-components'
 
 const CountBadgeSC = styled.div<{
   $size?: 'small' | 'medium'
-  $count?: number
-}>(({ $count = 0, $size = 'medium', theme }) => {
+  $label?: string
+}>(({ $label = '', $size = 'medium', theme }) => {
   const width =
-    $size === 'small' ? ($count >= 10 ? 18 : 14) : $count >= 10 ? 18 : 16
-  const fontSize = $size === 'small' ? 10.5 : $count >= 10 ? 10.5 : 12
+    $size === 'small'
+      ? $label.length > 1
+        ? 18
+        : 14
+      : $label.length > 1
+      ? 18
+      : 16
+  const fontSize = $size === 'small' ? 10.5 : $label.length > 1 ? 10.5 : 12
 
   return {
     ...theme.partials.text.badgeLabel,
@@ -31,13 +37,22 @@ export function CountBadge({
   count?: number
   size?: 'small' | 'medium'
 }) {
+  if (!count) {
+    return null
+  }
+  let badgeLabel: string = count.toString()
+
+  if (count > 99) {
+    badgeLabel = '!'
+  }
+
   return (
     <CountBadgeSC
-      $count={count}
+      $label={badgeLabel}
       $size={size}
       {...props}
     >
-      {count}
+      {badgeLabel}
     </CountBadgeSC>
   )
 }
