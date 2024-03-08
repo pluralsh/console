e1aa357d2f2853211695caa91ad4f0469387188e
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-03-08 01:02:07 +0100

    feat: add notification router (#758)

--- controller/internal/test/mocks/ConsoleClient_mock.go
+++ controller/internal/test/mocks/ConsoleClient_mock.go
@@ -723,6 +723,53 @@ func (_c *ConsoleClientMock_DeleteGlobalService_Call) RunAndReturn(run func(stri
 	return _c
 }
 
+// DeleteNotificationRouter provides a mock function with given fields: ctx, id
+func (_m *ConsoleClientMock) DeleteNotificationRouter(ctx context.Context, id string) error {
+	ret := _m.Called(ctx, id)
+
+	if len(ret) == 0 {
+		panic("no return value specified for DeleteNotificationRouter")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string) error); ok {
+		r0 = rf(ctx, id)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// ConsoleClientMock_DeleteNotificationRouter_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'DeleteNotificationRouter'
+type ConsoleClientMock_DeleteNotificationRouter_Call struct {
+	*mock.Call
+}
+
+// DeleteNotificationRouter is a helper method to define mock.On call
+//   - ctx context.Context
+//   - id string
+func (_e *ConsoleClientMock_Expecter) DeleteNotificationRouter(ctx interface{}, id interface{}) *ConsoleClientMock_DeleteNotificationRouter_Call {
+	return &ConsoleClientMock_DeleteNotificationRouter_Call{Call: _e.mock.On("DeleteNotificationRouter", ctx, id)}
+}
+
+func (_c *ConsoleClientMock_DeleteNotificationRouter_Call) Run(run func(ctx context.Context, id string)) *ConsoleClientMock_DeleteNotificationRouter_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(string))
+	})
+	return _c
+}
+
+func (_c *ConsoleClientMock_DeleteNotificationRouter_Call) Return(_a0 error) *ConsoleClientMock_DeleteNotificationRouter_Call {
+	_c.Call.Return(_a0)
+	return _c
+}
+
+func (_c *ConsoleClientMock_DeleteNotificationRouter_Call) RunAndReturn(run func(context.Context, string) error) *ConsoleClientMock_DeleteNotificationRouter_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // DeleteNotificationSink provides a mock function with given fields: ctx, id
 func (_m *ConsoleClientMock) DeleteNotificationSink(ctx context.Context, id string) error {
 	ret := _m.Called(ctx, id)
@@ -1408,6 +1455,124 @@ func (_c *ConsoleClientMock_GetGroup_Call) Return(_a0 *gqlclient.GroupFragment, 
 }
 
 func (_c *ConsoleClientMock_GetGroup_Call) RunAndReturn(run func(string) (*gqlclient.GroupFragment, error)) *ConsoleClientMock_GetGroup_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// GetNotificationRouter provides a mock function with given fields: ctx, id
+func (_m *ConsoleClientMock) GetNotificationRouter(ctx context.Context, id string) (*gqlclient.NotificationRouterFragment, error) {
+	ret := _m.Called(ctx, id)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetNotificationRouter")
+	}
+
+	var r0 *gqlclient.NotificationRouterFragment
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, string) (*gqlclient.NotificationRouterFragment, error)); ok {
+		return rf(ctx, id)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, string) *gqlclient.NotificationRouterFragment); ok {
+		r0 = rf(ctx, id)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*gqlclient.NotificationRouterFragment)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, id)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// ConsoleClientMock_GetNotificationRouter_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetNotificationRouter'
+type ConsoleClientMock_GetNotificationRouter_Call struct {
+	*mock.Call
+}
+
+// GetNotificationRouter is a helper method to define mock.On call
+//   - ctx context.Context
+//   - id string
+func (_e *ConsoleClientMock_Expecter) GetNotificationRouter(ctx interface{}, id interface{}) *ConsoleClientMock_GetNotificationRouter_Call {
+	return &ConsoleClientMock_GetNotificationRouter_Call{Call: _e.mock.On("GetNotificationRouter", ctx, id)}
+}
+
+func (_c *ConsoleClientMock_GetNotificationRouter_Call) Run(run func(ctx context.Context, id string)) *ConsoleClientMock_GetNotificationRouter_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(string))
+	})
+	return _c
+}
+
+func (_c *ConsoleClientMock_GetNotificationRouter_Call) Return(_a0 *gqlclient.NotificationRouterFragment, _a1 error) *ConsoleClientMock_GetNotificationRouter_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *ConsoleClientMock_GetNotificationRouter_Call) RunAndReturn(run func(context.Context, string) (*gqlclient.NotificationRouterFragment, error)) *ConsoleClientMock_GetNotificationRouter_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// GetNotificationRouterByName provides a mock function with given fields: ctx, name
+func (_m *ConsoleClientMock) GetNotificationRouterByName(ctx context.Context, name string) (*gqlclient.NotificationRouterFragment, error) {
+	ret := _m.Called(ctx, name)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetNotificationRouterByName")
+	}
+
+	var r0 *gqlclient.NotificationRouterFragment
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, string) (*gqlclient.NotificationRouterFragment, error)); ok {
+		return rf(ctx, name)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, string) *gqlclient.NotificationRouterFragment); ok {
+		r0 = rf(ctx, name)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*gqlclient.NotificationRouterFragment)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, name)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// ConsoleClientMock_GetNotificationRouterByName_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetNotificationRouterByName'
+type ConsoleClientMock_GetNotificationRouterByName_Call struct {
+	*mock.Call
+}
+
+// GetNotificationRouterByName is a helper method to define mock.On call
+//   - ctx context.Context
+//   - name string
+func (_e *ConsoleClientMock_Expecter) GetNotificationRouterByName(ctx interface{}, name interface{}) *ConsoleClientMock_GetNotificationRouterByName_Call {
+	return &ConsoleClientMock_GetNotificationRouterByName_Call{Call: _e.mock.On("GetNotificationRouterByName", ctx, name)}
+}
+
+func (_c *ConsoleClientMock_GetNotificationRouterByName_Call) Run(run func(ctx context.Context, name string)) *ConsoleClientMock_GetNotificationRouterByName_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(string))
+	})
+	return _c
+}
+
+func (_c *ConsoleClientMock_GetNotificationRouterByName_Call) Return(_a0 *gqlclient.NotificationRouterFragment, _a1 error) *ConsoleClientMock_GetNotificationRouterByName_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *ConsoleClientMock_GetNotificationRouterByName_Call) RunAndReturn(run func(context.Context, string) (*gqlclient.NotificationRouterFragment, error)) *ConsoleClientMock_GetNotificationRouterByName_Call {
 	_c.Call.Return(run)
 	return _c
 }
@@ -3449,6 +3614,65 @@ func (_c *ConsoleClientMock_UpdateService_Call) Return(_a0 error) *ConsoleClient
 }
 
 func (_c *ConsoleClientMock_UpdateService_Call) RunAndReturn(run func(string, gqlclient.ServiceUpdateAttributes) error) *ConsoleClientMock_UpdateService_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// UpsertNotificationRouter provides a mock function with given fields: ctx, attr
+func (_m *ConsoleClientMock) UpsertNotificationRouter(ctx context.Context, attr gqlclient.NotificationRouterAttributes) (*gqlclient.NotificationRouterFragment, error) {
+	ret := _m.Called(ctx, attr)
+
+	if len(ret) == 0 {
+		panic("no return value specified for UpsertNotificationRouter")
+	}
+
+	var r0 *gqlclient.NotificationRouterFragment
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, gqlclient.NotificationRouterAttributes) (*gqlclient.NotificationRouterFragment, error)); ok {
+		return rf(ctx, attr)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, gqlclient.NotificationRouterAttributes) *gqlclient.NotificationRouterFragment); ok {
+		r0 = rf(ctx, attr)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*gqlclient.NotificationRouterFragment)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, gqlclient.NotificationRouterAttributes) error); ok {
+		r1 = rf(ctx, attr)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// ConsoleClientMock_UpsertNotificationRouter_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'UpsertNotificationRouter'
+type ConsoleClientMock_UpsertNotificationRouter_Call struct {
+	*mock.Call
+}
+
+// UpsertNotificationRouter is a helper method to define mock.On call
+//   - ctx context.Context
+//   - attr gqlclient.NotificationRouterAttributes
+func (_e *ConsoleClientMock_Expecter) UpsertNotificationRouter(ctx interface{}, attr interface{}) *ConsoleClientMock_UpsertNotificationRouter_Call {
+	return &ConsoleClientMock_UpsertNotificationRouter_Call{Call: _e.mock.On("UpsertNotificationRouter", ctx, attr)}
+}
+
+func (_c *ConsoleClientMock_UpsertNotificationRouter_Call) Run(run func(ctx context.Context, attr gqlclient.NotificationRouterAttributes)) *ConsoleClientMock_UpsertNotificationRouter_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(gqlclient.NotificationRouterAttributes))
+	})
+	return _c
+}
+
+func (_c *ConsoleClientMock_UpsertNotificationRouter_Call) Return(_a0 *gqlclient.NotificationRouterFragment, _a1 error) *ConsoleClientMock_UpsertNotificationRouter_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *ConsoleClientMock_UpsertNotificationRouter_Call) RunAndReturn(run func(context.Context, gqlclient.NotificationRouterAttributes) (*gqlclient.NotificationRouterFragment, error)) *ConsoleClientMock_UpsertNotificationRouter_Call {
 	_c.Call.Return(run)
 	return _c
 }
