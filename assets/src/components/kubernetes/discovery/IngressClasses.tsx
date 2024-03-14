8096fa6733ab76ef63963b4dfcbaf114b4f291c4
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-03-14 15:38:09 +0100

    fix ingress classes

--- assets/src/components/kubernetes/discovery/IngressClasses.tsx
+++ assets/src/components/kubernetes/discovery/IngressClasses.tsx
@@ -48,7 +48,7 @@ export default function IngressClasses() {
         extendConnection(
           prev,
           fetchMoreResult,
-          'handleGetIngressList',
+          'handleGetIngressClassList',
           'items'
         ),
     })
