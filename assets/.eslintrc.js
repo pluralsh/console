61875b5d2c2df84b91a9f28872e4a56401c9cfa9
Renê Boaventura <reneboaventuraneto@gmail.com>
2024-03-28 12:55:22 -0300

    install prettier

--- assets/.eslintrc.js
+++ assets/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     project: './tsconfig.eslint.json',
   },
   extends: ['@pluralsh/eslint-config-typescript', 'prettier'],
+  plugins: ['prettier'],
   globals: {
     JSX: true,
   },
@@ -11,5 +12,6 @@ module.exports = {
     'import-newlines/enforce': 'off',
     // Allow css prop for styled-components
     'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'prettier/prettier': 'error',
   },
 }
