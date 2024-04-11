eec51214f74f3067395b570a8024b7c16829934c
Renê Boaventura <reneboaventuraneto@gmail.com>
2024-04-11 13:31:50 -0300

    extract GitProviderSelect

---
+++ assets/src/components/pr/scm/GitProviderSelect.tsx
@@ -0,0 +1,38 @@
+import { FormField, ListBoxItem, Select } from '@pluralsh/design-system'
+import { ScmType } from 'generated/graphql'
+
+import { scmTypeToIcon, scmTypeToLabel } from './PrScmConnectionsColumns'
+
+function GitProviderSelect({
+  selectedKey,
+  updateSelectedKey,
+}: {
+  selectedKey: ScmType | undefined
+  updateSelectedKey: (key: ScmType | undefined) => void
+}) {
+  return (
+    <FormField
+      label="Provider type"
+      required
+    >
+      <Select
+        selectedKey={selectedKey}
+        leftContent={
+          !selectedKey ? undefined : scmTypeToIcon[selectedKey || '']
+        }
+        label="Select provider type"
+        onSelectionChange={(key) => updateSelectedKey(key as ScmType)}
+      >
+        {[ScmType.Github, ScmType.Gitlab].map((type) => (
+          <ListBoxItem
+            key={type}
+            leftContent={scmTypeToIcon[type]}
+            label={scmTypeToLabel[type]}
+          />
+        ))}
+      </Select>
+    </FormField>
+  )
+}
+
+export default GitProviderSelect
