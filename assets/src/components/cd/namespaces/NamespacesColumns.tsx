301af646671d23d025cee5cb376c59009ad4f237
Renê Boaventura <reneboaventuraneto@gmail.com>
2024-04-15 11:07:43 -0300

    create namespaces table

---
+++ assets/src/components/cd/namespaces/NamespacesColumns.tsx
@@ -0,0 +1,69 @@
+import { createColumnHelper } from '@tanstack/react-table'
+
+import { Namespace } from 'generated/graphql'
+import { toDateOrUndef } from 'utils/date'
+
+import { DateTimeCol } from 'components/utils/table/DateTimeCol'
+
+const columnHelper = createColumnHelper<Namespace>()
+
+export const ColName = columnHelper.accessor((namespace) => namespace, {
+  id: 'name',
+  header: 'Name',
+  meta: { truncate: true, gridTemplate: 'minmax(180px,300px)' },
+  cell: function Cell({ getValue }) {
+    const namespace = getValue()
+
+    return namespace ? namespace.metadata.name : '--'
+  },
+})
+
+export const ColNamespace = columnHelper.accessor((namespace) => namespace, {
+  id: 'namespace',
+  header: 'Namespace',
+  meta: { truncate: true, gridTemplate: 'minmax(180px,300px)' },
+  cell: function Cell({ getValue }) {
+    const namespace = getValue()
+
+    return namespace ? namespace.metadata.name : '--'
+  },
+})
+
+export const ColStatus = columnHelper.accessor((namespace) => namespace, {
+  id: 'status',
+  header: 'Status',
+  meta: { truncate: true, gridTemplate: 'minmax(180px,300px)' },
+  cell: function Cell({ getValue }) {
+    const namespace = getValue()
+
+    return namespace ? namespace.status.phase : '--'
+  },
+})
+
+export const ColFinalizers = columnHelper.accessor((namespace) => namespace, {
+  id: 'finalizers',
+  header: 'Finalizers',
+  meta: { truncate: true, gridTemplate: 'minmax(150px,1fr)' },
+  cell: ({ getValue }) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const namespace = getValue()
+
+    const finalizers = namespace?.spec.finalizers?.join(', ')
+
+    return finalizers || ''
+  },
+})
+
+export const ColCreatedAt = columnHelper.accessor(
+  (namespace) => {
+    const insertedAt = toDateOrUndef(namespace?.metadata.creationTimestamp)
+
+    return insertedAt || undefined
+  },
+  {
+    id: 'createdAt',
+    header: 'Created at',
+    sortingFn: 'datetime',
+    cell: ({ getValue }) => <DateTimeCol date={getValue()?.toISOString()} />,
+  }
+)
