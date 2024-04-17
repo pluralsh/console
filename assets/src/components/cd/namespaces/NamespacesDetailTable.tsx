2741f8135379c8b8f45b3c7306cad310babb2b43
Renê Boaventura <reneboaventuraneto@gmail.com>
2024-04-17 14:12:56 -0300

    fix services in namespaces details page

--- assets/src/components/cd/namespaces/NamespacesDetailTable.tsx
+++ assets/src/components/cd/namespaces/NamespacesDetailTable.tsx
@@ -1,7 +1,14 @@
-import { useRef } from 'react'
-import { Card, TabPanel, Table } from '@pluralsh/design-system'
+import { useCallback, useRef } from 'react'
+import { Card, EmptyState, TabPanel, Table } from '@pluralsh/design-system'
+import { useNavigate } from 'react-router'
 import { useTheme } from 'styled-components'
-import { useGetManagedNamespaceQuery } from 'generated/graphql'
+import type { Row } from '@tanstack/react-table'
+import {
+  type ServiceDeploymentsRowFragment,
+  useGetManagedNamespaceQuery,
+} from 'generated/graphql'
+import { getServiceDetailsPath } from 'routes/cdRoutesConsts'
+import { Edge, extendConnection } from 'utils/graphql'
 import { FullHeightTableWrap } from 'components/utils/layout/FullHeightTableWrap'
 import LoadingIndicator from 'components/utils/LoadingIndicator'
 import { GqlError } from 'components/utils/Alert'
@@ -9,11 +16,10 @@ import { GqlError } from 'components/utils/Alert'
 import { Body2BoldP, Body2P } from 'components/utils/typography/Text'
 
 import {
-  ColName,
-  ColRef,
-  ColRepo,
-  ColTemplated,
-} from './NamespaceDetailColumns'
+  SERVICES_QUERY_PAGE_SIZE,
+  SERVICES_REACT_VIRTUAL_OPTIONS,
+  columns,
+} from '../services/Services'
 
 export function NamespacesDetailTable({
   namespaceId,
@@ -21,23 +27,51 @@ export function NamespacesDetailTable({
   namespaceId?: string
 }) {
   const theme = useTheme()
+  const navigate = useNavigate()
   const tabStateRef = useRef<any>(null)
 
   const queryResult = useGetManagedNamespaceQuery({
     variables: {
+      first: SERVICES_QUERY_PAGE_SIZE,
       namespaceId: namespaceId || '',
     },
     fetchPolicy: 'cache-and-network',
     // Important so loading will be updated on fetchMore to send to Table
     notifyOnNetworkStatusChange: true,
   })
-  const { error, loading, data: currentData, previousData } = queryResult
+  const {
+    error,
+    fetchMore,
+    loading,
+    data: currentData,
+    previousData,
+  } = queryResult
   const data = currentData || previousData
 
   const managedNamespace = data?.managedNamespace
-  const service = managedNamespace?.service
+  const services = managedNamespace?.services?.edges
+  const pageInfo = managedNamespace?.services?.pageInfo
 
-  const columns = [ColName, ColRef, ColRepo, ColTemplated]
+  const fetchNextPage = useCallback(() => {
+    if (!pageInfo?.endCursor) {
+      return
+    }
+    fetchMore({
+      variables: { after: pageInfo.endCursor },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!prev.managedNamespace) return prev
+
+        return {
+          ...prev,
+          managedNamespace: extendConnection(
+            prev.managedNamespace,
+            fetchMoreResult.managedNamespace?.services,
+            'services'
+          ),
+        }
+      },
+    })
+  }, [fetchMore, pageInfo?.endCursor])
 
   if (error) {
     return <GqlError error={error} />
@@ -87,19 +121,41 @@ export function NamespacesDetailTable({
         stateRef={tabStateRef}
         css={{ height: '100%', overflow: 'hidden' }}
       >
-        <FullHeightTableWrap>
-          <Table
-            virtualizeRows
-            data={[service] || []}
-            columns={columns}
-            css={{
-              maxHeight: 'unset',
-              height: '100%',
-            }}
-            isFetchingNextPage={loading}
-            reactTableOptions={{ meta: { refetch: () => null } }}
-          />
-        </FullHeightTableWrap>
+        {!data ? (
+          <LoadingIndicator />
+        ) : services?.length ? (
+          <FullHeightTableWrap>
+            <Table
+              virtualizeRows
+              data={services || []}
+              columns={columns}
+              css={{
+                maxHeight: 'unset',
+                height: '100%',
+              }}
+              onRowClick={(
+                _e,
+                { original }: Row<Edge<ServiceDeploymentsRowFragment>>
+              ) =>
+                navigate(
+                  getServiceDetailsPath({
+                    clusterId: original.node?.cluster?.id,
+                    serviceId: original.node?.id,
+                  })
+                )
+              }
+              hasNextPage={pageInfo?.hasNextPage}
+              fetchNextPage={fetchNextPage}
+              isFetchingNextPage={loading}
+              reactTableOptions={{ meta: { refetch: () => null } }}
+              reactVirtualOptions={SERVICES_REACT_VIRTUAL_OPTIONS}
+            />
+          </FullHeightTableWrap>
+        ) : (
+          <div css={{ height: '100%' }}>
+            <EmptyState message="Looks like you don't have any service deployments yet." />
+          </div>
+        )}
       </TabPanel>
     </div>
   )
