b21cec0ed421b1078aedb2f7190f4caf42d64289
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-04-19 14:31:07 +0200

    generate mock client

--- controller/internal/test/mocks/ConsoleClient_mock.go
+++ controller/internal/test/mocks/ConsoleClient_mock.go
@@ -1272,6 +1272,53 @@ func (_c *ConsoleClientMock_DeleteService_Call) RunAndReturn(run func(string) er
 	return _c
 }
 
+// DeleteStack provides a mock function with given fields: ctx, id
+func (_m *ConsoleClientMock) DeleteStack(ctx context.Context, id string) error {
+	ret := _m.Called(ctx, id)
+
+	if len(ret) == 0 {
+		panic("no return value specified for DeleteStack")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, string) error); ok {
+		r0 = rf(ctx, id)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// ConsoleClientMock_DeleteStack_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'DeleteStack'
+type ConsoleClientMock_DeleteStack_Call struct {
+	*mock.Call
+}
+
+// DeleteStack is a helper method to define mock.On call
+//   - ctx context.Context
+//   - id string
+func (_e *ConsoleClientMock_Expecter) DeleteStack(ctx interface{}, id interface{}) *ConsoleClientMock_DeleteStack_Call {
+	return &ConsoleClientMock_DeleteStack_Call{Call: _e.mock.On("DeleteStack", ctx, id)}
+}
+
+func (_c *ConsoleClientMock_DeleteStack_Call) Run(run func(ctx context.Context, id string)) *ConsoleClientMock_DeleteStack_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(string))
+	})
+	return _c
+}
+
+func (_c *ConsoleClientMock_DeleteStack_Call) Return(_a0 error) *ConsoleClientMock_DeleteStack_Call {
+	_c.Call.Return(_a0)
+	return _c
+}
+
+func (_c *ConsoleClientMock_DeleteStack_Call) RunAndReturn(run func(context.Context, string) error) *ConsoleClientMock_DeleteStack_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // GetCluster provides a mock function with given fields: id
 func (_m *ConsoleClientMock) GetCluster(id *string) (*gqlclient.ClusterFragment, error) {
 	ret := _m.Called(id)
@@ -2617,6 +2664,65 @@ func (_c *ConsoleClientMock_GetServices_Call) Return(_a0 []*gqlclient.ServiceDep
 }
 
 func (_c *ConsoleClientMock_GetServices_Call) RunAndReturn(run func() ([]*gqlclient.ServiceDeploymentBaseFragment, error)) *ConsoleClientMock_GetServices_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// GetStack provides a mock function with given fields: ctx, id
+func (_m *ConsoleClientMock) GetStack(ctx context.Context, id string) (*gqlclient.InfrastructureStackFragment, error) {
+	ret := _m.Called(ctx, id)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetStack")
+	}
+
+	var r0 *gqlclient.InfrastructureStackFragment
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, string) (*gqlclient.InfrastructureStackFragment, error)); ok {
+		return rf(ctx, id)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, string) *gqlclient.InfrastructureStackFragment); ok {
+		r0 = rf(ctx, id)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*gqlclient.InfrastructureStackFragment)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, id)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// ConsoleClientMock_GetStack_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetStack'
+type ConsoleClientMock_GetStack_Call struct {
+	*mock.Call
+}
+
+// GetStack is a helper method to define mock.On call
+//   - ctx context.Context
+//   - id string
+func (_e *ConsoleClientMock_Expecter) GetStack(ctx interface{}, id interface{}) *ConsoleClientMock_GetStack_Call {
+	return &ConsoleClientMock_GetStack_Call{Call: _e.mock.On("GetStack", ctx, id)}
+}
+
+func (_c *ConsoleClientMock_GetStack_Call) Run(run func(ctx context.Context, id string)) *ConsoleClientMock_GetStack_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(string))
+	})
+	return _c
+}
+
+func (_c *ConsoleClientMock_GetStack_Call) Return(_a0 *gqlclient.InfrastructureStackFragment, _a1 error) *ConsoleClientMock_GetStack_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *ConsoleClientMock_GetStack_Call) RunAndReturn(run func(context.Context, string) (*gqlclient.InfrastructureStackFragment, error)) *ConsoleClientMock_GetStack_Call {
 	_c.Call.Return(run)
 	return _c
 }
