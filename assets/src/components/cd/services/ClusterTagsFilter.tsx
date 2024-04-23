ccbadb8b91fd863e937d2819fb28a8de20ca8413
Renê Boaventura <reneboaventuraneto@gmail.com>
2024-04-23 14:37:40 -0300

    use design system tag multi select in cluster filter (#881)

--- assets/src/components/cd/services/ClusterTagsFilter.tsx
+++ assets/src/components/cd/services/ClusterTagsFilter.tsx
@@ -1,49 +1,24 @@
 import { type ComponentProps, type Key, useMemo, useState } from 'react'
-import {
-  Chip,
-  ComboBox,
-  ListBoxFooter,
-  ListBoxItem,
-  TagIcon,
-  TextSwitch,
-  WarningIcon,
-} from '@pluralsh/design-system'
-import { useTheme } from 'styled-components'
-import isEmpty from 'lodash/isEmpty'
+import { TagMultiSelect } from '@pluralsh/design-system'
 import uniqWith from 'lodash/uniqWith'
 import isEqual from 'lodash/isEqual'
 import { Conjunction, useTagPairsQuery } from 'generated/graphql'
 import { useThrottle } from 'components/hooks/useThrottle'
 import { tagToKey } from 'utils/clusterTags'
 
-const tagOps: { value: Conjunction; label: string }[] = [
-  {
-    value: Conjunction.Or,
-    label: 'Any',
-  },
-  {
-    value: Conjunction.And,
-    label: 'All',
-  },
-]
-
 export function ClusterTagsFilter({
   selectedTagKeys,
   setSelectedTagKeys,
   searchOp,
   setSearchOp,
-  ...props
 }: {
   selectedTagKeys: Set<Key>
   setSelectedTagKeys: (keys: Set<Key>) => void
   searchOp: Conjunction
-  setSearchOp: (op: Conjunction) => void
-} & Omit<ComponentProps<typeof ComboBox>, 'children'>) {
-  const theme = useTheme()
-  const selectedTagArr = useMemo(() => [...selectedTagKeys], [selectedTagKeys])
+  setSearchOp: ComponentProps<typeof TagMultiSelect>['onChangeMatchType']
+}) {
   const [inputValue, setInputValue] = useState('')
   const throttledInputValue = useThrottle(inputValue, 100)
-  const [isOpen, setIsOpen] = useState(false)
 
   const {
     data: currentData,
@@ -70,89 +45,14 @@ export function ClusterTagsFilter({
     [data?.tagPairs?.edges, selectedTagKeys]
   )
 
-  const onSelectionChange: ComponentProps<
-    typeof ComboBox
-  >['onSelectionChange'] = (key) => {
-    if (key) {
-      setSelectedTagKeys(new Set([...selectedTagArr, key]))
-      setInputValue('')
-    }
-  }
-
-  const onInputChange: ComponentProps<typeof ComboBox>['onInputChange'] = (
-    value
-  ) => {
-    setInputValue(value)
-  }
-
   return (
-    <ComboBox
-      isOpen={isOpen}
-      inputValue={inputValue}
-      onSelectionChange={onSelectionChange}
-      onInputChange={onInputChange}
-      inputContent={
-        selectedTagArr.length > -1 && (
-          <TextSwitch
-            onClick={(e: MouseEvent) => e.stopPropagation()}
-            size="small"
-            value={searchOp}
-            onChange={setSearchOp as any}
-            options={tagOps}
-            css={{ marginRight: theme.spacing.xxsmall }}
-            label="Match"
-            labelPosition="start"
-          />
-        )
-      }
-      chips={selectedTagArr.map((key) => ({
-        key,
-        children: key,
-      }))}
-      onDeleteChip={(chipKey) => {
-        const newKeys = new Set(selectedTagKeys)
-
-        newKeys.delete(chipKey)
-        setSelectedTagKeys(newKeys)
-        // setSelectedTagKeys(new Set())
-      }}
-      inputProps={{
-        placeholder: 'Filter by tag',
-      }}
-      onOpenChange={(isOpen, _trigger) => {
-        setIsOpen(isOpen)
-      }}
-      dropdownFooterFixed={
-        isEmpty(searchResults) ? (
-          <ListBoxFooter
-            leftContent={<WarningIcon color={theme.colors['icon-warning']} />}
-          >
-            No tags found
-          </ListBoxFooter>
-        ) : null
-      }
-      maxHeight={232}
-      allowsEmptyCollection
+    <TagMultiSelect
+      onSelectedTagsChange={setSelectedTagKeys}
+      onFilterChange={setInputValue}
       loading={loading}
-      startIcon={<TagIcon />}
-      showArrow
-      {...props}
-    >
-      {searchResults?.flatMap((tagStr) => (
-        <ListBoxItem
-          key={tagStr}
-          label={
-            <Chip
-              size="small"
-              label={tagStr}
-              textValue={tagStr}
-            >
-              {tagStr}
-            </Chip>
-          }
-          textValue={tagStr}
-        />
-      ))}
-    </ComboBox>
+      options={searchResults}
+      selectedMatchType={searchOp}
+      onChangeMatchType={setSearchOp}
+    />
   )
 }
