2f2379ad819f990a599ad16f12f165f46a3d5c77
Renê Boaventura <reneboaventuraneto@gmail.com>
2024-04-25 15:26:22 -0300

    add container and title

--- assets/src/components/policies/Policies.tsx
+++ assets/src/components/policies/Policies.tsx
@@ -1,14 +1,27 @@
 import { Breadcrumb, useSetBreadcrumbs } from '@pluralsh/design-system'
+import { Title1H1 } from 'components/utils/typography/Text'
 import { POLICIES_REL_PATH } from 'routes/policiesRoutesConsts'
+import { useTheme } from 'styled-components'
 
 const breadcrumbs: Breadcrumb[] = [
   { label: `${POLICIES_REL_PATH}`, url: `/${POLICIES_REL_PATH}` },
 ]
 
 function Policies() {
+  const theme = useTheme()
+
   useSetBreadcrumbs(breadcrumbs)
 
-  return <div>policies list</div>
+  return (
+    <div
+      css={{
+        padding: theme.spacing.large,
+        backgroundColor: theme.colors['fill-zero'],
+      }}
+    >
+      <Title1H1>Policies</Title1H1>
+    </div>
+  )
 }
 
 export default Policies
