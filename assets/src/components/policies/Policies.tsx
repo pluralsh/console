54d0b897e72d19f2d2d5fc3fe2c1d2225af7390b
Renê Boaventura <reneboaventuraneto@gmail.com>
2024-04-25 14:33:36 -0300

    add breadcrums to policies

--- assets/src/components/policies/Policies.tsx
+++ assets/src/components/policies/Policies.tsx
@@ -1,5 +1,14 @@
+import { Breadcrumb, useSetBreadcrumbs } from '@pluralsh/design-system'
+import { POLICIES_REL_PATH } from 'routes/policiesRoutesConsts'
+
+const breadcrumbs: Breadcrumb[] = [
+  { label: `${POLICIES_REL_PATH}`, url: `/${POLICIES_REL_PATH}` },
+]
+
 function Policies() {
-  return 'hello world'
+  useSetBreadcrumbs(breadcrumbs)
+
+  return <div>policies list</div>
 }
 
 export default Policies
