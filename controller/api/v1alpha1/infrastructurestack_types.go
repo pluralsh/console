866fb4f21c30e63c752680e11179ec6092a7e085
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-04-25 21:20:10 +0200

    feat: add infrastructure stack (#861)

---
+++ controller/api/v1alpha1/infrastructurestack_types.go
@@ -0,0 +1,128 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	console "github.com/pluralsh/console-client-go"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// InfrastructureStackSpec defines the desired state of InfrastructureStack
+type InfrastructureStackSpec struct {
+	// Name of this Stack. If not provided InfrastructureStack's own name from InfrastructureStack.ObjectMeta will be used.
+	// +kubebuilder:validation:Optional
+	Name *string `json:"name,omitempty"`
+
+	// Type specifies the tool to use to apply it
+	// +kubebuilder:validation:Enum=TERRAFORM;ANSIBLE
+	Type console.StackType `json:"type"`
+
+	// RepositoryRef to source IaC from
+	RepositoryRef corev1.ObjectReference `json:"repositoryRef"`
+
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="Cluster is immutable"
+	ClusterRef corev1.ObjectReference `json:"clusterRef"`
+
+	// Git reference w/in the repository where the IaC lives
+	Git GitRef `json:"git"`
+
+	// JobSpec optional k8s job configuration for the job that will apply this stack
+	// +kubebuilder:validation:Optional
+	JobSpec *batchv1.JobSpec `json:"jobSpec,omitempty"`
+
+	// Configuration version/image config for the tool you're using
+	Configuration StackConfiguration `json:"configuration"`
+
+	// Approval whether to require approval
+	// +kubebuilder:validation:Optional
+	Approval *bool `json:"approval,omitempty"`
+
+	// Bindings contain read and write policies of this cluster
+	// +kubebuilder:validation:Optional
+	Bindings *Bindings `json:"bindings,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	Environment []StackEnvironment `json:"environment,omitempty"`
+
+	// Files reference to ConfigMap with a key as a path and value as a content
+	// +kubebuilder:validation:Optional
+	Files *corev1.LocalObjectReference `json:"files,omitempty"`
+
+	// Detach determined if user want to delete or detach stack
+	Detach bool `json:"detach"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// InfrastructureStack is the Schema for the infrastructurestacks API
+type InfrastructureStack struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   InfrastructureStackSpec `json:"spec,omitempty"`
+	Status Status                  `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// InfrastructureStackList contains a list of InfrastructureStack
+type InfrastructureStackList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []InfrastructureStack `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&InfrastructureStack{}, &InfrastructureStackList{})
+}
+
+type StackFile struct {
+	Path    string `json:"path"`
+	Content string `json:"content"`
+}
+
+type StackConfiguration struct {
+	// Image optional custom image you might want to use
+	// +kubebuilder:validation:Optional
+	Image *string `json:"image,omitempty"`
+	// Version the semver of the tool you wish to use
+	Version string `json:"version"`
+}
+
+type StackEnvironment struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+	// +kubebuilder:validation:Optional
+	Secret *bool `json:"secret,omitempty"`
+}
+
+func (p *InfrastructureStack) StackName() string {
+	if p.Spec.Name != nil && len(*p.Spec.Name) > 0 {
+		return *p.Spec.Name
+	}
+
+	return p.Name
+}
+
+func (p *InfrastructureStack) SetCondition(condition metav1.Condition) {
+	meta.SetStatusCondition(&p.Status.Conditions, condition)
+}
---
+++ controller/internal/client/stack.go
@@ -0,0 +1,66 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	console "github.com/pluralsh/console-client-go"
+	internalerror "github.com/pluralsh/console/controller/internal/errors"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+func (c *client) GetStack(ctx context.Context, id string) (*console.InfrastructureStackFragment, error) {
+	response, err := c.consoleClient.GetInfrastructureStack(ctx, id)
+	if internalerror.IsNotFound(err) {
+		return nil, errors.NewNotFound(schema.GroupResource{}, id)
+	}
+	if err == nil && (response == nil || response.InfrastructureStack == nil) {
+		return nil, errors.NewNotFound(schema.GroupResource{}, id)
+	}
+	if response == nil {
+		return nil, err
+	}
+
+	return response.InfrastructureStack, err
+}
+
+func (c *client) DeleteStack(ctx context.Context, id string) error {
+	_, err := c.consoleClient.DeleteStack(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *client) DetachStack(ctx context.Context, id string) error {
+	_, err := c.consoleClient.DetachStack(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *client) CreateStack(ctx context.Context, attributes console.StackAttributes) (*console.InfrastructureStackFragment, error) {
+	result, err := c.consoleClient.CreateStack(ctx, attributes)
+	if err != nil {
+		return nil, err
+	}
+	if result == nil {
+		return nil, fmt.Errorf("new created stack %s is nil", attributes.Name)
+	}
+	return result.CreateStack, nil
+
+}
+
+func (c *client) UpdateStack(ctx context.Context, id string, attributes console.StackAttributes) (*console.InfrastructureStackFragment, error) {
+	result, err := c.consoleClient.UpdateStack(ctx, id, attributes)
+	if err != nil {
+		return nil, err
+	}
+	if result == nil {
+		return nil, fmt.Errorf("updated stack %s is nil", attributes.Name)
+	}
+	return result.UpdateStack, nil
+
+}
