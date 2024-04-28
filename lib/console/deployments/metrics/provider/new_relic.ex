6425cb4c86b70fa0eab18e3f7b94245d83d088a4
michaeljguarino <mguarino46@gmail.com>
2024-04-28 13:30:06 -0400

    Implement newrelic poller (#894)

---
+++ lib/console/deployments/metrics/provider/new_relic.ex
@@ -0,0 +1,43 @@
+defmodule Console.Deployments.Metrics.Provider.NewRelic do
+  @behaviour Console.Deployments.Metrics.Provider
+  alias Console.Schema.{ObservableMetric, ObservabilityProvider}
+
+  @url "https://api.newrelic.com/graphql"
+
+  @gql """
+    query Workload($guid: String!) {
+      actor {
+        entity(guid: $guid) {
+          ... on WorkloadEntity {
+            guid
+            workloadStatus {
+              statusValue
+            }
+          }
+        }
+      }
+    }
+  """
+
+  def query(%ObservableMetric{identifier: name, provider: provider}) do
+    case gql(provider, name) do
+      {:ok, %{body: %{"data" => %{"actor" => %{"entity" => %{"workloadStatus" => %{"statusValue" => status}}}}}}} ->
+        result(status, name)
+      {:ok, %{body: body}} -> {:error, "failed to query newrelic: #{inspect(body)}"}
+      _ -> {:error, "newrelic request failed"}
+    end
+  end
+
+  defp gql(%ObservabilityProvider{credentials: %{newrelic: %{api_key: api_key}}}, name) do
+    Req.new(base_url: @url)
+    |> Req.merge(Console.conf(__MODULE__))
+    |> Req.Request.put_header("Api-Key", api_key)
+    |> AbsintheClient.attach()
+    |> Req.post(graphql: {@gql, %{guid: name}})
+  end
+  defp gql(_, _), do: {:error, "invalid newrelic credentials"}
+
+  defp result("DISRUPTED", name), do: {:error, "newrelic workload #{name} is disrupted"}
+  defp result("CRITICAL", name), do: {:error, "newrelic workload #{name} is critical"}
+  defp result(_, _), do: :ok
+end
