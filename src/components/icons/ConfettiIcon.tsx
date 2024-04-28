508282a109770c540aa0de529f8de79ac6a03b81
michaeljguarino <mguarino46@gmail.com>
2024-04-28 15:45:44 -0400

    feat: Add Confetti Icon (#587)

---
+++ src/components/icons/ConfettiIcon.tsx
@@ -0,0 +1,54 @@
+import createIcon from './createIcon'
+
+export default createIcon(({ size, color }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M3.55309 19.4905L8.59684 5.60613C8.64002 5.48733 8.71253 5.38136 8.80763 5.29807C8.90273 5.21479 9.01734 5.1569 9.1408 5.12977C9.26427 5.10265 9.39259 5.10717 9.51384 5.14292C9.63509 5.17867 9.74534 5.24449 9.83434 5.33426L18.6656 14.1655C18.7554 14.2545 18.8212 14.3648 18.8569 14.486C18.8927 14.6073 18.8972 14.7356 18.8701 14.859C18.843 14.9825 18.7851 15.0971 18.7018 15.1922C18.6185 15.2873 18.5125 15.3598 18.3937 15.403L4.50934 20.4468C4.37577 20.4978 4.23026 20.5091 4.0904 20.4793C3.95055 20.4495 3.82232 20.3798 3.7212 20.2787C3.62008 20.1775 3.5504 20.0493 3.52057 19.9094C3.49074 19.7696 3.50203 19.6241 3.55309 19.4905V19.4905Z"
+      stroke={color}
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M15.75 6.75C15.75 6.75 15.75 4.5 18 4.5C20.25 4.5 20.25 2.25 20.25 2.25"
+      stroke={color}
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M9.5999 18.6001L5.3999 14.4001"
+      stroke={color}
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M13.5 1.5V3.75"
+      stroke={color}
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M20.25 10.5L21.75 12"
+      stroke={color}
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M20.25 7.5L22.5 6.75"
+      stroke={color}
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M7.20007 9.4502L14.5501 16.8002"
+      stroke={color}
+      strokeWidth="1.5"
+      strokeMiterlimit="10"
+    />
+  </svg>
+))
