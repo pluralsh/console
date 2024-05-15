37c7d1463d53455124faba076c872d0dc80e4edc
Renê Boaventura <reneboaventuraneto@gmail.com>
2024-05-15 15:17:00 -0300

    add more fields to namespace sidecar

--- assets/src/components/cd/namespaces/NamespaceSidecar.tsx
+++ assets/src/components/cd/namespaces/NamespaceSidecar.tsx
@@ -1,13 +1,22 @@
-import { Sidecar, SidecarItem } from '@pluralsh/design-system'
+import {
+  AppIcon,
+  GlobeIcon,
+  Sidecar,
+  SidecarItem,
+} from '@pluralsh/design-system'
+import { getDistroProviderIconUrl } from 'components/utils/ClusterDistro'
 import { Body2P } from 'components/utils/typography/Text'
 import { GetManagedNamespaceQuery } from 'generated/graphql'
 import moment from 'moment'
+import { useTheme } from 'styled-components'
 
 function NamespaceSidecar({
   namespace,
 }: {
   namespace: GetManagedNamespaceQuery['managedNamespace']
 }) {
+  const theme = useTheme()
+
   return (
     <Sidecar
       width={200}
@@ -22,6 +31,44 @@ function NamespaceSidecar({
       {namespace?.description ? (
         <SidecarItem heading="Description">
           {namespace?.description}
+        </SidecarItem>
+      ) : null}
+      <SidecarItem heading="Distribution">
+        <div
+          css={{
+            ...theme.partials.text.body2,
+            display: 'flex',
+            alignItems: 'center',
+            gap: theme.spacing.small,
+          }}
+        >
+          <AppIcon
+            spacing="padding"
+            size="xxsmall"
+            icon={
+              namespace?.target?.distro ? undefined : <GlobeIcon size={16} />
+            }
+            url={
+              namespace?.target?.distro
+                ? getDistroProviderIconUrl({
+                    distro: namespace?.target?.distro,
+                    provider: null,
+                    mode: theme.mode,
+                  })
+                : undefined
+            }
+          />
+          {namespace?.target?.distro || 'All distribution'}
+        </div>
+      </SidecarItem>
+      {namespace?.cascade?.delete ? (
+        <SidecarItem heading="Cascade (Delete)">
+          {namespace?.cascade?.delete}
+        </SidecarItem>
+      ) : null}
+      {namespace?.cascade?.detach ? (
+        <SidecarItem heading="Cascade (Detach)">
+          {namespace?.cascade?.detach}
         </SidecarItem>
       ) : null}
       {namespace?.labels ? (
