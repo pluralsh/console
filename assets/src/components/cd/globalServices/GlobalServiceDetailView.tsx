a83c186ff372aafe770ab8e93a3e0f503fec6e9f
Renê Boaventura <reneboaventuraneto@gmail.com>
2024-05-15 13:30:54 -0300

    extract sidecar

--- assets/src/components/cd/globalServices/GlobalServiceDetailView.tsx
+++ assets/src/components/cd/globalServices/GlobalServiceDetailView.tsx
@@ -1,11 +1,7 @@
 import { ComponentProps, useCallback, useMemo } from 'react'
 import {
-  AppIcon,
   Chip,
   EmptyState,
-  GlobeIcon,
-  Sidecar,
-  SidecarItem,
   Table,
   useSetBreadcrumbs,
 } from '@pluralsh/design-system'
@@ -28,11 +24,7 @@ import { useTheme } from 'styled-components'
 
 import { extendConnection } from 'utils/graphql'
 
-import { getDistroProviderIconUrl } from 'components/utils/ClusterDistro'
-
-import { Body2P, Title1H1 } from 'components/utils/typography/Text'
-
-import moment from 'moment'
+import { Title1H1 } from 'components/utils/typography/Text'
 
 import LoadingIndicator from 'components/utils/LoadingIndicator'
 
@@ -47,6 +39,7 @@ import {
   ColTags,
 } from './GlobalServicesColumns'
 import { GlobalServiceDetailTable } from './GlobalServiceDetailTable'
+import GlobalServiceSidecar from './GlobalServiceSidecar'
 
 const authMethodToLabel = createMapperWithFallback<AuthMethod, string>(
   {
@@ -170,65 +163,7 @@ export default function GlobalServiceDetailView() {
                 fetchNextPage={fetchNextPage}
                 loading={loading}
               />
-              <Sidecar
-                width={200}
-                minWidth={200}
-              >
-                <SidecarItem heading="Last Updated">
-                  {moment(
-                    globalService?.updatedAt || globalService?.insertedAt
-                  ).format('M/D/YYYY')}
-                </SidecarItem>
-                <SidecarItem heading="Distribution">
-                  <div
-                    css={{
-                      ...theme.partials.text.body2,
-                      display: 'flex',
-                      alignItems: 'center',
-                      gap: theme.spacing.small,
-                    }}
-                  >
-                    <AppIcon
-                      spacing="padding"
-                      size="xxsmall"
-                      icon={
-                        globalService?.distro ? undefined : (
-                          <GlobeIcon size={16} />
-                        )
-                      }
-                      url={
-                        globalService?.distro
-                          ? getDistroProviderIconUrl({
-                              distro: globalService?.distro,
-                              provider: globalService?.provider?.cloud,
-                              mode: theme.mode,
-                            })
-                          : undefined
-                      }
-                    />
-                    {globalService?.distro || 'All distribution'}
-                  </div>
-                </SidecarItem>
-                {globalService?.tags?.length ? (
-                  <SidecarItem heading="Tags">
-                    <Body2P>
-                      {globalService?.tags
-                        ?.map((tag) => `${tag?.name}: ${tag?.value}`)
-                        .join(', ')}
-                    </Body2P>
-                  </SidecarItem>
-                ) : null}
-                <SidecarItem heading="ID">{globalService?.id}</SidecarItem>
-                <SidecarItem heading="Cascade (Delete)">
-                  {globalService?.cascade?.delete}
-                </SidecarItem>
-                <SidecarItem heading="Cascade (Detach)">
-                  {globalService?.cascade?.detach}
-                </SidecarItem>
-                <SidecarItem heading="Reparent">
-                  {globalService?.reparent}
-                </SidecarItem>
-              </Sidecar>
+              <GlobalServiceSidecar globalService={globalService} />
             </Flex>
           </>
         ) : (
