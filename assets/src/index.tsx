7c83eb1f1c732f6d658c3648c065cdc9a1ede793
Jake Laderman <jsladerman@gmail.com>
2024-05-16 10:40:16 -0400

    add strict mode

--- assets/src/index.tsx
+++ assets/src/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'regenerator-runtime/runtime'
 
+import { StrictMode } from 'react'
+
 import App from './App'
 
 // import * as serviceWorker from './serviceWorker'
@@ -10,7 +12,11 @@ import App from './App'
 const container = document.getElementById('root') as Element
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
 
 // TODO(floreks): Enable once fixed
 // serviceWorker.register({})
