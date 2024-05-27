1031578e20c91bc3ca1cb92a75f863afbdca398d
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-05-27 12:27:00 +0200

    init edit form

--- assets/src/components/stacks/stack/edit/StackEdit.tsx
+++ assets/src/components/stacks/stack/edit/StackEdit.tsx
@@ -1,19 +1,43 @@
-import { Card, Prop, useSetBreadcrumbs } from '@pluralsh/design-system'
-import React, { useMemo } from 'react'
-
+import {
+  Button,
+  ContentCard,
+  FormField,
+  Input,
+  useSetBreadcrumbs,
+} from '@pluralsh/design-system'
+import React, { useMemo, useState } from 'react'
 import { useOutletContext, useParams } from 'react-router-dom'
-
 import LoadingIndicator from 'components/utils/LoadingIndicator'
-
 import { useTheme } from 'styled-components'
+import { Flex, P } from 'honorable'
+import { isEmpty } from 'lodash'
 
 import { StackOutletContextT, getBreadcrumbs } from '../Stack'
 import ConsolePageTitle from '../../../utils/layout/ConsolePageTitle'
+import { useGitRepositoriesQuery } from '../../../../generated/graphql'
+import { RepositorySelector } from '../../../cd/services/deployModal/DeployServiceSettingsGit'
+import { mapExistingNodes } from '../../../../utils/graphql'
 
 export default function StackEdit() {
   const theme = useTheme()
   const { stackId = '' } = useParams()
   const { stack } = useOutletContext() as StackOutletContextT
+  const [image, setImage] = useState(stack.configuration.image)
+  const [version, setVersion] = useState(stack.configuration.version)
+  const [repositoryId, setRepositoryId] = useState(stack.repository?.id)
+  const [ref, setRef] = useState(stack.git.ref)
+  const [folder, setFolder] = useState(stack.git.folder)
+  const changed =
+    image !== stack.configuration.image ||
+    version !== stack.configuration.version ||
+    repositoryId !== stack.repository?.id ||
+    ref !== stack.git.ref ||
+    folder !== stack.git.folder
+  const valid =
+    !isEmpty(version) &&
+    !isEmpty(repositoryId) &&
+    !isEmpty(ref) &&
+    !isEmpty(folder)
 
   useSetBreadcrumbs(
     useMemo(
@@ -21,6 +45,16 @@ export default function StackEdit() {
       [stackId]
     )
   )
+
+  const { data } = useGitRepositoriesQuery()
+
+  const repos = mapExistingNodes(data?.gitRepositories).filter(
+    (repo) => repo.health === 'PULLABLE'
+  )
+
+  // const { data: repoData } = useGitRepositoryQuery({
+  //   variables: { id: repositoryId },
+  // })
 
   if (!stack) {
     return <LoadingIndicator />
@@ -35,21 +69,82 @@ export default function StackEdit() {
           paddingBottom: theme.spacing.medium,
         }}
       />
-      <Card
-        padding="large"
-        css={{
-          display: 'flex',
-          flexWrap: 'wrap',
-        }}
-      >
-        {stack.configuration.image && (
-          <Prop title="Image">{stack.configuration.image}</Prop>
-        )}
-        <Prop title="Version">{stack.configuration.version}</Prop>
-        <Prop title="Repository">{stack.repository?.url}</Prop>
-        <Prop title="Ref">{stack.git.ref}</Prop>
-        <Prop title="Folder">{stack.git.folder}</Prop>
-      </Card>
+      <ContentCard>
+        <div
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: theme.spacing.small,
+            marginBottom: theme.spacing.large,
+          }}
+        >
+          <FormField label="Image">
+            <Input
+              value={image}
+              onChange={(e) => setImage(e.currentTarget.value)}
+            />
+          </FormField>
+          <FormField
+            label="Version"
+            required
+          >
+            <Input
+              value={version}
+              onChange={(e) => setVersion(e.currentTarget.value)}
+            />
+          </FormField>
+          <FormField
+            label="Repository"
+            required
+          >
+            <RepositorySelector
+              repositories={repos}
+              repositoryId={repositoryId}
+              setRepositoryId={setRepositoryId}
+            />
+          </FormField>
+          <FormField
+            label="Ref"
+            required
+          >
+            <Input
+              value={ref}
+              onChange={(e) => setRef(e.currentTarget.value)}
+            />
+          </FormField>
+          <FormField
+            label="Folder"
+            required
+          >
+            <Input
+              value={folder}
+              onChange={(e) => setFolder(e.currentTarget.value)}
+            />
+          </FormField>
+        </div>
+        <Flex
+          align="center"
+          gap="medium"
+          justifyContent="flex-end"
+          marginTop="small"
+        >
+          {changed && (
+            <P
+              body2
+              color="text-xlight"
+            >
+              Unsaved changes
+            </P>
+          )}
+          <Button
+            disabled={!valid || !changed}
+            // onClick={() => mutation()}
+            // loading={loading}
+          >
+            Save
+          </Button>
+        </Flex>
+      </ContentCard>
     </>
   )
 }
