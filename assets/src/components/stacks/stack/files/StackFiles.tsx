337aeeada73b54d7b0583826e466865f307e6c92
Sebastian Florek <sebastian@plural.sh>
2024-05-27 15:25:04 +0200

    add files display to the stack

--- assets/src/components/stacks/stack/files/StackFiles.tsx
+++ assets/src/components/stacks/stack/files/StackFiles.tsx
@@ -1,19 +1,55 @@
-import { Card, useSetBreadcrumbs } from '@pluralsh/design-system'
-import React, { useMemo } from 'react'
-
+import {
+  Card,
+  CheckIcon,
+  CloseIcon,
+  Table,
+  useSetBreadcrumbs,
+} from '@pluralsh/design-system'
+import React, { ReactNode, useMemo } from 'react'
 import { useOutletContext, useParams } from 'react-router-dom'
-
 import LoadingIndicator from 'components/utils/LoadingIndicator'
-
 import { useTheme } from 'styled-components'
+
+import { createColumnHelper } from '@tanstack/react-table'
 
 import { StackOutletContextT, getBreadcrumbs } from '../Stack'
 import ConsolePageTitle from '../../../utils/layout/ConsolePageTitle'
+import { StackFile } from '../../../../generated/graphql'
+import OutputValue from '../run/output/Value'
+
+const columnHelper = createColumnHelper<StackFile>()
+
+const colPath = columnHelper.accessor((o) => o.path, {
+  id: 'path',
+  header: 'Path',
+  cell: ({ getValue }) => <span>{getValue()}</span>,
+})
+
+const colContent = columnHelper.accessor((o) => o, {
+  id: 'content',
+  header: 'Content',
+  cell: function Cell({ getValue }): ReactNode {
+    const output = getValue()
+
+    return (
+      <OutputValue
+        value={output.content}
+        secret={false}
+      />
+    )
+  },
+})
+
+function useColumns(): Array<object> {
+  return useMemo(() => [colPath, colContent], [])
+}
 
 export default function StackFiles() {
   const theme = useTheme()
   const { stackId = '' } = useParams()
   const { stack } = useOutletContext() as StackOutletContextT
+  const columns = useColumns()
+  const hasFiles = stack.files?.length ?? 0 > 0
 
   useSetBreadcrumbs(
     useMemo(
@@ -35,15 +71,15 @@ export default function StackFiles() {
           paddingBottom: theme.spacing.medium,
         }}
       />
-      <Card
-        padding="large"
-        css={{
-          display: 'flex',
-          flexWrap: 'wrap',
-        }}
-      >
-        ...
-      </Card>
+      {!hasFiles ? (
+        <Card padding="large">No files available for this stack.</Card>
+      ) : (
+        <Table
+          data={stack.files ?? []}
+          columns={columns}
+          maxHeight="100%"
+        />
+      )}
     </>
   )
 }
