e59bf2ac5c3670d3f6d409e7be7187e80d800a9e
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-05-29 11:43:50 +0200

    use grid in run repo card

--- assets/src/components/stacks/run/repository/Repository.tsx
+++ assets/src/components/stacks/run/repository/Repository.tsx
@@ -2,25 +2,46 @@ import { ReactNode } from 'react'
 import { useOutletContext } from 'react-router-dom'
 import { Card, Prop } from '@pluralsh/design-system'
 
+import { useTheme } from 'styled-components'
+
 import { StackRun } from '../../../../generated/graphql'
 
 export default function StackRunRepository(): ReactNode {
+  const theme = useTheme()
   const { stackRun } = useOutletContext<{ stackRun: StackRun }>()
 
   return (
     <Card
-      padding="large"
       css={{
-        display: 'flex',
-        flexWrap: 'wrap',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(180px, 1fr))',
+        gridGap: theme.spacing.large,
+        padding: theme.spacing.large,
       }}
     >
-      <Prop title="Repository">{stackRun.repository?.url}</Prop>
-      <Prop title="Ref">{stackRun.git.ref}</Prop>
-      <Prop title="Folder">{stackRun.git.folder}</Prop>
-
+      <Prop
+        title="Repository"
+        margin={0}
+      >
+        {stackRun.repository?.url}
+      </Prop>
+      <Prop
+        title="Ref"
+        margin={0}
+      >
+        {stackRun.git.ref}
+      </Prop>
+      <Prop
+        title="Folder"
+        margin={0}
+      >
+        {stackRun.git.folder}
+      </Prop>
       {stackRun.git.files && (
-        <Prop title="Files">
+        <Prop
+          title="Files"
+          margin={0}
+        >
           {stackRun.git.files.map((f) => (
             <div>{f}</div>
           ))}
