c43bd2b8756407d5f8cc35a8b773740247fba9de
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-06-04 15:47:25 +0200

    feat: Remove legacy image from login portal (#1011)

--- assets/src/components/login/LoginPortal.tsx
+++ assets/src/components/login/LoginPortal.tsx
@@ -1,71 +1,31 @@
-import { Div, Flex, Img, ImgProps } from 'honorable'
-
-import { ComponentPropsWithoutRef } from 'react'
-
 import { Footer, FooterBalancer } from './LoginFooter'
-import {
-  LOGIN_SIDEBAR_IMAGE,
-  RIGHT_CONTENT_MAX_WIDTH,
-  RIGHT_CONTENT_PAD,
-} from './constants'
-
-export type PictureProps = {
-  sources: ComponentPropsWithoutRef<'source'>[]
-} & ImgProps
-export function Picture({ sources, ...props }: PictureProps) {
-  return (
-    <picture style={{ display: 'contents' }}>
-      {sources.map((source, i) => (
-        <source
-          key={source.srcSet || i}
-          {...source}
-        />
-      ))}
-      <Img {...props} />
-    </picture>
-  )
-}
+import { RIGHT_CONTENT_MAX_WIDTH, RIGHT_CONTENT_PAD } from './constants'
 
 export function LoginPortal({ children }: any) {
   return (
-    <Flex height="100vh">
-      {/* LEFT SIDE */}
-      <Flex
-        direction="column"
-        align="center"
-        background="fill-one"
-        display-desktop-down="none"
-        overflow="hidden"
-        width={504}
-        height="100%"
+    <div
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1,
+        flexShrink: 1,
+        height: '100vh',
+        overflow: 'auto',
+        paddingLeft: RIGHT_CONTENT_PAD,
+        paddingRight: RIGHT_CONTENT_PAD,
+      }}
+    >
+      <FooterBalancer />
+      <div
+        css={{
+          margin: 'auto',
+          maxWidth: RIGHT_CONTENT_MAX_WIDTH,
+          width: '100%',
+        }}
       >
-        <Picture
-          {...LOGIN_SIDEBAR_IMAGE}
-          width="100%"
-          height="100%"
-          objectFit="cover"
-          objectPosition="top center"
-        />
-      </Flex>
-      {/* RIGHT SIDE */}
-      <Flex
-        overflow="auto"
-        flexDirection="column"
-        grow={1}
-        shrink={1}
-        paddingHorizontal={RIGHT_CONTENT_PAD}
-      >
-        <FooterBalancer />
-        <Div
-          maxWidth={RIGHT_CONTENT_MAX_WIDTH}
-          width="100%"
-          marginVertical="auto"
-          marginHorizontal="auto"
-        >
-          {children}
-        </Div>
-        <Footer />
-      </Flex>
-    </Flex>
+        {children}
+      </div>
+      <Footer />
+    </div>
   )
 }
