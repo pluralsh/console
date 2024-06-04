9c78bbda33116f47b78a39ccc5106c22887d74dd
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-06-04 11:20:31 +0200

    stop using honorable in login portal

--- assets/src/components/login/LoginPortal.tsx
+++ assets/src/components/login/LoginPortal.tsx
@@ -1,28 +1,31 @@
-import { Div, Flex } from 'honorable'
-
 import { Footer, FooterBalancer } from './LoginFooter'
 import { RIGHT_CONTENT_MAX_WIDTH, RIGHT_CONTENT_PAD } from './constants'
 
 export function LoginPortal({ children }: any) {
   return (
-    <Flex
-      height="100vh"
-      overflow="auto"
-      flexDirection="column"
-      grow={1}
-      shrink={1}
-      paddingHorizontal={RIGHT_CONTENT_PAD}
+    <div
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1,
+        flexShrink: 1,
+        height: '100vh',
+        overflow: 'auto',
+        paddingLeft: RIGHT_CONTENT_PAD,
+        paddingRight: RIGHT_CONTENT_PAD,
+      }}
     >
       <FooterBalancer />
-      <Div
-        maxWidth={RIGHT_CONTENT_MAX_WIDTH}
-        width="100%"
-        marginVertical="auto"
-        marginHorizontal="auto"
+      <div
+        css={{
+          margin: 'auto',
+          maxWidth: RIGHT_CONTENT_MAX_WIDTH,
+          width: '100%',
+        }}
       >
         {children}
-      </Div>
+      </div>
       <Footer />
-    </Flex>
+    </div>
   )
 }
