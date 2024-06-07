7c3fbce1b5f4fa380bd5f8801c086dbb50cbd07b
Lukasz Zajaczkowski <zreigz@gmail.com>
2024-06-07 14:50:38 +0200

    add DeploymentSettings CRD (#1026)

---
+++ controller/api/v1alpha1/deploymentsettings_types.go
@@ -0,0 +1,117 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	console "github.com/pluralsh/console-client-go"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// DeploymentSettingsSpec defines the desired state of DeploymentSettings
+type DeploymentSettingsSpec struct {
+	// AgentHelmValues custom helm values to apply to all agents (useful for things like adding customary annotations/labels)
+	// +kubebuilder:validation:Optional
+	AgentHelmValues *runtime.RawExtension `json:"agentHelmValues,omitempty"`
+
+	// Stacks global configuration for stack execution
+	Stacks *StackSettings `json:"stacks,omitempty"`
+
+	// Bindings
+	// +kubebuilder:validation:Optional
+	Bindings *DeploymentSettingsBindings `json:"bindings,omitempty"`
+
+	// PrometheusConnection connection details for a prometheus instance to use
+	PrometheusConnection *HTTPConnection `json:"prometheusConnection,omitempty"`
+
+	// connection details for a loki instance to use
+	LokiConnection *HTTPConnection `json:"lokiConnection,omitempty"`
+}
+
+type HTTPConnection struct {
+	Host string `json:"host"`
+	// user to connect with basic auth
+	User *string `json:"user,omitempty"`
+	// password to connect w/ for basic auth
+	Password *string `json:"password,omitempty"`
+}
+
+func (r *HTTPConnection) Attributes() *console.HTTPConnectionAttributes {
+	return &console.HTTPConnectionAttributes{
+		Host:     r.Host,
+		User:     r.User,
+		Password: r.Password,
+	}
+}
+
+type DeploymentSettingsBindings struct {
+	// Read bindings.
+	// +kubebuilder:validation:Optional
+	Read []Binding `json:"read,omitempty"`
+
+	// Write bindings.
+	// +kubebuilder:validation:Optional
+	Write []Binding `json:"write,omitempty"`
+
+	// Create bindings.
+	// +kubebuilder:validation:Optional
+	Create []Binding `json:"create,omitempty"`
+
+	// Git bindings.
+	// +kubebuilder:validation:Optional
+	Git []Binding `json:"git,omitempty"`
+}
+
+type StackSettings struct {
+	// JobSpec optional k8s job configuration for the job that will apply this stack
+	// +kubebuilder:validation:Optional
+	JobSpec *JobSpec `json:"jobSpec,omitempty"`
+	// ConnectionRef reference to ScmConnection
+	// +kubebuilder:validation:Optional
+	ConnectionRef *corev1.ObjectReference `json:"connectionRef,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// DeploymentSettings is the Schema for the deploymentsettings API
+type DeploymentSettings struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DeploymentSettingsSpec `json:"spec,omitempty"`
+	Status Status                 `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// DeploymentSettingsList contains a list of DeploymentSettings
+type DeploymentSettingsList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DeploymentSettings `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&DeploymentSettings{}, &DeploymentSettingsList{})
+}
+
+func (p *DeploymentSettings) SetCondition(condition metav1.Condition) {
+	meta.SetStatusCondition(&p.Status.Conditions, condition)
+}
---
+++ controller/internal/controller/deploymentsettings_controller.go
@@ -0,0 +1,189 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	console "github.com/pluralsh/console-client-go"
+	"github.com/pluralsh/console/controller/api/v1alpha1"
+	consoleclient "github.com/pluralsh/console/controller/internal/client"
+	"github.com/pluralsh/console/controller/internal/utils"
+	"github.com/pluralsh/polly/algorithms"
+	"github.com/samber/lo"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/yaml"
+)
+
+const (
+	deploymentSettingsName      = "global"
+	deploymentSettingsNamespace = "plrl-deploy-operator"
+)
+
+// DeploymentSettingsReconciler reconciles a DeploymentSettings object
+type DeploymentSettingsReconciler struct {
+	client.Client
+	Scheme        *runtime.Scheme
+	ConsoleClient consoleclient.ConsoleClient
+}
+
+//+kubebuilder:rbac:groups=deployments.plural.sh,resources=deploymentsettings,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=deployments.plural.sh,resources=deploymentsettings/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=deployments.plural.sh,resources=deploymentsettings/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
+func (r *DeploymentSettingsReconciler) Reconcile(ctx context.Context, req ctrl.Request) (_ ctrl.Result, reterr error) {
+	logger := log.FromContext(ctx)
+
+	settings := &v1alpha1.DeploymentSettings{}
+	if err := r.Get(ctx, req.NamespacedName, settings); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// make sure there is only one CRD object with the `global` name and the agent namespace
+	if settings.Name != deploymentSettingsName || settings.Namespace != deploymentSettingsNamespace {
+		return ctrl.Result{}, nil
+	}
+
+	scope, err := NewDeploymentSettingsScope(ctx, r.Client, settings)
+	if err != nil {
+		logger.Error(err, "failed to create deployment settings scope")
+		utils.MarkCondition(settings.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionFalse, v1alpha1.SynchronizedConditionReasonError, err.Error())
+		return ctrl.Result{}, err
+	}
+	// Always patch object when exiting this function, so we can persist any object changes.
+	defer func() {
+		if err := scope.PatchObject(); err != nil && reterr == nil {
+			reterr = err
+		}
+	}()
+	if !settings.GetDeletionTimestamp().IsZero() {
+		logger.Info("deployment settings CRD deleted successfully")
+		return ctrl.Result{}, nil
+	}
+
+	ds, err := r.ConsoleClient.GetDeploymentSettings(ctx)
+	if err != nil {
+		utils.MarkCondition(settings.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionFalse, v1alpha1.SynchronizedConditionReasonError, err.Error())
+		return ctrl.Result{}, err
+	}
+
+	sha, err := utils.HashObject(settings.Spec)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if !settings.Status.IsSHAEqual(sha) {
+		logger.Info("upsert deployment settings", "name", settings.Name)
+		attr, err := r.genDeploymentSettingsAttr(ctx, settings)
+		if err != nil {
+			utils.MarkCondition(settings.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionFalse, v1alpha1.SynchronizedConditionReasonError, err.Error())
+			return ctrl.Result{}, err
+		}
+		_, err = r.ConsoleClient.UpdateDeploymentSettings(ctx, *attr)
+		if err != nil {
+			utils.MarkCondition(settings.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionFalse, v1alpha1.SynchronizedConditionReasonError, err.Error())
+			return ctrl.Result{}, err
+		}
+		settings.Status.SHA = lo.ToPtr(sha)
+	}
+	settings.Status.ID = lo.ToPtr(ds.ID)
+	utils.MarkCondition(settings.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionTrue, v1alpha1.SynchronizedConditionReason, "")
+	utils.MarkCondition(settings.SetCondition, v1alpha1.ReadyConditionType, v1.ConditionTrue, v1alpha1.ReadyConditionReason, "")
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *DeploymentSettingsReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&v1alpha1.DeploymentSettings{}).
+		Complete(r)
+}
+
+func (r *DeploymentSettingsReconciler) genDeploymentSettingsAttr(ctx context.Context, settings *v1alpha1.DeploymentSettings) (*console.DeploymentSettingsAttributes, error) {
+	attr := &console.DeploymentSettingsAttributes{}
+	if settings.Spec.AgentHelmValues != nil {
+		var obj runtime.Object
+		if err := runtime.Convert_runtime_RawExtension_To_runtime_Object(settings.Spec.AgentHelmValues, &obj, nil); err != nil {
+			return nil, err
+		}
+		rawHelmValues, err := yaml.Marshal(obj)
+		if err != nil {
+			return nil, err
+		}
+		attr.AgentHelmValues = lo.ToPtr(string(rawHelmValues))
+	}
+	if settings.Spec.PrometheusConnection != nil {
+		attr.PrometheusConnection = settings.Spec.PrometheusConnection.Attributes()
+	}
+	if settings.Spec.LokiConnection != nil {
+		attr.LokiConnection = settings.Spec.LokiConnection.Attributes()
+	}
+	if settings.Spec.Stacks != nil {
+		var jobSpec *console.GateJobAttributes
+		var err error
+		var connectionID *string
+		if settings.Spec.Stacks.JobSpec != nil {
+			jobSpec, err = gateJobAttributes(settings.Spec.Stacks.JobSpec)
+			if err != nil {
+				return nil, err
+			}
+		}
+		if settings.Spec.Stacks.ConnectionRef != nil {
+			connection := &v1alpha1.ScmConnection{}
+			if err := r.Get(ctx, types.NamespacedName{Name: settings.Spec.Stacks.ConnectionRef.Name, Namespace: settings.Spec.Stacks.ConnectionRef.Namespace}, connection); err != nil {
+				return nil, err
+			}
+			connectionID = connection.Status.ID
+		}
+		attr.Stacks = &console.StackSettingsAttributes{
+			JobSpec:      jobSpec,
+			ConnectionID: connectionID,
+		}
+	}
+	if settings.Spec.Bindings != nil {
+		if settings.Spec.Bindings.Read != nil {
+			attr.ReadBindings = make([]*console.PolicyBindingAttributes, 0)
+			attr.ReadBindings = algorithms.Map(settings.Spec.Bindings.Read,
+				func(b v1alpha1.Binding) *console.PolicyBindingAttributes { return b.Attributes() })
+		}
+		if settings.Spec.Bindings.Write != nil {
+			attr.WriteBindings = make([]*console.PolicyBindingAttributes, 0)
+			attr.WriteBindings = algorithms.Map(settings.Spec.Bindings.Write,
+				func(b v1alpha1.Binding) *console.PolicyBindingAttributes { return b.Attributes() })
+		}
+		if settings.Spec.Bindings.Create != nil {
+			attr.CreateBindings = make([]*console.PolicyBindingAttributes, 0)
+			attr.CreateBindings = algorithms.Map(settings.Spec.Bindings.Create,
+				func(b v1alpha1.Binding) *console.PolicyBindingAttributes { return b.Attributes() })
+		}
+		if settings.Spec.Bindings.Git != nil {
+			attr.GitBindings = make([]*console.PolicyBindingAttributes, 0)
+			attr.GitBindings = algorithms.Map(settings.Spec.Bindings.Git,
+				func(b v1alpha1.Binding) *console.PolicyBindingAttributes { return b.Attributes() })
+		}
+	}
+	return attr, nil
+}
