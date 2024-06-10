67514236fee8c5ea1d9c4ab227f7926e61e224b6
Sebastian Florek <sebastian@plural.sh>
2024-06-10 14:16:13 +0200

    add more unit tests

--- controller/internal/controller/scmconnection_controller_test.go
+++ controller/internal/controller/scmconnection_controller_test.go
@@ -23,10 +23,13 @@ import (
 var _ = Describe("SCM Connection Controller", Ordered, func() {
 	Context("When reconciling a resource", func() {
 		const (
-			scmName   = "scm-connection-test"
-			scmType   = gqlclient.ScmTypeGithub
-			namespace = "default"
-			sha       = "SJQ6GH4SZX7YCR7PM726XIBLSLH5TP6Q33HX4OMUARQGCULTOXTA===="
+			scmName         = "scm-connection-test"
+			scmType         = gqlclient.ScmTypeGithub
+			namespace       = "default"
+			id              = "123"
+			sha             = "SJQ6GH4SZX7YCR7PM726XIBLSLH5TP6Q33HX4OMUARQGCULTOXTA===="
+			readonlyScmName = "readonly-scm-connection"
+			readonlyScmID   = "readonly"
 		)
 
 		ctx := context.Background()
@@ -34,6 +37,11 @@ var _ = Describe("SCM Connection Controller", Ordered, func() {
 		typeNamespacedName := types.NamespacedName{
 			Name:      scmName,
 			Namespace: namespace,
+		}
+
+		readonlyTypeNamespacedName := types.NamespacedName{
+			Namespace: namespace,
+			Name:      readonlyScmName,
 		}
 
 		BeforeAll(func() {
@@ -53,6 +61,23 @@ var _ = Describe("SCM Connection Controller", Ordered, func() {
 				},
 			}
 			Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+
+			By("creating readonly custom resource for the Kind ScmConnection")
+			scm = &v1alpha1.ScmConnection{}
+			if err := k8sClient.Get(ctx, readonlyTypeNamespacedName, scm); err == nil {
+				Expect(k8sClient.Delete(ctx, scm)).To(Succeed())
+			}
+			resource = &v1alpha1.ScmConnection{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      readonlyScmName,
+					Namespace: namespace,
+				},
+				Spec: v1alpha1.ScmConnectionSpec{
+					Name: readonlyScmName,
+					Type: scmType,
+				},
+			}
+			Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 		})
 
 		AfterAll(func() {
@@ -63,14 +88,14 @@ var _ = Describe("SCM Connection Controller", Ordered, func() {
 			}
 		})
 
-		It("should successfully reconcile the resource", func() {
+		It("should successfully reconcile the resource on create", func() {
 			By("Create resource")
 			test := struct {
 				returnGetScmConnection *gqlclient.ScmConnectionFragment
 				expectedStatus         v1alpha1.Status
 			}{
 				expectedStatus: v1alpha1.Status{
-					ID:  lo.ToPtr("123"),
+					ID:  lo.ToPtr(id),
 					SHA: lo.ToPtr(sha),
 					Conditions: []metav1.Condition{
 						{
@@ -93,7 +118,7 @@ var _ = Describe("SCM Connection Controller", Ordered, func() {
 					},
 				},
 				returnGetScmConnection: &gqlclient.ScmConnectionFragment{
-					ID: "123",
+					ID: id,
 				},
 			}
 
@@ -120,166 +145,164 @@ var _ = Describe("SCM Connection Controller", Ordered, func() {
 			Expect(common.SanitizeStatusConditions(scm.Status)).To(Equal(common.SanitizeStatusConditions(test.expectedStatus)))
 		})
 
-		//	It("should successfully reconcile the resource", func() {
-		//		By("Update resource")
-		//		test := struct {
-		//			returnGetService *gqlclient.ServiceDeploymentExtended
-		//			expectedStatus   v1alpha1.ServiceStatus
-		//		}{
-		//			expectedStatus: v1alpha1.ServiceStatus{
-		//				Status: v1alpha1.Status{
-		//					ID:  lo.ToPtr("123"),
-		//					SHA: lo.ToPtr(sha),
-		//					Conditions: []metav1.Condition{
-		//						{
-		//							Type:    v1alpha1.ReadyConditionType.String(),
-		//							Status:  metav1.ConditionFalse,
-		//							Reason:  v1alpha1.ReadyConditionReason.String(),
-		//							Message: "The service components are not ready yet",
-		//						},
-		//						{
-		//							Type:   v1alpha1.SynchronizedConditionType.String(),
-		//							Status: metav1.ConditionTrue,
-		//							Reason: v1alpha1.SynchronizedConditionReason.String(),
-		//						},
-		//					},
-		//				},
-		//			},
-		//			returnGetService: &gqlclient.ServiceDeploymentExtended{
-		//				ID: "123",
-		//			},
-		//		}
-		//
-		//		Expect(common.MaybePatch(k8sClient, &v1alpha1.ServiceDeployment{
-		//			ObjectMeta: metav1.ObjectMeta{Name: scmName, Namespace: namespace},
-		//		}, func(p *v1alpha1.ServiceDeployment) {
-		//			p.Status.ID = lo.ToPtr(id)
-		//			p.Status.SHA = lo.ToPtr("ABC")
-		//		})).To(Succeed())
-		//
-		//		fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
-		//		fakeConsoleClient.On("GetService", mock.Anything, mock.Anything).Return(test.returnGetService, nil)
-		//		fakeConsoleClient.On("UpdateService", mock.Anything, mock.Anything).Return(nil)
-		//		serviceReconciler := &controller.ServiceReconciler{
-		//			Client:        k8sClient,
-		//			Scheme:        k8sClient.Scheme(),
-		//			ConsoleClient: fakeConsoleClient,
-		//		}
-		//
-		//		_, err := serviceReconciler.Reconcile(ctx, reconcile.Request{
-		//			NamespacedName: typeNamespacedName,
-		//		})
-		//
-		//		Expect(err).NotTo(HaveOccurred())
-		//
-		//		service := &v1alpha1.ServiceDeployment{}
-		//		err = k8sClient.Get(ctx, typeNamespacedName, service)
-		//
-		//		Expect(err).NotTo(HaveOccurred())
-		//		Expect(sanitizeServiceConditions(service.Status)).To(Equal(sanitizeServiceConditions(test.expectedStatus)))
-		//	})
-		//
-		//	It("should successfully reconcile the resource", func() {
-		//		By("Update resource with the dependencies")
-		//		test := struct {
-		//			returnGetService *gqlclient.ServiceDeploymentExtended
-		//			expectedStatus   v1alpha1.ServiceStatus
-		//		}{
-		//			expectedStatus: v1alpha1.ServiceStatus{
-		//				Status: v1alpha1.Status{
-		//					ID:  lo.ToPtr(id),
-		//					SHA: lo.ToPtr("HNVNOPAXHYMV5XQMPCT3ILWU4LQKJFEFF5EF5SDVNZRDLSP7E6DQ===="),
-		//					Conditions: []metav1.Condition{
-		//						{
-		//							Type:    v1alpha1.ReadyConditionType.String(),
-		//							Status:  metav1.ConditionFalse,
-		//							Reason:  v1alpha1.ReadyConditionReason.String(),
-		//							Message: "The service components are not ready yet",
-		//						},
-		//						{
-		//							Type:   v1alpha1.SynchronizedConditionType.String(),
-		//							Status: metav1.ConditionTrue,
-		//							Reason: v1alpha1.SynchronizedConditionReason.String(),
-		//						},
-		//					},
-		//				},
-		//			},
-		//			returnGetService: &gqlclient.ServiceDeploymentExtended{
-		//				ID: "123",
-		//			},
-		//		}
-		//		dep1 := "dep-1"
-		//		dep2 := "dep-2"
-		//		createService(ctx, namespace, dep1, clusterName, repoName)
-		//		createService(ctx, namespace, dep2, clusterName, repoName)
-		//		Expect(common.MaybePatch(k8sClient, &v1alpha1.ServiceDeployment{
-		//			ObjectMeta: metav1.ObjectMeta{Name: scmName, Namespace: namespace},
-		//		}, func(p *v1alpha1.ServiceDeployment) {
-		//			p.Status.ID = lo.ToPtr(id)
-		//			p.Status.SHA = lo.ToPtr(sha)
-		//		})).To(Succeed())
-		//		Expect(common.MaybePatchObject(k8sClient, &v1alpha1.ServiceDeployment{
-		//			ObjectMeta: metav1.ObjectMeta{Name: scmName, Namespace: namespace},
-		//		}, func(p *v1alpha1.ServiceDeployment) {
-		//			p.Spec.Dependencies = []v1alpha1.ServiceDependency{
-		//				{
-		//					Name: dep1,
-		//				},
-		//				{
-		//					Name: dep2,
-		//				},
-		//			}
-		//		})).To(Succeed())
-		//
-		//		fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
-		//		fakeConsoleClient.On("GetService", mock.Anything, mock.Anything).Return(test.returnGetService, nil)
-		//		fakeConsoleClient.On("UpdateService", mock.Anything, mock.Anything).Return(nil)
-		//		serviceReconciler := &controller.ServiceReconciler{
-		//			Client:        k8sClient,
-		//			Scheme:        k8sClient.Scheme(),
-		//			ConsoleClient: fakeConsoleClient,
-		//		}
-		//
-		//		_, err := serviceReconciler.Reconcile(ctx, reconcile.Request{
-		//			NamespacedName: typeNamespacedName,
-		//		})
-		//
-		//		Expect(err).NotTo(HaveOccurred())
-		//
-		//		service := &v1alpha1.ServiceDeployment{}
-		//		err = k8sClient.Get(ctx, typeNamespacedName, service)
-		//
-		//		Expect(err).NotTo(HaveOccurred())
-		//		Expect(sanitizeServiceConditions(service.Status)).To(Equal(sanitizeServiceConditions(test.expectedStatus)))
-		//	})
-		//
-		//	It("should successfully reconcile the resource", func() {
-		//		By("Delete resource")
-		//		resource := &v1alpha1.ServiceDeployment{}
-		//		err := k8sClient.Get(ctx, typeNamespacedName, resource)
-		//		Expect(err).NotTo(HaveOccurred())
-		//		err = k8sClient.Delete(ctx, resource)
-		//		Expect(err).NotTo(HaveOccurred())
-		//
-		//		fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
-		//		fakeConsoleClient.On("GetService", mock.Anything, mock.Anything).Return(nil, nil).Once()
-		//		serviceReconciler := &controller.ServiceReconciler{
-		//			Client:        k8sClient,
-		//			Scheme:        k8sClient.Scheme(),
-		//			ConsoleClient: fakeConsoleClient,
-		//		}
-		//
-		//		_, err = serviceReconciler.Reconcile(ctx, reconcile.Request{
-		//			NamespacedName: typeNamespacedName,
-		//		})
-		//
-		//		Expect(err).NotTo(HaveOccurred())
-		//
-		//		service := &v1alpha1.ServiceDeployment{}
-		//		err = k8sClient.Get(ctx, typeNamespacedName, service)
-		//		Expect(err).To(HaveOccurred())
-		//
-		//	})
+		It("should successfully reconcile the resource on update", func() {
+			By("Update resource")
+			test := struct {
+				returnGetScmConnection *gqlclient.ScmConnectionFragment
+				expectedStatus         v1alpha1.Status
+			}{
+				expectedStatus: v1alpha1.Status{
+					ID:  lo.ToPtr(id),
+					SHA: lo.ToPtr(sha),
+					Conditions: []metav1.Condition{
+						{
+							Type:    v1alpha1.ReadonlyConditionType.String(),
+							Status:  metav1.ConditionFalse,
+							Reason:  v1alpha1.ReadonlyConditionType.String(),
+							Message: "",
+						},
+						{
+							Type:    v1alpha1.ReadyConditionType.String(),
+							Status:  metav1.ConditionTrue,
+							Reason:  v1alpha1.ReadyConditionReason.String(),
+							Message: "",
+						},
+						{
+							Type:   v1alpha1.SynchronizedConditionType.String(),
+							Status: metav1.ConditionTrue,
+							Reason: v1alpha1.SynchronizedConditionReason.String(),
+						},
+					},
+				},
+				returnGetScmConnection: &gqlclient.ScmConnectionFragment{
+					ID: id,
+				},
+			}
+
+			Expect(common.MaybePatch(k8sClient, &v1alpha1.ScmConnection{
+				ObjectMeta: metav1.ObjectMeta{Name: scmName, Namespace: namespace},
+			}, func(p *v1alpha1.ScmConnection) {
+				p.Status.ID = lo.ToPtr(id)
+				p.Status.SHA = lo.ToPtr("ABC")
+			})).To(Succeed())
+
+			fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
+			fakeConsoleClient.On("GetScmConnectionByName", mock.Anything, mock.Anything).Return(test.returnGetScmConnection, nil)
+			fakeConsoleClient.On("IsScmConnectionExists", mock.Anything, mock.Anything).Return(true, nil).Once()
+			fakeConsoleClient.On("UpdateScmConnection", mock.Anything, mock.Anything, mock.Anything).Return(test.returnGetScmConnection, nil)
+			serviceReconciler := &controller.ScmConnectionReconciler{
+				Client:        k8sClient,
+				Scheme:        k8sClient.Scheme(),
+				ConsoleClient: fakeConsoleClient,
+			}
+
+			_, err := serviceReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+
+			Expect(err).NotTo(HaveOccurred())
+
+			scm := &v1alpha1.ScmConnection{}
+			err = k8sClient.Get(ctx, typeNamespacedName, scm)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(common.SanitizeStatusConditions(scm.Status)).To(Equal(common.SanitizeStatusConditions(test.expectedStatus)))
+		})
+
+		It("should successfully reconcile the resource", func() {
+			By("Delete resource")
+			resource := &v1alpha1.ScmConnection{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+			err = k8sClient.Delete(ctx, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
+			fakeConsoleClient.On("GetScmConnectionByName", mock.Anything, mock.Anything).Return(nil, nil).Once()
+			fakeConsoleClient.On("IsScmConnectionExists", mock.Anything, mock.Anything).Return(true, nil).Once()
+			fakeConsoleClient.On("DeleteScmConnection", mock.Anything, mock.Anything).Return(nil).Once()
+			fakeConsoleClient.On("IsScmConnectionExists", mock.Anything, mock.Anything).Return(false, nil).Once()
+			scmReconciler := &controller.ScmConnectionReconciler{
+				Client:        k8sClient,
+				Scheme:        k8sClient.Scheme(),
+				ConsoleClient: fakeConsoleClient,
+			}
+
+			// Reconcile once to initiate ScmConnection deletion from the API
+			_, err = scmReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+
+			Expect(err).NotTo(HaveOccurred())
+
+			// Second reconcile sees that the object no longer exists in the API and removes the finalizer.
+			_, err = scmReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+
+			Expect(err).NotTo(HaveOccurred())
+
+			scm := &v1alpha1.ScmConnection{}
+			err = k8sClient.Get(ctx, typeNamespacedName, scm)
+			Expect(err).To(HaveOccurred())
+		})
+
+		It("should successfully reconcile readonly resource", func() {
+			test := struct {
+				returnGetScmConnection *gqlclient.ScmConnectionFragment
+				expectedStatus         v1alpha1.Status
+			}{
+				&gqlclient.ScmConnectionFragment{
+					ID:   readonlyScmID,
+					Name: readonlyScmName,
+				},
+				v1alpha1.Status{
+					ID:  lo.ToPtr(readonlyScmID),
+					SHA: nil,
+					Conditions: []metav1.Condition{
+						{
+							Type:    v1alpha1.ReadonlyConditionType.String(),
+							Status:  metav1.ConditionTrue,
+							Reason:  v1alpha1.ReadonlyConditionType.String(),
+							Message: v1alpha1.ReadonlyTrueConditionMessage.String(),
+						},
+						{
+							Type:    v1alpha1.ReadyConditionType.String(),
+							Status:  metav1.ConditionTrue,
+							Reason:  v1alpha1.ReadyConditionReason.String(),
+							Message: "",
+						},
+						{
+							Type:   v1alpha1.SynchronizedConditionType.String(),
+							Status: metav1.ConditionTrue,
+							Reason: v1alpha1.SynchronizedConditionReason.String(),
+						},
+					},
+				},
+			}
+
+			fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
+			fakeConsoleClient.On("GetScmConnectionByName", mock.Anything, mock.Anything).Return(test.returnGetScmConnection, nil)
+			fakeConsoleClient.On("IsScmConnectionExists", mock.Anything, mock.Anything).Return(true, nil)
+
+			scmReconciler := &controller.ScmConnectionReconciler{
+				Client:        k8sClient,
+				Scheme:        k8sClient.Scheme(),
+				ConsoleClient: fakeConsoleClient,
+			}
+
+			_, err := scmReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: readonlyTypeNamespacedName,
+			})
+
+			Expect(err).NotTo(HaveOccurred())
+
+			scm := &v1alpha1.ScmConnection{}
+			err = k8sClient.Get(ctx, readonlyTypeNamespacedName, scm)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(common.SanitizeStatusConditions(scm.Status)).To(Equal(common.SanitizeStatusConditions(test.expectedStatus)))
+		})
 	})
 })
 
