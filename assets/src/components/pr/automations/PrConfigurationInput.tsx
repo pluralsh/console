3956ab3921b0ee417c9300e5c617429ff3ddcbc1
Jake Laderman <jsladerman@gmail.com>
2024-06-19 15:11:00 -0400

    add enum type support to pr automation configs

--- assets/src/components/pr/automations/PrConfigurationInput.tsx
+++ assets/src/components/pr/automations/PrConfigurationInput.tsx
@@ -1,4 +1,4 @@
-import { Input, Switch } from '@pluralsh/design-system'
+import { Input, ListBoxItem, Select, Switch } from '@pluralsh/design-system'
 import { ConfigurationType, PrConfiguration } from 'generated/graphql'
 
 import { parseToBool } from 'utils/parseToBool'
@@ -21,20 +21,40 @@ export function PrConfigurationInput({
     configBoolVal = parseToBool(value)
   }
 
-  return type === ConfigurationType.Bool ? (
-    <Switch
-      checked={!!configBoolVal}
-      onChange={(isChecked) => {
-        setValue(isChecked.toString())
-      }}
-    />
-  ) : (
-    <Input
-      value={value}
-      placeContent={config.placeholder}
-      onChange={(e) => {
-        setValue(e.currentTarget.value)
-      }}
-    />
-  )
+  switch (type) {
+    case ConfigurationType.Bool:
+      return (
+        <Switch
+          checked={!!configBoolVal}
+          onChange={(isChecked) => {
+            setValue(isChecked.toString())
+          }}
+        />
+      )
+    case ConfigurationType.Enum:
+      return (
+        <Select
+          selectedKey={value}
+          onSelectionChange={(key) => setValue(key as string)}
+        >
+          {(config?.values || [])?.map((val, index) => (
+            <ListBoxItem
+              key={`${index}`}
+              label={val}
+              textValue={val ?? ''}
+            />
+          ))}
+        </Select>
+      )
+    default:
+      return (
+        <Input
+          value={value}
+          placeContent={config.placeholder}
+          onChange={(e) => {
+            setValue(e.currentTarget.value)
+          }}
+        />
+      )
+  }
 }
