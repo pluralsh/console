4f8dc00cc889a5dea20c8ae552a1350a06093ead
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-06-28 15:20:52 +0200

    allow tag edit

--- assets/src/components/cd/globalServices/details/GlobalServiceInfo.tsx
+++ assets/src/components/cd/globalServices/details/GlobalServiceInfo.tsx
@@ -7,20 +7,27 @@ import {
   CardProps,
   Chip,
   ChipList,
+  FormField,
   GlobeIcon,
+  Modal,
   useSetBreadcrumbs,
 } from '@pluralsh/design-system'
-import React, { ReactElement, useMemo } from 'react'
-
+import React, { ComponentProps, ReactElement, useMemo, useState } from 'react'
 import { useNavigate, useOutletContext } from 'react-router-dom'
 
+import isEqual from 'lodash/isEqual'
+
 import { useSetPageScrollable } from '../../ContinuousDeployment'
-
 import { OverlineH1 } from '../../../utils/typography/Text'
-
 import { getDistroProviderIconUrl } from '../../../utils/ClusterDistro'
-
 import { getServiceDetailsPath } from '../../../../routes/cdRoutesConsts'
+
+import { useUpdateGlobalServiceMutation } from '../../../../generated/graphql'
+import { useUpdateState } from '../../../hooks/useUpdateState'
+import { tagsToNameValue } from '../../services/CreateGlobalService'
+import { TagSelection } from '../../services/TagSelection'
+import { GqlError } from '../../../utils/Alert'
+import { ModalMountTransition } from '../../../utils/ModalMountTransition'
 
 import { GlobalServiceContextT, getBreadcrumbs } from './GlobalService'
 
@@ -61,11 +68,124 @@ function PropCard({
   )
 }
 
+function TagsModalInner({
+  open,
+  onClose,
+  ...props
+}: ComponentProps<typeof Modal>) {
+  const theme = useTheme()
+  const { globalService, refetch } = useOutletContext<GlobalServiceContextT>()
+  const initialTags: Record<string, string> = useMemo(
+    () =>
+      Object.fromEntries(
+        globalService?.tags
+          ?.map((tag) => [tag?.name, tag?.value || ''])
+          .filter((t) => !!t[0]) || []
+      ),
+    [globalService?.tags]
+  )
+
+  const {
+    state,
+    update: updateState,
+    hasUpdates,
+  } = useUpdateState({ tags: initialTags }, { tags: (a, b) => !isEqual(a, b) })
+  const [mutation, { loading, error }] = useUpdateGlobalServiceMutation({
+    onCompleted: () => {
+      refetch?.()
+      onClose?.()
+    },
+  })
+
+  if (!globalService) {
+    return null
+  }
+
+  return (
+    <Modal
+      asForm
+      portal
+      open={open}
+      onClose={onClose}
+      header="Edit tags"
+      formProps={{
+        onSubmit: (e) => {
+          e.preventDefault()
+          if (hasUpdates) {
+            mutation({
+              variables: {
+                id: globalService.id,
+                attributes: {
+                  tags: tagsToNameValue(state.tags),
+                  name: globalService.name,
+                },
+              },
+            })
+          }
+        },
+      }}
+      actions={
+        <div
+          css={{
+            display: 'flex',
+            flexDirection: 'row-reverse',
+            gap: theme.spacing.medium,
+          }}
+        >
+          <Button
+            primary
+            type="submit"
+            disabled={!hasUpdates}
+            loading={loading}
+          >
+            Submit
+          </Button>
+          <Button
+            secondary
+            type="button"
+            onClick={() => {
+              onClose?.()
+            }}
+          >
+            Cancel
+          </Button>
+        </div>
+      }
+      {...props}
+    >
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: theme.spacing.large,
+        }}
+      >
+        <FormField label="Tags">
+          <TagSelection
+            tags={state.tags}
+            setTags={(tags) => updateState({ tags })}
+          />
+        </FormField>
+        {error && <GqlError error={error} />}
+      </div>
+    </Modal>
+  )
+}
+
+export function TagsModal(props: ComponentProps<typeof TagsModalInner>) {
+  return (
+    <ModalMountTransition open={!!props.open}>
+      <TagsModalInner {...props} />
+    </ModalMountTransition>
+  )
+}
+
 export default function GlobalServiceInfo() {
   const theme = useTheme()
   const navigate = useNavigate()
   const { globalServiceId, globalService } =
     useOutletContext<GlobalServiceContextT>()
+  const [open, setOpen] = useState(false)
 
   useSetBreadcrumbs(
     useMemo(
@@ -114,6 +234,15 @@ export default function GlobalServiceInfo() {
       )}
       <PropCard
         title="Tags"
+        titleContent={
+          <Button
+            small
+            secondary
+            onClick={() => setOpen(true)}
+          >
+            Edit tags
+          </Button>
+        }
         css={{ gridColumn: 'span 2' }}
       >
         <ChipList
@@ -123,6 +252,10 @@ export default function GlobalServiceInfo() {
           emptyState={<div>No tags found</div>}
         />
       </PropCard>
+      <TagsModal
+        open={open}
+        onClose={() => setOpen(false)}
+      />
       <PropCard title="Distribution">
         <div
           css={{
