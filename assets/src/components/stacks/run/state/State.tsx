b52f3e80c55c1015523dc67eda6c1c1dff5cd300
Jake Laderman <jsladerman@gmail.com>
2024-07-01 18:13:56 -0400

    add fullscreen option to stack state (#1130)

--- assets/src/components/stacks/run/state/State.tsx
+++ assets/src/components/stacks/run/state/State.tsx
@@ -1,18 +1,25 @@
+import { EmptyState } from '@pluralsh/design-system'
 import { ReactNode } from 'react'
 import { useOutletContext } from 'react-router-dom'
-import { CodeEditor } from '@pluralsh/design-system'
+
+import { StackStateGraph } from 'components/stacks/state/StackStateGraph'
+import { ReactFlowProvider } from 'reactflow'
 
 import { StackRun } from '../../../../generated/graphql'
 
 export default function StackRunState(): ReactNode {
   const { stackRun } = useOutletContext<{ stackRun: StackRun }>()
-  const value = JSON.stringify(stackRun.state?.state ?? {}, null, 2)
+  const { state } = stackRun
 
   return (
-    <CodeEditor
-      value={value}
-      language="json"
-      options={{ readOnly: true }}
-    />
+    <div css={{ height: '100%' }}>
+      {state ? (
+        <ReactFlowProvider>
+          <StackStateGraph state={state} />
+        </ReactFlowProvider>
+      ) : (
+        <EmptyState message="No state found." />
+      )}
+    </div>
   )
 }
