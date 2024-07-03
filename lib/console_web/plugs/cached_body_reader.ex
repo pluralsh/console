5d7f6557ee81fde5986fa52be37b3090e3293fd2
michaeljguarino <mguarino46@gmail.com>
2024-07-03 18:00:06 -0400

    Need to handle {:more, _, _} clauses in cached body reader (#1134)

--- lib/console_web/plugs/cached_body_reader.ex
+++ lib/console_web/plugs/cached_body_reader.ex
@@ -1,7 +1,11 @@
 defmodule ConsoleWeb.CacheBodyReader do
   def read_body(conn, opts) do
-    {:ok, body, conn} = Plug.Conn.read_body(conn, opts)
-    conn = update_in(conn.assigns[:raw_body], &[body | (&1 || [])])
-    {:ok, body, conn}
+    case Plug.Conn.read_body(conn, opts) do
+      {:ok, body, conn} -> {:ok, body, append_body(conn, body)}
+      {:more, body, conn} -> {:more, body, append_body(conn, body)}
+      err -> err
+    end
   end
+
+  defp append_body(conn, body), do: update_in(conn.assigns[:raw_body], &[body | (&1 || [])])
 end
