1828dfc5263ed90c1337ffcdfcb5fc3faa1b147b
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-07-18 12:27:11 +0200

    export react flow component

--- assets/src/components/stacks/state/StackStateGraph.tsx
+++ assets/src/components/stacks/state/StackStateGraph.tsx
@@ -1,11 +1,4 @@
-import {
-  CloseIcon,
-  IconFrame,
-  LinkoutIcon,
-  ReloadIcon,
-  WrapWithIf,
-  usePrevious,
-} from '@pluralsh/design-system'
+import { usePrevious } from '@pluralsh/design-system'
 import { StackState } from 'generated/graphql'
 import {
   useCallback,
@@ -14,21 +7,16 @@ import {
   useMemo,
   useState,
 } from 'react'
-import ReactFlow, {
-  Background,
-  BackgroundVariant,
+import {
   type Edge,
   type Node,
   useEdgesState,
   useNodesState,
   useReactFlow,
 } from 'reactflow'
-import chroma from 'chroma-js'
 
 import 'reactflow/dist/style.css'
 import { useTheme } from 'styled-components'
-
-import { useKeyDown } from '@react-hooks-library/core'
 
 import {
   type DagreDirection,
@@ -39,10 +27,7 @@ import { edgeTypes } from '../../cd/pipelines/EdgeLine'
 import { NodeType } from '../../cd/pipelines/utils/getNodesAndEdges'
 import { isNonNullable } from '../../../utils/isNonNullable'
 
-import {
-  ReactFlowFullScreenWrapperSC,
-  ReactFlowWrapperSC,
-} from '../../utils/ReactFlow'
+import { ReactFlowGraph } from '../../utils/ReactFlow'
 
 import { StackStateGraphNode } from './StackStateGraphNode'
 import { STACK_STATE_GRAPH_EDGE_NAME } from './StackStateGraphEdge'
@@ -79,10 +64,6 @@ export function getNodesAndEdges(state: StackState) {
 
 export function StackStateGraph({ state }: { state: StackState }) {
   const theme = useTheme()
-  const margin = theme.spacing.large
-  const [isFullscreen, setIsFullscreen] = useState(false)
-
-  useKeyDown('Escape', () => setIsFullscreen(false))
 
   const { nodes: initialNodes, edges: initialEdges } = useMemo(
     () => getNodesAndEdges(state),
@@ -102,14 +83,14 @@ export function StackStateGraph({ state }: { state: StackState }) {
         direction,
         zoom: getViewport().zoom,
         gridGap: theme.spacing.large,
-        margin,
+        margin: theme.spacing.large,
       })
 
       setNodes([...layouted.nodes])
       setEdges([...layouted.edges])
       setNeedsLayout(false)
     },
-    [nodes, edges, getViewport, theme.spacing.large, margin, setNodes, setEdges]
+    [nodes, edges, getViewport, theme.spacing.large, setNodes, setEdges]
   )
 
   useLayoutEffect(() => {
@@ -141,77 +122,20 @@ export function StackStateGraph({ state }: { state: StackState }) {
   }, [state, prevState, setEdges, setNodes])
 
   return (
-    <WrapWithIf
-      condition={isFullscreen}
-      wrapper={<ReactFlowFullScreenWrapperSC />}
-    >
-      <div
-        css={{
-          backgroundColor:
-            theme.mode === 'dark'
-              ? theme.colors.grey[950]
-              : theme.colors['fill-one'],
-          border: theme.borders.default,
-          width: '100%',
-          height: '100%',
-          borderRadius: theme.borderRadiuses.large,
-          position: 'relative',
-          overflow: 'hidden',
-        }}
-      >
-        <ReactFlowWrapperSC>
-          <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
-            nodeTypes={nodeTypes}
-            draggable
-            nodesDraggable
-            edgesUpdatable={false}
-            edgesFocusable={false}
-            nodesConnectable={false}
-            edgeTypes={edgeTypes}
-          >
-            <Background
-              variant={BackgroundVariant.Dots}
-              gap={theme.spacing.large}
-              size={1}
-              color={`${chroma(theme.colors['border-fill-three']).alpha(1)}`}
-            />
-          </ReactFlow>
-          <div
-            css={{
-              position: 'absolute',
-              top: theme.spacing.xsmall,
-              right: theme.spacing.xsmall,
-              display: 'flex',
-              gap: theme.spacing.xsmall,
-            }}
-          >
-            <IconFrame
-              clickable
-              type="floating"
-              icon={isFullscreen ? <CloseIcon /> : <LinkoutIcon />}
-              tooltip={isFullscreen ? 'Exit fullscreen' : 'Fullscreen'}
-              onClick={() => setIsFullscreen(!isFullscreen)}
-            >
-              Fullscreen
-            </IconFrame>
-            <IconFrame
-              clickable
-              type="floating"
-              icon={<ReloadIcon />}
-              tooltip="Reset view"
-              onClick={() =>
-                setViewport({ x: 0, y: 0, zoom: 1 }, { duration: 500 })
-              }
-            >
-              Reset view
-            </IconFrame>
-          </div>
-        </ReactFlowWrapperSC>
-      </div>
-    </WrapWithIf>
+    <ReactFlowGraph
+      allowFullscreen
+      resetView={() => setViewport({ x: 0, y: 0, zoom: 1 }, { duration: 500 })}
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      nodeTypes={nodeTypes}
+      draggable
+      nodesDraggable
+      edgesUpdatable={false}
+      edgesFocusable={false}
+      nodesConnectable={false}
+      edgeTypes={edgeTypes}
+    />
   )
 }
