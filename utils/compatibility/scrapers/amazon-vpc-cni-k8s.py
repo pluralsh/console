b515ca223cb0b67c73883dd6433b2c8bba9f04d3
Kevin Jayne <seemywingz@users.noreply.github.com>
2024-07-23 10:11:49 -0400

    Add Scrapers from Backlog and Update Utils (#1200)

---
+++ utils/compatibility/scrapers/amazon-vpc-cni-k8s.py
@@ -0,0 +1,52 @@
+# scrapers/new-scraper.py
+
+from bs4 import BeautifulSoup
+from collections import OrderedDict
+from utils import (
+    update_compatibility_info,
+    update_chart_versions,
+    get_github_releases,
+    expand_kube_versions,
+)
+
+app_name = "amazon-vpc-cni-k8s"
+compatibility_url = (
+    "https://docs.aws.amazon.com/eks/latest/userguide/managing-vpc-cni.html"
+)
+
+
+def scrape():
+
+    # Amazon VPC CNI plugin for Kubernetes versions v1.16.0 to v1.16.1
+    #   removed compatibility with Kubernetes versions 1.23 and earlier.
+    # VPC CNI version v1.16.2 restores compatibility with Kubernetes versions 1.23 and earlier
+
+    versions = []
+    releases = get_github_releases("aws", "amazon-vpc-cni-k8s")
+    base_kube_versions = expand_kube_versions("1.23", "1.30")
+
+    for release in releases:
+        ver = release.lstrip("v")
+
+        # Set default kube_versions to base_kube_versions
+        kube_versions = base_kube_versions
+
+        # Apply specific conditions
+        if "v1.16.0" <= release <= "v1.16.1":
+            kube_versions = expand_kube_versions("1.24", "1.30")
+        elif ver == "v1.16.2":
+            kube_versions = base_kube_versions
+
+        version_info = OrderedDict(
+            [
+                ("version", ver),
+                ("kube", kube_versions),
+            ]
+        )
+        versions.append(version_info)
+
+    update_compatibility_info(
+        f"../../static/compatibilities/{app_name}.yaml", versions
+    )
+
+    update_chart_versions(app_name, "aws-vpc-cni")
---
+++ utils/compatibility/scrapers/aws-load-balancer-controller.py
@@ -0,0 +1,137 @@
+import re
+from bs4 import BeautifulSoup
+from collections import OrderedDict
+from utils import (
+    print_error,
+    fetch_page,
+    update_compatibility_info,
+    update_chart_versions,
+    current_kube_version,
+    get_latest_github_release,
+    expand_kube_versions,
+)
+
+app_name = "aws-load-balancer-controller"
+compatibility_url = "https://kubernetes-sigs.github.io/aws-load-balancer-controller/v2.4/deploy/installation/"
+
+
+def parse_page(content):
+    soup = BeautifulSoup(content, "html.parser")
+    sections = soup.find_all("h2")
+    return sections
+
+
+def find_target_tables(sections):
+    target_tables = []
+    for section in sections:
+        if "Supported Kubernetes versions" in section.text:
+            section = section.find_next("ul")
+            target_tables.append(section)
+    return target_tables
+
+
+def expand_versions(start, end):
+    start_major, start_minor, start_patch = start.split(".")
+    end_major, end_minor, end_patch = end.split(".")
+
+    expanded_versions = []
+    major = int(start_major)
+    minor = int(start_minor)
+    patch = int(start_patch)
+    while (
+        (major < int(end_major))
+        or (major == int(end_major) and minor <= int(end_minor))
+        or (
+            major == int(end_major)
+            and minor == int(end_minor)
+            and patch <= int(end_patch)
+        )
+    ):
+        expanded_versions.append(f"{major}.{minor}.{patch}")
+        if (
+            major == int(end_major)
+            and minor == int(end_minor)
+            and patch == int(end_patch)
+        ):
+            break
+        if patch == 9:
+            if minor == 9:
+                major += 1
+                minor = 0
+            else:
+                minor += 1
+            patch = 0
+        else:
+            patch += 1
+    return expanded_versions
+
+
+def extract_versions(version_string):
+    versions = []
+    pattern = r"(\d+\.\d+\.\d+)"
+    # Find all matches in the string
+    matches = re.findall(pattern, version_string)
+
+    start = matches[0]
+    if len(matches) == 2:
+        end = matches[1]
+    else:
+        end = get_latest_github_release(
+            "kubernetes-sigs", "aws-load-balancer-controller"
+        ).strip("v")
+
+    versions = expand_versions(start, end)
+
+    return versions
+
+
+def extract_table_data(target_tables):
+    versions = []
+    for table in target_tables:
+        rows = table.find_all("li")
+        rows = [row.text for row in rows]
+        for row in rows:
+            app_versions = extract_versions(row)
+            k8s_versions = row.split(" ")[-1]
+
+            if "+" in k8s_versions:
+                k8s_versions = expand_kube_versions(
+                    k8s_versions.strip("+"), current_kube_version()
+                )
+
+            if "-" in k8s_versions:
+                start = k8s_versions.split("-")[0]
+                end = k8s_versions.split("-")[1]
+                k8s_versions = expand_kube_versions(start, end)
+
+            for ver in app_versions:
+                version_info = OrderedDict(
+                    [
+                        ("version", ver),
+                        ("kube", k8s_versions),
+                        ("requirements", []),
+                        ("incompatibilities", []),
+                    ]
+                )
+                versions.append(version_info)
+
+    return versions
+
+
+def scrape():
+
+    page_content = fetch_page(compatibility_url)
+    if not page_content:
+        return
+
+    sections = parse_page(page_content)
+    target_tables = find_target_tables(sections)
+    if target_tables.__len__() >= 1:
+        rows = extract_table_data(target_tables)
+        update_compatibility_info(
+            f"../../static/compatibilities/{app_name}.yaml", rows
+        )
+    else:
+        print_error("No compatibility information found.")
+
+    update_chart_versions(app_name)
