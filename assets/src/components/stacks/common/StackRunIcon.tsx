8b3d5297d4fc345a9adcf0804761ffe05847c96c
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-07-30 17:45:52 +0200

    update stack run icons

--- assets/src/components/stacks/common/StackRunIcon.tsx
+++ assets/src/components/stacks/common/StackRunIcon.tsx
@@ -1,28 +1,39 @@
-import React from 'react'
-import { AppIcon, CliIcon, WarningShieldIcon } from '@pluralsh/design-system'
+import React, { ComponentType } from 'react'
+import {
+  AppIcon,
+  StackRunIcon as DefaultStackRunIcon,
+  StackRunCanceledIcon,
+  StackRunPausedIcon,
+  StackRunPendingIcon,
+} from '@pluralsh/design-system'
 
 import { ChipProps } from '@pluralsh/design-system/dist/components/Chip'
 
+import { IconProps } from '@pluralsh/design-system/dist/components/icons/createIcon'
+
 import { StackStatus } from '../../../generated/graphql'
+
+const statusToIcon = {
+  [StackStatus.Queued]: StackRunPausedIcon,
+  [StackStatus.Running]: DefaultStackRunIcon,
+  [StackStatus.Pending]: DefaultStackRunIcon,
+  [StackStatus.Cancelled]: StackRunCanceledIcon,
+  [StackStatus.Failed]: StackRunCanceledIcon,
+  [StackStatus.Successful]: DefaultStackRunIcon,
+  [StackStatus.PendingApproval]: StackRunPendingIcon,
+} as const satisfies Record<StackStatus, ComponentType<IconProps>>
 
 export default function StackRunIcon({
   status,
 }: {
   status: StackStatus
 } & ChipProps) {
+  const Icon = statusToIcon[status]
+
   return (
     <AppIcon
       size="xxsmall"
-      icon={
-        status === StackStatus.PendingApproval ? (
-          <WarningShieldIcon
-            width={32}
-            color="icon-warning"
-          />
-        ) : (
-          <CliIcon width={32} />
-        )
-      }
+      icon={<Icon size={32} />}
     />
   )
 }
