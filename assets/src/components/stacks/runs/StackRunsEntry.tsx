d98753e78a35da9623f4815559991896f271f56b
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-07-31 21:40:45 +0200

    feat: Stack run cancelation and observability metrics (#1231)
    Co-authored-by: michaeljguarino <mguarino46@gmail.com>

--- assets/src/components/stacks/runs/StackRunsEntry.tsx
+++ assets/src/components/stacks/runs/StackRunsEntry.tsx
@@ -1,23 +1,21 @@
 import {
-  AppIcon,
   CaretRightIcon,
-  CliIcon,
   GitCommitIcon,
   IconFrame,
   Tooltip,
-  WarningShieldIcon,
 } from '@pluralsh/design-system'
 import moment from 'moment'
 import { CSSObject, useTheme } from 'styled-components'
 import { useNavigate } from 'react-router'
 import { useParams } from 'react-router-dom'
 
-import { StackRunFragment, StackStatus } from '../../../generated/graphql'
+import { StackRunFragment } from '../../../generated/graphql'
 import { getStackRunsAbsPath } from '../../../routes/stacksRoutesConsts'
 
 import { TRUNCATE } from '../../utils/truncate'
 
 import StackStatusChip from '../common/StackStatusChip'
+import StackRunIcon from '../common/StackRunIcon'
 
 export default function StackRunsEntry({
   stackRun,
@@ -57,19 +55,7 @@ export default function StackRunsEntry({
       }}
       onClick={() => navigate(getStackRunsAbsPath(stackId, id))}
     >
-      <AppIcon
-        size="xxsmall"
-        icon={
-          stackRun.status === StackStatus.PendingApproval ? (
-            <WarningShieldIcon
-              width={32}
-              color="icon-warning"
-            />
-          ) : (
-            <CliIcon width={32} />
-          )
-        }
-      />
+      <StackRunIcon status={stackRun.status} />
       <div
         css={{
           display: 'flex',
