06cf6cacc3101b32165b27e2413b5c5f13d0fff1
Jake Laderman <jsladerman@gmail.com>
2024-08-06 23:30:21 -0400

    add flux table to repositories tab (#1249)

---
+++ assets/src/components/cd/repos/FluxHelmRepositoriesTable.tsx
@@ -0,0 +1,49 @@
+import { FluxHelmRepositoriesQuery } from 'generated/graphql'
+
+import { ComponentProps } from 'react'
+
+import { EmptyState, Table } from '@pluralsh/design-system'
+
+import { isEmpty } from 'lodash'
+
+import {
+  ColName,
+  ColNamespace,
+  ColProvider,
+  ColStatus,
+  ColType,
+  ColUrl,
+} from './FluxHelmRepositoriesColumns'
+
+export function FluxHelmRepositoriesTable({
+  data,
+  reactTableOptions,
+}: {
+  data: FluxHelmRepositoriesQuery
+  reactTableOptions?: ComponentProps<typeof Table>['reactTableOptions']
+}) {
+  const repos = data?.fluxHelmRepositories
+
+  return !isEmpty(repos) ? (
+    <Table
+      data={data?.fluxHelmRepositories || []}
+      columns={fluxHelmRepoColumns}
+      css={{
+        maxHeight: 'unset',
+        height: '100%',
+      }}
+      reactTableOptions={reactTableOptions}
+    />
+  ) : (
+    <EmptyState message="Looks like you don't have any Flux repositories yet." />
+  )
+}
+
+const fluxHelmRepoColumns = [
+  ColName,
+  ColStatus,
+  ColNamespace,
+  ColProvider,
+  ColType,
+  ColUrl,
+]
--- assets/src/components/cd/repos/HelmRepositoriesTable.tsx
+++ assets/src/components/cd/repos/HelmRepositoriesTable.tsx
@@ -1,41 +1,31 @@
 import { EmptyState, Table } from '@pluralsh/design-system'
-import { GitRepositoriesQuery } from 'generated/graphql'
+import { HelmRepositoriesQuery } from 'generated/graphql'
 import { ComponentProps } from 'react'
 
 import { isEmpty } from 'lodash'
 
 import {
-  ColActions,
-  ColAuthMethod,
   ColCreatedAt,
+  ColProvider,
   ColPulledAt,
   ColRepo,
   ColStatus,
   ColUpdatedAt,
 } from './GitRepositoriesColumns'
 
-export const gitRepoColumns = [
-  ColRepo,
-  ColAuthMethod,
-  ColStatus,
-  ColCreatedAt,
-  ColUpdatedAt,
-  ColPulledAt,
-  ColActions,
-]
-export function GitRepositoriesTable({
+export function HelmRepositoriesTable({
   data,
   reactTableOptions,
 }: {
-  data: GitRepositoriesQuery
+  data: HelmRepositoriesQuery
   reactTableOptions?: ComponentProps<typeof Table>['reactTableOptions']
 }) {
-  const edges = data?.gitRepositories?.edges
+  const edges = data?.helmRepositories?.edges
 
   return !isEmpty(edges) ? (
     <Table
       data={edges || []}
-      columns={gitRepoColumns}
+      columns={helmRepoColumns}
       css={{
         maxHeight: 'unset',
         height: '100%',
@@ -43,6 +33,15 @@ export function GitRepositoriesTable({
       reactTableOptions={reactTableOptions}
     />
   ) : (
-    <EmptyState message="Looks like you don't have any Git repositories yet." />
+    <EmptyState message="Looks like you don't have any Helm repositories yet." />
   )
 }
+
+const helmRepoColumns = [
+  ColRepo,
+  ColProvider,
+  ColStatus,
+  ColCreatedAt,
+  ColPulledAt,
+  ColUpdatedAt,
+]
