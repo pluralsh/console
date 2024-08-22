ae4b6e59a07753626e4391f3698a04469c158f7b
Jake Laderman <jsladerman@gmail.com>
2024-08-22 11:04:48 -0400

    add json expander component to canary rollout info

--- assets/src/components/component/info/rollout/CanarySpecTableColumns.tsx
+++ assets/src/components/component/info/rollout/CanarySpecTableColumns.tsx
@@ -1,17 +1,36 @@
-import { Chip } from '@pluralsh/design-system'
+import {
+  BrowserIcon,
+  Button,
+  Card,
+  Chip,
+  Code,
+  Flex,
+  Modal,
+} from '@pluralsh/design-system'
 import { createColumnHelper } from '@tanstack/react-table'
 import { ArgoStrategyStep } from 'generated/graphql'
+import { useState } from 'react'
+import styled from 'styled-components'
+
+import { Body1BoldP } from 'components/utils/typography/Text'
 
 const columnHelper = createColumnHelper<ArgoStrategyStep>()
+
+function StepTypeChip({ step }: { step: ArgoStrategyStep }) {
+  let type = 'scale'
+
+  if (step.analysis) type = 'analysis'
+  else if (step.experiment) type = 'experiment'
+  else if (step.pause) type = 'pause'
+
+  return <Chip css={{ width: 'fit-content' }}>{type}</Chip>
+}
+
 const ColType = columnHelper.accessor((step) => step, {
   id: 'type',
   header: 'Type',
   cell: function Cell({ getValue }) {
-    if (getValue().analysis) return <Chip>analysis</Chip>
-    if (getValue().experiment) return <Chip>experiment</Chip>
-    if (getValue().pause) return <Chip>pause</Chip>
-
-    return <Chip>scale</Chip>
+    return <StepTypeChip step={getValue()} />
   },
 })
 
@@ -27,18 +46,81 @@ const ColPause = columnHelper.accessor((step) => step.pause?.duration, {
   header: 'Pause Duration',
 })
 
-const ColConfiguration = columnHelper.accessor(
-  (step) =>
-    JSON.stringify(
-      step.analysis?.templates || step.experiment?.templates || {}
-    ),
-  {
-    id: 'configuration',
-    header: 'Configuration',
-    cell: function Cell({ getValue }) {
-      return <span>{getValue()}</span>
-    },
-  }
-)
+const ColConfiguration = columnHelper.accessor((step) => step, {
+  id: 'configuration',
+  header: 'Configuration',
+  cell: function Cell({ getValue }) {
+    const [open, setOpen] = useState(false)
+
+    return (
+      <>
+        <Button
+          css={{ width: 'max-content' }}
+          floating
+          onClick={() => setOpen(true)}
+        >
+          <Flex gap="small">
+            <BrowserIcon size={16} />
+            Show JSON
+          </Flex>
+        </Button>
+        <Modal
+          header="Canary rollout strategy"
+          size="large"
+          open={open}
+          onClose={() => setOpen(false)}
+        >
+          <CanarySpecModalInner step={getValue()} />
+        </Modal>
+      </>
+    )
+  },
+})
+
+function CanarySpecModalInner({ step }: { step: ArgoStrategyStep }) {
+  return (
+    <ModalInnerWrapper>
+      <ModalHeaderCard>
+        <ModalHeaderCardTitle>Type</ModalHeaderCardTitle>
+        <StepTypeChip step={step} />
+      </ModalHeaderCard>
+      <ModalHeaderCard>
+        <ModalHeaderCardTitle>Weight</ModalHeaderCardTitle>
+        <Body1BoldP>{step.setWeight ? `${step.setWeight}%` : ''}</Body1BoldP>
+      </ModalHeaderCard>
+      <ModalHeaderCard>
+        <ModalHeaderCardTitle>Pause duration</ModalHeaderCardTitle>
+        <Body1BoldP>{step.pause?.duration ?? ''}</Body1BoldP>
+      </ModalHeaderCard>
+      <Code
+        language="json"
+        css={{ gridColumn: '1/-1' }}
+      >
+        {JSON.stringify(
+          step.analysis?.templates || step.experiment?.templates || '',
+          null,
+          2
+        )}
+      </Code>
+    </ModalInnerWrapper>
+  )
+}
+
+const ModalHeaderCard = styled(Card).attrs({ fillLevel: 2 })(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing.xsmall,
+  padding: theme.spacing.medium,
+}))
+const ModalHeaderCardTitle = styled.h4(({ theme }) => ({
+  ...theme.partials.text.overline,
+  color: theme.colors['text-xlight'],
+}))
+const ModalInnerWrapper = styled.div(({ theme }) => ({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridTemplateRows: 'auto 1fr',
+  gap: theme.spacing.large,
+}))
 
 export const canarySpecCols = [ColType, ColWeight, ColPause, ColConfiguration]
