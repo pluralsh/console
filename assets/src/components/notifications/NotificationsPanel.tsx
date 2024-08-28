2ba6120cc157eb4a9589b121688500717b38d46d
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-08-28 14:52:47 +0200

    use read mutation

--- assets/src/components/notifications/NotificationsPanel.tsx
+++ assets/src/components/notifications/NotificationsPanel.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from 'styled-components'
-import { ComponentProps } from 'react'
+import { ComponentProps, useState } from 'react'
 import {
+  Banner,
   Button,
   Card,
   CloseIcon,
@@ -8,8 +9,10 @@ import {
   IconFrame,
 } from '@pluralsh/design-system'
 import { useNavigate } from 'react-router-dom'
+import { ApolloError } from 'apollo-boost'
 
 import { NOTIFICATIONS_ABS_PATH } from '../../routes/settingsRoutesConst'
+import { useReadAppNotificationsMutation } from '../../generated/graphql'
 
 export function NotificationsPanel({
   onClose,
@@ -17,57 +20,82 @@ export function NotificationsPanel({
 }: ComponentProps<typeof Card> & { onClose: () => void }) {
   const theme = useTheme()
   const navigate = useNavigate()
+  const [error, setError] = useState<ApolloError>()
+
+  const [mutation, { loading }] = useReadAppNotificationsMutation({
+    onError: (error) => {
+      setError(error)
+      setTimeout(() => setError(undefined), 3000)
+    },
+  })
 
   return (
-    <Card
-      fillLevel={1}
-      css={{ border: theme.borders.input, flexGrow: 1 }}
-      {...props}
-    >
-      <div
-        css={{
-          alignItems: 'center',
-          borderBottom: theme.borders.input,
-          display: 'flex',
-          flexGrow: 1,
-          gap: theme.spacing.small,
-          height: 64,
-          padding: theme.spacing.medium,
-        }}
+    <>
+      <Card
+        fillLevel={1}
+        css={{ border: theme.borders.input, flexGrow: 1 }}
+        {...props}
       >
-        <span
+        <div
           css={{
-            ...theme.partials.text.overline,
-            color: theme.colors['text-xlight'],
+            alignItems: 'center',
+            borderBottom: theme.borders.input,
+            display: 'flex',
             flexGrow: 1,
+            gap: theme.spacing.small,
+            height: 64,
+            padding: theme.spacing.medium,
           }}
         >
-          Notifications
-        </span>
-        <IconFrame
-          clickable
-          icon={<GearTrainIcon />}
-          onClick={() => {
-            navigate(NOTIFICATIONS_ABS_PATH)
-            onClose()
-          }}
-          tooltip="Go to notifications settings"
-          type="secondary"
-        />
-        <Button
-          small
-          secondary
+          <span
+            css={{
+              ...theme.partials.text.overline,
+              color: theme.colors['text-xlight'],
+              flexGrow: 1,
+            }}
+          >
+            Notifications
+          </span>
+          <IconFrame
+            clickable
+            icon={<GearTrainIcon />}
+            onClick={() => {
+              navigate(NOTIFICATIONS_ABS_PATH)
+              onClose()
+            }}
+            tooltip="Go to notifications settings"
+            type="secondary"
+          />
+          <Button
+            loading={loading}
+            onClick={mutation}
+            small
+            secondary
+          >
+            Mark all as read
+          </Button>
+          <IconFrame
+            clickable
+            icon={<CloseIcon />}
+            onClick={onClose}
+            tooltip="Close"
+          />
+        </div>
+        content
+      </Card>
+      {error && (
+        <Banner
+          heading="Failed to mark notifications as read"
+          severity="error"
+          position="fixed"
+          bottom={24}
+          right={100}
+          zIndex={theme.zIndexes.modal}
+          onClose={() => setError(undefined)}
         >
-          Mark all as read
-        </Button>
-        <IconFrame
-          clickable
-          icon={<CloseIcon />}
-          onClick={onClose}
-          tooltip="Close"
-        />
-      </div>
-      content
-    </Card>
+          {error.message}
+        </Banner>
+      )}
+    </>
   )
 }
