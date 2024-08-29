c4fd4f48bf0cabf94f8739c151c23c3fe3ccfd94
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-08-29 12:52:52 +0200

    add line clamp

--- assets/src/components/notifications/Notification.tsx
+++ assets/src/components/notifications/Notification.tsx
@@ -1,6 +1,8 @@
 import { useTheme } from 'styled-components'
 import React from 'react'
 
+import { Button } from '@pluralsh/design-system'
+
 import { AppNotificationFragment } from '../../generated/graphql'
 
 export default function Notification({
@@ -9,6 +11,7 @@ export default function Notification({
   notification: AppNotificationFragment
 }) {
   const theme = useTheme()
+  const [expand, setExpand] = React.useState<boolean>(false)
 
   return (
     <div
@@ -16,12 +19,31 @@ export default function Notification({
         backgroundColor: notification.readAt
           ? theme.colors['fill-one']
           : theme.colors['fill-two'],
-        height: '100%',
         padding: `${theme.spacing.large}px ${theme.spacing.medium}px `,
         width: '100%',
       }}
     >
-      {notification.text}
+      <div
+        css={{
+          ...(expand
+            ? {}
+            : {
+                display: '-webkit-box',
+                '-webkit-line-clamp': '2',
+                '-webkit-box-orient': 'vertical',
+                overflow: 'hidden',
+              }),
+        }}
+      >
+        {notification.text}
+      </div>
+      <Button
+        onClick={() => setExpand(!expand)}
+        small
+        secondary
+      >
+        Read more
+      </Button>
     </div>
   )
 }
