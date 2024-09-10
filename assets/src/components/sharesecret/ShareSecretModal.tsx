abbdecd54dbd8fd963640bce6f928c25de26342d
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-09-10 12:12:53 +0200

    add form logic

--- assets/src/components/sharesecret/ShareSecretModal.tsx
+++ assets/src/components/sharesecret/ShareSecretModal.tsx
@@ -1,22 +1,37 @@
-import { FormField, Input, Modal } from '@pluralsh/design-system'
+import { Button, FormField, Input, Modal } from '@pluralsh/design-system'
 
-import { useEffect, useMemo, useRef, useState } from 'react'
+import {
+  FormEvent,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
+import { useTheme } from 'styled-components'
 
 import { splitBindings } from '../settings/usermanagement/roles/RoleFormBindings'
-
 import { BindingInput } from '../utils/BindingInput'
 
-import { PolicyBindingFragment } from '../../generated/graphql'
+import {
+  PolicyBindingFragment,
+  useShareSecretMutation,
+} from '../../generated/graphql'
+import { isNonNullable } from '../../utils/isNonNullable'
+import { bindingToBindingAttributes } from '../settings/usermanagement/roles/misc'
 
 import { useShareSecretContext } from './ShareSecretContext'
 
 export default function ShareSecretModal() {
+  const theme = useTheme()
   const { open, setOpen } = useShareSecretContext()
-  const [secretName, setSecretName] = useState('')
-  const [secretString, setSecretString] = useState('')
+  const [completed, setCompleted] = useState(false)
+  const [name, setName] = useState('')
+  const [secret, setSecret] = useState('')
   const [bindings, setBindings] = useState<
     Pick<PolicyBindingFragment, 'user' | 'group'>[]
   >([])
+  const disabled = !name || !secret
   const inputRef = useRef<HTMLInputElement>()
 
   useEffect(() => inputRef.current?.focus?.(), [])
@@ -30,51 +45,134 @@ export default function ShareSecretModal() {
     }
   }, [bindings])
 
+  const [mutation, { loading, reset, data }] = useShareSecretMutation({
+    onCompleted: () => setCompleted(true),
+  })
+
+  const onSubmit = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault()
+
+      if (disabled) return
+
+      mutation({
+        variables: {
+          attributes: {
+            name,
+            secret,
+            notificationBindings: bindings
+              .filter(isNonNullable)
+              .map(bindingToBindingAttributes),
+          },
+        },
+      })
+    },
+    [bindings, disabled, mutation, name, secret]
+  )
+
+  const onRestart = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault()
+      reset()
+      setCompleted(false)
+    },
+    [reset, setCompleted]
+  )
+
   return (
     <Modal
+      actions={
+        <div
+          css={{
+            display: 'flex',
+            gap: theme.spacing.medium,
+          }}
+        >
+          <Button
+            type="button"
+            secondary
+            onClick={() => setOpen(false)}
+          >
+            Cancel
+          </Button>
+          {completed ? (
+            <Button
+              type="button"
+              onClick={onRestart}
+            >
+              Restart
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              disabled={disabled}
+              loading={loading}
+            >
+              Generate secret
+            </Button>
+          )}
+        </div>
+      }
+      asForm
+      formProps={{ onSubmit }}
       header="Share secret"
       open={open}
       onClose={() => setOpen(false)}
+      size="large"
     >
-      <FormField
-        required
-        label="Secret name"
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: theme.spacing.large,
+        }}
       >
-        <Input
-          inputProps={{ ref: inputRef }}
-          value={secretName}
-          onChange={(e) => setSecretName(e.currentTarget.value)}
-        />
-      </FormField>
-      <FormField
-        required
-        label="Secret string"
-      >
-        <Input
-          value={secretString}
-          onChange={(e) => setSecretString(e.currentTarget.value)}
-        />
-      </FormField>
-      <BindingInput
-        type="group"
-        bindings={groupBindings}
-        add={(group) => setBindings([...bindings, { group }])}
-        remove={(name) =>
-          setBindings(
-            bindings.filter(({ group }) => !group || group.name !== name)
-          )
-        }
-      />
-      <BindingInput
-        type="user"
-        bindings={userBindings}
-        add={(user) => setBindings([...bindings, { user }])}
-        remove={(email) =>
-          setBindings(
-            bindings.filter(({ user }) => !user || user.email !== email)
-          )
-        }
-      />
+        <FormField
+          required
+          label="Secret name"
+        >
+          <Input
+            disabled={completed}
+            inputProps={{ ref: inputRef }}
+            value={name}
+            onChange={(e) => setName(e.currentTarget.value)}
+          />
+        </FormField>
+        <FormField
+          required
+          label="Secret string"
+        >
+          <Input
+            disabled={completed}
+            value={secret}
+            onChange={(e) => setSecret(e.currentTarget.value)}
+          />
+        </FormField>
+        {!completed && (
+          <>
+            <BindingInput
+              type="group"
+              bindings={groupBindings}
+              add={(group) => setBindings([...bindings, { group }])}
+              remove={(name) =>
+                setBindings(
+                  bindings.filter(({ group }) => !group || group.name !== name)
+                )
+              }
+            />
+            <BindingInput
+              type="user"
+              bindings={userBindings}
+              add={(user) => setBindings([...bindings, { user }])}
+              remove={(email) =>
+                setBindings(
+                  bindings.filter(({ user }) => !user || user.email !== email)
+                )
+              }
+            />
+          </>
+        )}
+      </div>
     </Modal>
   )
 }
