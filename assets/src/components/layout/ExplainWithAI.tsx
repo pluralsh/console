ec24313fd192097d479705d67c890296848bd2de
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-10-24 16:48:50 +0200

    update hover style

--- assets/src/components/layout/ExplainWithAI.tsx
+++ assets/src/components/layout/ExplainWithAI.tsx
@@ -2,10 +2,10 @@ import { AiSparkleFilledIcon, Button } from '@pluralsh/design-system'
 import { useTheme } from 'styled-components'
 import { useState } from 'react'
 
-const gradientBorder = (theme) => ({
+const gradientBorder = (theme, hover: boolean) => ({
   backgroundClip: 'padding-box, border-box',
   backgroundOrigin: 'border-box',
-  backgroundImage: `linear-gradient(${theme.colors['fill-zero']}, ${theme.colors['fill-zero']}), linear-gradient(to bottom, ${theme.colors.semanticBlue}, ${theme.colors['border-input']})`,
+  backgroundImage: `linear-gradient(${theme.colors[hover ? 'fill-zero-selected' : 'fill-zero']}, ${theme.colors[hover ? 'fill-zero-selected' : 'fill-zero']}), linear-gradient(to bottom, ${theme.colors.semanticBlue}, ${theme.colors['border-input']})`,
   border: '1px solid transparent',
 })
 
@@ -18,11 +18,14 @@ export default function ExplainWithAI() {
       onClick={() => setOpen(true)}
       secondary
       small
-      startIcon={<AiSparkleFilledIcon size={14} />}
+      startIcon={<AiSparkleFilledIcon size={13} />}
       marginRight={66}
       width={163}
       {...(open
-        ? { ...gradientBorder(theme), _hover: gradientBorder(theme) }
+        ? {
+            ...gradientBorder(theme, false),
+            _hover: gradientBorder(theme, true),
+          }
         : {})}
     >
       Explain with AI
