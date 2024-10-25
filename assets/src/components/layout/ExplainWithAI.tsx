d245bd66af88caeda5762dc0f6539265f314b862
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-10-25 10:30:13 +0200

    add context

--- assets/src/components/layout/ExplainWithAI.tsx
+++ assets/src/components/layout/ExplainWithAI.tsx
@@ -1,6 +1,39 @@
 import { AiSparkleFilledIcon, Button } from '@pluralsh/design-system'
 import { useTheme } from 'styled-components'
-import { useState } from 'react'
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
+
+type ExplainWithAIContextT = {
+  prompt?: string
+  setPrompt: Dispatch<SetStateAction<string | undefined>>
+}
+
+export const ExplainWithAIContext = createContext<
+  ExplainWithAIContextT | undefined
+>(undefined)
+
+export const useExplainWithAIContext = (prompt?: string) => {
+  const ctx = useContext(ExplainWithAIContext)
+
+  if (!ctx) {
+    console.warn(
+      'useExplainWithAIContext() must be used within a ExplainWithAIContext'
+    )
+  }
+  const { setPrompt } = ctx || {}
+
+  useEffect(() => {
+    setPrompt?.(prompt)
+
+    return () => setPrompt?.(undefined)
+  }, [setPrompt, prompt])
+}
 
 const gradientBorder = (theme, hover: boolean) => ({
   backgroundClip: 'padding-box, border-box',
