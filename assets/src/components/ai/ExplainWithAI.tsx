3b60588161f818acfbdd74b7ef71ee5f7b198385
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-10-25 13:21:50 +0200

    use query

--- assets/src/components/ai/ExplainWithAI.tsx
+++ assets/src/components/ai/ExplainWithAI.tsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 import AIButton from './ExplainWithAIButton.tsx'
 import AIPanel from './AIPanel.tsx'
 import { useExplainWithAIPrompt } from './ExplainWithAIContext.tsx'
+import { useAiQuery } from '../../generated/graphql.ts'
+import LoadingIndicator from '../utils/LoadingIndicator.tsx'
+import { useTheme } from 'styled-components'
 
 export default function ExplainWithAI() {
+  const theme = useTheme()
   const [open, setOpen] = useState(false)
   const prompt = useExplainWithAIPrompt()
 
   if (!prompt) return null
 
   return (
-    <div css={{ marginRight: 66, position: 'relative' }}>
+    <div
+      css={{
+        marginRight: 66,
+        position: 'relative',
+        zIndex: theme.zIndexes.modal,
+      }}
+    >
       <AIButton
         active={open}
         onClick={() => setOpen(true)}
@@ -18,15 +28,39 @@ export default function ExplainWithAI() {
       >
         Explain with AI
       </AIButton>
-      <AIPanel
+      <ExplainWithAIPanel
+        prompt={prompt}
         open={open}
         onClose={() => setOpen(false)}
-        showCloseIcon
-        header={'AI explain'}
-        subheader={'Learn more about the current page with AI'}
-      >
-        <div>{prompt}</div>
-      </AIPanel>
+      />
     </div>
+  )
+}
+
+function ExplainWithAIPanel({
+  prompt,
+  open,
+  onClose,
+}: {
+  prompt: string
+  open: boolean
+  onClose: () => void
+}) {
+  const { data, loading, error } = useAiQuery({ variables: { prompt } })
+
+  console.error(error)
+
+  return (
+    <AIPanel
+      open={open}
+      onClose={onClose}
+      showCloseIcon
+      header={'AI explain'}
+      subheader={'Learn more about the current page with AI'}
+    >
+      <div>{data?.ai}</div>
+      {loading && <LoadingIndicator></LoadingIndicator>}
+      {error && <div>{error.message}</div>}
+    </AIPanel>
   )
 }
