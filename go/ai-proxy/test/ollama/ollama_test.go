8bbbe59b4739483112634b3f9e4bb35b8b9f3560
Sebastian Florek <sebastian@plural.sh>
2024-10-28 18:36:07 +0100

    add simple unit test

---
+++ go/ai-proxy/test/ollama/ollama_test.go
@@ -0,0 +1,95 @@
+package ollama_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	ollamaapi "github.com/ollama/ollama/api"
+	"github.com/spf13/pflag"
+	"k8s.io/klog/v2"
+
+	"github.com/pluralsh/console/go/ai-proxy/test/helpers"
+)
+
+func init() {
+	_ = pflag.Set("provider", "ollama")
+	_ = pflag.Set("provider-host", "localhost:8080")
+	_ = pflag.Set("provider-token", "test")
+}
+
+var (
+	server         *httptest.Server
+	providerServer *httptest.Server
+	// TODO: for parallel runs concurrent map should be used
+	handlers = make(map[string]http.HandlerFunc)
+)
+
+func TestMain(m *testing.M) {
+	var err error
+	server, err = helpers.SetupServer()
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	providerServer, err = helpers.SetupProviderServer(handlers)
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	m.Run()
+}
+
+func TestOllamaAIProxy(t *testing.T) {
+	cases := []helpers.TestStruct[any, any]{
+		{
+			Name:     "chat request should return correct ollama response",
+			Method:   "POST",
+			Endpoint: "/api/chat",
+			Request: ollamaapi.ChatRequest{
+				Model: "testmodel",
+				Messages: []ollamaapi.Message{{
+					Role:    "user",
+					Content: "test prompt",
+				}},
+			},
+			WantData: ollamaapi.ChatResponse{
+				Model: "testmodel",
+				Message: ollamaapi.Message{
+					Role:    "user",
+					Content: "test response",
+				},
+			},
+			WantErr:    nil,
+			WantStatus: http.StatusOK,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			wantDataBytes, err := json.Marshal(tc.WantData)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			mockResponseFuck := helpers.MockResponse(tc.Endpoint, wantDataBytes, tc.WantErr, tc.WantStatus)
+			err = mockResponseFuck(handlers)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			requestFunc := helpers.CreateRequest(tc.Method, tc.Endpoint, tc.Request)
+			res, err := requestFunc(server, providerServer)
+			if !errors.Is(err, tc.WantErr) {
+				t.Fatalf("\nwant:\n%v\ngot:\n%v", tc.WantErr, err)
+			}
+
+			if !bytes.Equal(wantDataBytes, res) {
+				t.Errorf("\nwant:\n%s\ngot:\n%s", tc.WantData, res)
+			}
+		})
+	}
+}
