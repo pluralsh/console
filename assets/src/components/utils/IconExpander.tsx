08619fd865c3ccbc88c6351b7cee4e304bcdf6c1
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-10-28 12:14:27 +0100

    add animation

--- assets/src/components/utils/IconExpander.tsx
+++ assets/src/components/utils/IconExpander.tsx
@@ -1,6 +1,9 @@
 import {
   Accordion,
   AccordionItem,
+  AiSparkleFilledIcon,
+  AnimatedDiv,
+  Button,
   CloseIcon,
   IconFrame,
   Input,
@@ -15,6 +18,7 @@ import {
   useState,
 } from 'react'
 import styled, { CSSProperties, useTheme } from 'styled-components'
+import { useTransition } from 'react-spring'
 
 export function IconExpander({
   icon,
@@ -31,6 +35,28 @@ export function IconExpander({
   const theme = useTheme()
   // will hold the item's randomly assigned id if open, empty string if closed
   const [openItem, setOpenItem] = useState('')
+  const transitions = useTransition(active && !openItem ? [true] : [], {
+    from: { opacity: 0, scale: `40%` },
+    enter: { opacity: 1, scale: '100%' },
+    leave: { opacity: 0, scale: `40%` },
+    config: { duration: 200 },
+  })
+
+  const indicator = transitions((styles) => (
+    <AnimatedDiv
+      css={{
+        position: 'absolute',
+        right: -4,
+        top: -4,
+        height: 12,
+        width: 12,
+        backgroundColor: theme.colors['border-primary'],
+        borderRadius: '50%',
+        zIndex: 9999,
+      }}
+      style={styles}
+    ></AnimatedDiv>
+  ))
 
   return (
     <div css={{ position: 'relative' }}>
@@ -63,20 +89,7 @@ export function IconExpander({
           </div>
         </AccordionItem>
       </Accordion>
-      {active && !openItem && (
-        <div
-          css={{
-            position: 'absolute',
-            right: -4,
-            top: -4,
-            height: 12,
-            width: 12,
-            backgroundColor: theme.colors['border-primary'],
-            borderRadius: '50%',
-            zIndex: 9999,
-          }}
-        ></div>
-      )}
+      {indicator}
     </div>
   )
 }
