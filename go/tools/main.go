7401edea614e0fd5b96d6fe164375b7499937c57
Sebastian Florek <sebastian@plural.sh>
2024-10-29 13:17:34 +0100

    feat: add ai proxy (#1506)

--- go/tools/main.go
+++ go/tools/main.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/elastic/crd-ref-docs"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/vektra/mockery/v2"
+	_ "golang.org/x/tools/cmd/goimports"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/kustomize/v5"
