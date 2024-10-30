e2844a95fdf2b4f2e38ac56259f11df28c0d5432
seemywings <kevin.jayne@icloud.com>
2024-10-30 14:56:53 -0400

    add contour scraper

--- utils/compatibility/scrapers/contour.py
+++ utils/compatibility/scrapers/contour.py
@@ -1,15 +1,15 @@
-# scrapers/cert_manager.py
-
 from bs4 import BeautifulSoup
+from collections import OrderedDict
 from utils import (
     print_error,
     fetch_page,
     update_compatibility_info,
     update_chart_versions,
+    validate_semver,
 )
 
 app_name = "contour"
-compatibility_url = "https://projectcontour.io/resources/compatibility-matrix"
+compatibility_url = "https://projectcontour.io/resources/compatibility-matrix/"
 
 
 def parse_page(content):
@@ -20,11 +20,32 @@ def parse_page(content):
 
 def find_target_tables(sections):
     target_tables = []
+    for section in sections:
+        if section.get_text(strip=True) in [
+            "Compatibility Matrix",
+        ]:
+            table = section.find_next("table")
+            if table:
+                target_tables.append(table)
     return target_tables
 
 
 def extract_table_data(target_tables):
     rows = []
+    for row in target_tables[0].find_all("tr")[1:]:  # Skip the header row
+        columns = row.find_all("td")
+        app_version = validate_semver(columns[0].text)
+        kube_versions = columns[2].get_text(strip=True).split(", ")
+        if app_version:
+            version_info = OrderedDict(
+                [
+                    ("version", str(app_version)),
+                    ("kube", kube_versions),
+                    ("requirements", []),
+                    ("incompatibilities", []),
+                ]
+            )
+            rows.append(version_info)
     return rows
 
 
@@ -44,4 +65,4 @@ def scrape():
     else:
         print_error("No compatibility information found.")
 
-    # update_chart_versions(app_name)
+    update_chart_versions(app_name)
