4d334b6f7fd9aaf3c17e0381c46d3e2359510f81
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-11-06 16:16:05 +0100

    resource icons and links

--- assets/src/components/ai/AIThreadsTableEntry.tsx
+++ assets/src/components/ai/AIThreadsTableEntry.tsx
@@ -1,6 +1,7 @@
 import {
   AiSparkleFilledIcon,
   AiSparkleOutlineIcon,
+  ArrowTopRightIcon,
   ChatFilledIcon,
   ChatOutlineIcon,
   Chip,
@@ -15,57 +16,31 @@ import { StackedText } from 'components/utils/table/StackedText'
 
 import { CaptionP } from 'components/utils/typography/Text'
 
-import { MoreMenuTrigger } from 'components/utils/MoreMenu.tsx'
 import {
-  AiInsightFragment,
   AiInsightSummaryFragment,
   AiPinFragment,
   ChatThreadTinyFragment,
 } from 'generated/graphql'
-import { Dispatch, ReactElement, ReactNode } from 'react'
+import { ReactNode, useCallback } from 'react'
 import styled, { useTheme } from 'styled-components'
 import { dayjsExtended as dayjs } from 'utils/datetime.ts'
 import { useChatbot } from './AIContext.tsx'
-import { AiThreadsTableActions } from './AiThreadsTableActions'
 import { ClusterProviderIcon } from '../utils/Provider.tsx'
 import { StackTypeIcon } from '../stacks/common/StackTypeIcon.tsx'
+import { MoreMenuTrigger } from '../utils/MoreMenu.tsx'
+import { AiThreadsTableActions } from './AiThreadsTableActions.tsx'
+import { useNavigate } from 'react-router-dom'
+import {
+  getClusterDetailsPath,
+  getServiceComponentPath,
+  getServiceDetailsPath,
+} from '../../routes/cdRoutesConsts.tsx'
+import {
+  getStackRunsAbsPath,
+  getStacksAbsPath,
+} from '../../routes/stacksRoutesConsts.tsx'
 
-export function AIThreadsTableEntry({
-  item,
-  onClickPin,
-  pinLoading,
-  modal,
-}: {
-  item: ChatThreadTinyFragment | AiPinFragment
-  onClickPin?: () => void
-  pinLoading?: boolean
-  modal?: boolean | null
-}) {
-  const isPin = item.__typename === 'AiPin'
-  const isInsight = isPin && !item.thread
-  const thread = isPin ? item.thread : (item as ChatThreadTinyFragment)
-
-  return isInsight ? (
-    <InsightEntry
-      insight={item.insight}
-      modal={modal}
-      isPin={isPin}
-      pinLoading={pinLoading}
-      onClickPin={onClickPin}
-    />
-  ) : (
-    <ThreadEntry
-      thread={thread}
-      modal={modal}
-      isPin={isPin}
-      pinLoading={pinLoading}
-      onClickPin={onClickPin}
-      actions={<AiThreadsTableActions thread={thread} />}
-    />
-  )
-}
-
-const TableEntrySC = styled.div(({ theme }) => ({
+const AIThreadsTableEntrySC = styled.div(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   gap: theme.spacing.xlarge,
@@ -81,35 +56,46 @@ const TableEntrySC = styled.div(({ theme }) => ({
   },
 }))
 
-function TableEntry({
-  onClick,
-  icon,
-  title,
-  subtitle,
-  timestamp,
-  stale,
+export function AIThreadsTableEntry({
+  item,
   onClickPin,
   pinLoading,
-  pinned,
   modal,
-  actions,
 }: {
-  onClick: Dispatch<void>
-  icon: ReactElement
-  title: string
-  subtitle?: Nullable<string>
-  timestamp?: string
-  stale?: boolean
-  onClickPin?: Dispatch<void>
+  item: ChatThreadTinyFragment | AiPinFragment
+  onClickPin?: () => void
   pinLoading?: boolean
-  pinned?: boolean
-  actions?: ReactNode
-  modal?: Nullable<boolean>
-}): ReactNode {
+  modal?: boolean | null
+}) {
   const theme = useTheme()
+  const { goToThread, goToInsight } = useChatbot()
+
+  const isPin = item.__typename === 'AiPin'
+  const isInsight = isPin && !item.thread
+
+  const thread = isPin ? item.thread : (item as ChatThreadTinyFragment)
+  const insight = item.insight
+
+  const timestamp = isInsight
+    ? insight?.updatedAt || insight?.insertedAt || dayjs().toNow()
+    : thread?.lastMessageAt ||
+      thread?.updatedAt ||
+      thread?.insertedAt ||
+      dayjs().toNow()
+
+  const isStale = dayjs().isAfter(dayjs(timestamp).add(24, 'hours'))
+
+  // TODO: Memoization.
+
+  const onClick = useCallback(() => {
+    if (isInsight && insight) goToInsight(insight)
+    if (!isInsight && thread) goToThread(thread)
+  }, [isInsight, insight, goToInsight, thread, goToThread])
+
+  if ((isInsight && !insight) || (!isInsight && !thread)) return null
 
   return (
-    <TableEntrySC
+    <AIThreadsTableEntrySC
       onClick={(e) => {
         e.preventDefault()
         onClick()
@@ -121,34 +107,45 @@ function TableEntry({
       >
         <IconFrame
           type="floating"
-          icon={icon}
+          icon={
+            <TableEntryIcon
+              isInsight={isInsight}
+              isStale={isStale}
+              insight={insight}
+            />
+          }
         />
         <StackedText
           css={{ maxWidth: '450px', color: theme.colors['text'] }}
           truncate
-          first={title}
-          second={subtitle}
+          first={
+            isInsight
+              ? (insight?.summary?.substring(0, 250) ?? '')
+              : thread!.summary
+          }
+          second={<TableEntryResource insight={insight} />}
           firstPartialType="body1Bold"
         />
       </Flex>
-      <CaptionP css={{ opacity: stale ? 0.6 : 1 }}>
+      <CaptionP css={{ opacity: isStale ? 0.6 : 1 }}>
         {dayjs(timestamp).fromNow()}
       </CaptionP>
       {!modal && (
         <>
-          <Chip severity={stale ? 'neutral' : 'success'}>
-            {stale ? 'Stale' : 'Active'}
+          <Chip severity={isStale ? 'neutral' : 'success'}>
+            {isStale ? 'Stale' : 'Active'}
           </Chip>
           <IconFrame
             clickable
             onClick={(e) => {
               e.stopPropagation()
+              if (pinLoading) return
               onClickPin?.()
             }}
             icon={
               pinLoading ? (
                 <Spinner />
-              ) : pinned ? (
+              ) : isPin ? (
                 <PushPinFilledIcon color={theme.colors['icon-info']} />
               ) : (
                 <PushPinOutlineIcon />
@@ -157,127 +154,29 @@ function TableEntry({
           />
         </>
       )}
-      {actions}
-    </TableEntrySC>
-  )
-}
-
-function InsightEntry({
-  insight,
-  isPin,
-  pinLoading,
-  onClickPin,
-  modal,
-}: {
-  insight: Nullable<AiInsightFragment>
-  modal: Nullable<boolean>
-  isPin: boolean
-  pinLoading?: boolean
-  onClickPin?: Dispatch<void>
-}) {
-  const { goToInsight } = useChatbot()
-  const theme = useTheme()
-  const timestamp = insight?.updatedAt || insight?.insertedAt || dayjs().toNow()
-  const isStale = dayjs().isAfter(dayjs(timestamp).add(24, 'hours'))
-  const icon = isStale ? (
-    <AiSparkleOutlineIcon color={theme.colors['icon-light']} />
-  ) : (
-    <AiSparkleFilledIcon color={theme.colors['icon-info']} />
-  )
-
-  if (!insight) return null
-
-  return (
-    <TableEntry
-      onClick={() => goToInsight(insight)}
-      icon={icon}
-      title={insight.summary?.substring(0, 250) ?? ''}
-      subtitle={getInsightResourceName(insight)}
-      timestamp={timestamp}
-      pinned={isPin}
-      pinLoading={pinLoading}
-      onClickPin={() => {
-        if (pinLoading) return
-        onClickPin?.()
-      }}
-      stale={isStale}
-      modal={modal}
-      actions={
+      {isInsight ? (
         <MoreMenuTrigger
           disabled
           css={{ cursor: 'default' }}
         />
-      }
-    />
+      ) : (
+        <AiThreadsTableActions thread={thread} />
+      )}
+    </AIThreadsTableEntrySC>
   )
 }
 
-function ThreadEntry({
-  thread,
-  isPin,
-  pinLoading,
-  onClickPin,
-  modal,
-  actions,
+function TableEntryIcon({
+  isInsight,
+  isStale,
+  insight,
 }: {
-  thread: Nullable<ChatThreadTinyFragment>
-  isPin: boolean
-  pinLoading?: boolean
-  onClickPin?: Dispatch<void>
-  modal: Nullable<boolean>
-  actions: ReactNode
+  isInsight: boolean
+  isStale: boolean
+  insight: Nullable<AiInsightSummaryFragment>
 }): ReactNode {
-  const { goToThread } = useChatbot()
   const theme = useTheme()
-  const timestamp =
-    thread?.lastMessageAt ||
-    thread?.updatedAt ||
-    thread?.insertedAt ||
-    dayjs().toNow()
-  const isStale = dayjs().isAfter(dayjs(timestamp).add(24, 'hours'))
-  const icon = isStale ? (
-    <ChatOutlineIcon color={theme.colors['icon-light']} />
-  ) : (
-    <ChatFilledIcon color={theme.colors['icon-info']} />
-  )
 
-  if (!thread) return null
-
-  return (
-    <TableEntry
-      onClick={() => goToThread(thread)}
-      icon={icon}
-      title={thread.summary}
-      subtitle={getInsightResourceName(thread.insight)}
-      timestamp={timestamp}
-      pinned={isPin}
-      pinLoading={pinLoading}
-      onClickPin={() => {
-        if (pinLoading) return
-        onClickPin?.()
-      }}
-      stale={isStale}
-      modal={modal}
-      actions={actions}
-    />
-  )
-}
-
-export const getInsightResourceName = (
-  insight: Nullable<AiInsightSummaryFragment>
-): Nullable<string> =>
-  insight?.cluster?.name ||
-  insight?.clusterInsightComponent?.name ||
-  insight?.service?.name ||
-  insight?.serviceComponent?.name ||
-  insight?.stack?.name ||
-  insight?.stackRun?.message
-
-function TableEntryIcon(
-  isInsight: boolean,
-  isStale: boolean,
-  insight: Nullable<AiInsightSummaryFragment>
-): ReactNode {
   if (!!insight?.cluster)
     return <ClusterProviderIcon cluster={insight.cluster} />
 
@@ -296,7 +195,119 @@ function TableEntryIcon(
   if (!!insight?.stackRun)
     return <StackTypeIcon stackType={insight.stackRun.type} />
 
-  //   insight?.clusterInsightComponent?.name ||
+  // TODO: Add handler for insight?.clusterInsightComponent.
 
-  return <div>-</div>
+  return isInsight ? (
+    isStale ? (
+      <ChatOutlineIcon color={theme.colors['icon-light']} />
+    ) : (
+      <ChatFilledIcon color={theme.colors['icon-info']} />
+    )
+  ) : isStale ? (
+    <AiSparkleOutlineIcon color={theme.colors['icon-light']} />
+  ) : (
+    <AiSparkleFilledIcon color={theme.colors['icon-info']} />
+  )
+}
+
+export function TableEntryResource({
+  insight,
+}: {
+  insight: Nullable<AiInsightSummaryFragment>
+}): ReactNode {
+  if (!!insight?.cluster)
+    return (
+      <TableEntryResourceLink
+        path={[insight.cluster.name]}
+        url={getClusterDetailsPath({ clusterId: insight.cluster.id })}
+      />
+    )
+
+  if (!!insight?.clusterInsightComponent)
+    return (
+      <TableEntryResourceLink path={[insight.clusterInsightComponent.name]} />
+    )
+
+  if (!!insight?.service)
+    return (
+      <TableEntryResourceLink
+        path={[insight.service.cluster?.name, insight.service.name]}
+        url={getServiceDetailsPath({
+          clusterId: insight.service.cluster?.id,
+          serviceId: insight.service.id,
+        })}
+      />
+    )
+
+  if (!!insight?.serviceComponent)
+    return (
+      <TableEntryResourceLink
+        path={[
+          insight.serviceComponent.service?.cluster?.name,
+          insight.serviceComponent.service?.name,
+          insight.serviceComponent.name,
+        ]}
+        url={getServiceComponentPath({
+          clusterId: insight.serviceComponent.service?.cluster?.id,
+          serviceId: insight.serviceComponent.service?.id,
+          componentId: insight.serviceComponent.id,
+        })}
+      />
+    )
+
+  if (!!insight?.stack)
+    return (
+      <TableEntryResourceLink
+        path={[insight.stack.name]}
+        url={getStacksAbsPath(insight.stack.id)}
+      />
+    )
+
+  if (!!insight?.stackRun)
+    return (
+      <TableEntryResourceLink
+        path={[insight.stackRun.stack?.name, insight.stackRun.id]}
+        url={getStackRunsAbsPath(
+          insight.stackRun.stack?.id,
+          insight.stackRun.id
+        )}
+      />
+    )
+
+  return null
+}
+
+function TableEntryResourceLink({
+  path,
+  url,
+}: {
+  path: Nullable<string>[]
+  url?: string
+}) {
+  const theme = useTheme()
+  const navigate = useNavigate()
+
+  return (
+    <div
+      onClick={(e) => {
+        e.preventDefault() // FIXME
+        if (url) navigate(url)
+      }}
+      css={{
+        display: 'flex',
+        gap: theme.spacing.xsmall,
+
+        '&:hover': {
+          color: theme.colors.text,
+          textDecoration: 'underline',
+        },
+      }}
+    >
+      {path.join('  •  ')}
+      <ArrowTopRightIcon
+        color="icon-default"
+        size={12}
+      />
+    </div>
+  )
 }
