67068558d401dd0006425da06d6de2222e2ad988
michaeljguarino <mjg@plural.sh>
2024-11-08 12:03:54 -0500

    retry goth token fetches

---
+++ lib/console/retrier.ex
@@ -0,0 +1,30 @@
+defmodule Console.Retrier do
+  require Logger
+
+  defstruct [:retry, :res, max: 3, pause: 100]
+
+  def new(opts), do: struct(__MODULE__, opts)
+
+  def retry(fun, opts \\ []), do: do_retry(new(opts), fun)
+
+  defp do_retry(%__MODULE__{max: r, retry: r, res: res}, _), do: res
+  defp do_retry(%__MODULE__{retry: r, pause: p} = retrier, fun) do
+    if r > 0 do
+      :timer.sleep(p + jitter(p))
+    end
+
+    try do
+      case fun.() do
+        {:error, _} = res -> do_retry(%{retrier | retry: r + 1, res: res}, fun)
+        :error -> do_retry(%{retrier | retry: r + 1, res: :error}, fun)
+        res -> res
+      end
+    rescue
+      err ->
+        Logger.error(Exception.format(:error, err, __STACKTRACE__))
+        do_retry(%{retrier | retry: r + 1, res: {:error, {:exception, err}}}, fun)
+    end
+  end
+
+  defp jitter(pause), do: :rand.uniform(floor(pause / 2)) - :timer.seconds(floor(pause / 4))
+end
