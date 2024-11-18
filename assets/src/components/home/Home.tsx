4525ec34d2c86c0df8c1ed90d3223127d68f53e7
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-11-18 14:36:57 +0100

    fix styling

--- assets/src/components/home/Home.tsx
+++ assets/src/components/home/Home.tsx
@@ -1,6 +1,6 @@
 import { Breadcrumb, useSetBreadcrumbs } from '@pluralsh/design-system'
 import { ResponsivePageFullWidth } from 'components/utils/layout/ResponsivePageFullWidth'
-import styled, { useTheme } from 'styled-components'
+import { useTheme } from 'styled-components'
 import { useIsManager } from 'components/contexts'
 import { ClusterOverviewCard } from './clusteroverview/ClusterOverviewCard'
 import { DeploymentsCard } from './deployments/DeploymentsCard'
@@ -11,16 +11,21 @@ import { AiThreads } from './AiThreads.tsx'
 const breadcrumbs: Breadcrumb[] = [{ label: 'home', url: '/' }]
 
 export default function Home() {
-  useSetBreadcrumbs(breadcrumbs)
   const theme = useTheme()
   const isManager = useIsManager()
 
+  useSetBreadcrumbs(breadcrumbs)
+
   return (
-    <ResponsivePageFullWidth
-      noPadding
-      maxContentWidth={1440}
-    >
-      <HomeContentWrapperSC>
+    <ResponsivePageFullWidth maxContentWidth={1440}>
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: theme.spacing.large,
+          paddingBottom: theme.spacing.large,
+        }}
+      >
         <ClusterOverviewCard />
         <AiThreads />
         {isManager && <ConstraintViolationsCard />}
@@ -38,14 +43,7 @@ export default function Home() {
           <PrCard />
           <DeploymentsCard />
         </div>
-      </HomeContentWrapperSC>
+      </div>
     </ResponsivePageFullWidth>
   )
 }
-
-const HomeContentWrapperSC = styled.div(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: theme.spacing.large,
-  paddingBottom: theme.spacing.large,
-}))
