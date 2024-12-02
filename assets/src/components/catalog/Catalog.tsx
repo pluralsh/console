9573711d30c51e429f6d636b2c4d2d219e93f81d
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-12-02 14:11:14 +0100

    add content

--- assets/src/components/catalog/Catalog.tsx
+++ assets/src/components/catalog/Catalog.tsx
@@ -54,51 +54,57 @@ export function Catalog() {
         }}
       >
         <Flex height="100%">
-          <div
-            css={{
-              alignItems: 'center',
-              borderBottom: theme.borders['fill-two'],
-              display: 'flex',
-              gap: theme.spacing.large,
-              justifyContent: 'space-between',
-              paddingBottom: theme.spacing.large,
-              marginBottom: theme.spacing.large,
-            }}
+          <Flex
+            direction="column"
+            grow={1}
           >
-            <Flex
-              align="center"
-              gap="medium"
-            >
-              <AppIcon
-                size="xsmall"
-                url={catalogImageUrl(
-                  catalog.icon,
-                  catalog.darkIcon,
-                  theme.mode
-                )}
-                icon={<PrQueueIcon size={32} />}
-              />
-              <StackedText
-                first={catalog.name}
-                second={catalog.description}
-                firstPartialType="subtitle1"
-                secondPartialType="body2"
-              />
-            </Flex>
             <div
               css={{
+                alignItems: 'center',
+                borderBottom: theme.borders['fill-two'],
                 display: 'flex',
-                gap: theme.spacing.medium,
+                gap: theme.spacing.large,
+                justifyContent: 'space-between',
+                paddingBottom: theme.spacing.large,
+                marginBottom: theme.spacing.large,
               }}
             >
-              <Button
-                secondary
-                startIcon={<PersonIcon />}
+              <Flex
+                align="center"
+                gap="medium"
               >
-                Permissions
-              </Button>
+                <AppIcon
+                  size="xsmall"
+                  url={catalogImageUrl(
+                    catalog.icon,
+                    catalog.darkIcon,
+                    theme.mode
+                  )}
+                  icon={<PrQueueIcon size={32} />}
+                />
+                <StackedText
+                  first={catalog.name}
+                  second={catalog.description}
+                  firstPartialType="subtitle1"
+                  secondPartialType="body2"
+                />
+              </Flex>
+              <div
+                css={{
+                  display: 'flex',
+                  gap: theme.spacing.medium,
+                }}
+              >
+                <Button
+                  secondary
+                  startIcon={<PersonIcon />}
+                >
+                  Permissions
+                </Button>
+              </div>
             </div>
-          </div>
+            <div>...</div>
+          </Flex>
           <Sidecar
             marginLeft={'large'}
             width={220}
