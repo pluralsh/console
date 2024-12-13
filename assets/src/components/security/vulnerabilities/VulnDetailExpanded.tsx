90bcc23b201cb99db44f4cb8b5406ef6fe2ae1c4
Jake Laderman <jsladerman@gmail.com>
2024-12-13 18:15:48 -0500

    reflect new schema

--- assets/src/components/security/vulnerabilities/VulnDetailExpanded.tsx
+++ assets/src/components/security/vulnerabilities/VulnDetailExpanded.tsx
@@ -1,19 +1,14 @@
-// @ts-nocheck
-
 import { Chip, ChipProps, Flex } from '@pluralsh/design-system'
 import styled, { useTheme } from 'styled-components'
 
 import { Overline } from 'components/cd/utils/PermissionsModal'
 import { StackedText } from 'components/utils/table/StackedText'
 import { Body2BoldP } from 'components/utils/typography/Text'
-import { CvssBundle, VulnerabilityFragment } from 'generated/graphql'
-
-const AttackVector = {
-  PHYSICAL: 'PHYSICAL',
-  LOCAL: 'LOCAL',
-  ADJACENT: 'ADJACENT',
-  NETWORK: 'NETWORK',
-}
+import {
+  CvssBundle,
+  VulnAttackVector,
+  VulnerabilityFragment,
+} from 'generated/graphql'
 
 export function VulnDetailExpanded({ v }: { v: VulnerabilityFragment }) {
   const theme = useTheme()
@@ -21,7 +16,6 @@ export function VulnDetailExpanded({ v }: { v: VulnerabilityFragment }) {
     return <VulnerabilityDetailSC>No details available.</VulnerabilityDetailSC>
   }
 
-  console.log('v', v)
   return (
     <VulnerabilityDetailSC>
       <StackedText
@@ -31,10 +25,10 @@ export function VulnDetailExpanded({ v }: { v: VulnerabilityFragment }) {
         second={v.description}
         secondPartialType="body2"
       />
-      {/* <CVSSSection
+      <CVSSSection
         bundle={v.cvss}
         source={v.cvssSource}
-      /> */}
+      />
     </VulnerabilityDetailSC>
   )
 }
@@ -82,14 +76,19 @@ function CVSSRow({
   )
 }
 
-function _CVSSSection({
-  _bundle,
+function CVSSSection({
+  bundle,
   source,
 }: {
   bundle?: Nullable<CvssBundle>
   source?: Nullable<string>
 }) {
   const theme = useTheme()
+  const hasMetrics =
+    bundle &&
+    Object.values(bundle).some(
+      (v) => typeof v === 'string' && v !== 'CvssBundle'
+    )
 
   return (
     <Flex
@@ -105,110 +104,129 @@ function _CVSSSection({
           secondPartialType="body2"
         />
       )}
-      <Flex gap="xxxxlarge">
-        <Flex
-          direction="column"
-          gap="medium"
-        >
-          <Overline>Exploitability metrics</Overline>
-          <CVSSRow
-            text="Attack vector"
-            value={parsedVector.attackVector}
-            options={[
-              { name: 'Physical', value: AttackVector.PHYSICAL },
-              { name: 'Local', value: AttackVector.LOCAL },
-              { name: 'Adjacent Network', value: AttackVector.ADJACENT },
-              { name: 'Network', value: AttackVector.NETWORK },
-            ]}
-            colorMap={{
-              PHYSICAL: 'success',
-              LOCAL: 'warning',
-              ADJACENT: 'danger',
-              NETWORK: 'critical',
-            }}
-          />
-          <CVSSRow
-            text="Attack complexity"
-            value={parsedVector.attackComplexity}
-            options={[
-              { name: 'High', value: 'HIGH' },
-              { name: 'Low', value: 'LOW' },
-            ]}
-            colorMap={{ HIGH: 'warning', LOW: 'critical' }}
-          />
-          <CVSSRow
-            text="Privileges required"
-            value={parsedVector.privilegesRequired}
-            options={[
-              { name: 'High', value: 'HIGH' },
-              { name: 'Low', value: 'LOW' },
-              { name: 'None', value: 'NONE' },
-            ]}
-            colorMap={{
-              HIGH: 'warning',
-              LOW: 'danger',
-              NONE: 'critical',
-            }}
-          />
-          <CVSSRow
-            text="User interaction"
-            value={parsedVector.userInteraction}
-            options={[
-              { name: 'Required', value: 'REQUIRED' },
-              { name: 'None', value: 'NONE' },
-            ]}
-            colorMap={{ REQUIRED: 'warning', NONE: 'danger' }}
-          />
+      {hasMetrics && (
+        <Flex gap="xxxxlarge">
+          <Flex
+            direction="column"
+            gap="medium"
+          >
+            <Overline>Exploitability metrics</Overline>
+            {bundle?.attackVector && (
+              <CVSSRow
+                text="Attack vector"
+                value={bundle.attackVector}
+                options={[
+                  { name: 'Physical', value: VulnAttackVector.Physical },
+                  { name: 'Local', value: VulnAttackVector.Local },
+                  {
+                    name: 'Adjacent Network',
+                    value: VulnAttackVector.Adjacent,
+                  },
+                  { name: 'Network', value: VulnAttackVector.Network },
+                ]}
+                colorMap={{
+                  PHYSICAL: 'success',
+                  LOCAL: 'warning',
+                  ADJACENT: 'danger',
+                  NETWORK: 'critical',
+                }}
+              />
+            )}
+            {bundle?.attackComplexity && (
+              <CVSSRow
+                text="Attack complexity"
+                value={bundle.attackComplexity}
+                options={[
+                  { name: 'High', value: 'HIGH' },
+                  { name: 'Low', value: 'LOW' },
+                ]}
+                colorMap={{ HIGH: 'warning', LOW: 'critical' }}
+              />
+            )}
+            {bundle?.privilegesRequired && (
+              <CVSSRow
+                text="Privileges required"
+                value={bundle.privilegesRequired}
+                options={[
+                  { name: 'High', value: 'HIGH' },
+                  { name: 'Low', value: 'LOW' },
+                  { name: 'None', value: 'NONE' },
+                ]}
+                colorMap={{
+                  HIGH: 'warning',
+                  LOW: 'danger',
+                  NONE: 'critical',
+                }}
+              />
+            )}
+            {bundle?.userInteraction && (
+              <CVSSRow
+                text="User interaction"
+                value={bundle.userInteraction}
+                options={[
+                  { name: 'Required', value: 'REQUIRED' },
+                  { name: 'None', value: 'NONE' },
+                ]}
+                colorMap={{ REQUIRED: 'warning', NONE: 'danger' }}
+              />
+            )}
+          </Flex>
+          <Flex
+            direction="column"
+            gap="medium"
+          >
+            <Overline>Impact metrics</Overline>
+            {bundle?.confidentiality && (
+              <CVSSRow
+                text="Confidentiality"
+                value={bundle.confidentiality}
+                options={[
+                  { name: 'None', value: 'NONE' },
+                  { name: 'Low', value: 'LOW' },
+                  { name: 'High', value: 'HIGH' },
+                ]}
+                colorMap={{
+                  NONE: 'warning',
+                  LOW: 'danger',
+                  HIGH: 'critical',
+                }}
+              />
+            )}
+            {bundle?.integrity && (
+              <CVSSRow
+                text="Integrity"
+                value={bundle.integrity}
+                options={[
+                  { name: 'None', value: 'NONE' },
+                  { name: 'Low', value: 'LOW' },
+                  { name: 'High', value: 'HIGH' },
+                ]}
+                colorMap={{
+                  NONE: 'warning',
+                  LOW: 'danger',
+                  HIGH: 'critical',
+                }}
+              />
+            )}
+            {bundle?.availability && (
+              <CVSSRow
+                text="Availability"
+                value={bundle.availability}
+                options={[
+                  { name: 'None', value: 'NONE' },
+                  { name: 'Low', value: 'LOW' },
+                  { name: 'High', value: 'HIGH' },
+                ]}
+                colorMap={{
+                  NONE: 'warning',
+                  LOW: 'danger',
+                  HIGH: 'critical',
+                }}
+              />
+            )}
+          </Flex>
         </Flex>
-        <Flex
-          direction="column"
-          gap="medium"
-        >
-          <Overline>Impact metrics</Overline>
-          <CVSSRow
-            text="Confidentiality"
-            value={parsedVector.confidentiality}
-            options={[
-              { name: 'None', value: 'NONE' },
-              { name: 'Low', value: 'LOW' },
-              { name: 'High', value: 'HIGH' },
-            ]}
-            colorMap={{
-              NONE: 'warning',
-              LOW: 'danger',
-              HIGH: 'critical',
-            }}
-          />
-          <CVSSRow
-            text="Integrity"
-            value={parsedVector.integrity}
-            options={[
-              { name: 'None', value: 'NONE' },
-              { name: 'Low', value: 'LOW' },
-              { name: 'High', value: 'HIGH' },
-            ]}
-            colorMap={{
-              NONE: 'warning',
-              LOW: 'danger',
-              HIGH: 'critical',
-            }}
-          />
-          <CVSSRow
-            text="Availability"
-            value={parsedVector.availability}
-            options={[
-              { name: 'None', value: 'NONE' },
-              { name: 'Low', value: 'LOW' },
-              { name: 'High', value: 'HIGH' },
-            ]}
-            colorMap={{
-              NONE: 'warning',
-              LOW: 'danger',
-              HIGH: 'critical',
-            }}
-          />
-        </Flex>
-      </Flex>
+      )}
     </Flex>
   )
 }
