b1e50d68229e936ca1fe5a522c82a17e2237a065
Kevin Jayne <seemywingz@users.noreply.github.com>
2024-12-17 13:05:26 -0500

    update aws efs scraper (#1688)

--- utils/compatibility/scrapers/aws-efs-csi-driver.py
+++ utils/compatibility/scrapers/aws-efs-csi-driver.py
@@ -1,21 +1,24 @@
 import re
 from collections import OrderedDict
+from packaging.version import Version
 from utils import (
     print_error,
     update_compatibility_info,
     update_chart_versions,
     get_github_releases,
+    get_latest_github_release,
     expand_kube_versions,
     current_kube_version,
 )
 
 app_name = "aws-efs-csi-driver"
-compatibility_url = "https://github.com/kubernetes-sigs/aws-efs-csi-driver"
+compatibility_url = "https://github.com/kubernetes-sigs/aws-efs-csi-driver?tab=readme-ov-file#kubernetes-version-compability-matrix"
 
 
 def get_kube_versions(version):
     latest_kube_versions = expand_kube_versions("1.17", current_kube_version())
     compatibility = [
+        ("2.1.x", latest_kube_versions),
         ("2.0.x", latest_kube_versions),
         ("1.7.x", latest_kube_versions),
         ("1.6.x", latest_kube_versions),
@@ -30,7 +33,12 @@ def get_kube_versions(version):
         ("0.1.0", ["1.11", "1.12", "1.13"]),
     ]
 
+    latest = get_latest_github_release("kubernetes-sigs", "aws-efs-csi-driver")
+    if Version(version) >= Version(latest):
+        return latest_kube_versions
+
     for pattern, kube_versions in compatibility:
+        # handle version patterns like "2.1.x" and match them against actual versions like "2.1.0", "2.1.1", etc.
         if re.match(pattern.replace(".", r"\.").replace("x", r".*"), version):
             return kube_versions
 
@@ -41,6 +49,7 @@ def scrape():
     versions = []
     releases = get_github_releases("kubernetes-sigs", "aws-efs-csi-driver")
     for release in releases:
+        print(f"Processing release: {release}")
         if release.startswith("v"):
             ver = release.lstrip("v")
             kube_ver = get_kube_versions(ver)
