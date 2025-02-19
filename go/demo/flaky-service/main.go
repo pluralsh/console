53e5ecc8f9531836f2adf06da0f52541c5ff9fc9
Kinjal Haldar <30448446+kinjalh@users.noreply.github.com>
2025-02-19 17:09:07 -0500

    slog instead of klog in flaky service (#1928)

--- go/demo/flaky-service/main.go
+++ go/demo/flaky-service/main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/pluralsh/console/go/demo/flaky-service/api"
 	"github.com/pluralsh/console/go/demo/flaky-service/args"
-	"github.com/pluralsh/console/go/demo/flaky-service/internal/log"
 	"github.com/pluralsh/console/go/demo/flaky-service/metrics"
-
-	"k8s.io/klog/v2"
 )
 
 func main() {
-	klog.V(log.LogLevelMinimal).Info("Starting flaky service")
-
 	args.Init()
-	klog.V(log.LogLevelMinimal).InfoS("args", "BehaviorModifer", args.ResponseBehaviorModifier(), "BehaviorModifierTimestampModulus", args.BehaviorModifierTimestampModulus())
+
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})
+	slog.SetDefault(slog.New(handler))
+
+	slog.Info("Starting flaky service", "BehaviorModifer", args.ResponseBehaviorModifier(), "BehaviorModifierTimestampModulus", args.BehaviorModifierTimestampModulus())
 
 	go api.StartApiServer(args.ApiAddress(), args.ApiPath(), args.ResponseBehaviorModifier(), args.BehaviorModifierTimestampModulus())
 	go metrics.StartMetricsServer(args.MetricsAddress(), args.MetricsPath())
--- go/demo/flaky-service/metrics/prometheus_metric_server.go
+++ go/demo/flaky-service/metrics/prometheus_metric_server.go
@@ -1,14 +1,12 @@
 package metrics
 
 import (
+	"log/slog"
 	"net/http"
 	"strconv"
 
-	"github.com/pluralsh/console/go/demo/flaky-service/internal/log"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-
-	"k8s.io/klog/v2"
 )
 
 var requestCounter = prometheus.NewCounterVec(
@@ -26,16 +24,16 @@ func IncrementRequestCounter(code int, method string) {
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-	klog.V(log.LogLevelMinimal).Info("Handling metrics request")
+	slog.Info("Handling metrics request")
 	promhttp.HandlerFor(registry, promhttp.HandlerOpts{}).ServeHTTP(w, r)
 }
 
 func StartMetricsServer(address string, metricsPath string) {
-	klog.V(log.LogLevelMinimal).InfoS("Starting metrics server", "address", address, "metricsPath", metricsPath)
+	slog.Info("Starting metrics server", "address", address, "metricsPath", metricsPath)
 
 	registry.MustRegister(requestCounter)
 	http.HandleFunc(metricsPath, handleRequest)
 	if err := http.ListenAndServe(address, nil); err != nil {
-		klog.ErrorS(err, "Could not start metrics server")
+		slog.Error("Could not start metrics server", "error", err)
 	}
 }
