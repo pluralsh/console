7e8c1740f83fc60d6744367162dbb9fc5e108a20
Sebastian Florek <sebastian@plural.sh>
2025-02-24 10:55:06 +0100

    update violations UI

--- assets/src/components/stacks/run/violations/Violations.tsx
+++ assets/src/components/stacks/run/violations/Violations.tsx
@@ -1,17 +1,19 @@
 import { Chip, Code, Table, Tooltip } from '@pluralsh/design-system'
 import { createColumnHelper, Row } from '@tanstack/react-table'
 import { DateTimeCol } from 'components/utils/table/DateTimeCol.tsx'
+import dayjs from 'dayjs'
 import { ReactNode } from 'react'
-import { Link, useOutletContext } from 'react-router-dom'
+import { useOutletContext } from 'react-router-dom'
+import { useTheme } from 'styled-components'
 import {
   StackPolicyViolation,
   VulnSeverity,
 } from '../../../../generated/graphql.ts'
 import { ColExpander } from '../../../cd/cluster/pod/PodContainers.tsx'
 import { vulnSeverityToChipSeverity } from '../../../security/vulnerabilities/VulnReportDetailsTableCols.tsx'
-import { SubTitle } from '../../../utils/SubTitle.tsx'
 import { StackedText } from '../../../utils/table/StackedText.tsx'
 import { DEFAULT_REACT_VIRTUAL_OPTIONS } from '../../../utils/table/useFetchPaginatedData.tsx'
+import { TooltipTime } from '../../../utils/TooltipTime.tsx'
 import { InlineLink } from '../../../utils/typography/InlineLink.tsx'
 import { StackRunOutletContextT } from '../Route.tsx'
 
@@ -28,7 +30,18 @@ const columns = [
       truncate: true,
       gridTemplate: 'minmax(150px, .5fr)',
     },
-    cell: ({ getValue }) => getValue()?.policyId,
+    cell: ({ getValue }) => (
+      <StackedText
+        first={getValue()?.policyId}
+        second={
+          <TooltipTime date={getValue()?.updatedAt ?? getValue()?.insertedAt}>
+            {dayjs(getValue()?.updatedAt ?? getValue()?.insertedAt).format(
+              'MMM D, YYYY h:mm A'
+            )}
+          </TooltipTime>
+        }
+      />
+    ),
   }),
   columnHelper.accessor((violation) => violation, {
     id: 'title',
@@ -126,6 +139,7 @@ function ViolationExpansionPanel({
 }: {
   row: Row<StackPolicyViolation>
 }): ReactNode {
+  const theme = useTheme()
   const violation = row.original
   const policyUrl = violation.policyUrl ?? 'N/A'
 
@@ -134,21 +148,26 @@ function ViolationExpansionPanel({
       css={{
         display: 'flex',
         flexDirection: 'column',
-        gap: '24px',
+        gap: theme.spacing.large,
       }}
     >
-      <SubTitle
-        css={{
-          marginBottom: 0,
-        }}
-      >
-        Violation Details
-      </SubTitle>
+      <div>
+        <h3
+          css={{
+            ...theme.partials.text.title2,
+            color: theme.colors.text,
+            marginBottom: 0,
+          }}
+        >
+          {violation.policyId}
+        </h3>
+        <span>{violation.title}</span>
+      </div>
       <div
         css={{
-          display: 'grid',
+          display: 'flex',
           flexWrap: 'wrap',
-          gap: '16px',
+          gap: theme.spacing.xlarge,
         }}
       >
         <StackedText
@@ -171,27 +190,66 @@ function ViolationExpansionPanel({
           }
         />
         <StackedText
+          first="Policy module"
+          second={violation.policyModule}
+        />
+        <StackedText
           first="Resolution"
           second={violation.resolution}
         />
+        <StackedText
+          first="Severity"
+          second={
+            <Chip
+              severity={
+                vulnSeverityToChipSeverity[violation.severity ?? 'Unknown']
+              }
+            >
+              {violation.severity}
+            </Chip>
+          }
+        />
       </div>
       {(violation?.causes?.length ?? 0) > 0 && (
-        <div>
-          <SubTitle>Causes</SubTitle>
-          <span>
-            The code below shows the lines of code that caused the security
-            violation.
-          </span>
+        <div
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: theme.spacing.xlarge,
+          }}
+        >
+          <div>
+            <h3
+              css={{
+                ...theme.partials.text.title2,
+                color: theme.colors.text,
+              }}
+            >
+              Causes
+            </h3>
+            <span
+              css={{
+                ...theme.partials.text.body2LooseLineHeight,
+              }}
+            >
+              The code below shows the lines of code that caused the security
+              violation.
+            </span>
+          </div>
+
           {violation?.causes?.map((v) => {
             const firstLine = v?.lines?.find((l) => l?.first)?.line ?? 0
             const lastLine = v?.lines?.find((l) => l?.last)?.line ?? 0
 
             return (
               <div>
-                <Code language="terraform">
+                <Code
+                  language="terraform"
+                  title={v?.filename}
+                >
                   {v?.lines
                     ?.map((l) => {
-                      const isEmpty = l?.content == 'N/A'
+                      const isEmpty = l?.content == '..'
                       const firstChar = l?.first ? '┌' : ''
                       const lastChar = l?.last ? '└' : ''
                       const line = l?.line ?? 0
