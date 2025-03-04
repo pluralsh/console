f5080bedacb4cd3018ca4a2a0f244a513b345813
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-03-04 17:00:17 +0100

    apply review suggestions

--- assets/src/components/kubernetes/cluster/PodDisruptionBudget.tsx
+++ assets/src/components/kubernetes/cluster/PodDisruptionBudget.tsx
@@ -63,8 +63,6 @@ export default function PodDisruptionBudget(): ReactElement<any> {
 
   if (loading) return <LoadingIndicator />
 
-  console.log(pdb)
-
   return (
     <ResourceDetails
       tabs={directory}
