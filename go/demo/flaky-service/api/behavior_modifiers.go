c93660c85ee973746a4df1a7791a85aef89fc215
Michael Guarino <mjg@plural.sh>
2025-03-06 16:56:32 +0000

    Fix HTTP 500 Internal Server Errors in flaky-service

--- go/demo/flaky-service/api/behavior_modifiers.go
+++ go/demo/flaky-service/api/behavior_modifiers.go
@@ -1,10 +1,8 @@
 package api
 
 import (
-	"fmt"
 	"log/slog"
 	"net/http"
-	"runtime/debug"
 	"time"
 
 	"github.com/pluralsh/console/go/demo/flaky-service/metrics"
@@ -25,22 +23,13 @@ func HandleRequestTimestampModulus(timestampModulus int64) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("Handling API request using timestamp")
 		time_now := time.Now().UnixNano() / int64(time.Millisecond)
+	
+		// Removing modulus check, always succeed
+		slog.Info("Returning status.OK", "time_now", time_now)
 
-		if time_now%timestampModulus == 0 {
-			slog.Error("found unknown error, returning status.InternalServerError", "time_now", time_now, "modulus", timestampModulus)
-			slog.Error(fmt.Sprintf("printing stacktrace: %s", string(debug.Stack())))
-
-			metrics.IncrementRequestCounter(http.StatusInternalServerError, r.Method)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Header().Set("Content-Type", "application/json")
-			w.Write([]byte(`{"message": "req failed"}`))
-		} else {
-			slog.Info("Everything seems fine, returning status.OK", "time_now", time_now, "modulus", timestampModulus)
-
-			metrics.IncrementRequestCounter(http.StatusOK, r.Method)
-			w.WriteHeader(http.StatusOK)
-			w.Header().Set("Content-Type", "application/json")
-			w.Write([]byte(`{"message": "req processed successfully"}`))
-		}
+		metrics.IncrementRequestCounter(http.StatusOK, r.Method)
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"message": "req processed successfully"}`))
 	}
 }
