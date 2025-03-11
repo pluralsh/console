62d962972b705ceab6dba1bf971d106147c58ab9
Lukasz Zajaczkowski <zreigz@gmail.com>
2025-03-11 15:20:40 +0100

    Flow controller (#1987)

---
+++ go/controller/internal/controller/flow_controller.go
@@ -0,0 +1,192 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+
+	"github.com/pluralsh/console/go/controller/api/v1alpha1"
+	"github.com/pluralsh/console/go/controller/internal/cache"
+	consoleclient "github.com/pluralsh/console/go/controller/internal/client"
+	"github.com/pluralsh/console/go/controller/internal/credentials"
+	"github.com/pluralsh/console/go/controller/internal/utils"
+	"k8s.io/apimachinery/pkg/api/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+const (
+	FlowFinalizer = "deployments.plural.sh/flow-protection"
+)
+
+// FlowReconciler reconciles a Flow object
+type FlowReconciler struct {
+	client.Client
+	Scheme           *runtime.Scheme
+	ConsoleClient    consoleclient.ConsoleClient
+	CredentialsCache credentials.NamespaceCredentialsCache
+	UserGroupCache   cache.UserGroupCache
+}
+
+//+kubebuilder:rbac:groups=deployments.plural.sh,resources=flows,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=deployments.plural.sh,resources=flows/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=deployments.plural.sh,resources=flows/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
+func (r *FlowReconciler) Reconcile(ctx context.Context, req ctrl.Request) (_ ctrl.Result, reterr error) {
+	logger := log.FromContext(ctx)
+
+	flow := &v1alpha1.Flow{}
+	if err := r.Get(ctx, req.NamespacedName, flow); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	utils.MarkCondition(flow.SetCondition, v1alpha1.ReadyConditionType, v1.ConditionFalse, v1alpha1.ReadyConditionReason, "")
+
+	scope, err := NewDefaultScope(ctx, r.Client, flow)
+	if err != nil {
+		utils.MarkCondition(flow.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionFalse, v1alpha1.SynchronizedConditionReasonError, err.Error())
+		return ctrl.Result{}, err
+	}
+	// Always patch object when exiting this function, so we can persist any object changes.
+	defer func() {
+		if err := scope.PatchObject(); err != nil && reterr == nil {
+			reterr = err
+		}
+	}()
+
+	// Switch to namespace credentials if configured. This has to be done before sending any request to the console.
+	nc, err := r.ConsoleClient.UseCredentials(req.Namespace, r.CredentialsCache)
+	credentials.SyncCredentialsInfo(flow, flow.SetCondition, nc, err)
+	if err != nil {
+		logger.Error(err, "failed to use namespace credentials", "namespaceCredentials", nc, "namespacedName", req.NamespacedName)
+		utils.MarkCondition(flow.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionFalse, v1alpha1.SynchronizedConditionReasonError, fmt.Sprintf("failed to use %s namespace credentials: %s", nc, err.Error()))
+		return ctrl.Result{}, err
+	}
+
+	if !flow.GetDeletionTimestamp().IsZero() {
+		return ctrl.Result{}, r.handleDelete(ctx, flow)
+	}
+
+	if err = r.ensure(flow); err != nil {
+		return handleRequeue(nil, err, flow.SetCondition)
+	}
+	changed, sha, err := flow.Diff(utils.HashObject)
+	if err != nil {
+		logger.Error(err, "unable to calculate flow SHA")
+		utils.MarkCondition(flow.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionFalse, v1alpha1.SynchronizedConditionReasonError, err.Error())
+		return ctrl.Result{}, err
+	}
+	if changed {
+		projectID, res, err := r.getProjectIdAndSetOwnerRef(ctx, flow)
+		if res != nil || err != nil {
+			return handleRequeue(res, err, flow.SetCondition)
+		}
+
+		apiFlow, err := r.ConsoleClient.UpsertFlow(ctx, flow.Attributes(projectID))
+		if err != nil {
+			logger.Error(err, "unable to create or update flow")
+			utils.MarkCondition(flow.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionFalse, v1alpha1.SynchronizedConditionReasonError, err.Error())
+			return ctrl.Result{}, err
+		}
+		if !controllerutil.ContainsFinalizer(flow, FlowFinalizer) {
+			controllerutil.AddFinalizer(flow, FlowFinalizer)
+		}
+		flow.Status.ID = &apiFlow.ID
+		flow.Status.SHA = &sha
+	}
+	utils.MarkCondition(flow.SetCondition, v1alpha1.ReadyConditionType, v1.ConditionTrue, v1alpha1.ReadyConditionReason, "")
+	utils.MarkCondition(flow.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionTrue, v1alpha1.SynchronizedConditionReason, "")
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *FlowReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		WithOptions(controller.Options{MaxConcurrentReconciles: 1}).
+		Watches(&v1alpha1.NamespaceCredentials{}, credentials.OnCredentialsChange(r.Client, new(v1alpha1.FlowList))).
+		For(&v1alpha1.Flow{}, builder.WithPredicates(predicate.ResourceVersionChangedPredicate{})).
+		Complete(r)
+}
+
+func (r *FlowReconciler) handleDelete(ctx context.Context, flow *v1alpha1.Flow) error {
+	if controllerutil.ContainsFinalizer(flow, FlowFinalizer) {
+		if flow.Status.GetID() != "" {
+			existingFlow, err := r.ConsoleClient.GetFlow(ctx, flow.Status.GetID())
+			if err != nil && !errors.IsNotFound(err) {
+				utils.MarkCondition(flow.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionFalse, v1alpha1.SynchronizedConditionReasonError, err.Error())
+				return err
+			}
+			if existingFlow != nil {
+				if err := r.ConsoleClient.DeleteFlow(ctx, flow.Status.GetID()); err != nil {
+					utils.MarkCondition(flow.SetCondition, v1alpha1.SynchronizedConditionType, v1.ConditionFalse, v1alpha1.SynchronizedConditionReasonError, err.Error())
+					return err
+				}
+			}
+		}
+		controllerutil.RemoveFinalizer(flow, FlowFinalizer)
+	}
+	return nil
+}
+
+// ensure makes sure that user-friendly input such as userEmail/groupName in
+// bindings are transformed into valid IDs on the v1alpha1.Binding object before creation
+func (r *FlowReconciler) ensure(flow *v1alpha1.Flow) error {
+	if flow.Spec.Bindings == nil {
+		return nil
+	}
+	bindings, req, err := ensureBindings(flow.Spec.Bindings.Read, r.UserGroupCache)
+	if err != nil {
+		return err
+	}
+	flow.Spec.Bindings.Read = bindings
+
+	bindings, req2, err := ensureBindings(flow.Spec.Bindings.Write, r.UserGroupCache)
+	if err != nil {
+		return err
+	}
+	flow.Spec.Bindings.Write = bindings
+
+	if req || req2 {
+		return apierrors.NewNotFound(schema.GroupResource{}, "bindings")
+	}
+
+	return nil
+}
+
+func (r *FlowReconciler) getProjectIdAndSetOwnerRef(ctx context.Context, flow *v1alpha1.Flow) (*string, *ctrl.Result, error) {
+	if !flow.Spec.HasProjectRef() {
+		return nil, nil, nil
+	}
+
+	project := &v1alpha1.Project{}
+	if err := r.Get(ctx, types.NamespacedName{Name: flow.Spec.ProjectRef.Name}, project); err != nil {
+		return nil, nil, err
+	}
+
+	if !project.Status.HasID() {
+		return nil, &waitForResources, fmt.Errorf("project is not ready")
+	}
+
+	if err := controllerutil.SetOwnerReference(project, flow, r.Scheme); err != nil {
+		return nil, nil, fmt.Errorf("could not set owner reference: %+v", err)
+	}
+
+	return project.Status.ID, nil, nil
+}
---
+++ go/controller/internal/controller/flow_controller_test.go
@@ -0,0 +1,202 @@
+package controller_test
+
+import (
+	"context"
+
+	"github.com/Yamashou/gqlgenc/clientv2"
+	"github.com/pluralsh/console/go/controller/internal/cache"
+	"github.com/vektah/gqlparser/v2/gqlerror"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/mock"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	gqlclient "github.com/pluralsh/console/go/client"
+	"github.com/pluralsh/console/go/controller/api/v1alpha1"
+	"github.com/pluralsh/console/go/controller/internal/controller"
+	common "github.com/pluralsh/console/go/controller/internal/test/common"
+	"github.com/pluralsh/console/go/controller/internal/test/mocks"
+)
+
+var _ = Describe("Flow Controller", Ordered, func() {
+	Context("When reconciling a resource", func() {
+		const (
+			flowName  = "test"
+			namespace = "default"
+			id        = "123"
+		)
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      flowName,
+			Namespace: namespace,
+		}
+
+		BeforeAll(func() {
+			By("creating the custom resource for the Kind Flow")
+			flow := &v1alpha1.Flow{}
+			err := k8sClient.Get(ctx, typeNamespacedName, flow)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &v1alpha1.Flow{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      flowName,
+						Namespace: namespace,
+					},
+					Spec: v1alpha1.FlowSpec{
+						Name: lo.ToPtr(flowName),
+					},
+				}
+				Expect(common.MaybeCreate(k8sClient, resource, nil)).To(Succeed())
+			}
+		})
+
+		AfterAll(func() {
+			flow := &v1alpha1.Flow{}
+			if err := k8sClient.Get(ctx, typeNamespacedName, flow); err == nil {
+				By("Cleanup the specific resource instance Flow")
+				Expect(k8sClient.Delete(ctx, flow)).To(Succeed())
+			}
+		})
+
+		It("should successfully reconcile the resource", func() {
+			By("Create resource")
+			test := struct {
+				flowFragment   *gqlclient.FlowFragment
+				expectedStatus v1alpha1.Status
+			}{
+				expectedStatus: v1alpha1.Status{
+					ID:  lo.ToPtr("123"),
+					SHA: lo.ToPtr("PWP5EBI7YMVTF7VLCCKG7K3LXEKCNK36HGVFIOJIT3MJFBSKVEOQ===="),
+					Conditions: []metav1.Condition{
+						{
+							Type:    v1alpha1.NamespacedCredentialsConditionType.String(),
+							Status:  metav1.ConditionFalse,
+							Reason:  v1alpha1.NamespacedCredentialsReasonDefault.String(),
+							Message: v1alpha1.NamespacedCredentialsConditionMessage.String(),
+						},
+						{
+							Type:    v1alpha1.ReadyConditionType.String(),
+							Status:  metav1.ConditionTrue,
+							Reason:  v1alpha1.ReadyConditionReason.String(),
+							Message: "",
+						},
+						{
+							Type:   v1alpha1.SynchronizedConditionType.String(),
+							Status: metav1.ConditionTrue,
+							Reason: v1alpha1.SynchronizedConditionReason.String(),
+						},
+					},
+				},
+				flowFragment: &gqlclient.FlowFragment{
+					ID: id,
+				},
+			}
+
+			fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
+			fakeConsoleClient.On("UseCredentials", mock.Anything, mock.Anything).Return("", nil)
+			fakeConsoleClient.On("GetFlow", mock.Anything, mock.Anything, mock.Anything).Return(nil, errors.NewNotFound(schema.GroupResource{}, id))
+			fakeConsoleClient.On("UpsertFlow", mock.Anything, mock.Anything).Return(test.flowFragment, nil)
+
+			nr := &controller.FlowReconciler{
+				Client:        k8sClient,
+				Scheme:        k8sClient.Scheme(),
+				ConsoleClient: fakeConsoleClient,
+			}
+
+			_, err := nr.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+
+			Expect(err).NotTo(HaveOccurred())
+
+			flw := &v1alpha1.Flow{}
+			err = k8sClient.Get(ctx, typeNamespacedName, flw)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(common.SanitizeStatusConditions(flw.Status)).To(Equal(common.SanitizeStatusConditions(test.expectedStatus)))
+		})
+
+		It("should requeue when binding is not ready", func() {
+			Expect(common.MaybePatchObject(k8sClient, &v1alpha1.Flow{
+				ObjectMeta: metav1.ObjectMeta{Name: flowName, Namespace: namespace},
+			}, func(p *v1alpha1.Flow) {
+				p.Spec.Bindings = &v1alpha1.Bindings{
+					Read: []v1alpha1.Binding{
+						{
+							UserEmail: lo.ToPtr("user@example.com"),
+						},
+					},
+				}
+			})).To(Succeed())
+
+			fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
+			fakeConsoleClient.On("UseCredentials", mock.Anything, mock.Anything).Return("", nil)
+			fakeConsoleClient.On("GetFlow", mock.Anything, mock.Anything, mock.Anything).Return(nil, errors.NewNotFound(schema.GroupResource{}, id))
+			fakeConsoleClient.On("GetUser", mock.Anything).Return(nil, &clientv2.ErrorResponse{
+				GqlErrors: &gqlerror.List{gqlerror.Errorf("%s", "could not find resource")},
+			})
+
+			fr := &controller.FlowReconciler{
+				Client:         k8sClient,
+				Scheme:         k8sClient.Scheme(),
+				ConsoleClient:  fakeConsoleClient,
+				UserGroupCache: cache.NewUserGroupCache(fakeConsoleClient),
+			}
+
+			result, err := fr.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result.RequeueAfter).ToNot(BeZero())
+		})
+
+		It("should successfully reconcile the resource", func() {
+			By("Delete resource")
+			Expect(common.MaybePatch(k8sClient, &v1alpha1.Flow{
+				ObjectMeta: metav1.ObjectMeta{Name: flowName, Namespace: namespace},
+			}, func(p *v1alpha1.Flow) {
+				p.Status.ID = lo.ToPtr(id)
+				p.Status.SHA = lo.ToPtr("WAXTBLTM6PFWW6BBRLCPV2ILX2J4EOHQKDISWH4QAM5IODNRMBJQ====")
+			})).To(Succeed())
+			resource := &v1alpha1.Flow{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+			err = k8sClient.Delete(ctx, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			flowFragment := &gqlclient.FlowFragment{
+				ID: id,
+			}
+			fakeConsoleClient := mocks.NewConsoleClientMock(mocks.TestingT)
+			fakeConsoleClient.On("UseCredentials", mock.Anything, mock.Anything).Return("", nil)
+			fakeConsoleClient.On("GetFlow", mock.Anything, mock.Anything, mock.Anything).Return(flowFragment, nil)
+			fakeConsoleClient.On("DeleteFlow", mock.Anything, mock.Anything).Return(nil)
+
+			nsReconciler := &controller.FlowReconciler{
+				Client:        k8sClient,
+				Scheme:        k8sClient.Scheme(),
+				ConsoleClient: fakeConsoleClient,
+			}
+
+			_, err = nsReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+
+			Expect(err).NotTo(HaveOccurred())
+
+			flow := &v1alpha1.Flow{}
+			err = k8sClient.Get(ctx, typeNamespacedName, flow)
+
+			Expect(err.Error()).To(Equal("flows.deployments.plural.sh \"test\" not found"))
+		})
+
+	})
+
+})
