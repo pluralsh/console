2e98ee02c74518d046c465d9d585c241cd3f109d
Jake Laderman <jsladerman@gmail.com>
2025-04-14 12:34:44 -0400

    network tab

--- assets/src/components/cd/cluster/ClusterNetwork.tsx
+++ assets/src/components/cd/cluster/ClusterNetwork.tsx
@@ -1,10 +1,60 @@
+import { useThrottle } from 'components/hooks/useThrottle'
+import { GqlError } from 'components/utils/Alert'
 import { NetworkGraph } from 'components/utils/NetworkGraph'
+import Fuse from 'fuse.js'
+import {
+  NetworkMeshEdgeFragment,
+  useClusterNetworkGraphQuery,
+} from 'generated/graphql'
+import { useMemo, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { isNonNullable } from 'utils/isNonNullable'
+
+const searchOptions: Fuse.IFuseOptions<NetworkMeshEdgeFragment> = {
+  keys: ['from.name', 'from.service', 'to.name', 'to.service'],
+  threshold: 0.25,
+  ignoreLocation: true,
+}
 
 export function ClusterNetwork() {
+  const { clusterId = '' } = useParams()
+  const [q, setQ] = useState('')
+  const throttledQ = useThrottle(q, 500)
+  const [namespace, setNamespace] = useState<string | undefined>(undefined)
+  const [timestamp, setTimestamp] = useState<string | undefined>(undefined)
+  const { data, loading, error } = useClusterNetworkGraphQuery({
+    variables: { clusterId, namespace, time: timestamp },
+    fetchPolicy: 'cache-and-network',
+  })
+
+  const network = useMemo(() => {
+    const filteredData =
+      data?.cluster?.networkGraph
+        ?.filter(isNonNullable)
+        .filter(
+          (edge) =>
+            !namespace ||
+            edge.from.namespace === namespace ||
+            edge.to.namespace === namespace
+        ) ?? []
+    return throttledQ
+      ? new Fuse(filteredData, searchOptions)
+          .search(throttledQ)
+          .map(({ item }) => item)
+      : filteredData
+  }, [data?.cluster?.networkGraph, namespace, throttledQ])
+
+  if (error) return <GqlError error={error} />
+
   return (
-    <div>
-      cluster network graph
-      <NetworkGraph />
-    </div>
+    <NetworkGraph
+      networkData={network}
+      loading={!data && loading}
+      q={q}
+      setQ={setQ}
+      namespace={namespace}
+      setNamespace={setNamespace}
+      setTimestamp={setTimestamp}
+    />
   )
 }
--- assets/src/components/utils/NetworkGraph.tsx
+++ assets/src/components/utils/NetworkGraph.tsx
@@ -1,3 +1,95 @@
-export function NetworkGraph() {
-  return <div>NetworkGraph</div>
+import {
+  Card,
+  EmptyState,
+  Flex,
+  Input,
+  NamespaceIcon,
+  SearchIcon,
+} from '@pluralsh/design-system'
+import { NamespaceFilter } from 'components/kubernetes/common/NamespaceFilter'
+import {
+  NetworkMeshEdgeFragment,
+  useClusterNamespacesQuery,
+} from 'generated/graphql'
+import { isEmpty } from 'lodash'
+import { useParams } from 'react-router-dom'
+import styled, { useTheme } from 'styled-components'
+import { isNonNullable } from 'utils/isNonNullable'
+import LoadingIndicator from './LoadingIndicator'
+import { TimestampSliderButton } from './TimestampSlider'
+
+export function NetworkGraph({
+  networkData,
+  loading,
+  q,
+  setQ,
+  namespace,
+  setNamespace,
+  setTimestamp,
+}: {
+  networkData: NetworkMeshEdgeFragment[]
+  loading?: boolean
+  q: string
+  setQ: (q: string) => void
+  namespace?: string
+  setNamespace: (namespace?: string) => void
+  setTimestamp: (timestamp: string | undefined) => void
+}) {
+  const { colors } = useTheme()
+  const { clusterId } = useParams()
+  const { data: namespacesData, error: namespacesError } =
+    useClusterNamespacesQuery({
+      variables: { clusterId },
+    })
+  const namespaces =
+    namespacesData?.namespaces
+      ?.filter(isNonNullable)
+      .map((ns) => ns.metadata.name) ?? []
+
+  return (
+    <WrapperSC>
+      <Flex
+        gap="medium"
+        width="100%"
+      >
+        <Input
+          value={q}
+          onChange={(e) => setQ(e.target.value)}
+          placeholder="Search service names"
+          startIcon={<SearchIcon color="icon-light" />}
+          flex={1}
+        />
+        {!namespacesError && (
+          <NamespaceFilter
+            namespaces={namespaces}
+            namespace={namespace ?? ''}
+            onChange={setNamespace}
+            startIcon={<NamespaceIcon color="icon-light" />}
+            inputProps={{ placeholder: 'Filter by namespace' }}
+            containerProps={{
+              style: { background: colors['fill-one'], flex: 1 },
+            }}
+          />
+        )}
+        <TimestampSliderButton setTimestamp={setTimestamp} />
+      </Flex>
+      <Card flex={1}>
+        {loading ? (
+          <LoadingIndicator />
+        ) : isEmpty(networkData) ? (
+          <EmptyState message="No network data found." />
+        ) : (
+          <div>NetworkGraph</div>
+        )}
+      </Card>
+    </WrapperSC>
+  )
 }
+
+const WrapperSC = styled.div(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing.medium,
+  height: '100%',
+  width: '100%',
+}))
