997b4434380ceb36214c1a36591560375f59b147
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-04-23 13:09:00 +0200

    add past reports

--- assets/src/components/security/policies/PastReportsModal.tsx
+++ assets/src/components/security/policies/PastReportsModal.tsx
@@ -1,7 +1,50 @@
-import { Button, IconFrame, ListIcon, Modal } from '@pluralsh/design-system'
+import {
+  Button,
+  IconFrame,
+  ListIcon,
+  LoopingLogo,
+  Modal,
+  Table,
+} from '@pluralsh/design-system'
 import { useState } from 'react'
 
 import { ModalMountTransition } from 'components/utils/ModalMountTransition'
+import {
+  DEFAULT_REACT_VIRTUAL_OPTIONS,
+  useFetchPaginatedData,
+} from '../../utils/table/useFetchPaginatedData.tsx'
+import {
+  ComplianceReportFragment,
+  useComplianceReportsQuery,
+} from '../../../generated/graphql.ts'
+import { createColumnHelper } from '@tanstack/react-table'
+import { DateTimeCol } from '../../utils/table/DateTimeCol.tsx'
+import { GqlError } from '../../utils/Alert.tsx'
+import { Edge } from '../../../utils/graphql.ts'
+
+const columnHelper = createColumnHelper<Edge<ComplianceReportFragment>>()
+
+export const columns = [
+  columnHelper.accessor(({ node }) => node?.name, {
+    id: 'name',
+    header: 'Report name',
+
+    cell: ({ getValue }) => <div>{getValue()}</div>,
+  }),
+  columnHelper.accessor(({ node }) => node?.sha256, {
+    id: 'sha256',
+    header: 'SHA256',
+    meta: { truncate: true },
+    cell: ({ getValue }) => <div>{getValue()}</div>,
+  }),
+  columnHelper.accessor(({ node }) => node?.insertedAt, {
+    id: 'insertedAt',
+    header: 'Created',
+    enableSorting: true,
+    enableGlobalFilter: true,
+    cell: ({ getValue }) => <DateTimeCol date={getValue()} />,
+  }),
+]
 
 export function PastReportsModal({
   open,
@@ -10,11 +53,21 @@ export function PastReportsModal({
   open: boolean
   onClose: Nullable<() => void>
 }) {
+  const { data, loading, error, fetchNextPage, setVirtualSlice } =
+    useFetchPaginatedData({
+      queryHook: useComplianceReportsQuery,
+      keyPath: ['complianceReports'],
+    })
+
+  if (error) return <GqlError error={error} />
+
+  if (!data) return <LoopingLogo />
+
   return (
     <Modal
       open={open}
       onClose={onClose}
-      size={'large'}
+      size="custom"
       header="Past reports"
       actions={
         <Button
@@ -25,8 +78,21 @@ export function PastReportsModal({
           Close
         </Button>
       }
+      css={{ maxHeight: '80vh' }}
     >
-      ...
+      <Table
+        fillLevel={1}
+        fullHeightWrap
+        virtualizeRows
+        data={data?.complianceReports?.edges || []}
+        loading={!data && loading}
+        columns={columns}
+        hasNextPage={data?.complianceReports?.pageInfo?.hasNextPage}
+        isFetchingNextPage={loading}
+        reactVirtualOptions={DEFAULT_REACT_VIRTUAL_OPTIONS}
+        onVirtualSliceChange={setVirtualSlice}
+        fetchNextPage={fetchNextPage}
+      />
     </Modal>
   )
 }
@@ -41,6 +107,7 @@ export function PastReportsButton() {
         size={'large'}
         icon={<ListIcon />}
         css={{ height: 42, width: 42 }}
+        clickable
         onClick={() => setOpen(true)}
       />
       <ModalMountTransition open={open}>
