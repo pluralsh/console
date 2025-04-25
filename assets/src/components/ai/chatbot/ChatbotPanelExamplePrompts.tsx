6933393789cc976bd1d4c902531860327e9191fe
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-04-25 13:34:24 +0200

    add prompt controls

--- assets/src/components/ai/chatbot/ChatbotPanelExamplePrompts.tsx
+++ assets/src/components/ai/chatbot/ChatbotPanelExamplePrompts.tsx
@@ -1,28 +1,90 @@
 import { useTheme } from 'styled-components'
-import { Chip, MagicWandIcon } from '@pluralsh/design-system'
+import {
+  CaretDownIcon,
+  Chip,
+  ChipProps,
+  MagicWandIcon,
+  PlusIcon,
+} from '@pluralsh/design-system'
+import { Dispatch, SetStateAction, useState } from 'react'
+import chroma from 'chroma-js'
+import { isEmpty } from 'lodash'
 
-function Prompt({ children }: { children: React.ReactNode }) {
+const PROMPTS_LIMIT = 3
+
+function Prompt({ children, ...props }: ChipProps) {
   const theme = useTheme()
 
   return (
     <Chip
       clickable
+      props={props}
       css={{
         borderRadius: 16,
         height: 32,
       }}
     >
-      <MagicWandIcon
-        size={12}
-        marginRight={theme.spacing.xxsmall}
-      />
-      <span css={{ ...theme.partials.text.body2 }}>{children}</span>
+      <MagicWandIcon size={12} />
+      <span
+        css={{
+          ...theme.partials.text.body2,
+          marginLeft: theme.spacing.xxsmall,
+        }}
+      >
+        {children}
+      </span>
+    </Chip>
+  )
+}
+
+function PromptsControl({
+  showAll,
+  setShowAll,
+}: {
+  showAll: boolean
+  setShowAll: Dispatch<SetStateAction<boolean>>
+}) {
+  const theme = useTheme()
+
+  return (
+    <Chip
+      onClick={() => setShowAll(!showAll)}
+      css={{
+        borderRadius: 16,
+        cursor: 'pointer',
+        height: 32,
+        backgroundColor: chroma(theme.colors['fill-one']).alpha(0.5).hex(),
+
+        '&:hover': {
+          backgroundColor: chroma(theme.colors['fill-one-hover'])
+            .alpha(0.5)
+            .hex(),
+
+          '.children': {
+            color: theme.colors['text-light'],
+          },
+        },
+
+        '&:not(:hover) .children': {
+          color: theme.colors['text-xlight'],
+        },
+      }}
+    >
+      {/* TODO: Add minus icon. */}
+      {showAll ? <CaretDownIcon size={12} /> : <PlusIcon size={12} />}
+      <span
+        css={{
+          ...theme.partials.text.body2,
+          marginLeft: theme.spacing.xxsmall,
+        }}
+      >
+        {showAll ? 'Show less' : 'Show all prompts'}
+      </span>
     </Chip>
   )
 }
 
 export function ChatbotPanelExamplePrompts() {
-  const theme = useTheme()
   const prompts = [
     'Give me the details of the deployment resource.',
     'Query the error logs on the prod cluster.',
@@ -30,7 +92,13 @@ export function ChatbotPanelExamplePrompts() {
     'Give me the details of the deployment resource.',
     'Query the error logs on the prod cluster.',
     'What are all the components of the prod clusters service?',
-  ] // TODO
+  ] // TODO: Replace with real data.
+
+  const theme = useTheme()
+  const hasMorePrompts = prompts.length > PROMPTS_LIMIT
+  const [showAll, setShowAll] = useState(!hasMorePrompts)
+
+  if (isEmpty(prompts)) return null
 
   return (
     <div
@@ -44,6 +112,7 @@ export function ChatbotPanelExamplePrompts() {
         flexDirection: 'column',
         justifyContent: 'flex-end',
         overflowY: 'auto',
+        zIndex: 1,
       }}
     >
       <div
@@ -55,9 +124,15 @@ export function ChatbotPanelExamplePrompts() {
           padding: theme.spacing.medium,
         }}
       >
-        {prompts.map((p, i) => (
+        {(showAll ? prompts : prompts.slice(0, PROMPTS_LIMIT)).map((p, i) => (
           <Prompt key={i}>{p}</Prompt>
         ))}
+        {hasMorePrompts && (
+          <PromptsControl
+            showAll={showAll}
+            setShowAll={setShowAll}
+          />
+        )}
       </div>
     </div>
   )
