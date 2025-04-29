0466e295f7ec0acb4b8ccdbdf81e91c99443ed68
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-04-29 15:56:43 +0200

    feat: Add deprecated custom resources to upgrade panel (#2181)

---
+++ assets/src/components/cd/clusters/clusterDeprecatedCustomResourcesColumns.tsx
@@ -0,0 +1,40 @@
+import { createColumnHelper } from '@tanstack/react-table'
+
+import { DeprecatedCustomResourceFragment } from '../../../generated/graphql'
+import { Chip } from '@pluralsh/design-system'
+
+const columnHelperUpgrade =
+  createColumnHelper<DeprecatedCustomResourceFragment>()
+
+export const clusterDeprecatedCustomResourcesColumns = [
+  columnHelperUpgrade.accessor((cr) => cr?.name, {
+    id: 'name',
+    header: 'Name',
+    cell: ({ getValue }) => <div>{getValue()}</div>,
+  }),
+  columnHelperUpgrade.accessor((cr) => cr?.group, {
+    id: 'group',
+    header: 'Group',
+    cell: ({ getValue }) => <div>{getValue()}</div>,
+  }),
+  columnHelperUpgrade.accessor((cr) => cr?.kind, {
+    id: 'kind',
+    header: 'Kind',
+    cell: ({ getValue }) => <div>{getValue()}</div>,
+  }),
+  columnHelperUpgrade.accessor((cr) => cr?.namespace, {
+    id: 'namespace',
+    header: 'Namespace',
+    cell: ({ getValue }) => <div>{getValue()}</div>,
+  }),
+  columnHelperUpgrade.accessor((cr) => cr?.version, {
+    id: 'version',
+    header: 'Version',
+    cell: ({ getValue }) => <Chip severity={'warning'}>{getValue()}</Chip>,
+  }),
+  columnHelperUpgrade.accessor((cr) => cr?.nextVersion, {
+    id: 'nextVersion',
+    header: 'Next version',
+    cell: ({ getValue }) => <Chip severity={'success'}>{getValue()}</Chip>,
+  }),
+]
