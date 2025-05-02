82e8636dad0356d33727c5d058ad30632fd5642f
Jake Laderman <jsladerman@gmail.com>
2025-05-02 15:02:37 -0400

    add knowledge graph enable/disable

--- assets/src/components/ai/chatbot/ChatbotHeader.tsx
+++ assets/src/components/ai/chatbot/ChatbotHeader.tsx
@@ -7,16 +7,23 @@ import {
   Flex,
   IconFrame,
   ShrinkIcon,
+  Spinner,
+  Toast,
 } from '@pluralsh/design-system'
+import { Body1BoldP, CaptionP } from 'components/utils/typography/Text'
 import dayjs from 'dayjs'
-import { AiInsightFragment, ChatThreadTinyFragment } from 'generated/graphql'
-import styled from 'styled-components'
+import {
+  AiInsightFragment,
+  ChatThreadTinyFragment,
+  useUpdateChatThreadMutation,
+} from 'generated/graphql'
+import { useCallback } from 'react'
+import styled, { useTheme } from 'styled-components'
 import { useChatbot } from '../AIContext'
 import { AIPinButton } from '../AIPinButton'
 import { AIEntryLabel, getThreadOrPinTimestamp } from '../AITableEntry'
-import { ChatWithAIButton, insightMessage } from './ChatbotButton'
-import { Body1BoldP } from 'components/utils/typography/Text'
 import { AISuggestFix } from './AISuggestFix'
+import { ChatWithAIButton, insightMessage } from './ChatbotButton'
 import { ChatbotThreadMoreMenu } from './ChatbotThreadMoreMenu'
 
 type HeaderState = 'list' | 'thread' | 'insight'
@@ -30,6 +37,7 @@ export function ChatbotHeader({
   currentThread?: Nullable<ChatThreadTinyFragment>
   currentInsight?: Nullable<AiInsightFragment>
 }) {
+  const { colors } = useTheme()
   const { setFullscreen, closeChatbot, goToThreadList } = useChatbot()
   const insight = currentThread?.insight || currentInsight
   let state: HeaderState = 'list'
@@ -39,6 +47,23 @@ export function ChatbotHeader({
   const timestamp = getThreadOrPinTimestamp(currentThread)
   const isStale =
     !!timestamp && dayjs().isAfter(dayjs(timestamp).add(24, 'hours'))
+
+  const [
+    updateThread,
+    { loading: updateThreadLoading, error: updateThreadError },
+  ] = useUpdateChatThreadMutation()
+
+  const toggleKnowledgeGraph = useCallback(() => {
+    updateThread({
+      variables: {
+        id: currentThread?.id ?? '',
+        attributes: {
+          summary: currentThread?.summary ?? '',
+          settings: { memory: !currentThread?.settings?.memory },
+        },
+      },
+    })
+  }, [currentThread, updateThread])
 
   return (
     <WrapperSC $fullscreen={fullscreen}>
@@ -103,10 +128,33 @@ export function ChatbotHeader({
       {state === 'thread' ? (
         <>
           <IconFrame
+            tooltip={
+              <Flex direction="column">
+                <CaptionP $color="text-light">
+                  {currentThread?.settings?.memory ? 'Disable' : 'Enable'}
+                  {' knowledge graph'}
+                </CaptionP>
+                <CaptionP $color="text-xlight">
+                  {"Use and add to Plural AI's memory with this thread"}
+                </CaptionP>
+              </Flex>
+            }
             clickable
             type="secondary"
+            style={{
+              borderColor: currentThread?.settings?.memory
+                ? colors['border-primary']
+                : undefined,
+            }}
             size={fullscreen ? 'large' : 'medium'}
-            icon={<BrainIcon css={{ width: 16 }} />}
+            icon={
+              updateThreadLoading ? (
+                <Spinner css={{ width: 16 }} />
+              ) : (
+                <BrainIcon css={{ width: 16 }} />
+              )
+            }
+            onClick={toggleKnowledgeGraph}
           />
           <ChatbotThreadMoreMenu fullscreen={fullscreen} />
         </>
@@ -120,6 +168,14 @@ export function ChatbotHeader({
           onClick={() => closeChatbot()}
         />
       )}
+      <Toast
+        show={!!updateThreadError}
+        severity="danger"
+        position="bottom"
+        marginBottom="medium"
+      >
+        Error updating thread settings.
+      </Toast>
     </WrapperSC>
   )
 }
