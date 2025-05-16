6d6fdf78548c27b988d2c7dbb5c0383f1a8f217a
michaeljguarino <mjg@plural.sh>
2025-05-16 15:41:21 -0400

    Try blobless clones for repo caches
    The premise behind this is having to round trip to the remote for each net new commit (alongside the fetch during the pull phase) could be exagerrating the total cost of serving git tarballs.  If we just ensure everything is there locally, it will only be filesystem bound.

--- lib/console/deployments/helm/discovery.ex
+++ lib/console/deployments/helm/discovery.ex
@@ -1,31 +1,30 @@
 defmodule Console.Deployments.Helm.Discovery do
   alias Console.Deployments.Helm.{Supervisor, Agent}
 
-  def agent(url) do
-    case maybe_rpc(url, Supervisor, :start_child, [url]) do
-      {:ok, pid} -> {:ok, pid}
-      {:error, {:already_started, pid}} -> {:ok, pid}
-      err -> err
-    end
-  end
-
   def fetch(url, chart, vsn) do
-    with {:ok, pid} <- agent(url),
-      do: Agent.fetch(pid, chart, vsn)
+    maybe_rpc(url, fn pid -> Agent.fetch(pid, chart, vsn) end)
   end
 
   def digest(url, chart, vsn) do
-    with {:ok, pid} <- agent(url),
-      do: Agent.digest(pid, chart, vsn)
+    maybe_rpc(url, fn pid -> Agent.digest(pid, chart, vsn) end)
   end
 
-  defp maybe_rpc(id, module, func, args) do
+  defp maybe_rpc(url, fun) when is_function(fun, 1) do
     me = node()
-    case worker_node(id) do
-      ^me -> apply(module, func, args)
-      node -> :rpc.call(node, module, func, args)
+    case worker_node(url) do
+      ^me -> start_and_run(url, fun)
+      node -> :erpc.call(node, __MODULE__, :start_and_run, [url, fun], :timer.seconds(30))
     end
   end
+
+  def start_and_run(url, fun) when is_function(fun, 1) do
+    case Supervisor.start_child(url) do
+      {:ok, pid} -> fun.(pid)
+      {:error, {:already_started, pid}} -> fun.(pid)
+      err -> err
+    end
+  end
+  def start_and_run(_, _), do: {:error, "no helm repository located"}
 
   def worker_node(url), do: HashRing.key_to_node(ring(), url)
 
