c2685995132393b0e56ce6fa2008bf2fd1ad001d
michaeljguarino <mjg@plural.sh>
2025-05-20 10:31:31 -0400

    truncate vulnerability reports before migration
    have to remove possibility of dupes, and it's not a big deal since it'll be reuploaded anyway

--- priv/repo/migrations/20250520004950_add_vuln_report_uniq.exs
+++ priv/repo/migrations/20250520004950_add_vuln_report_uniq.exs
@@ -2,6 +2,10 @@ defmodule Console.Repo.Migrations.AddVulnReportUniq do
   use Ecto.Migration
 
   def change do
+    Console.Repo.delete_all(Console.Schema.VulnerabilityReport)
+
+    flush()
+
     create unique_index(:vulnerability_reports, [:cluster_id, :artifact_url])
     create index(:vulnerability_reports, [:updated_at])
     create index(:policy_constraints, [:updated_at])
