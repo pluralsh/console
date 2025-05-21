313439698ed95b06601f409a6b6bd84683584015
Sebastian Florek <s.florek91@gmail.com>
2025-05-21 14:44:25 +0200

    feat: implement ServiceComponentsTree component with React Flow and GraphQL integration for visualizing service components hierarchy

--- assets/src/components/cd/services/service/ServiceComponents.tsx
+++ assets/src/components/cd/services/service/ServiceComponents.tsx
@@ -1,20 +1,15 @@
 import {
   ArrowScroll,
   Callout,
-  Chip,
-  ChipSeverity,
   ComponentsIcon,
   FillLevelProvider,
   Flex,
-  IconFrame,
-  InfoOutlineIcon,
   ListIcon,
   NetworkInterfaceIcon,
   SearchIcon,
   UpdatesIcon,
 } from '@pluralsh/design-system'
 import { type Key } from '@react-types/shared'
-import { Node, NodeProps, ReactFlowProvider } from '@xyflow/react'
 
 import { useThrottle } from 'components/hooks/useThrottle.tsx'
 import { GqlError } from 'components/utils/Alert.tsx'
@@ -24,31 +19,25 @@ import { ScrollablePage } from 'components/utils/layout/ScrollablePage'
 import LoadingIndicator from 'components/utils/LoadingIndicator'
 
 import { ModalMountTransition } from 'components/utils/ModalMountTransition'
-import { LayoutOptions } from 'elkjs'
 import {
   ComponentState,
-  ServiceComponent,
   ServiceDeploymentComponentFragment,
   useServiceDeploymentComponentsQuery,
-  useServiceDeploymentComponentsWithChildrenQuery,
 } from 'generated/graphql'
 import { ReactNode, useEffect, useMemo, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 
 import { getServiceComponentPath } from 'routes/cdRoutesConsts'
-import styled, { useTheme } from 'styled-components'
+import styled from 'styled-components'
 import { isNonNullable } from 'utils/isNonNullable'
 import ButtonGroup from '../../../utils/ButtonGroup.tsx'
-import { NodeBase } from '../../../utils/reactflow/nodes.tsx'
-import { ReactFlowGraph } from '../../../utils/reactflow/ReactFlowGraph.tsx'
-import { TRUNCATE, TRUNCATE_LEFT } from '../../../utils/truncate.ts'
 import { ComponentList } from './component/ComponentList.tsx'
 import {
   ComponentStateFilter,
   useComponentKindSelect,
 } from './component/Components.tsx'
-import { ComponentIcon } from './component/misc.tsx'
 import { countDeprecations } from './deprecationUtils'
+import { ComponentsTreeView } from './ServiceComponentsTree.tsx'
 import { ServiceDeprecationsModal } from './ServiceDeprecationsModal'
 
 const directory = [
@@ -56,11 +45,18 @@ const directory = [
   { path: 'tree', icon: <NetworkInterfaceIcon /> },
 ]
 
+const defaultView = 'list'
+
 export function ServiceComponents() {
   const [selectedState, setSelectedState] = useState<Key | null>(null)
   const [searchQuery, setSearchQuery] = useState('')
-  const [tab, setTab] = useState(directory[0].path)
+  const [searchParams, setSearchParams] = useSearchParams()
   const [components, setComponents] = useState()
+
+  const view = useMemo(
+    () => searchParams.get('view') || defaultView,
+    [searchParams]
+  )
 
   const { kindSelector, selectedKinds, setSelectedKinds, allKinds } =
     useComponentKindSelect(components, {
@@ -74,7 +70,7 @@ export function ServiceComponents() {
       headingContent={
         <ArrowScroll>
           <FiltersWrapperSC>
-            {tab === 'list' && (
+            {view === 'list' && (
               <>
                 <IconExpander
                   tooltip="Search components"
@@ -111,14 +107,14 @@ export function ServiceComponents() {
               </>
             )}
             <ComponentsViewSwitch
-              tab={tab}
-              setTab={setTab}
+              tab={view}
+              setTab={(view: string) => setSearchParams({ view })}
             ></ComponentsViewSwitch>
           </FiltersWrapperSC>
         </ArrowScroll>
       }
     >
-      {tab === 'list' && (
+      {view === 'list' && (
         <ComponentsListView
           setComponents={setComponents}
           selectedKinds={selectedKinds}
@@ -127,7 +123,7 @@ export function ServiceComponents() {
           searchQuery={searchQuery}
         ></ComponentsListView>
       )}
-      {tab === 'tree' && <ComponentsTreeView></ComponentsTreeView>}
+      {view === 'tree' && <ComponentsTreeView />}
     </ScrollablePage>
   )
 }
@@ -233,192 +229,4 @@ function ComponentsListView({
       </ModalMountTransition>
     </>
   )
-}
-
-type ServiceComponentNodeType = Node<
-  ServiceComponent,
-  typeof ServiceComponentNodeKey
->
-const ServiceComponentNodeKey = 'plural-service-component-tree-node'
-
-const nodeTypes = {
-  [ServiceComponentNodeKey]: ServiceComponentTreeNode,
-}
-
-function ServiceComponentTreeNode({
-  id,
-  data,
-}: NodeProps<ServiceComponentNodeType>) {
-  const theme = useTheme()
-  const stateToSeverity: { [key in ComponentState]: ChipSeverity } = {
-    [ComponentState.Failed]: 'danger',
-    [ComponentState.Pending]: 'warning',
-    [ComponentState.Running]: 'success',
-    [ComponentState.Paused]: 'warning',
-  }
-  const [open, setOpen] = useState(false)
-
-  return (
-    <NodeBase
-      id={id}
-      css={{
-        backgroundColor: theme.colors['fill-two'],
-        borderColor:
-          data.state === ComponentState.Failed
-            ? theme.colors['border-danger']
-            : data.state === ComponentState.Pending
-              ? theme.colors['border-warning']
-              : undefined,
-        gap: 0,
-        padding: 0,
-        width: 336,
-      }}
-    >
-      <div
-        css={{
-          ...theme.partials.text.caption,
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          display: 'flex',
-          backgroundColor: theme.colors['fill-one'],
-          borderBottom: '1px solid',
-          borderColor:
-            theme.mode === 'light'
-              ? theme.colors['border-fill-two']
-              : theme.colors.border,
-          color: theme.colors['text-xlight'],
-          gap: theme.spacing.medium,
-          borderTopLeftRadius: theme.borderRadiuses.large,
-          borderTopRightRadius: theme.borderRadiuses.large,
-          padding: `${theme.spacing.small}px ${theme.spacing.medium}px`,
-        }}
-      >
-        <div
-          css={{
-            display: 'flex',
-            gap: theme.spacing.xsmall,
-            minWidth: 50,
-          }}
-        >
-          <ComponentIcon kind={data.kind} />
-          <span css={{ ...TRUNCATE_LEFT }}>
-            {!data.group?.replace('group', '') ? '' : `${data.group}.`}
-            {data.kind}
-          </span>
-        </div>
-        <Chip
-          severity={stateToSeverity[data.state ?? ComponentState.Pending]}
-          css={{ whiteSpace: 'nowrap' }}
-          size="small"
-        >
-          {data.state}
-        </Chip>
-      </div>
-      <div
-        css={{
-          padding: `${theme.spacing.small}px ${theme.spacing.medium}px`,
-        }}
-      >
-        <div css={{ flex: 1 }}>
-          <div css={{ display: 'flex', gap: theme.spacing.small }}>
-            <div css={{ flex: 1, minWidth: 50 }}>
-              <div css={{ ...TRUNCATE, ...theme.partials.text.body2Bold }}>
-                {data.name}
-              </div>
-              <div
-                css={{
-                  ...TRUNCATE,
-                  ...theme.partials.text.caption,
-                  color: theme.colors['text-xlight'],
-                }}
-              >
-                {data?.namespace}
-              </div>
-            </div>
-            <div css={{ display: 'flex', gap: theme.spacing.xsmall }}>
-              <IconFrame
-                clickable
-                onClick={() => setOpen(true)}
-                icon={<InfoOutlineIcon />}
-                type="secondary"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </NodeBase>
-  )
-}
-
-function ComponentsTreeView(): ReactNode {
-  const { serviceId } = useParams()
-  const options: LayoutOptions = {
-    'elk.algorithm': 'mrtree',
-    'elk.direction': 'RIGHT',
-    'elk.spacing.nodeNode': '60',
-  }
-
-  const { data, error } = useServiceDeploymentComponentsWithChildrenQuery({
-    variables: { id: serviceId || '' },
-  })
-
-  const { nodes: baseNodes, edges: baseEdges } = useMemo(
-    () =>
-      getNodesAndEdges(
-        (data?.serviceDeployment?.components as Array<ServiceComponent>) ?? []
-      ),
-    [data?.serviceDeployment?.components]
-  )
-
-  if (error) return <GqlError error={error} />
-  if (!data) return <LoadingIndicator />
-
-  return (
-    <ReactFlowProvider>
-      <ReactFlowGraph
-        allowFullscreen
-        baseNodes={baseNodes}
-        baseEdges={baseEdges}
-        elkOptions={options}
-        minZoom={0.01}
-        nodeTypes={nodeTypes}
-      />
-    </ReactFlowProvider>
-  )
-}
-
-function getNodesAndEdges(components: Array<ServiceComponent>): {
-  nodes: Array<any>
-  edges: Array<any>
-} {
-  const nodes: Array<any> = []
-  const edges: Array<any> = []
-
-  components.forEach((component) => {
-    nodes.push({
-      id: component.id,
-      position: { x: 0, y: 0 },
-      type: ServiceComponentNodeKey,
-      data: { ...component },
-    })
-
-    component.children?.forEach((child) => {
-      nodes.push({
-        id: child?.id,
-        position: { x: 0, y: 0 },
-        type: ServiceComponentNodeKey,
-        data: { ...child },
-      })
-
-      edges.push({
-        type: 'smooth',
-        reconnectable: false,
-        id: `${child?.id}${component.id}`,
-        source: component?.id,
-        target: child?.id,
-      })
-    })
-  })
-
-  return { nodes, edges }
 }
