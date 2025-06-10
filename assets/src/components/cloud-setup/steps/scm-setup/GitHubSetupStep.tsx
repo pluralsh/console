28d1c77b6f1b27c7292ce1f0048b986b0ec616df
Jake Laderman <jsladerman@gmail.com>
2025-06-10 12:26:48 -0400

    add webhook creation to new cloud instance modal (#2346)

--- assets/src/components/cloud-setup/steps/scm-setup/GitHubSetupStep.tsx
+++ assets/src/components/cloud-setup/steps/scm-setup/GitHubSetupStep.tsx
@@ -1,14 +1,23 @@
-import { Flex, Tab, TabList, TabPanel } from '@pluralsh/design-system'
+import {
+  Button,
+  Flex,
+  FormField,
+  Input,
+  Tab,
+  TabList,
+  TabPanel,
+} from '@pluralsh/design-system'
 import { GqlError } from 'components/utils/Alert'
 import { Body1P } from 'components/utils/typography/Text'
 import {
   ScmConnectionDocument,
+  useCreateScmWebhookMutation,
   useRegisterGitHubAppMutation,
   useScmConnectionSuspenseQuery,
 } from 'generated/graphql'
 import { use, useLayoutEffect, useRef, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
 import {
   CloudWelcomeCtx,
   CloudWelcomeStep,
@@ -23,16 +32,29 @@ enum SetupType {
 const INSTALLATION_ID_PARAM = 'installation_id'
 
 export function GitHubSetupStep() {
+  const { colors } = useTheme()
   const { setModalActions, setStep } = use(CloudWelcomeCtx)
   const [setupType, setSetupType] = useState<SetupType>(SetupType.GhApp)
   const tabStateRef = useRef<any>(null)
   const [params, setParams] = useSearchParams()
   const installationId = params.get(INSTALLATION_ID_PARAM)
 
+  const [showWebhookSetup, setShowWebhookSetup] = useState(false)
+  const [owner, setOwner] = useState('')
+
   const { data, error: scmConnectionError } = useScmConnectionSuspenseQuery({
     variables: { name: 'plural' },
     fetchPolicy: 'cache-and-network',
   })
+
+  const [createWebhook, { loading: webhookLoading, error: webhookError }] =
+    useCreateScmWebhookMutation({
+      variables: { connectionId: data.scmConnection?.id ?? '', owner },
+      onCompleted: () => {
+        setParams(undefined)
+        setShowWebhookSetup(false)
+      },
+    })
 
   const [
     registerGhApp,
@@ -43,15 +65,17 @@ export function GitHubSetupStep() {
     refetchQueries: [
       { query: ScmConnectionDocument, variables: { name: 'plural' } },
     ],
+    onCompleted: () => setShowWebhookSetup(true),
   })
 
   useLayoutEffect(() => {
     setModalActions(null)
     if (!!data.scmConnection) {
-      setStep(CloudWelcomeStep.Authenticate)
-      return
-    }
-    if (installationId) registerGhApp()
+      // if the user installed manually, then there will be no installationId in the search params and this moves forward
+      // when installing with our gh app, user needs to also create a webhook first which then clears the installationId
+      if (!installationId) setStep(CloudWelcomeStep.Authenticate)
+      else setShowWebhookSetup(true)
+    } else if (installationId) registerGhApp()
   }, [data, installationId, registerGhApp, setModalActions, setStep, setupType])
 
   if (scmConnectionError) return <GqlError error={scmConnectionError} />
@@ -62,42 +86,100 @@ export function GitHubSetupStep() {
       gap="medium"
     >
       {registerGhAppError && <GqlError error={registerGhAppError} />}
-      <Body1P>
-        Plural Console is best with a Git connection. You can either use the
-        pre-made Plural GitHub application or install your own.
-      </Body1P>
-      <TabList
-        stateRef={tabStateRef}
-        stateProps={{
-          orientation: 'horizontal',
-          selectedKey: setupType,
-          onSelectionChange: (key) => setSetupType(key as SetupType),
-        }}
-      >
-        <StretchedTabSC key={SetupType.GhApp}>Install on GitHub</StretchedTabSC>
-        <StretchedTabSC key={SetupType.Manual}>
-          Manual installation
-        </StretchedTabSC>
-      </TabList>
-      <TabPanel
-        stateRef={tabStateRef}
-        tabKey={setupType}
-      >
-        {setupType === SetupType.GhApp ? (
-          <GitHubAppSetup
-            submitInstallationId={(id) => {
-              // this will trigger the mutation in the effect
-              setParams({ [INSTALLATION_ID_PARAM]: id })
+      {showWebhookSetup ? (
+        <WebhookFormSC
+          onSubmit={(e) => {
+            e.preventDefault()
+            createWebhook()
+          }}
+        >
+          {webhookError && <GqlError error={webhookError} />}
+          <Body1P>
+            Please provide the GitHub organization or repository slug so Plural
+            can create a webhook to listen for updates.
+          </Body1P>
+          <FormField
+            required
+            label="Owner"
+            hint="should be a GitHub organization or repo slug"
+          >
+            <Input
+              placeholder="Enter webhook owner"
+              value={owner}
+              onChange={(e) => setOwner(e.target.value)}
+            />
+          </FormField>
+          <Flex
+            width="100%"
+            justify="space-between"
+          >
+            <Button
+              tertiary
+              padding="none"
+              css={{ color: colors['text-xlight'] }}
+              onClick={() => setParams(undefined)}
+            >
+              {"Skip (I'll do it later)"}
+            </Button>
+            <Button
+              type="submit"
+              disabled={!owner}
+              loading={webhookLoading}
+            >
+              Create webhook
+            </Button>
+          </Flex>
+        </WebhookFormSC>
+      ) : (
+        <>
+          <Body1P>
+            The Plural Console works best with a connection to your source
+            control provider. You can either use the pre-made Plural GitHub
+            application or install your own.
+          </Body1P>
+          <TabList
+            stateRef={tabStateRef}
+            stateProps={{
+              orientation: 'horizontal',
+              selectedKey: setupType,
+              onSelectionChange: (key) => setSetupType(key as SetupType),
             }}
-            loading={registerGhAppLoading}
-          />
-        ) : (
-          <ManualSetup />
-        )}
-      </TabPanel>
+          >
+            <StretchedTabSC key={SetupType.GhApp}>
+              Install on GitHub
+            </StretchedTabSC>
+            <StretchedTabSC key={SetupType.Manual}>
+              Manual installation
+            </StretchedTabSC>
+          </TabList>
+          <TabPanel
+            stateRef={tabStateRef}
+            tabKey={setupType}
+          >
+            {setupType === SetupType.GhApp ? (
+              <GitHubAppSetup
+                submitInstallationId={(id) => {
+                  // this will trigger the mutation in the effect
+                  setParams({ [INSTALLATION_ID_PARAM]: id })
+                }}
+                loading={registerGhAppLoading}
+              />
+            ) : (
+              <ManualSetup />
+            )}
+          </TabPanel>
+        </>
+      )}
     </Flex>
   )
 }
+
+const WebhookFormSC = styled.form(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing.medium,
+  wordBreak: 'break-word',
+}))
 
 const StretchedTabSC = styled(Tab)({
   flex: 1,
