0aadee3ea33e79021a62ca3f880435d0e7d71907
Sebastian Florek <s.florek91@gmail.com>
2025-06-12 14:43:21 +0200

    refactor(proto): move cloud query gRPC service code and update Makefile for protoc integration

--- go/tools/tools.go
+++ go/tools/tools.go
@@ -12,6 +12,8 @@ import (
 	_ "github.com/onsi/ginkgo/v2/ginkgo"
 	_ "github.com/vektra/mockery/v2"
 	_ "golang.org/x/tools/cmd/goimports"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/kustomize/v5"
