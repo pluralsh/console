a5e649186768e0701b11471aa3fa1d3bc8f7073c
JohnBlackwell <john@plural.sh>
2025-06-13 15:27:46 -0400

    refactored unit tests

---
+++ test/console/compliance/clusters_test.exs
@@ -0,0 +1,24 @@
+defmodule Console.Compliance.Datasource.ClustersTest do
+  use Console.DataCase, async: true
+
+  test "it includes read and write users in the compliance report" do
+    user1 = insert(:user, email: "user1@example.com")
+    user2 = insert(:user, email: "user2@example.com")
+    group = insert(:group, name: "compliance-group")
+
+    cluster = insert(:cluster)
+
+    insert(:policy_binding, policy_id: cluster.read_policy_id, user_id: user1.id)
+    insert(:policy_binding, policy_id: cluster.read_policy_id, group_id: group.id)
+    insert(:policy_binding, policy_id: cluster.write_policy_id, user_id: user2.id)
+
+    clusters_content = Console.Compliance.Datasource.Clusters.stream()
+    |> CSV.encode(headers: true)
+    |> Enum.to_list()
+    |> Enum.join()
+
+    assert clusters_content =~ "user1@example.com"
+    assert clusters_content =~ "group:compliance-group"
+    assert clusters_content =~ "user2@example.com"
+  end
+end
--- test/console_web/controllers/compliance_controller_test.exs
+++ test/console_web/controllers/compliance_controller_test.exs
@@ -2,37 +2,6 @@ defmodule ConsoleWeb.ComplianceControllerTest do
   use ConsoleWeb.ConnCase, async: true
 
   describe "#report/2" do
-    test "it includes read and write users in the compliance report", %{conn: conn} do
-      user1 = insert(:user, email: "user1@example.com")
-      user2 = insert(:user, email: "user2@example.com")
-      group = insert(:group, name: "compliance-group")
-
-      cluster = insert(:cluster)
-
-      insert(:policy_binding, policy_id: cluster.read_policy_id, user_id: user1.id)
-      insert(:policy_binding, policy_id: cluster.read_policy_id, group_id: group.id)
-
-      insert(:policy_binding, policy_id: cluster.write_policy_id, user_id: user2.id)
-
-      clusters_content = Console.Compliance.Datasource.Clusters.stream()
-      |> CSV.encode(headers: true)
-      |> Enum.to_list()
-      |> Enum.join()
-
-      assert clusters_content =~ "user1@example.com"
-      assert clusters_content =~ "group:compliance-group"
-      assert clusters_content =~ "user2@example.com"
-
-      conn
-      |> add_auth_headers(admin_user())
-      |> get("/v1/compliance/report?format=csv")
-      |> response(200)
-
-      [report] = Console.Repo.all(Console.Schema.ComplianceReport)
-      assert report.name
-      assert is_binary(report.sha256)
-    end
-
     test "it will generate a compliance report for a permitted user", %{conn: conn} do
       insert_list(3, :cluster)
       insert_list(3, :service)
