37128e097be3e1d5e48ba2c18a5fa7a1397cbe9a
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-06-24 11:46:21 +0200

    add exec all

--- go/cloud-query/internal/connection/exec.go
+++ go/cloud-query/internal/connection/exec.go
@@ -5,5 +5,16 @@ import (
 )
 
 func (in *connection) Exec(q string, args ...any) (sql.Result, error) {
-	return in.db.Exec(q, args)
+	return in.db.Exec(q, args...)
+}
+
+func (in *connection) ExecAll(q map[string][]any) error {
+	for query, args := range q {
+		_, err := in.db.Exec(query, args...)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
