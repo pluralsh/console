211d6077ccc4ddd863d40af2c4f74c1f70d0597b
Jake Laderman <jsladerman@gmail.com>
2025-06-24 11:15:52 -0400

    webhooks table

--- assets/src/components/self-service/pr/scm/PrScmManagement.tsx.tsx
+++ assets/src/components/self-service/pr/scm/PrScmManagement.tsx.tsx
@@ -1,6 +1,6 @@
 import { Flex, Table } from '@pluralsh/design-system'
 
-import { useScmConnectionsQuery } from 'generated/graphql'
+import { useScmConnectionsQuery, useScmWebhooksQuery } from 'generated/graphql'
 
 import { useSetPageHeaderContent } from 'components/cd/ContinuousDeployment'
 import { GqlError } from 'components/utils/Alert'
@@ -11,33 +11,30 @@ import {
 } from 'components/utils/table/useFetchPaginatedData'
 
 import { CreateScmConnection } from './CreateScmConnection'
-import { columns } from './PrScmConnectionsColumns'
+import { columns as connectionsColumns } from './PrScmConnectionsColumns'
+import { columns as webhooksColumns } from './ScmWebhooksColumns'
 import { SetupDependencyAutomation } from './SetupDependencyAutomation'
 
 export const PR_QUERY_PAGE_SIZE = 100
 
 export function ScmManagement() {
-  const {
-    data,
-    loading,
-    error,
-    refetch,
-    pageInfo,
-    fetchNextPage,
-    setVirtualSlice,
-  } = useFetchPaginatedData({
+  const connectionsQ = useFetchPaginatedData({
     queryHook: useScmConnectionsQuery,
     keyPath: ['scmConnections'],
   })
 
+  const webhooksQ = useFetchPaginatedData({
+    queryHook: useScmWebhooksQuery,
+    keyPath: ['scmWebhooks'],
+  })
+
   useSetPageHeaderContent(
     <Flex gap="small">
-      <SetupDependencyAutomation refetch={refetch} />
-      <CreateScmConnection refetch={refetch} />
+      <SetupDependencyAutomation refetch={connectionsQ.refetch} />
+      <CreateScmConnection refetch={connectionsQ.refetch} />
+      <div>webhooks action</div>
     </Flex>
   )
-
-  if (error) return <GqlError error={error} />
 
   return (
     <Flex
@@ -45,20 +42,40 @@ export function ScmManagement() {
       gap="small"
       height="100%"
     >
-      <div>TODO ADD WEBHOOKS</div>
-      <Table
-        fullHeightWrap
-        columns={columns}
-        loading={!data && loading}
-        reactTableOptions={{ meta: { refetch } }}
-        reactVirtualOptions={DEFAULT_REACT_VIRTUAL_OPTIONS}
-        data={data?.scmConnections?.edges || []}
-        virtualizeRows
-        hasNextPage={pageInfo?.hasNextPage}
-        fetchNextPage={fetchNextPage}
-        isFetchingNextPage={loading}
-        onVirtualSliceChange={setVirtualSlice}
-      />
+      {connectionsQ.error ? (
+        <GqlError error={connectionsQ.error} />
+      ) : (
+        <Table
+          fullHeightWrap
+          columns={columns}
+          loading={!connectionsQ.data && connectionsQ.loading}
+          reactTableOptions={{ meta: { refetch: connectionsQ.refetch } }}
+          reactVirtualOptions={DEFAULT_REACT_VIRTUAL_OPTIONS}
+          data={connectionsQ.data?.scmConnections?.edges || []}
+          virtualizeRows
+          hasNextPage={connectionsQ.pageInfo?.hasNextPage}
+          fetchNextPage={connectionsQ.fetchNextPage}
+          isFetchingNextPage={connectionsQ.loading}
+          onVirtualSliceChange={connectionsQ.setVirtualSlice}
+        />
+      )}
+      {webhooksQ.error ? (
+        <GqlError error={webhooksQ.error} />
+      ) : (
+        <Table
+          fullHeightWrap
+          columns={columns}
+          loading={!webhooksQ.data && webhooksQ.loading}
+          reactTableOptions={{ meta: { refetch: webhooksQ.refetch } }}
+          reactVirtualOptions={DEFAULT_REACT_VIRTUAL_OPTIONS}
+          data={webhooksQ.data?.scmWebhooks?.edges || []}
+          virtualizeRows
+          hasNextPage={webhooksQ.pageInfo?.hasNextPage}
+          fetchNextPage={webhooksQ.fetchNextPage}
+          isFetchingNextPage={webhooksQ.loading}
+          onVirtualSliceChange={webhooksQ.setVirtualSlice}
+        />
+      )}
     </Flex>
   )
 }
