3fc15f62a4af359b5d14c5f6a26f705e15d7c79f
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-07-18 19:00:53 +0200

    feat: Improvement to compliance reports (#2473)

---
+++ assets/src/components/security/compliance/Permissions.tsx
@@ -0,0 +1,138 @@
+import {
+  Button,
+  Flex,
+  IconFrame,
+  Modal,
+  PeopleIcon,
+} from '@pluralsh/design-system'
+import { FormEvent, useCallback, useEffect, useMemo, useState } from 'react'
+
+import { ModalMountTransition } from 'components/utils/ModalMountTransition'
+import {
+  ComplianceReportGeneratorFragment,
+  useUpsertComplianceReportGeneratorMutation,
+} from '../../../generated/graphql.ts'
+import RoleFormBindings from '../../settings/usermanagement/roles/RoleFormBindings.tsx'
+import uniqWith from 'lodash/uniqWith'
+import isEqual from 'lodash/isEqual'
+import { isNonNullable } from '../../../utils/isNonNullable.ts'
+import { bindingToBindingAttributes } from '../../settings/usermanagement/roles/misc.ts'
+import { GqlError } from '../../utils/Alert.tsx'
+
+export function PermissionsModal({
+  generator,
+  open,
+  onClose,
+}: {
+  generator: ComplianceReportGeneratorFragment
+  open: boolean
+  onClose: Nullable<() => void>
+}) {
+  const [bindings, setBindings] = useState(generator.readBindings)
+
+  useEffect(() => setBindings(generator.readBindings), [generator.readBindings])
+
+  const uniqueBindings = useMemo(() => uniqWith(bindings, isEqual), [bindings])
+
+  const [mutation, { loading, error }] =
+    useUpsertComplianceReportGeneratorMutation({
+      onCompleted: () => {
+        onClose?.()
+      },
+    })
+
+  const onSubmit = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault()
+      if (bindings) {
+        mutation({
+          variables: {
+            attributes: {
+              name: generator.name,
+              format: generator.format,
+              readBindings: bindings
+                ?.filter(isNonNullable)
+                .map(bindingToBindingAttributes),
+            },
+          },
+        })
+      }
+    },
+    [bindings, generator.name, generator.format, mutation]
+  )
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      size={'large'}
+      asForm
+      formProps={{ onSubmit }}
+      header={
+        <Flex
+          align={'center'}
+          justify={'space-between'}
+        >
+          Permissions
+          <Flex gap={'small'}>
+            <Button
+              type={'button'}
+              secondary
+              small
+              onClick={() => onClose?.()}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              loading={loading}
+              small
+              primary
+            >
+              Save
+            </Button>
+          </Flex>
+        </Flex>
+      }
+      css={{ maxHeight: '75vh' }}
+    >
+      <Flex
+        gap={'medium'}
+        direction={'column'}
+      >
+        <RoleFormBindings
+          bindings={uniqueBindings}
+          setBindings={setBindings}
+        />
+        {error && <GqlError error={error} />}
+      </Flex>
+    </Modal>
+  )
+}
+
+export function Permissions({
+  generator,
+}: {
+  generator: ComplianceReportGeneratorFragment
+}) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <IconFrame
+        tooltip={'Manage permissions'}
+        clickable
+        onClick={() => setOpen(true)}
+        icon={<PeopleIcon />}
+        type={'floating'}
+      />
+      <ModalMountTransition open={open}>
+        <PermissionsModal
+          generator={generator}
+          open={open}
+          onClose={() => setOpen(false)}
+        />
+      </ModalMountTransition>
+    </>
+  )
+}
---
+++ assets/src/components/security/compliance/ReportHistory.tsx
@@ -0,0 +1,131 @@
+import {
+  Button,
+  Flex,
+  IconFrame,
+  ListIcon,
+  Modal,
+  Table,
+} from '@pluralsh/design-system'
+import { useState } from 'react'
+
+import { ModalMountTransition } from 'components/utils/ModalMountTransition'
+import { createColumnHelper } from '@tanstack/react-table'
+import { Edge } from '../../../utils/graphql.ts'
+import {
+  ComplianceReportFragment,
+  useComplianceReportsQuery,
+} from '../../../generated/graphql.ts'
+import { DateTimeCol } from '../../utils/table/DateTimeCol.tsx'
+import {
+  DEFAULT_REACT_VIRTUAL_OPTIONS,
+  useFetchPaginatedData,
+} from '../../utils/table/useFetchPaginatedData.tsx'
+import { GqlError } from '../../utils/Alert.tsx'
+
+const columnHelper = createColumnHelper<Edge<ComplianceReportFragment>>()
+
+export const columns = [
+  columnHelper.accessor(({ node }) => node?.name, {
+    id: 'name',
+    header: 'Report name',
+    cell: ({ getValue }) => <div>{getValue()}</div>,
+  }),
+  columnHelper.accessor(({ node }) => node?.sha256, {
+    id: 'sha256',
+    header: 'SHA256',
+    meta: { truncate: true },
+    cell: ({ getValue }) => <div>{getValue()}</div>,
+  }),
+  columnHelper.accessor(({ node }) => node?.insertedAt, {
+    id: 'insertedAt',
+    header: 'Created',
+    enableSorting: true,
+    enableGlobalFilter: true,
+    cell: ({ getValue }) => <DateTimeCol date={getValue()} />,
+  }),
+]
+
+export function ReportHistoryModal({
+  name,
+  open,
+  onClose,
+}: {
+  name: string
+  open: boolean
+  onClose: Nullable<() => void>
+}) {
+  const { data, loading, error, fetchNextPage, setVirtualSlice } =
+    useFetchPaginatedData(
+      {
+        queryHook: useComplianceReportsQuery,
+        keyPath: ['complianceReportGenerator', 'complianceReports'],
+      },
+      { name }
+    )
+
+  if (error) return <GqlError error={error} />
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      size={'large'}
+      header={
+        <Flex
+          align={'center'}
+          justify={'space-between'}
+        >
+          Report history
+          <Button
+            secondary
+            small
+            onClick={() => onClose?.()}
+          >
+            Close
+          </Button>
+        </Flex>
+      }
+      css={{ maxHeight: '75vh' }}
+    >
+      <Table
+        fillLevel={2}
+        fullHeightWrap
+        virtualizeRows
+        data={data?.complianceReportGenerator?.complianceReports?.edges || []}
+        loading={!data && loading}
+        columns={columns}
+        hasNextPage={
+          data?.complianceReportGenerator?.complianceReports?.pageInfo
+            ?.hasNextPage
+        }
+        isFetchingNextPage={loading}
+        reactVirtualOptions={DEFAULT_REACT_VIRTUAL_OPTIONS}
+        onVirtualSliceChange={setVirtualSlice}
+        fetchNextPage={fetchNextPage}
+      />
+    </Modal>
+  )
+}
+
+export function ReportHistory({ name }: { name: string }) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <IconFrame
+        tooltip={'View report history'}
+        clickable
+        onClick={() => setOpen(true)}
+        icon={<ListIcon />}
+        type={'floating'}
+      />
+      <ModalMountTransition open={open}>
+        <ReportHistoryModal
+          name={name}
+          open={open}
+          onClose={() => setOpen(false)}
+        />
+      </ModalMountTransition>
+    </>
+  )
+}
