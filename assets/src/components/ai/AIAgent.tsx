5abd920128e3527e73b743c3c19461dfc4c64693
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-07-29 15:37:50 +0200

    fetch agent sessions

--- assets/src/components/ai/AIAgent.tsx
+++ assets/src/components/ai/AIAgent.tsx
@@ -1,6 +1,13 @@
 import { Flex } from '@pluralsh/design-system'
+import { useFetchPaginatedData } from '../utils/table/useFetchPaginatedData.tsx'
+import { useAgentSessionsQuery } from '../../generated/graphql.ts'
 
 export function AIAgent() {
+  const { data } = useFetchPaginatedData({
+    queryHook: useAgentSessionsQuery,
+    keyPath: ['agentSessions'],
+  })
+
   return (
     <Flex
       direction="column"
@@ -13,7 +20,7 @@ export function AIAgent() {
         gap="large"
         height="100%"
       >
-        ...
+        {JSON.stringify(data, null, 2)}
       </Flex>
     </Flex>
   )
