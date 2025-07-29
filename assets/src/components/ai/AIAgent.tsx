c2fa4fcf7bc748819b2a83e436a54973c2f380fd
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-07-29 15:55:17 +0200

    list agent sessions in a table

--- assets/src/components/ai/AIAgent.tsx
+++ assets/src/components/ai/AIAgent.tsx
@@ -1,6 +1,12 @@
-import { Flex, RobotIcon } from '@pluralsh/design-system'
-import { useFetchPaginatedData } from '../utils/table/useFetchPaginatedData.tsx'
-import { useAgentSessionsQuery } from '../../generated/graphql.ts'
+import { Flex, RobotIcon, Table } from '@pluralsh/design-system'
+import {
+  DEFAULT_REACT_VIRTUAL_OPTIONS,
+  useFetchPaginatedData,
+} from '../utils/table/useFetchPaginatedData.tsx'
+import {
+  ChatThreadTinyFragment,
+  useAgentSessionsQuery,
+} from '../../generated/graphql.ts'
 import { isEmpty } from 'lodash'
 import { EmptyStateCompact } from './AIThreads.tsx'
 import { useMemo } from 'react'
@@ -8,13 +14,15 @@ import { mapExistingNodes } from '../../utils/graphql.ts'
 import { GqlError } from '../utils/Alert.tsx'
 import { TableSkeleton } from '../utils/SkeletonLoaders.tsx'
 import { useTheme } from 'styled-components'
+import { createColumnHelper } from '@tanstack/react-table'
 
 export function AIAgent() {
   const theme = useTheme()
-  const { data, error } = useFetchPaginatedData({
-    queryHook: useAgentSessionsQuery,
-    keyPath: ['agentSessions'],
-  })
+  const { data, error, loading, pageInfo, fetchNextPage, setVirtualSlice } =
+    useFetchPaginatedData({
+      queryHook: useAgentSessionsQuery,
+      keyPath: ['agentSessions'],
+    })
 
   const agentSessions = useMemo(
     () => mapExistingNodes(data?.agentSessions),
@@ -67,8 +75,55 @@ export function AIAgent() {
         gap="large"
         height="100%"
       >
-        {JSON.stringify(data, null, 2)}
+        <Table
+          rowBg="raised"
+          fullHeightWrap
+          virtualizeRows
+          padCells={false}
+          data={agentSessions}
+          columns={columns}
+          hideHeader
+          hasNextPage={pageInfo?.hasNextPage}
+          fetchNextPage={fetchNextPage}
+          isFetchingNextPage={loading}
+          onVirtualSliceChange={setVirtualSlice}
+          reactVirtualOptions={DEFAULT_REACT_VIRTUAL_OPTIONS}
+          emptyStateProps={{ message: 'No entries found.' }}
+        />
       </Flex>
     </Flex>
   )
 }
+
+const columnHelper = createColumnHelper<ChatThreadTinyFragment>()
+
+const columns = [
+  columnHelper.accessor((item) => item, {
+    id: 'row',
+    cell: function Cell({ getValue }) {
+      const theme = useTheme()
+      const item = getValue()
+
+      return (
+        <div
+          css={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: theme.spacing.xlarge,
+            height: '100%',
+            width: '100%',
+            padding: theme.spacing.medium,
+            '&:not(:has(button:hover)):not(:has(a:hover))': {
+              '&:hover': {
+                background: theme.colors['fill-zero-hover'],
+                cursor: 'pointer',
+              },
+            },
+          }}
+        >
+          {JSON.stringify(item)}
+        </div>
+      )
+    },
+  }),
+]
