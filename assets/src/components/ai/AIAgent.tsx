a6ed01d8a90f4313344a6487cb807f89792372e2
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-07-29 15:43:51 +0200

    add empty state

--- assets/src/components/ai/AIAgent.tsx
+++ assets/src/components/ai/AIAgent.tsx
@@ -1,12 +1,37 @@
-import { Flex } from '@pluralsh/design-system'
+import { Flex, RobotIcon } from '@pluralsh/design-system'
 import { useFetchPaginatedData } from '../utils/table/useFetchPaginatedData.tsx'
 import { useAgentSessionsQuery } from '../../generated/graphql.ts'
+import { isEmpty } from 'lodash'
+import { EmptyStateCompact } from './AIThreads.tsx'
+import { useMemo } from 'react'
+import { mapExistingNodes } from '../../utils/graphql.ts'
 
 export function AIAgent() {
   const { data } = useFetchPaginatedData({
     queryHook: useAgentSessionsQuery,
     keyPath: ['agentSessions'],
   })
+
+  const agentSessions = useMemo(
+    () => mapExistingNodes(data?.agentSessions),
+    [data]
+  )
+
+  if (data && isEmpty(agentSessions)) {
+    return (
+      <EmptyStateCompact
+        icon={
+          <RobotIcon
+            color="icon-primary"
+            size={24}
+          />
+        }
+        message="No agent sessions"
+        description="You can create a new agent session from the AI Copilot panel."
+        cssProps={{ overflow: 'auto' }}
+      />
+    )
+  }
 
   return (
     <Flex
