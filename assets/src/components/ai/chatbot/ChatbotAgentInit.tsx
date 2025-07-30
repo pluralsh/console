eb1c971e5745a59d3896fa735fcd110cade1e33c
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-07-30 17:30:19 +0200

    update agent init state

--- assets/src/components/ai/chatbot/ChatbotAgentInit.tsx
+++ assets/src/components/ai/chatbot/ChatbotAgentInit.tsx
@@ -2,34 +2,39 @@ import { useTheme } from 'styled-components'
 
 import { ChatInput } from './input/ChatInput.tsx'
 
-import { Body1P } from '../../utils/typography/Text.tsx'
-import { ChatbotMessagesWrapperSC } from './ChatbotPanelThread.tsx'
+import { Body1BoldP, Body2P } from '../../utils/typography/Text.tsx'
 import { useChatbot } from '../AIContext.tsx'
-import { Toast } from '@pluralsh/design-system'
+import { Flex, Toast } from '@pluralsh/design-system'
 import { useCreateAgentSessionMutation } from '../../../generated/graphql.ts'
 
 export function ChatbotAgentInit() {
   const theme = useTheme()
-  const { currentThread, goToThread, agentInitMode } = useChatbot()
+  const { goToThread, agentInitMode } = useChatbot()
 
-  const [createAgentSession, { loading, error }] =
-    useCreateAgentSessionMutation({
-      onCompleted: ({ createAgentSession }) => {
-        if (createAgentSession?.id) {
-          goToThread(createAgentSession.id)
-        }
-      },
-    })
-
-  if (!currentThread) return null
+  const [createAgentSession, { error }] = useCreateAgentSessionMutation({
+    onCompleted: ({ createAgentSession }) => {
+      if (createAgentSession?.id) {
+        goToThread(createAgentSession.id)
+      }
+    },
+  })
 
   return (
     <>
-      <ChatbotMessagesWrapperSC>
-        <Body1P css={{ color: theme.colors['text-long-form'] }}>
-          {loading ? 'Generating response...' : 'How can I help you?'}
-        </Body1P>
-      </ChatbotMessagesWrapperSC>
+      <Flex
+        direction="column"
+        gap="medium"
+        css={{
+          padding: theme.spacing.medium,
+          paddingTop: theme.spacing.xxxxxlarge,
+        }}
+      >
+        <Body1BoldP>Copilot Agent</Body1BoldP>
+        <Body2P css={{ color: theme.colors['text-xlight'] }}>
+          Our agent is a background coding agent that can run background tasks
+          by writing terraform, finding code, suggest fixes, and creating PRs...
+        </Body2P>
+      </Flex>
       <ChatInput
         enableExamplePrompts={false}
         sendMessage={(prompt) =>
