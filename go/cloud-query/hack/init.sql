d9bef6d22883f7e327558d1453bd4fa9def300e4
Sebastian Florek <s.florek91@gmail.com>
2025-08-06 10:44:43 +0200

    fix(db-init): enhance PostgreSQL initialization and readiness checks

--- go/cloud-query/hack/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS steampipe_postgres_aws;
-CREATE EXTENSION IF NOT EXISTS steampipe_postgres_azure;
-CREATE EXTENSION IF NOT EXISTS steampipe_postgres_gcp;
-CREATE SCHEMA extensions;
-CREATE EXTENSION IF NOT EXISTS ltree WITH SCHEMA extensions;
