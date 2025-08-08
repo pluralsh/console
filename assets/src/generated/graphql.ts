35c10819661d07ad8283f9bab0d769239d694cc8
Jake Laderman <jsladerman@gmail.com>
2025-08-08 14:21:00 -0400

    add search bar to global services

--- assets/src/generated/graphql.ts
+++ assets/src/generated/graphql.ts
@@ -12410,6 +12410,7 @@ export type GlobalServicesQueryVariables = Exact<{
   first?: InputMaybe<Scalars['Int']['input']>;
   after?: InputMaybe<Scalars['String']['input']>;
   projectId?: InputMaybe<Scalars['ID']['input']>;
+  q?: InputMaybe<Scalars['String']['input']>;
 }>;
 
 
@@ -22889,8 +22890,8 @@ export type DeleteGlobalServiceMutationHookResult = ReturnType<typeof useDeleteG
 export type DeleteGlobalServiceMutationResult = Apollo.MutationResult<DeleteGlobalServiceMutation>;
 export type DeleteGlobalServiceMutationOptions = Apollo.BaseMutationOptions<DeleteGlobalServiceMutation, DeleteGlobalServiceMutationVariables>;
 export const GlobalServicesDocument = gql`
-    query GlobalServices($first: Int, $after: String, $projectId: ID) {
-  globalServices(first: $first, after: $after, projectId: $projectId) {
+    query GlobalServices($first: Int, $after: String, $projectId: ID, $q: String) {
+  globalServices(first: $first, after: $after, projectId: $projectId, q: $q) {
     pageInfo {
       ...PageInfo
     }
@@ -22919,6 +22920,7 @@ ${GlobalServiceFragmentDoc}`;
  *      first: // value for 'first'
  *      after: // value for 'after'
  *      projectId: // value for 'projectId'
+ *      q: // value for 'q'
  *   },
  * });
  */
