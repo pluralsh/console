5a658e159f82806a47d22821b220b1b51f0e1085
michaeljguarino <mjg@plural.sh>
2025-08-09 13:23:54 -0400

    Add read permission logic on catalogs
    The original behavior was open to support browsability, but actually think permissioning it can help restrict the provisioning ai in a useful way, so working w/ that now

--- test/console/ai/tools/agent/catalogs_test.exs
+++ test/console/ai/tools/agent/catalogs_test.exs
@@ -4,12 +4,25 @@ defmodule Console.AI.Tools.Agent.CatalogsTest do
 
   describe "implement/1" do
     test "it can fetch catalogs" do
-      catalogs = insert_list(2, :catalog)
+      user = insert(:user)
+      catalogs = insert_list(2, :catalog, read_bindings: [%{user_id: user.id}])
+
+      Console.AI.Tool.context(%{user: user})
 
       {:ok, result} = Catalogs.implement(%Catalogs{})
       {:ok, decoded} = Jason.decode(result)
 
       assert ids_equal(decoded, catalogs)
+    end
+
+    test "non-readers cannot list" do
+      user = insert(:user)
+      insert_list(2, :catalog)
+
+      Console.AI.Tool.context(%{user: user})
+
+      {:ok, result} = Catalogs.implement(%Catalogs{})
+      {:ok, []} = Jason.decode(result)
     end
   end
 end
