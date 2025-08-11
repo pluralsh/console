3f3f26a9ca5a6ae25d3370343ded2d274289bc1d
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-08-11 16:24:14 +0200

    add more error details

--- go/controller/internal/client/common.go
+++ go/controller/internal/client/common.go
@@ -10,10 +10,10 @@ import (
 func (c *client) GetUser(email string) (*console.UserFragment, error) {
 	response, err := c.consoleClient.GetUser(c.ctx, email)
 	if internalerror.IsNotFound(err) {
-		return nil, errors.NewNotFound(schema.GroupResource{}, email)
+		return nil, errors.NewNotFound(schema.GroupResource{Resource: "user"}, email)
 	}
 	if err == nil && (response == nil || response.User == nil) {
-		return nil, errors.NewNotFound(schema.GroupResource{}, email)
+		return nil, errors.NewNotFound(schema.GroupResource{Resource: "user"}, email)
 	}
 	if response == nil {
 		return nil, err
@@ -25,10 +25,10 @@ func (c *client) GetUser(email string) (*console.UserFragment, error) {
 func (c *client) GetGroup(name string) (*console.GroupFragment, error) {
 	response, err := c.consoleClient.GetGroup(c.ctx, name)
 	if internalerror.IsNotFound(err) {
-		return nil, errors.NewNotFound(schema.GroupResource{}, name)
+		return nil, errors.NewNotFound(schema.GroupResource{Resource: "group"}, name)
 	}
 	if err == nil && (response == nil || response.Group == nil) {
-		return nil, errors.NewNotFound(schema.GroupResource{}, name)
+		return nil, errors.NewNotFound(schema.GroupResource{Resource: "group"}, name)
 	}
 	if response == nil {
 		return nil, err
