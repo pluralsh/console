9b4a5f2376411b9a6e07729411f55b80e24f772a
Jake Laderman <jsladerman@gmail.com>
2025-08-19 17:13:39 -0400

    don't force reload on new session

--- assets/src/components/layout/ApplicationUpdateToast.tsx
+++ assets/src/components/layout/ApplicationUpdateToast.tsx
@@ -3,53 +3,64 @@ import { useCallback, useContext, useEffect, useState } from 'react'
 import { LoginContext } from 'components/contexts'
 
 import * as serviceWorker from '../../serviceWorkerRegistration'
+import { Toast } from '@pluralsh/design-system'
+import { useTheme } from 'styled-components'
 
 const COMMIT_KEY = 'git-commit'
 
-export const DEFAULT_COMMIT = 'plural-default-commit'
-export const getCommit = () =>
-  sessionStorage.getItem(COMMIT_KEY) || DEFAULT_COMMIT
-export const setCommit = (sha) => sessionStorage.setItem(COMMIT_KEY, sha)
+const DEFAULT_COMMIT = 'plural-default-commit'
+const getCommit = () => sessionStorage.getItem(COMMIT_KEY) || DEFAULT_COMMIT
+const setCommit = (sha) => sessionStorage.setItem(COMMIT_KEY, sha)
 
-function WithApplicationUpdate({ children }: any) {
-  const [time, setTime] = useState(Date.now())
+export function ApplicationUpdateToast() {
+  const theme = useTheme()
   const { configuration: config } = useContext(LoginContext)
-  const commit = getCommit()
+  const [stale, setStale] = useState(false)
 
-  const [stale, setStale] = useState(commit !== config?.gitCommit)
   const reloadApplication = useCallback(() => {
     const promise = serviceWorker.unregister() || Promise.resolve('done')
-
-    setStale(false)
     promise.then(() => {
       setCommit(config?.gitCommit)
       window.location.reload()
     })
   }, [config?.gitCommit])
-  const reloadOnStale = () => (stale ? reloadApplication() : null)
 
-  // force rerender every 60s to check if version hasn't changed
   useEffect(() => {
-    const interval = setInterval(() => setTime(Date.now()), 60000)
+    const current = getCommit()
+    const next = config?.gitCommit
 
-    setStale(commit !== config?.gitCommit)
+    if (!next) return
 
-    return () => clearInterval(interval)
-  }, [time, setStale, commit, config])
+    if (current === DEFAULT_COMMIT) {
+      setCommit(next)
+      setStale(false)
+      return
+    }
 
-  // Run only once on first app load
-  // It enforces full app reload if app has been updated
-  // Empty deps array is intentional here as it allows to run this effect only once.
-  useEffect(() => {
-    reloadOnStale()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    setStale(current !== next)
+  }, [config?.gitCommit])
 
-  if (commit === DEFAULT_COMMIT) setCommit(config?.gitCommit)
-
-  if (stale) return children({ reloadApplication })
-
-  return undefined
+  return (
+    <Toast
+      show={stale}
+      closeTimeout="none"
+      severity="info"
+      marginBottom="medium"
+      marginRight="xxxxlarge"
+    >
+      <span css={{ marginRight: theme.spacing.small }}>
+        Time for a new update!
+      </span>
+      <a
+        onClick={() => reloadApplication()}
+        style={{
+          textDecoration: 'none',
+          cursor: 'pointer',
+          color: theme.colors['action-link-inline'],
+        }}
+      >
+        Update now
+      </a>
+    </Toast>
+  )
 }
-
-export default WithApplicationUpdate
