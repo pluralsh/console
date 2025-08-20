4e67c436ae1ad338e90883a76c603f99c546d6b4
Jake Laderman <jsladerman@gmail.com>
2025-08-19 23:03:39 -0400

    embed git sha in bundle

--- assets/src/components/layout/ApplicationUpdateToast.tsx
+++ assets/src/components/layout/ApplicationUpdateToast.tsx
@@ -1,48 +1,20 @@
-import { useCallback, useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 
 import { LoginContext } from 'components/contexts'
 
-import * as serviceWorker from '../../serviceWorkerRegistration'
 import { Toast } from '@pluralsh/design-system'
 import { useTheme } from 'styled-components'
+import * as serviceWorker from '../../serviceWorkerRegistration'
 
-const COMMIT_KEY = 'git-commit'
-
-const DEFAULT_COMMIT = 'plural-default-commit'
-const getCommit = () => sessionStorage.getItem(COMMIT_KEY) || DEFAULT_COMMIT
-const setCommit = (sha) => sessionStorage.setItem(COMMIT_KEY, sha)
+const BUNDLED_GIT_COMMIT = import.meta.env.VITE_GIT_COMMIT
 
 export function ApplicationUpdateToast() {
   const theme = useTheme()
   const { configuration: config } = useContext(LoginContext)
-  const [stale, setStale] = useState(false)
-
-  const reloadApplication = useCallback(() => {
-    const promise = serviceWorker.unregister() || Promise.resolve('done')
-    promise.then(() => {
-      setCommit(config?.gitCommit)
-      window.location.reload()
-    })
-  }, [config?.gitCommit])
-
-  useEffect(() => {
-    const current = getCommit()
-    const next = config?.gitCommit
-
-    if (!next) return
-
-    if (current === DEFAULT_COMMIT) {
-      setCommit(next)
-      setStale(false)
-      return
-    }
-
-    setStale(current !== next)
-  }, [config?.gitCommit])
 
   return (
     <Toast
-      show={stale}
+      show={!!config?.gitCommit && config?.gitCommit !== BUNDLED_GIT_COMMIT}
       closeTimeout="none"
       severity="info"
       marginBottom="medium"
@@ -63,4 +35,11 @@ export function ApplicationUpdateToast() {
       </a>
     </Toast>
   )
+}
+
+const reloadApplication = () => {
+  const promise = serviceWorker.unregister() || Promise.resolve('done')
+  promise.then(() => {
+    window.location.reload()
+  })
 }
