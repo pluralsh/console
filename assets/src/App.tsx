c755fe21bc81946182f1f93aee6194373bbfe262
Jake Laderman <jsladerman@gmail.com>
2025-09-08 17:53:30 -0400

    add sentry to console

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -6,9 +6,9 @@ import {
   styledThemeLight,
   useThemeColorMode,
 } from '@pluralsh/design-system'
+import * as Sentry from '@sentry/react'
 import { ReactNode } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
 import {
   ThemeProvider as StyledThemeProvider,
   StyleSheetManager,
@@ -19,17 +19,19 @@ import DocSearchStyles from 'components/help/DocSearchStyles'
 import { client } from './helpers/client'
 import { rootRoutes } from './routes/rootRoutes'
 
+import { loadDevMessages, loadErrorMessages } from '@apollo/client/dev'
 import { shouldForwardProp } from 'utils/shouldForwardProp'
 import { PluralErrorBoundary } from './components/cd/PluralErrorBoundary'
-import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev'
 
 // required by apollo so we can see errors in dev console
 if (process.env.NODE_ENV === 'development') {
   loadDevMessages()
   loadErrorMessages()
 }
+const sentryCreateBrowserRouter =
+  Sentry.wrapCreateBrowserRouterV6(createBrowserRouter)
 
-const router = createBrowserRouter(rootRoutes)
+const router = sentryCreateBrowserRouter(rootRoutes)
 
 export default function App() {
   return (
--- assets/src/components/cd/PluralErrorBoundary.tsx
+++ assets/src/components/cd/PluralErrorBoundary.tsx
@@ -1,35 +1,52 @@
 import { Callout } from '@pluralsh/design-system'
-import { ComponentProps } from 'react'
+import * as Sentry from '@sentry/react'
+import { ErrorInfo, ReactNode, useEffect } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
+import { useRouteError } from 'react-router-dom'
+import { useTheme } from 'styled-components'
 
-function logError(error: Error, info: { componentStack: string }) {
-  // Do something with the error, e.g. log to an external API
+function logError(error: Error, info: ErrorInfo) {
+  Sentry.captureException(error)
   console.error('Error:', error)
   console.error(`Component stack:\n${info.componentStack}`)
 }
 
-function ErrorFallback({ error }: any) {
+export function PluralErrorBoundary({ children }: { children: ReactNode }) {
   return (
-    <Callout
-      severity="danger"
-      title="Sorry, something went wrong"
+    <ErrorBoundary
+      onError={logError}
+      FallbackComponent={ErrorFallback}
     >
-      {error.message}
-    </Callout>
+      {children}
+    </ErrorBoundary>
   )
 }
 
-export function PluralErrorBoundary(
-  props: Partial<ComponentProps<typeof ErrorBoundary>>
-) {
+export function PluralRouteErrorBoundary() {
+  const { spacing } = useTheme()
+  const error = useRouteError() as Error
+
+  useEffect(() => {
+    Sentry.captureException(error)
+  }, [error])
+
   return (
-    <ErrorBoundary
-      // @ts-ignore
-      onError={logError}
-      {...props}
-      {...(!props.fallbackRender && !props.fallback && !props.FallbackComponent
-        ? { FallbackComponent: ErrorFallback }
-        : {})}
-    />
+    <div css={{ padding: spacing.large }}>
+      <ErrorFallback
+        error={error}
+        title="Something went wrong. Check logs for more details."
+      />
+    </div>
+  )
+}
+
+function ErrorFallback({ error, title }: { error: Error; title?: string }) {
+  return (
+    <Callout
+      severity="danger"
+      title={title || 'Sorry, something went wrong'}
+    >
+      {error.message}
+    </Callout>
   )
 }
