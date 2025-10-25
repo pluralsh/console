3d7f45a15150f8abbef2a2bea82a8c60d17c3d16
Jake Laderman <jsladerman@gmail.com>
2025-10-25 09:31:17 -0400

    basic output display

--- assets/src/components/ai/sentinels/sentinel/run/jobs/job/SentinelRunJobOutput.tsx
+++ assets/src/components/ai/sentinels/sentinel/run/jobs/job/SentinelRunJobOutput.tsx
@@ -1,3 +1,10 @@
+import { useOutletContext } from 'react-router-dom'
+import { SentinelRunJobOutletCtxT } from './SentinelRunJob'
+import { Code, EmptyState } from '@pluralsh/design-system'
+
 export function SentinelRunJobOutput() {
-  return <div>SentinelRunJobOutput</div>
+  const { job } = useOutletContext<SentinelRunJobOutletCtxT>()
+  if (!job.output)
+    return <EmptyState message="No output available yet for this job." />
+  return <Code language="auto">{job.output}</Code>
 }
