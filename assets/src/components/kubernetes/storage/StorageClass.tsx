ae0d65fbf32dcf4a0f05fdcbb39534859c861c8e
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-01-15 13:25:28 +0100

    remove unused import of getStorageClassPersistentVolumes in StorageClass component

--- assets/src/components/kubernetes/storage/StorageClass.tsx
+++ assets/src/components/kubernetes/storage/StorageClass.tsx
@@ -10,7 +10,6 @@ import { useQuery } from '@tanstack/react-query'
 import { AxiosInstance } from '../../../helpers/axios.ts'
 
 import {
-  getStorageClassPersistentVolumes,
   PersistentvolumePersistentVolume,
   PersistentvolumePersistentVolumeList,
   StorageclassStorageClass,
