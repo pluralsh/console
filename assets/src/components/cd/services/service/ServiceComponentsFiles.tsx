4d2d42e0497dcff87e308ab6bd64a0cc219577cd
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-01-20 15:58:27 +0100

    use query

--- assets/src/components/cd/services/service/ServiceComponentsFiles.tsx
+++ assets/src/components/cd/services/service/ServiceComponentsFiles.tsx
@@ -1,3 +1,19 @@
+import { GqlError } from 'components/utils/Alert'
+import LoadingIndicator from 'components/utils/LoadingIndicator'
+import { useServiceTarballQuery } from 'generated/graphql'
+import { useParams } from 'react-router-dom'
+
 export function ComponentsFilesView() {
-  return <div>...</div>
+  const { serviceId } = useParams<{ serviceId: string }>()
+
+  const { data, loading, error } = useServiceTarballQuery({
+    variables: { id: serviceId! },
+    skip: !serviceId,
+  })
+
+  if (error) return <GqlError error={error} />
+
+  if (loading) return <LoadingIndicator />
+
+  return <div>{JSON.stringify(data)}</div>
 }
