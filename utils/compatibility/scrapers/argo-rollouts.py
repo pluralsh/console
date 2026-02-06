308cec38b517ba3f07e474c79859cd1929d9979c
JohnBlackwell <john@plural.sh>
2026-02-06 13:44:56 -0500

    fix argo-rollouts scraper

--- utils/compatibility/scrapers/argo-rollouts.py
+++ utils/compatibility/scrapers/argo-rollouts.py
@@ -2,69 +2,61 @@
 
 from collections import OrderedDict
 
-import requests
-import yaml
-
 from utils import (
     print_error,
-    fetch_page,
     update_compatibility_info,
     get_chart_versions,
     print_success,
+    get_kube_release_info,
+    get_github_releases_timestamps,
+    find_last_n_releases,
+    clean_kube_version,
 )
 
 app_name = "argo-rollouts"
-github_api_tags_url = "https://api.github.com/repos/argoproj/argo-rollouts/tags"
-
-
-def fetch_github_tags():
-    response = requests.get(github_api_tags_url)
-    if response.status_code != 200:
-        print_error(
-            f"Failed to fetch GitHub tags. Status code: {response.status_code}"
-        )
-        return []
-    return [tag["name"] for tag in response.json()]
-
-
 def scrape():
-    release_tags = fetch_github_tags()
-    if not release_tags:
+    kube_releases = get_kube_release_info()
+    argo_releases = list(
+        reversed(list(get_github_releases_timestamps("argoproj", "argo-rollouts")))
+    )
+    chart_versions = get_chart_versions(app_name)
+    rows = []
+    pruned_releases = [(r.lstrip("v"), ts) for r, ts in argo_releases if "-" not in r]
+    if not pruned_releases:
         print_error("No release tags found.")
         return
 
-    chart_versions = get_chart_versions(app_name)
-    rows = []
-    for tag in release_tags:
-        tag_version = tag.lstrip("v")
-        chart_version = chart_versions.get(tag_version)
+    for idx, argo_release in enumerate(pruned_releases):
+        release_vsn = argo_release[0]
+        future_release = argo_release
+        if idx < len(pruned_releases) - 1:
+            future_release = pruned_releases[idx + 1]
+        compatible_kube_releases = find_last_n_releases(
+            kube_releases, future_release[1], n=3
+        )
+        chart_version = chart_versions.get(release_vsn)
         # Skip if there's no chart version mapped
         if not chart_version:
             continue
 
-        response = requests.get(
-            f"https://raw.githubusercontent.com/argoproj/argo-rollouts/refs/tags/{tag}/.github/workflows/testing.yaml")
-        if response.status_code != 200:
-            print_error(f"Failed to fetch compatibility info for tag {tag}. Status code: {response.status_code}")
-            continue
+        kube_versions = [
+            clean_kube_version(kube_release[0])
+            for kube_release in compatible_kube_releases
+        ]
+        kube_versions = [vsn for vsn in kube_versions if vsn]
 
-        content = yaml.safe_load(response.text)
-        if not content:
-            print_error(f"Failed to parse compatibility info for {tag}")
-            return
-
-        matrix = content.get("jobs", {}).get("test-e2e", {}).get("strategy", {}).get("matrix", {}).get("kubernetes", [])
-        kube_versions = [str(entry["version"]) for entry in matrix]
-        rows.append(OrderedDict(
-            [
-                ("version", tag_version),
-                ("kube", kube_versions),
-                ("chart_version", chart_version),
-                ("images", []),
-                ("requirements", []),
-                ("incompatibilities", []),
-            ]
-        ))
-        print_success(f"Fetched compatibility info for tag: {tag}")
+        rows.append(
+            OrderedDict(
+                [
+                    ("version", release_vsn),
+                    ("kube", kube_versions),
+                    ("chart_version", chart_version),
+                    ("images", []),
+                    ("requirements", []),
+                    ("incompatibilities", []),
+                ]
+            )
+        )
+        print_success(f"Derived compatibility info for release: {release_vsn}")
 
     update_compatibility_info(f"../../static/compatibilities/{app_name}.yaml", rows)
