da952c7b670d1ac6e47c044b6f5ca91af13b4f8f
Lukasz Zajaczkowski <zreigz@gmail.com>
2026-02-06 14:29:59 +0100

    make unique project

--- go/controller/internal/controller/sentinel_controller_test.go
+++ go/controller/internal/controller/sentinel_controller_test.go
@@ -29,7 +29,7 @@ var _ = Describe("Sentinel Controller", Ordered, func() {
 			id          = "123"
 			clusterName = "cluster-test-sentinel"
 			repoName    = "repo-test-sentinel"
-			projectName = "default"
+			projectName = "default-project-sentinel"
 			secretName  = "secret-test-sentinel"
 			repoUrl     = "https://test"
 		)
