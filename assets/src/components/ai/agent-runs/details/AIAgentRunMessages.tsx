da566ec84f24000e62cf58da47c45aa196b2ac4f
Jake Laderman <jsladerman@gmail.com>
2026-02-18 17:55:18 -0500

    add support for runtime repositories

--- assets/src/components/ai/agent-runs/details/AIAgentRunMessages.tsx
+++ assets/src/components/ai/agent-runs/details/AIAgentRunMessages.tsx
@@ -88,7 +88,7 @@ const GradientWrapperSC = styled.div(({ theme }) => ({
   gap: theme.spacing.small,
   flexDirection: 'column',
   flex: 1,
-  minHeight: 625,
+  minHeight: 610,
   border: theme.borders.default,
   borderRadius: theme.borderRadiuses.large,
   background: `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(74, 81, 242, 0.13) 100%)`,
