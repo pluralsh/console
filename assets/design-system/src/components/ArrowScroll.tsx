2cde4c88dc0dee32ce17b8426d4a151d4bb1781d
Jake Laderman <jsladerman@gmail.com>
2026-03-05 16:48:18 -0500

    fix up arrow scroll

--- assets/design-system/src/components/ArrowScroll.tsx
+++ assets/design-system/src/components/ArrowScroll.tsx
@@ -1,132 +1,108 @@
-import {
-  Children,
-  type ComponentProps,
-  cloneElement,
-  useRef,
-  useState,
-} from 'react'
+import { ComponentPropsWithRef, ReactNode, useRef, useState } from 'react'
 import styled from 'styled-components'
 
+import { SemanticColorKey } from 'src/theme/colors'
 import useResizeObserver from '../hooks/useResizeObserver'
 import { ArrowLeftIcon, ArrowRightIcon } from '../icons'
 
-const ComponentWrapperSC = styled.div({
-  position: 'relative',
-  overflowX: 'auto',
-  '& > *': {
-    scrollbarWidth: 'none', // Firefox
-    msOverflowStyle: 'none', // Edge
-    '&::-webkit-scrollbar': {
-      display: 'none', // Chrome, Safari, Opera
-    },
-  },
-})
-const ArrowWrapperSC = styled.div<{
-  $direction?: 'left' | 'right'
-}>(({ theme, $direction }) => ({
-  color: theme.colors['icon-light'],
-  zIndex: theme.zIndexes.modal - 1,
-  position: 'absolute',
-  top: 0,
-  bottom: 0,
-  width: '36px',
-  display: 'flex',
-  justifyContent: $direction === 'left' ? 'flex-start' : 'flex-end',
-  padding: `0 ${theme.spacing.xxsmall}px`,
-  alignItems: 'center',
-  transition: 'opacity .2s ease',
-  background: `linear-gradient(${
-    $direction === 'left' ? 'to left' : 'to right'
-  }, transparent 0%, ${theme.colors['fill-one-selected']} 70%, ${
-    theme.colors['fill-one-selected']
-  } 100%)`,
-  ...($direction === 'left' ? { left: 0 } : { right: 0 }),
-  '&.visible': {
-    cursor: 'pointer',
-    opacity: 1,
-  },
-  '&.hidden': {
-    opacity: 0,
-    pointerEvents: 'none',
-  },
-}))
-
-function Arrow({
-  direction,
-  show,
+function ArrowScroll({
+  children,
+  showArrow = true,
+  color = 'fill-one-selected',
   ...props
 }: {
-  direction: 'left' | 'right'
-  show: boolean
-} & ComponentProps<typeof ArrowWrapperSC>) {
-  const Icon = direction === 'left' ? ArrowLeftIcon : ArrowRightIcon
-
-  return (
-    <ArrowWrapperSC
-      $direction={direction}
-      className={show ? 'visible' : 'hidden'}
-      {...props}
-    >
-      <Icon size={8} />
-    </ArrowWrapperSC>
-  )
-}
-
-const scroll = (
-  element: HTMLElement | null | undefined,
-  direction: 'left' | 'right'
-) => {
-  if (element) {
-    element.scrollBy({
-      left: direction === 'left' ? -100 : 100,
-      behavior: 'smooth',
-    })
-  }
-}
-
-function ArrowScroll({ children, ...props }: { children?: any }) {
-  const containerRef = useRef<HTMLElement>(undefined)
-  const [showLeftArrow, setShowLeftArrow] = useState(false)
-  const [showRightArrow, setShowRightArrow] = useState(false)
+  children?: ReactNode
+  showArrow?: boolean
+  color?: SemanticColorKey
+} & ComponentPropsWithRef<'div'>) {
+  const containerRef = useRef<HTMLDivElement>(undefined)
+  const [showLeftGradient, setShowLeftGradient] = useState(false)
+  const [showRightGradient, setShowRightGradient] = useState(false)
 
   const checkScroll = () => {
     if (containerRef.current) {
       const { scrollLeft, scrollWidth, clientWidth } = containerRef.current
 
-      setShowLeftArrow(scrollLeft > 0)
-      setShowRightArrow(Math.ceil(scrollLeft + clientWidth) < scrollWidth)
+      setShowLeftGradient(scrollLeft > 0)
+      setShowRightGradient(Math.ceil(scrollLeft + clientWidth) < scrollWidth)
     }
   }
 
   useResizeObserver(containerRef, checkScroll)
 
   return (
-    <ComponentWrapperSC {...props}>
-      <Arrow
-        onClick={() => scroll(containerRef.current, 'left')}
-        direction="left"
-        show={showLeftArrow}
-      />
-      <Arrow
-        onClick={() => scroll(containerRef.current, 'right')}
-        direction="right"
-        show={showRightArrow}
-      />
-      {cloneElement(Children.only(children), {
-        onScroll: checkScroll,
-        ref: (node: HTMLElement) => {
-          containerRef.current = node
-          if (children?.props?.ref) {
-            if (typeof children.props.ref === 'function') {
-              children.props.ref(node)
-            } else if (children.props.ref) {
-              children.props.ref.current = node
-            }
-          }
-        },
-      })}
-    </ComponentWrapperSC>
+    <WrapperSC {...props}>
+      <ArrowWrapperSC
+        onClick={() =>
+          containerRef.current?.scrollBy({ left: -100, behavior: 'smooth' })
+        }
+        $direction="left"
+        $hasArrow={showArrow}
+        $color={color}
+        $visible={showLeftGradient}
+      >
+        {showArrow ? <ArrowLeftIcon size={8} /> : null}
+      </ArrowWrapperSC>
+      <ArrowWrapperSC
+        onClick={() =>
+          containerRef.current?.scrollBy({ left: 100, behavior: 'smooth' })
+        }
+        $direction="right"
+        $hasArrow={showArrow}
+        $color={color}
+        $visible={showRightGradient}
+      >
+        {showArrow ? <ArrowRightIcon size={8} /> : null}
+      </ArrowWrapperSC>
+      <ScrollAreaSC
+        ref={containerRef}
+        onScroll={checkScroll}
+      >
+        {children}
+      </ScrollAreaSC>
+    </WrapperSC>
   )
 }
+
+const WrapperSC = styled.div({
+  position: 'relative',
+  width: '100%',
+  overflow: 'hidden',
+  minHeight: 'fit-content',
+})
+
+const ScrollAreaSC = styled.div({
+  overflowX: 'auto',
+  overflowY: 'hidden',
+  scrollbarWidth: 'none', // Firefox
+  msOverflowStyle: 'none', // Edge
+  '&::-webkit-scrollbar': {
+    display: 'none', // Chrome, Safari, Opera
+  },
+})
+
+const ArrowWrapperSC = styled.div<{
+  $direction: 'left' | 'right'
+  $color: SemanticColorKey
+  $hasArrow: boolean
+  $visible: boolean
+}>(({ theme, $direction, $color, $hasArrow, $visible }) => ({
+  color: theme.colors['icon-light'],
+  zIndex: theme.zIndexes.modal - 1,
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  ...{ [$direction]: 0 },
+  width: 36,
+  display: 'flex',
+  justifyContent: $direction === 'left' ? 'flex-start' : 'flex-end',
+  padding: `0 ${theme.spacing.xxsmall}px`,
+  alignItems: 'center',
+  transition: 'opacity .2s ease',
+  background: `linear-gradient(to ${$direction}, transparent 0%, ${theme.colors[$color]} 70%, ${theme.colors[$color]} 100%)`,
+  opacity: $visible ? 1 : 0,
+  cursor: $visible && $hasArrow ? 'pointer' : 'default',
+  pointerEvents: $visible && $hasArrow ? 'auto' : 'none',
+}))
 
 export default ArrowScroll
