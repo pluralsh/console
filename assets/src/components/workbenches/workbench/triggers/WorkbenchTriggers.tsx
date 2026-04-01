cb5266e2d80f8f681b4aacf4dae8aed3fa769e78
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-04-01 13:18:56 +0200

    add header and update layout

--- assets/src/components/workbenches/workbench/triggers/WorkbenchTriggers.tsx
+++ assets/src/components/workbenches/workbench/triggers/WorkbenchTriggers.tsx
@@ -1,9 +1,9 @@
 import { Flex, useSetBreadcrumbs } from '@pluralsh/design-system'
-import { SideNavEntries } from 'components/layout/SideNavEntries'
 import { GqlError } from 'components/utils/Alert'
+import { StackedText } from 'components/utils/table/StackedText'
 import { useWorkbenchQuery } from 'generated/graphql'
 import { useMemo } from 'react'
-import { Outlet, useLocation, useParams } from 'react-router-dom'
+import { Link, Outlet, useMatch, useParams } from 'react-router-dom'
 import {
   getWorkbenchAbsPath,
   WORKBENCH_PARAM_ID,
@@ -13,7 +13,11 @@ import {
 } from 'routes/workbenchesRoutesConsts'
 
 import { getWorkbenchBreadcrumbs } from '../Workbench'
-import { FormCardSC } from '../create-edit/WorkbenchCreateOrEdit'
+import {
+  FormCardSC,
+  SidebarBtnSC,
+  WorkbenchSplitLayoutSC,
+} from '../create-edit/WorkbenchCreateOrEdit'
 
 const DIRECTORY = [
   { path: WORKBENCHES_TRIGGERS_SCHEDULE_REL_PATH, label: 'Schedule trigger' },
@@ -22,10 +26,12 @@ const DIRECTORY = [
 
 export function WorkbenchTriggers() {
   const id = useParams()[WORKBENCH_PARAM_ID]
-  const { pathname } = useLocation()
   const pathPrefix = `${getWorkbenchAbsPath(id)}/${WORKBENCHES_TRIGGERS_REL_PATH}`
+  const tab =
+    useMatch(`${pathPrefix}/:tab`)?.params.tab ??
+    WORKBENCHES_TRIGGERS_SCHEDULE_REL_PATH
 
-  const { data, error } = useWorkbenchQuery({
+  const { data, loading, error } = useWorkbenchQuery({
     variables: { id },
     skip: !id,
     fetchPolicy: 'network-only',
@@ -57,24 +63,32 @@ export function WorkbenchTriggers() {
       overflow="auto"
       padding="large"
     >
-      <Flex
-        gap="medium"
-        height="100%"
-        minWidth={750}
-        maxWidth={968}
-        minHeight={0}
-      >
-        <div css={{ width: 200, flexShrink: 0 }}>
-          <SideNavEntries
-            directory={DIRECTORY}
-            pathname={pathname}
-            pathPrefix={pathPrefix}
-          />
-        </div>
+      <StackedText
+        loading={!data && loading}
+        first={workbench?.name}
+        second={workbench?.description}
+      />
+      <WorkbenchSplitLayoutSC>
+        <Flex
+          direction="column"
+          width={200}
+        >
+          {DIRECTORY.map(({ path, label }) => (
+            <SidebarBtnSC
+              key={path}
+              tertiary
+              as={Link}
+              to={path}
+              $active={path === tab}
+            >
+              {label}
+            </SidebarBtnSC>
+          ))}
+        </Flex>
         <FormCardSC>
           <Outlet />
         </FormCardSC>
-      </Flex>
+      </WorkbenchSplitLayoutSC>
     </Flex>
   )
 }
