f66c6f98c7dcbb3bd13c567676210fb28d355284
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-04-02 14:06:17 +0200

    implement WorkbenchWebhookTrigger component with conditional rendering for empty states

--- assets/src/components/workbenches/workbench/triggers/WorkbenchWebhookTrigger.tsx
+++ assets/src/components/workbenches/workbench/triggers/WorkbenchWebhookTrigger.tsx
@@ -1,3 +1,32 @@
+import { Flex } from '@pluralsh/design-system'
+import { Body2P } from 'components/utils/typography/Text'
+import { useOutletContext } from 'react-router-dom'
+import {
+  WorkbenchScheduleEmptyState,
+  WorkbenchWebhookEmptyState,
+} from './WorkbenchTriggersEmptyStates'
+import { WorkbenchTriggersOutletContext } from './WorkbenchTriggers'
+import { FormCardSC } from '../create-edit/WorkbenchCreateOrEdit'
+
 export function WorkbenchWebhookTrigger() {
-  return <>Webhook trigger</>
+  const { hasSchedules, hasWebhooks } =
+    useOutletContext<WorkbenchTriggersOutletContext>()
+
+  if (!hasWebhooks)
+    return (
+      <Flex
+        direction="column"
+        gap="medium"
+        flex={1}
+      >
+        <WorkbenchWebhookEmptyState />
+        {!hasSchedules && <WorkbenchScheduleEmptyState />}
+      </Flex>
+    )
+
+  return (
+    <FormCardSC>
+      <Body2P $color="text-light">...</Body2P>
+    </FormCardSC>
+  )
 }
