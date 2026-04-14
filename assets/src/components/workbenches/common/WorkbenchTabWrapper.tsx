4cf19b44598f689f91f77c708648e906372d1e18
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-04-14 12:34:48 +0200

    fix scroll bug

--- assets/src/components/workbenches/common/WorkbenchTabWrapper.tsx
+++ assets/src/components/workbenches/common/WorkbenchTabWrapper.tsx
@@ -4,5 +4,7 @@ export const WorkbenchTabWrapper = styled.div(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   gap: theme.spacing.large,
-  minHeight: 700,
+  minHeight: 0,
+  height: '100%',
+  overflow: 'hidden',
 }))
