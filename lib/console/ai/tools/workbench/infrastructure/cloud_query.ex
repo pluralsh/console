6c6ef989bd174560cf7b21e0f274256dea328dfb
michaeljguarino <mjg@plural.sh>
2026-04-20 16:59:33 -0400

    fix crash

--- lib/console/ai/tools/workbench/infrastructure/cloud_query.ex
+++ lib/console/ai/tools/workbench/infrastructure/cloud_query.ex
@@ -23,7 +23,7 @@ defmodule Console.AI.Tools.Workbench.Infrastructure.CloudQuery do
   def description(%__MODULE__{tool: %WorkbenchTool{cloud_connection: %CloudConnection{provider: provider}}}),
     do: "Performs a postgresql-compatible sql query against the #{provider} cloud account.  You *must* use the cloud schema tool to discover the schema of the sql database first before calling this so it uses the proper tables and columns."
 
-  def implement(%__MODULE__{query: query, tool: %WorkbenchTool{cloud_connection: %CloudConnection{} = connection}}) do
+  def implement(_, %__MODULE__{query: query, tool: %WorkbenchTool{cloud_connection: %CloudConnection{} = connection}}) do
     with {:ok, client} <- Client.connect(),
          input = %QueryInput{query: query, connection: to_pb(connection)},
          {:ok, result} <- Stub.query(client, input) do
