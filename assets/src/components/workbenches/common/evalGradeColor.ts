d50d4de65bbd2e3e8bd25d3e58dbcebbf76854a7
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-05-06 16:47:45 +0200

    update color mapping

--- assets/src/components/workbenches/common/evalGradeColor.ts
+++ assets/src/components/workbenches/common/evalGradeColor.ts
@@ -1,25 +1,20 @@
-import {
-  green,
-  red,
-  yellow,
-} from '../../../../design-system/src/theme/colors-base'
-
 export function evalGradeToColor(score: number): string {
   const normalized = clamp(score, 0, 10)
 
-  if (normalized < 1) return red[400]
-  if (normalized < 2) return red[300]
-  if (normalized < 3) return red[200]
-  if (normalized < 4) return red[100]
+  if (normalized < 1) return '#E95374' // red[400]
+  if (normalized < 2) return '#F2788D' // red[300]
+  if (normalized < 3) return '#F599A8' // red[200]
+  if (normalized < 4) return '#FAC7D0' // red[100]
 
-  if (normalized < 5) return yellow[300]
-  if (normalized < 6) return yellow[400]
-  if (normalized < 7) return yellow[500]
+  if (normalized < 5) return '#FFF170' // yellow[300]
+  if (normalized < 6) return '#FFCF33' // yellow[400]
+  if (normalized < 7) return '#F5AF00' // yellow[500]
 
-  if (normalized < 8) return green[300]
-  if (normalized < 9) return green[400]
-  if (normalized < 10) return green[500]
-  return green[500]
+  if (normalized < 8) return '#6AF1C2' // green[300]
+  if (normalized < 9) return '#3CECAF' // green[400]
+  if (normalized < 10) return '#17E8A0' // green[500]
+
+  return '#AEB3C0' // grey[300]
 }
 
 function clamp(value: number, min: number, max: number) {
