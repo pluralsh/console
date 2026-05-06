1ac3223b2d364a91f599329a90fd6414759c7f19
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-05-06 11:15:57 +0200

    add graph card component

---
+++ assets/src/components/workbenches/common/WorkbenchGraphCard.tsx
@@ -0,0 +1,58 @@
+import { Card } from '@pluralsh/design-system'
+import { RectangleSkeleton } from 'components/utils/SkeletonLoaders'
+import { Body1P, CaptionP } from 'components/utils/typography/Text'
+import { ReactNode } from 'react'
+import { useTheme } from 'styled-components'
+
+const MIN_HEIGHT = 360
+
+export function WorkbenchGraphCard({
+  title,
+  rightContent,
+  children,
+  hint,
+  loading,
+}: {
+  title: string
+  rightContent?: ReactNode
+  children: ReactNode
+  hint?: string
+  loading?: boolean
+}) {
+  const theme = useTheme()
+
+  if (loading)
+    return (
+      <RectangleSkeleton
+        $height={MIN_HEIGHT}
+        $width="100%"
+      />
+    )
+
+  return (
+    <Card
+      css={{
+        backgroundColor: theme.colors['fill-zero'],
+        display: 'flex',
+        flexDirection: 'column',
+        gap: theme.spacing.medium,
+        padding: theme.spacing.large,
+        minHeight: MIN_HEIGHT,
+      }}
+    >
+      <div
+        css={{
+          display: 'flex',
+          alignItems: 'flex-start',
+          justifyContent: 'space-between',
+          gap: theme.spacing.medium,
+        }}
+      >
+        <Body1P>{title}</Body1P>
+        {rightContent}
+      </div>
+      <div>{children}</div>
+      {hint ? <CaptionP $color="text-xlight">{hint}</CaptionP> : null}
+    </Card>
+  )
+}
