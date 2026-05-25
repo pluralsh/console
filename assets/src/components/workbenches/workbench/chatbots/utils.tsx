673230c66344aaa6fc136a90e645c747570b0050
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-05-25 17:13:47 +0200

    add utils

---
+++ assets/src/components/workbenches/workbench/chatbots/utils.tsx
@@ -0,0 +1,32 @@
+import {
+  BotIcon,
+  MsTeamsLogoIcon,
+  SlackLogoIcon,
+} from '@pluralsh/design-system'
+import { ChatProviderConnectionType as ChatProviderConnectionTypeGql } from 'generated/graphql'
+
+export function chatProviderConnectionIcon(
+  type?: Nullable<ChatProviderConnectionTypeGql>
+) {
+  switch (type) {
+    case ChatProviderConnectionTypeGql.Slack:
+      return <SlackLogoIcon />
+    case ChatProviderConnectionTypeGql.Teams:
+      return <MsTeamsLogoIcon />
+    default:
+      return <BotIcon />
+  }
+}
+
+export function chatProviderConnectionLabel(
+  type?: Nullable<ChatProviderConnectionTypeGql>
+) {
+  switch (type) {
+    case ChatProviderConnectionTypeGql.Slack:
+      return 'Slack'
+    case ChatProviderConnectionTypeGql.Teams:
+      return 'Microsoft Teams'
+    default:
+      return 'Chatbot'
+  }
+}
