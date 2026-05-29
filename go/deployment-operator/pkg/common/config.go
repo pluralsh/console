5ef060df1473492085c9bbe1782b1f4b338f69de
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-05-29 10:32:48 +0200

    add another missing assignment

--- go/deployment-operator/pkg/common/config.go
+++ go/deployment-operator/pkg/common/config.go
@@ -88,6 +88,7 @@ func (s *ConfigurationManager) SetValue(config v1alpha1.AgentConfigurationSpec) 
 	s.baseRegistryURL = config.BaseRegistryURL
 	s.maxSentinelRunJobs = config.MaxSentinelRunJobs
 	s.maxStackRunJobs = config.MaxStackRunJobs
+	s.maxAgentRunPods = config.MaxAgentRunPods
 	s.disableWebsocket = config.DisableWebsocket
 
 	return nil
