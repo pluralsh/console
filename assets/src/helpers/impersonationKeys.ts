549261342631bcbd35a6be6e7ee36cf1df088427
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-07-21 14:34:11 +0200

    extract impersonation keys

---
+++ assets/src/helpers/impersonationKeys.ts
@@ -0,0 +1,3 @@
+export const ORIGINAL_AUTH_TOKEN_KEY = 'impersonation-original-auth-token'
+export const ORIGINAL_REFRESH_TOKEN_KEY = 'impersonation-original-refresh-token'
+export const ORIGINAL_USER_LABEL_KEY = 'impersonation-original-user-label'
