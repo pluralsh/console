9b888f890ecdb5b77336bf3159f7bec3b5bcf252
michaeljguarino <mjg@plural.sh>
2026-07-22 16:06:17 -0400

    rm chatbot feature flag

--- lib/console/deployments/pubsub/consumers/cache.ex
+++ lib/console/deployments/pubsub/consumers/cache.ex
@@ -6,8 +6,10 @@ defmodule Console.PubSub.Consumers.Cache do
     protocol: Console.PubSub.Cacheable
 
   def handle_event(event) do
-    with {action, key, item} <- Console.PubSub.Cacheable.cache(event) do
-      handle_cache(action, key, item)
+    case Console.PubSub.Cacheable.cache(event) do
+      {action, key, item} -> handle_cache(action, key, item)
+      {action, key, item, opts} -> handle_cache(action, key, item, opts)
+      _ -> :ok
     end
   end
 
@@ -15,6 +17,8 @@ defmodule Console.PubSub.Consumers.Cache do
     Enum.zip(l, l2)
     |> Enum.map(fn {k, i} -> handle_cache(act, k, i) end)
   end
-  defp handle_cache(:set, key, item), do: Console.Cache.put(key, item)
-  defp handle_cache(:del, key, _), do: Console.Cache.delete(key)
+
+  defp handle_cache(action, key, item, opts \\ [])
+  defp handle_cache(:set, key, item, opts), do: Console.Cache.put(key, item, opts)
+  defp handle_cache(:del, key, _, _), do: Console.Cache.delete(key)
 end
